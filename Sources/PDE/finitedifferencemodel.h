0bac8d94c95aa763bf99f870a12df77ab63c7409
Ferdinando Ametrano <nando@ametrano.net>
2000-10-20 16:56:19 +0000

    copyright notice changed

--- Sources/PDE/finitedifferencemodel.h
+++ Sources/PDE/finitedifferencemodel.h
@@ -1,6 +1,6 @@
 
 /* 
-Copyright (C) 2000 F.Ametrano, L.Ballabio, A.Benin, M.Marchioro
+Copyright (C) 2000 Ferdinando Ametrano, Luigi Ballabio, Adolfo Benin, Marco Marchioro
 See the file LICENSE.TXT for information on usage and distribution
 Contact ferdinando@ametrano.net if LICENSE.TXT was not distributed with this file
 */
