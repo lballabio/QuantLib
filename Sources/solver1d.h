682acd8eb9933af83e8f0ecfae30a61d56b412bc
Ferdinando Ametrano <nando@ametrano.net>
2000-10-20 16:48:57 +0000

    1 dimensional solver base class (and interface)

---
+++ Sources/solver1d.h
@@ -0,0 +1,80 @@
+
+/* 
+Copyright (C) 2000 Ferdinando Ametrano, Luigi Ballabio, Adolfo Benin, Marco Marchioro
+See the file LICENSE.TXT for information on usage and distribution
+Contact ferdinando@ametrano.net if LICENSE.TXT was not distributed with this file
+*/
+
+#ifndef ql_solver1d_h
+#define ql_solver1d_h
+
+#include "qldefines.h"
+#include "null.h"
+#include "qlerrors.h"
+#include "formats.h"
+#include <cmath>
+#include <limits>
+#include <iostream>
+
+QL_BEGIN_NAMESPACE(QuantLib)
+
+QL_BEGIN_NAMESPACE(Solver1D)
+
+#define MAX_FUNCTION_EVALUATIONS 100
+
+class Function {
+public:
+	virtual ~Function() {}
+	virtual double value(double x) const = 0;
+	virtual double derivative(double x) const { return Null<double>(); }
+};
+
+class Solver1D {
+public:
+	// constructor
+	Solver1D() { maxEvaluations = MAX_FUNCTION_EVALUATIONS; lowBoundEnforced = false; hiBoundEnforced = false; }
+	// destructor
+	virtual ~Solver1D() {}
+	// methods
+	virtual double solve(const Function& f, double xAccuracy, double guess, double step) const;
+	virtual double solve(const Function& f, double xAccuracy, double guess, double xMin, double xMax) const;
+	void setMaxEvaluations(int evaluations);
+	void setLowBound(double lowBound) {theLowBound = lowBound; lowBoundEnforced = true; }
+	void setHiBound(double hiBound) {theHiBound = hiBound;  hiBoundEnforced = true; }
+protected:
+	// the following method assumes:
+	//  a) a valid bracket [xMin, xMax];
+	//  b) function already evaluated at xMin, xMax (fxMin, fxMax);
+	//  c) variable root initialized;
+	virtual double _solve(const Function& f, double xAccuracy) const = 0;
+	double enforceBounds(double x) const;
+	int maxEvaluations;
+	mutable int evaluationNumber;
+	mutable double root, xMin, xMax, fxMin, fxMax;
+	double theLowBound, theHiBound;
+	bool lowBoundEnforced, hiBoundEnforced;
+};
+
+
+// inline definitions
+
+inline void Solver1D::setMaxEvaluations(int evaluations) {
+	Require(evaluations > 0, "negative or null evaluations number");
+	maxEvaluations = evaluations;
+}
+
+inline double Solver1D::enforceBounds(double x) const {
+	if (lowBoundEnforced && x < theLowBound)
+	return theLowBound;
+
+	if (hiBoundEnforced && x > theHiBound)
+	return theHiBound;
+
+	return x;
+}
+
+QL_END_NAMESPACE(Solver1D)
+
+QL_END_NAMESPACE(QuantLib)
+
+#endif
