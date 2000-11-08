b3cd053015116012047b25a3c73550c4fb2b09b8
Ferdinando Ametrano <nando@ametrano.net>
2000-11-08 15:51:11 +0000

    new copyright notice

--- Include/null.h
+++ Include/null.h
@@ -3,17 +3,23 @@
  * Copyright (C) 2000
  * Ferdinando Ametrano, Luigi Ballabio, Adolfo Benin, Marco Marchioro
  * 
- * This file is part of QuantLib
+ * This file is part of QuantLib.
  * QuantLib is a C++ open source library for financial quantitative
  * analysts and developers --- http://quantlib.sourceforge.net/
  *
  * QuantLib is free software and you are allowed to use, copy, modify, merge,
  * publish, distribute, and/or sell copies of it under the conditions stated 
- * in the QuantLib License: see the file LICENSE.TXT for details.
- * Contact ferdinando@ametrano.net if LICENSE.TXT was not distributed with this file.
- * LICENCE.TXT is also available at http://quantlib.sourceforge.net/LICENSE.TXT
+ * in the QuantLib License.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+ *
+ * You should have received a copy of the license along with this file;
+ * if not, contact ferdinando@ametrano.net
+ *
+ * QuantLib license is also available at http://quantlib.sourceforge.net/LICENSE.TXT
 */
-
 
 /*! \file null.h
 	\brief null values
--- SWIG/Calendars.i
+++ SWIG/Calendars.i
@@ -1,8 +1,24 @@
 
-/* 
-Copyright (C) 2000 F.Ametrano, L.Ballabio, A.Benin, M.Marchioro
-See the file LICENSE.TXT for information on usage and distribution
-Contact ferdinando@ametrano.net if LICENSE.TXT was not distributed with this file
+/*
+ * Copyright (C) 2000
+ * Ferdinando Ametrano, Luigi Ballabio, Adolfo Benin, Marco Marchioro
+ * 
+ * This file is part of QuantLib.
+ * QuantLib is a C++ open source library for financial quantitative
+ * analysts and developers --- http://quantlib.sourceforge.net/
+ *
+ * QuantLib is free software and you are allowed to use, copy, modify, merge,
+ * publish, distribute, and/or sell copies of it under the conditions stated 
+ * in the QuantLib License.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+ *
+ * You should have received a copy of the license along with this file;
+ * if not, contact ferdinando@ametrano.net
+ *
+ * QuantLib license is also available at http://quantlib.sourceforge.net/LICENSE.TXT
 */
 
 #ifndef quantlib_calendar_i
