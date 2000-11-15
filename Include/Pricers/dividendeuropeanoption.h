b1c98fa3a6da227fe7eb8b0f3b02724f9766ba12
Marco Marchioro <marmar@users.sf.net>
2000-11-15 08:48:21 +0000

    Function riskless(...) has been moved inside class DividendEuropeanOption

--- Include/Pricers/dividendeuropeanoption.h
+++ Include/Pricers/dividendeuropeanoption.h
@@ -31,14 +31,13 @@
 namespace QuantLib {
 
 	namespace Pricers {
-		double DividendEuropeanOption_riskless(Rate r, Time t, std::vector<double> divs,std::vector<Time> divDates);
 	
 		class DividendEuropeanOption : public BSMEuropeanOption {
 		  public:
 				DividendEuropeanOption(Type type, double underlying, double strike, Rate underlyingGrowthRate, 
 				  Rate riskFreeRate, Time residualTime, double volatility, const std::vector<double>& dividends, 
 				  const std::vector<Time>& exdivdates):
-					BSMEuropeanOption(type, underlying - DividendEuropeanOption_riskless(riskFreeRate,residualTime,dividends,exdivdates), strike, underlyingGrowthRate, riskFreeRate, residualTime, volatility){} 
+					BSMEuropeanOption(type, underlying - riskless(riskFreeRate,residualTime,dividends,exdivdates), strike, underlyingGrowthRate, riskFreeRate, residualTime, volatility){} 
 				Handle<BSMOption> clone() const{ 
 						return Handle<BSMOption>(new DividendEuropeanOption(*this)); 
 				}
@@ -46,26 +45,24 @@ namespace QuantLib {
 		  			Require(1==0,"method rho() not defined for DividendEuropeanOption"); 
 		  			return 1234567890.0;
 		  	}
-		};
-		namespace DividendEuropeanOptionFunction {
-  
+		  private:
 				double riskless(Rate r, Time t, std::vector<double> divs,std::vector<Time> divDates){
-					Require(divs.size()==divDates.size(),"the number of dividends is diffrent from	that of	dates");
-					Require(divs.size()>=1,"the	number of dividends must be at least one");
-					Require(divDates[0]>0,"The	ex dividend dates must be positive");
-					Require(divDates[divDates.size()-1]<t,"The	x dividend dates must be within the residual time");
-					Require(divs[0]>=0,"Dividends cannot be	negative");
-					unsigned int	j;
-					for(j=1; j<divs.size();j++){
-						Require(double(divDates[j-1])<double(divDates[j]),"Dividend dates must be in increasing order");
-						Require(divs[j]>=0,"DividendEuropeanOption: Dividends cannot be negative");
-					}
-					double z = 0.0;
-					for(j=0;j<divs.size();j++)
-						z += divs[j]*QL_EXP(-r*divDates[j]);
-					return z;
+						Require(divs.size()==divDates.size(),"the number of dividends is diffrent from	that of	dates");
+						Require(divs.size()>=1,"the	number of dividends must be at least one");
+						Require(divDates[0]>0,"The	ex dividend dates must be positive");
+						Require(divDates[divDates.size()-1]<t,"The	x dividend dates must be within the residual time");
+						Require(divs[0]>=0,"Dividends cannot be	negative");
+						unsigned int	j;
+						for(j=1; j<divs.size();j++){
+							Require(double(divDates[j-1])<double(divDates[j]),"Dividend dates must be in increasing order");
+							Require(divs[j]>=0,"DividendEuropeanOption: Dividends cannot be negative");
+						}
+						double z = 0.0;
+						for(j=0;j<divs.size();j++)
+							z += divs[j]*QL_EXP(-r*divDates[j]);
+						return z;
 				}
-			}
+		};
 
 	}
 
