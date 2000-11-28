b0409122c698e4a8182b28cef7460f7ebb082b46
Marco Marchioro <marmar@users.sf.net>
2000-11-28 09:09:57 +0000

    rho now is working

--- Include/Pricers/dividendeuropeanoption.h
+++ Include/Pricers/dividendeuropeanoption.h
@@ -1,28 +1,28 @@
 
 /*
  * Copyright (C) 2000
- * Ferdinando Ametrano, Luigi Ballabio, Adolfo Benin, Marco Marchioro
+ * Ferdinando Ametrano,	Luigi Ballabio,	Adolfo Benin, Marco	Marchioro
  * 
- * This file is part of QuantLib.
- * QuantLib is a C++ open source library for financial quantitative
- * analysts and developers --- http://quantlib.sourceforge.net/
+ * This	file is	part of	QuantLib.
+ * QuantLib	is a C++ open source library for financial quantitative
+ * analysts	and	developers --- http://quantlib.sourceforge.net/
  *
- * QuantLib is free software and you are allowed to use, copy, modify, merge,
- * publish, distribute, and/or sell copies of it under the conditions stated 
+ * QuantLib	is free	software and you are allowed to	use, copy, modify, merge,
+ * publish,	distribute,	and/or sell	copies of it under the conditions stated 
  * in the QuantLib License.
  *
- * This program is distributed in the hope that it will be useful, but 
+ * This	program	is distributed in the hope that	it will	be useful, but 
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+ * or FITNESS FOR A	PARTICULAR PURPOSE.	See	the	license	for	more details.
  *
- * You should have received a copy of the license along with this file;
+ * You should have received	a copy of the license along	with this file;
  * if not, contact ferdinando@ametrano.net
  *
- * QuantLib license is also available at http://quantlib.sourceforge.net/LICENSE.TXT
+ * QuantLib	license	is also	available at http://quantlib.sourceforge.net/LICENSE.TXT
 */
 
-#ifndef BSM_dividend_european_option_pricer_h
-#define BSM_dividend_european_option_pricer_h
+#ifndef	BSM_dividend_european_option_pricer_h
+#define	BSM_dividend_european_option_pricer_h
 
 #include "qldefines.h"
 #include "bsmeuropeanoption.h"
@@ -33,42 +33,47 @@ namespace QuantLib {
 
 	namespace Pricers {
 	
-		class DividendEuropeanOption : public BSMEuropeanOption {
-            public:
-            DividendEuropeanOption(Type type, double underlying, double strike, Rate underlyingGrowthRate, 
-                Rate riskFreeRate, Time residualTime, double volatility, const std::vector<double>& dividends, 
-                const std::vector<Time>& exdivdates)
-                : BSMEuropeanOption(type, underlying - riskless(riskFreeRate,
-                residualTime,dividends,exdivdates),strike, underlyingGrowthRate,
-                riskFreeRate, residualTime, volatility) {}
-            Handle<BSMOption> clone() const{
-                return Handle<BSMOption>(new DividendEuropeanOption(*this));
-            }
-		  	double rho() const{
-		  		Require(1==0,"method rho() not yet defined for DividendEuropeanOption"); 
-		  		return 1234567890.0;
-		  	}
-            private:
-            double riskless(Rate r, Time t, std::vector<double> divs,std::vector<Time> divDates){
-                QuantLib::Require(divs.size()==divDates.size(),"the number of dividends is different from	that of	dates");
-				unsigned int j;
-				double z = 0.0;
-				for(j=0; j<divs.size();j++){
-                    QuantLib::Require(divDates[j]>0, "The " + IntegerFormatter::toString(j) + "-th" +
-                        "dividend date is not positive" + "(" + DoubleFormatter::toString(divDates[j]) + ")");
-                    QuantLib::Require(divDates[j]<t,"The " + IntegerFormatter::toString(j) + "-th" +
-                        "dividend date is greater than residual time" + "(" +
-                        DoubleFormatter::toString(divDates[j]) +
-                        ">" + DoubleFormatter::toString(t) + ")");
-					QuantLib::Require(divs[j]>=0,"The " + IntegerFormatter::toString(j) + "-th" +
-                        "dividend is negative" + "(" + DoubleFormatter::toString(divs[j]) + ")");
-                    z += divs[j]*QL_EXP(-r*divDates[j]);
-                }
-                return z;
-            }
-        };
+		class DividendEuropeanOption : public BSMEuropeanOption	{
+    		public:
+        	  DividendEuropeanOption(Type	type, double underlying, double	strike,	Rate underlyingGrowthRate, 
+        			Rate riskFreeRate, Time	residualTime, double volatility, const std::vector<double>&	dividends, 
+        			const std::vector<Time>& exdivdates): theDividends(dividends),theExDivDates(exdivdates),
+        			BSMEuropeanOption(type, underlying - riskless(riskFreeRate, residualTime,dividends,exdivdates),
+        			strike, underlyingGrowthRate,riskFreeRate,residualTime,volatility){
+        			    
+		    		QuantLib::Require(theDividends.size()==theExDivDates.size(),"the	number of dividends	is different from	that of	dates");				        			     
+        			for(unsigned int j=0; j<theDividends.size();j++){
+    					QuantLib::Require(theExDivDates[j]>0, "The "	+ IntegerFormatter::toString(j)	+ "-th"	+
+    				        "dividend date is not positive"	+ "(" +	DoubleFormatter::toString(theExDivDates[j]) + ")");
+    			        QuantLib::Require(theExDivDates[j]<residualTime,"The " + IntegerFormatter::toString(j) + "-th" +
+    				        "dividend date is greater than residual	time" +	"("	+
+    				        DoubleFormatter::toString(theExDivDates[j]) + ">" + DoubleFormatter::toString(residualTime)	+ ")");
+    					    QuantLib::Require(theDividends[j]>=0,"The	" +	IntegerFormatter::toString(j) +	"-th" +
+    				        "dividend is negative" + "(" + DoubleFormatter::toString(theDividends[j]) +	")");
+    		        }
+        		}
+        		Handle<BSMOption> clone() const{
+        			return Handle<BSMOption>(new DividendEuropeanOption(*this));
+        		}
+    			double rho() const{    			    
+    			    double tmp_rho = BSMEuropeanOption::rho();
+    			    double delta_rho = 0.0;
+    				for(unsigned int j=0; j<theDividends.size();j++)
+    			        delta_rho += theExDivDates[j]*theDividends[j]*QL_EXP(-theRiskFreeRate*theExDivDates[j]);
+    				return tmp_rho + delta_rho*BSMEuropeanOption::delta();
+    			}
+			private:
+			  std::vector<double> theDividends;
+			  std::vector<Time> theExDivDates;			
+			  double riskless(Rate r,	Time t,	std::vector<double>	divs,std::vector<Time> divDates){
+				double tmp_rho = 0.0;
+				for(unsigned int j=0; j<divs.size();j++)
+					tmp_rho += divs[j]*QL_EXP(-r*divDates[j]);
+				return tmp_rho;
+			}
+		};
 
-    }
+	}
 
 }
 
