41421b4e01dd0b8bfb8a244aaab5b2d247bf45dc
Luigi Ballabio <luigi.ballabio@gmail.com>
2000-12-11 11:00:09 +0000

    removed QL_INCLUDE_IN_SUBFOLDER

--- Include/quantlib.h
+++ Include/quantlib.h
@@ -57,93 +57,54 @@
 /*** date-related classes ***/
 
 #include "date.h"
+
 // day counters
 #include "daycounter.h"
-#if QL_INCLUDE_IN_SUBFOLDERS
-	#include "DayCounters/actual360.h"
-	#include "DayCounters/actual365.h"
-	#include "DayCounters/actualactual.h"
-	#include "DayCounters/thirty360.h"
-	#include "DayCounters/thirty360european.h"
-	#include "DayCounters/thirty360italian.h"
-#else
-	#include "actual360.h"
-	#include "actual365.h"
-	#include "actualactual.h"
-	#include "thirty360.h"
-	#include "thirty360european.h"
-	#include "thirty360italian.h"
-#endif
+#include "actual360.h"
+#include "actual365.h"
+#include "actualactual.h"
+#include "thirty360.h"
+#include "thirty360european.h"
+#include "thirty360italian.h"
+
 // calendars
 #include "calendar.h"
-#if QL_INCLUDE_IN_SUBFOLDERS
-	#include "Calendars/westerncalendar.h"
-	#include "Calendars/frankfurt.h"
-	#include "Calendars/london.h"
-	#include "Calendars/milan.h"
-	#include "Calendars/newyork.h"
-	#include "Calendars/target.h"
-	#include "Calendars/zurich.h"
-#else
-	#include "westerncalendar.h"
-	#include "frankfurt.h"
-	#include "london.h"
-	#include "milan.h"
-	#include "newyork.h"
-	#include "target.h"
-	#include "zurich.h"
-#endif
+#include "westerncalendar.h"
+#include "frankfurt.h"
+#include "london.h"
+#include "milan.h"
+#include "newyork.h"
+#include "target.h"
+#include "zurich.h"
+
 
 /*** currencies ***/
 
 #include "currency.h"
-#if QL_INCLUDE_IN_SUBFOLDERS
-	#include "Currencies/dem.h"
-	#include "Currencies/eur.h"
-	#include "Currencies/gbp.h"
-	#include "Currencies/itl.h"
-	#include "Currencies/usd.h"
-	#include "Currencies/aud.h"
-	#include "Currencies/cad.h"
-	#include "Currencies/chf.h"
-	#include "Currencies/dkk.h"
-	#include "Currencies/jpy.h"
-	#include "Currencies/sek.h"
-#else
-	#include "dem.h"
-	#include "eur.h"
-	#include "gbp.h"
-	#include "itl.h"
-	#include "usd.h"
-	#include "aud.h"
-	#include "cad.h"
-	#include "chf.h"
-	#include "dkk.h"
-	#include "jpy.h"
-	#include "sek.h"
-#endif
+#include "dem.h"
+#include "eur.h"
+#include "gbp.h"
+#include "itl.h"
+#include "usd.h"
+#include "aud.h"
+#include "cad.h"
+#include "chf.h"
+#include "dkk.h"
+#include "jpy.h"
+#include "sek.h"
 
 
 /*** instruments ***/
 
 #include "instrument.h"
-#if QL_INCLUDE_IN_SUBFOLDERS
-	#include "Instruments/stock.h"
-#else
-	#include "stock.h"
-#endif
+#include "stock.h"
 
 
 /*** term structures ***/
 
 #include "termstructure.h"
-#if QL_INCLUDE_IN_SUBFOLDERS
-	#include "TermStructures/piecewiseconstantforwards.h"
-	#include "TermStructures/flatforward.h"
-#else
-	#include "piecewiseconstantforwards.h"
-	#include "flatforward.h"
-#endif
+#include "piecewiseconstantforwards.h"
+#include "flatforward.h"
 
 
 /*** volatility surfaces ***/
@@ -155,94 +116,52 @@
 /*** math ***/
 
 #include "array.h"
+
 // math tools
-#if QL_INCLUDE_IN_SUBFOLDERS
-	#include "Math/normaldistribution.h"
-	#include "Math/statistics.h"
-	#include "Math/newcubicspline.h"
-	#include "Math/location.h"		
-	
-#else
-	#include "normaldistribution.h"
-	#include "statistics.h"
-	#include "newcubicspline.h"
-	#include "location.h"		
-#endif
+#include "normaldistribution.h"
+#include "statistics.h"
+#include "newcubicspline.h"
+#include "location.h"		
+
 // finite difference methods
-#if QL_INCLUDE_IN_SUBFOLDERS
-	#include "FiniteDifferences/backwardeuler.h"
-	#include "FiniteDifferences/bsmoperator.h"
-	#include "FiniteDifferences/boundarycondition.h"
-	#include "FiniteDifferences/cranknicolson.h"
-	#include "FiniteDifferences/finitedifferencemodel.h"
-	#include "FiniteDifferences/forwardeuler.h"
-	#include "FiniteDifferences/identity.h"
-	#include "FiniteDifferences/operator.h"
-	#include "FiniteDifferences/operatortraits.h"
-	#include "FiniteDifferences/stepcondition.h"
-	#include "FiniteDifferences/tridiagonaloperator.h"
-#else
-	#include "backwardeuler.h"
-	#include "bsmoperator.h"
-	#include "boundarycondition.h"
-	#include "cranknicolson.h"
-	#include "finitedifferencemodel.h"
-	#include "forwardeuler.h"
-	#include "identity.h"
-	#include "operator.h"
-	#include "operatortraits.h"
-	#include "stepcondition.h"
-	#include "tridiagonaloperator.h"
-#endif
+#include "backwardeuler.h"
+#include "bsmoperator.h"
+#include "boundarycondition.h"
+#include "cranknicolson.h"
+#include "finitedifferencemodel.h"
+#include "forwardeuler.h"
+#include "identity.h"
+#include "operator.h"
+#include "operatortraits.h"
+#include "stepcondition.h"
+#include "tridiagonaloperator.h"
+
 // 1-D solvers
-#if QL_INCLUDE_IN_SUBFOLDERS
-	#include "Solvers1D/bisection.h"
-	#include "Solvers1D/brent.h"
-	#include "Solvers1D/falseposition.h"
-	#include "Solvers1D/newton.h"
-	#include "Solvers1D/newtonsafe.h"
-	#include "Solvers1D/ridder.h"
-	#include "Solvers1D/secant.h"
-#else
-	#include "bisection.h"
-	#include "brent.h"
-	#include "falseposition.h"
-	#include "newton.h"
-	#include "newtonsafe.h"
-	#include "ridder.h"
-	#include "secant.h"
-#endif
+#include "solver1d.h"
+#include "bisection.h"
+#include "brent.h"
+#include "falseposition.h"
+#include "newton.h"
+#include "newtonsafe.h"
+#include "ridder.h"
+#include "secant.h"
 
 
 /*** patterns ***/
 
-#if QL_INCLUDE_IN_SUBFOLDERS
-	#include "Patterns/observable.h"
-#else
-	#include "observable.h"
-#endif
+#include "observable.h"
 
 
 /*** pricers ***/
 
 #include "options.h"
-#if QL_INCLUDE_IN_SUBFOLDERS
-	#include "Pricers/bsmoption.h"
-	#include "Pricers/bsmeuropeanoption.h"
-	#include "Pricers/bsmnumericaloption.h"
-	#include "Pricers/americancondition.h"
-	#include "Pricers/bsmamericanoption.h"
-	#include "Pricers/dividendeuropeanoption.h"
-	#include "Pricers/dividendamericanoption.h"
-#else
-	#include "bsmoption.h"
-	#include "bsmeuropeanoption.h"
-	#include "bsmnumericaloption.h"
-	#include "americancondition.h"
-	#include "bsmamericanoption.h"
-	#include "dividendeuropeanoption.h"
-	#include "dividendamericanoption.h"
-#endif
+#include "bsmoption.h"
+#include "bsmeuropeanoption.h"
+#include "bsmnumericaloption.h"
+#include "americancondition.h"
+#include "bsmamericanoption.h"
+#include "dividendeuropeanoption.h"
+#include "dividendamericanoption.h"
 
 
 /*** utilities ***/
