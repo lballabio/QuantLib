a53fa15973f7e2983ff1888dbe79160356fa0013
Marco Marchioro <marmar@users.sf.net>
2000-12-15 12:20:16 +0000

    Test for uniform and gaussian random number generators

---
+++ Python/Tests/testRNGs.py
@@ -0,0 +1,45 @@
+
+"""
+/*
+ * Copyright (C) 2000
+ * Ferdinando Ametrano, Luigi Ballabio, Adolfo Benin, Marco Marchioro
+ * 
+ * This file is part of QuantLib.
+ * QuantLib is a C++ open source library for financial quantitative
+ * analysts and developers --- http://quantlib.sourceforge.net/
+ *
+ * QuantLib is free software and you are allowed to use, copy, modify, merge,
+ * publish, distribute, and/or sell copies of it under the conditions stated 
+ * in the QuantLib License.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+ *
+ * You should have received a copy of the license along with this file;
+ * if not, contact ferdinando@ametrano.net
+ *
+ * QuantLib license is also available at http://quantlib.sourceforge.net/LICENSE.TXT
+*/
+"""
+print "Testing random number generator"
+
+from QuantLib import Statistics
+from QuantLib import RandomGenerator
+from QuantLib import GaussianGenerator
+from QuantLib import CLGaussianGenerator
+
+tol = 1e-9
+seed = 576919
+numIte = 100000
+print "Generator                         mean    sigma   skewness kurtosis   min     max"
+for RNG in [RandomGenerator, GaussianGenerator, CLGaussianGenerator]:
+  rn = RNG(seed=seed)
+  s = Statistics()
+  for ite in range(numIte):
+    s.add(rn.next())
+  print "%30s: %7.4f %8.4f %8.4f %8.4f %7.3f %7.3f " %  (RNG, s.mean(),
+    s.standardDeviation(), s.skewness(), s.kurtosis(), s.min(), s.max())
+
+print 'Press return to end this test'
+raw_input()
