823cd6a507985ff018b56cba458f52377b9908c5
Ferdinando Ametrano <nando@ametrano.net>
2000-12-20 18:01:49 +0000

    swig1.3a5 now is swig

--- Win/PyWrap.bat
+++ Win/PyWrap.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Generating wrappers...
-swig1.3a5 -python -c++ -shadow -keyword -opt -I..\SWIG -o ..\Python\quantlib_wrap.cpp ..\SWIG\QuantLib.i
+swig -python -c++ -shadow -keyword -opt -I..\SWIG -o ..\Python\quantlib_wrap.cpp ..\SWIG\QuantLib.i
 copy QuantLib.py ..\Python\QuantLib.py
 del QuantLib.py
 
