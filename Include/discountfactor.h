f4698b2618a09b35cd2c94a77c0d700c48a530dd
Luigi Ballabio <luigi.ballabio@gmail.com>
2000-12-20 15:26:04 +0000

    Temporarily removed debug implementations

--- Include/discountfactor.h
+++ Include/discountfactor.h
@@ -27,62 +27,24 @@
 	$Source$
 	$Name$
 	$Log$
+	Revision 1.7  2000/12/20 15:26:04  lballabio
+	Temporarily removed debug implementations
+
 	Revision 1.6  2000/12/14 12:32:29  lballabio
 	Added CVS tags in Doxygen file documentation blocks
-
+	
 */
 
 #ifndef quantlib_discount_factor_h
 #define quantlib_discount_factor_h
 
 #include "qldefines.h"
-#include "qlerrors.h"
-#include "dataformatters.h"
 
 namespace QuantLib {
 
-	#ifdef QL_DEBUG
+	//! used to describe discount factors between dates.
+	typedef double DiscountFactor;
 	
-		class DiscountFactor {
-		  public:
-			friend DiscountFactor operator*(DiscountFactor x, DiscountFactor y) { return DiscountFactor(x.factor*y.factor); }
-			friend double operator*(DiscountFactor x, double y) { return x.factor*y; }
-			friend double operator*(double x, DiscountFactor y) { return x*y.factor; }
-			friend double operator/(DiscountFactor x, DiscountFactor y) { return x.factor/y.factor; }
-			friend double operator/(DiscountFactor x, double y) { return x.factor/y; }
-			friend double operator/(double x, DiscountFactor y) { return x/y.factor; }
-			// constructor
-			DiscountFactor(double f = 0.0);
-			// assignment
-			DiscountFactor& operator=(double);
-			// cast
-			operator double() const { return factor; }
-			// computed assignment
-			DiscountFactor& operator*=(DiscountFactor x) { return (*this = DiscountFactor(factor*x.factor)); }
-		  private:
-			double factor;
-		};
-	
-		// inline definitions
-	
-		inline DiscountFactor::DiscountFactor(double f) {
-			Require(f >= 0.0 && f <= 1.0,"invalid discount factor ("+DoubleFormatter::toString(f)+"). Value must be between 0 and 1");
-			factor = f;
-		}
-	
-		inline DiscountFactor& DiscountFactor::operator=(double f) {
-			Require(f >= 0.0 && f <= 1.0,"invalid discount factor ("+DoubleFormatter::toString(f)+"). Value must be between 0 and 1");
-			factor = f;
-			return *this;
-		}
-	
-	#else
-	
-		//! used to describe discount factors between dates.
-		typedef double DiscountFactor;
-	
-	#endif
-
 }
 
 
--- Include/rate.h
+++ Include/rate.h
@@ -27,90 +27,23 @@
 	$Source$
 	$Name$
 	$Log$
+	Revision 1.7  2000/12/20 15:26:04  lballabio
+	Temporarily removed debug implementations
+
 	Revision 1.6  2000/12/14 12:32:29  lballabio
 	Added CVS tags in Doxygen file documentation blocks
-
+	
 */
 
 #ifndef quantlib_yield_h
 #define quantlib_yield_h
 
 #include "qldefines.h"
-#include "qlerrors.h"
-#include "spread.h"
-#include "dataformatters.h"
 
 namespace QuantLib {
 
-	#ifdef QL_DEBUG
-	
-		class Rate {
-			// arithmetic operators
-			friend Rate operator+(Rate y1, Rate y2) { return Rate(y1.yield+y2.yield); }
-			friend Rate operator+(Rate y, Spread s) { return Rate(y.yield+double(s)); }
-			friend Rate operator+(Spread s, Rate y) { return Rate(double(s)+y.yield); }
-			friend double operator+(Rate y, double x) { return y.yield+x; }
-			friend double operator+(double x, Rate y) { return x+y.yield; }
-			friend Spread operator-(Rate y1, Rate y2) { return Spread(y1.yield-y2.yield); }
-			friend Rate operator-(Rate y, Spread s) { return Rate(y.yield-double(s)); }
-			friend double operator-(Rate y, double x) { return y.yield-x; }
-			friend double operator-(double x, Rate y) { return x-y.yield; }
-			friend double operator*(Rate y, double x) { return y.yield*x; }
-			friend double operator*(double x, Rate y) { return y.yield*x; }
-			friend Rate operator/(Rate y, double x) { return Rate(y.yield/x); }
-			// comparison operators
-			friend bool operator==(Rate y1, Rate y2) { return (y1.yield==y2.yield); }
-			friend bool operator!=(Rate y1, Rate y2) { return (y1.yield!=y2.yield); }
-			friend bool operator<(Rate y1, Rate y2) { return (y1.yield<y2.yield); }
-			friend bool operator<=(Rate y1, Rate y2) { return (y1.yield<=y2.yield); }
-			friend bool operator>(Rate y1, Rate y2) { return (y1.yield>y2.yield); }
-			friend bool operator>=(Rate y1, Rate y2) { return (y1.yield>=y2.yield); }
-		  public:
-			// constructor
-			Rate(double y = 0.0);
-			// assignment
-			Rate& operator=(double y);
-			Rate& operator=(const Rate&);
-			// cast
-			operator double() const { return yield; }
-			// computed assignment
-			Rate& operator+=(Rate y) { return (*this = Rate(yield+y.yield)); }
-			Rate& operator+=(Spread s) { return (*this = Rate(yield+double(s))); }
-			Rate& operator-=(Rate y) { return (*this = Rate(yield-y.yield)); }
-			Rate& operator-=(Spread s) { return (*this = Rate(yield-double(s))); }
-			Rate& operator*=(double x) { return (*this = Rate(yield*x)); }
-			Rate& operator/=(double x) { return (*this = Rate(yield/x)); }
-		  private:
-			double yield;
-		};
-	
-	
-		// inline definitions
-	
-		inline Rate::Rate(double y) {
-			Require(y >= 0.0 && y <= 1.0,
-				"invalid yield value ("+RateFormatter::toString(y)+"). Value must be between 0 and 1");
-			yield = y;
-		}
-	
-		inline Rate& Rate::operator=(double y) {
-			Require(y >= 0.0 && y <= 1.0,
-				"invalid yield value ("+RateFormatter::toString(y)+"). Value must be between 0 and 1");
-			yield = y;
-			return *this;
-		}
-	
-		inline Rate& Rate::operator=(const Rate& y) {
-			yield = y.yield;
-			return *this;
-		}
-	
-	#else
-	
-		//! used to describe interest rates.
-		typedef double Rate;
-	
-	#endif
+	//! used to describe interest rates.
+	typedef double Rate;
 
 }
 
