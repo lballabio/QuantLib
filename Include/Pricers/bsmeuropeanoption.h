03d49858a4009bfc7c91fd8bd4b85a0b2c481f0d
Ferdinando Ametrano <nando@ametrano.net>
2001-01-16 11:23:27 +0000

    removed tabs and enforced 80 columns

--- Include/Pricers/bsmeuropeanoption.h
+++ Include/Pricers/bsmeuropeanoption.h
@@ -2,16 +2,16 @@
 /*
  * Copyright (C) 2000
  * Ferdinando Ametrano, Luigi Ballabio, Adolfo Benin, Marco Marchioro
- * 
+ *
  * This file is part of QuantLib.
  * QuantLib is a C++ open source library for financial quantitative
  * analysts and developers --- http://quantlib.sourceforge.net/
  *
  * QuantLib is free software and you are allowed to use, copy, modify, merge,
- * publish, distribute, and/or sell copies of it under the conditions stated 
+ * publish, distribute, and/or sell copies of it under the conditions stated
  * in the QuantLib License.
  *
- * This program is distributed in the hope that it will be useful, but 
+ * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
  *
@@ -22,13 +22,16 @@
 */
 
 /*! \file bsmeuropeanoption.h
-	\brief european option
-	
-	$Source$
-	$Name$
-	$Log$
-	Revision 1.4  2000/12/14 12:40:13  lballabio
-	Added CVS tags in Doxygen file documentation blocks
+    \brief european option
+
+    $Source$
+    $Name$
+    $Log$
+    Revision 1.5  2001/01/16 11:23:27  nando
+    removed tabs and enforced 80 columns
+
+    Revision 1.4  2000/12/14 12:40:13  lballabio
+    Added CVS tags in Doxygen file documentation blocks
 
 */
 
@@ -41,31 +44,34 @@
 
 namespace QuantLib {
 
-	namespace Pricers {
-	
-		class BSMEuropeanOption : public BSMOption {
-		  public:
-			  // constructor
-			  BSMEuropeanOption(Type type, double underlying, double strike, Rate underlyingGrowthRate, 
-			    Rate riskFreeRate, Time residualTime, double volatility)
-			    : BSMOption(type,underlying,strike,underlyingGrowthRate,riskFreeRate,residualTime,volatility) {}
-			  // accessors
-			  double value() const;
-			  double delta() const;
-			  double gamma() const;
-			  double theta() const;
-			  double vega() const;
-			  double rho() const;
-		    Handle<BSMOption> clone() const {	return Handle<BSMOption>(new BSMEuropeanOption(*this)); }
-		  private:
-		    // declared as mutable to preserve
-		    // the logical constness (does this word exist?) of value()
-		    mutable DiscountFactor growthDiscount, riskFreeDiscount;
-		    mutable double standardDeviation;
-		    mutable double alpha, beta, NID1;
-		};
+    namespace Pricers {
 
-	}
+        class BSMEuropeanOption : public BSMOption {
+          public:
+              // constructor
+              BSMEuropeanOption(Type type, double underlying, double strike,
+                        Rate underlyingGrowthRate, Rate riskFreeRate,
+                        Time residualTime, double volatility)
+              : BSMOption(type, underlying, strike, underlyingGrowthRate,
+                            riskFreeRate, residualTime, volatility) {}
+              // accessors
+              double value() const;
+              double delta() const;
+              double gamma() const;
+              double theta() const;
+              double vega() const;
+              double rho() const;
+            Handle<BSMOption> clone() const {
+                return Handle<BSMOption>(new BSMEuropeanOption(*this)); }
+          private:
+            // declared as mutable to preserve
+            // the logical constness (does this word exist?) of value()
+            mutable DiscountFactor growthDiscount, riskFreeDiscount;
+            mutable double standardDeviation;
+            mutable double alpha, beta, NID1;
+        };
+
+    }
 
 }
 
