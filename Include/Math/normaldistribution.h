cea897e2fdadb8078a7410dde1ff7410b5752676
Ferdinando Ametrano <nando@ametrano.net>
2001-01-16 11:22:17 +0000

    fixed typo in error message

--- Include/Math/normaldistribution.h
+++ Include/Math/normaldistribution.h
@@ -2,16 +2,16 @@
 /*
  * Copyright (C) 2000
  * Ferdinando Ametrano, Luigi Ballabio, Adolfo Benin, Marco Marchioro
- * 
+ *
  * This file is part of QuantLib.
  * QuantLib is a C++ open source library for financial quantitative
  * analysts and developers --- http://quantlib.sourceforge.net/
  *
  * QuantLib is free software and you are allowed to use, copy, modify, merge,
- * publish, distribute, and/or sell copies of it under the conditions stated 
+ * publish, distribute, and/or sell copies of it under the conditions stated
  * in the QuantLib License.
  *
- * This program is distributed in the hope that it will be useful, but 
+ * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
  *
@@ -23,10 +23,13 @@
 
 /*! \file normaldistribution.h
     \brief normal, cumulative and inverse cumulative distributions
-    
+
     $Source$
     $Name$
     $Log$
+    Revision 1.8  2001/01/16 11:22:17  nando
+    fixed typo in error message
+
     Revision 1.7  2001/01/11 18:06:09  nando
     generalized to sigma<>1 and average<>0
     Also added NormalDistribution.derivative().
@@ -38,13 +41,13 @@
 
     Revision 1.5  2001/01/04 11:59:29  lballabio
     Line wraps fixed when stumbling upon them
-    
+
     Revision 1.4  2000/12/27 17:18:35  lballabio
     Changes for compiling under Linux and Alpha Linux
-    
+
     Revision 1.3  2000/12/14 12:32:30  lballabio
     Added CVS tags in Doxygen file documentation blocks
-    
+
 */
 
 #ifndef quantlib_normal_distribution_h
@@ -68,11 +71,11 @@ namespace QuantLib {
             static const double pi_;
             double average_, sigma_, normalizationFactor_, denominator_;
         };
-        
-        typedef NormalDistribution GaussianDistribution;
-        
 
-        class CumulativeNormalDistribution : 
+        typedef NormalDistribution GaussianDistribution;
+
+
+        class CumulativeNormalDistribution :
           public std::unary_function<double,double> {
           public:
             CumulativeNormalDistribution(double average = 0.0,
@@ -85,9 +88,9 @@ namespace QuantLib {
             static const double a1_, a2_, a3_, a4_, a5_, gamma_, precision_;
             NormalDistribution gaussian_;
         };
-        
 
-        class InvCumulativeNormalDistribution : 
+
+        class InvCumulativeNormalDistribution :
           public std::unary_function<double,double> {
           public:
             InvCumulativeNormalDistribution(double average = 0.0,
@@ -108,7 +111,7 @@ namespace QuantLib {
 
             QL_REQUIRE(sigma_>0.0,
                 "NormalDistribution::NormalDistribution"
-                ": sigma must be greater 0.0 ("
+                ": sigma must be greater than 0.0 ("
                 + DoubleFormatter::toString(sigma_) + " not allowed");
 
             normalizationFactor_ = 1.0/(sigma_*QL_SQRT(2.0*pi_));
@@ -130,7 +133,7 @@ namespace QuantLib {
 
             QL_REQUIRE(sigma_>0.0,
                 "CumulativeNormalDistribution::CumulativeNormalDistribution"
-                ": sigma must be greater 0.0 ("
+                ": sigma must be greater than 0.0 ("
                 + DoubleFormatter::toString(sigma_) + " not allowed");
         }
 
@@ -145,12 +148,12 @@ namespace QuantLib {
 
             QL_REQUIRE(sigma_>0.0,
               "InvCumulativeNormalDistribution::InvCumulativeNormalDistribution"
-                ": sigma must be greater 0.0 ("
+                ": sigma must be greater than 0.0 ("
                 + DoubleFormatter::toString(sigma_) + " not allowed");
         }
 
     }
-    
+
 }
 
 
