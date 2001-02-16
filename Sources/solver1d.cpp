7cad841a73a2cf8b1e5dce3f8c11e5a786202458
Marco Marchioro <marmar@users.sf.net>
2001-02-16 07:57:41 +0000

    The condition "fxMax_ == 0.0" became "if (QL_FABS(fxMax_) <= xAccuracy", and other minor changes

--- Sources/solver1d.cpp
+++ Sources/solver1d.cpp
@@ -27,6 +27,10 @@
     $Source$
     $Name$
     $Log$
+    Revision 1.13  2001/02/16 07:57:41  marmar
+    The condition "fxMax_ == 0.0" became "if (QL_FABS(fxMax_) <= xAccuracy",
+    and other minor changes
+
     Revision 1.12  2001/01/17 13:53:47  nando
     80 columns enforced
     tabs removed
@@ -60,10 +64,10 @@ namespace QuantLib {
         fxMax_ = f.value(root_);
 
         // monotonically crescent bias, as in optionValue(volatility)
-        if (fxMax_ == 0.0)
+        if (QL_FABS(fxMax_) <= xAccuracy)
             return root_;
         else if (fxMax_ > 0.0) {
-            xMin_ = enforceBounds_(root_-step);
+            xMin_ = enforceBounds_(root_ - step);
             fxMin_ = f.value(xMin_);
             xMax_ = root_;
         } else {
@@ -73,8 +77,8 @@ namespace QuantLib {
             fxMax_ = f.value(xMax_);
         }
 
-        evaluationNumber_=2;
-        while (evaluationNumber_<=maxEvaluations_) {
+        evaluationNumber_ = 2;
+        while (evaluationNumber_ <= maxEvaluations_) {
             if (fxMin_*fxMax_ <= 0.0) {
                 if (fxMin_ == 0.0)    return xMin_;
                 if (fxMax_ == 0.0)    return xMax_;
@@ -82,19 +86,19 @@ namespace QuantLib {
                 return solve_(f, QL_MAX(QL_FABS(xAccuracy), QL_EPSILON));
             }
             if (QL_FABS(fxMin_) < QL_FABS(fxMax_)) {
-                xMin_ = enforceBounds_(xMin_+growthFactor*(xMin_-xMax_));
-                fxMin_=f.value(xMin_);
+                xMin_ = enforceBounds_(xMin_+growthFactor*(xMin_ - xMax_));
+                fxMin_= f.value(xMin_);
             } else if (QL_FABS(fxMin_) > QL_FABS(fxMax_)) {
-                xMax_ = enforceBounds_(xMax_+growthFactor*(xMax_-xMin_));
-                fxMax_=f.value(xMax_);
+                xMax_ = enforceBounds_(xMax_+growthFactor*(xMax_ - xMin_));
+                fxMax_= f.value(xMax_);
             } else if (flipflop == -1) {
-                xMin_ = enforceBounds_(xMin_+growthFactor*(xMin_-xMax_));
-                fxMin_=f.value(xMin_);
+                xMin_ = enforceBounds_(xMin_+growthFactor*(xMin_ - xMax_));
+                fxMin_= f.value(xMin_);
                 evaluationNumber_++;
                 flipflop = 1;
             } else if (flipflop == 1) {
-                xMax_ = enforceBounds_(xMax_+growthFactor*(xMax_-xMin_));
-                fxMax_=f.value(xMax_);
+                xMax_ = enforceBounds_(xMax_+growthFactor*(xMax_ - xMin_));
+                fxMax_= f.value(xMax_);
                 flipflop = -1;
             }
             evaluationNumber_++;
@@ -163,3 +167,8 @@ namespace QuantLib {
     }
 
 }
+
+
+
+
+
