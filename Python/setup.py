0c9fadb66e8c4b0890f12d655012b8f9ec1151c7
Marco Marchioro <marmar@users.sf.net>
2001-03-06 13:10:07 +0000

    Copyright notice added

--- Python/setup.py
+++ Python/setup.py
@@ -1,3 +1,34 @@
+"""
+/*
+ * Copyright (C) 2001 QuantLib Group
+ * 
+ * This file is part of QuantLib.
+ * QuantLib is a C++ open source library for financial quantitative
+ * analysts and developers --- http://quantlib.sourceforge.net/
+ *
+ * QuantLib is free software and you are allowed to use, copy, modify, merge,
+ * publish, distribute, and/or sell copies of it under the conditions stated 
+ * in the QuantLib License.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+ *
+ * You should have received a copy of the license along with this file;
+ * if not, contact ferdinando@ametrano.net
+ *
+ * QuantLib license is also available at:
+ * http://quantlib.sourceforge.net/LICENSE.TXT
+*/
+"""
+
+""" 
+    $Source$
+    $Log$
+    Revision 1.2  2001/03/06 13:10:07  marmar
+    Copyright notice added
+
+"""
 
 from distutils.core import setup, Extension
 
