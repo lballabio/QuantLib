9e8dd49ffc7d8c57dc90d60230131133cce5fb5d
Ferdinando Ametrano <nando@ametrano.net>
2001-03-19 20:46:22 +0000

    don't need normal distributions anymore to trap the bug

--- Python/SwigProblem/MyLib.i
+++ Python/SwigProblem/MyLib.i
@@ -3,17 +3,10 @@
 
 %{
 #include "helloworld.h"
-#include "normaldistribution.h"
-using QuantLib::Math::NormalDistribution;
 %}
 
 
 void HelloWorld();
 
 
-class NormalDistribution {
-  public:
-    NormalDistribution(double average = 0.0, double sigma = 1.0);
-    ~NormalDistribution();
-};
 
--- Python/SwigProblem/helloworld.cpp
+++ Python/SwigProblem/helloworld.cpp
@@ -1,11 +1,19 @@
 
 #include <stdio.h>
+#include <iostream>
 #include "helloworld.h"
+#include "dataformatters.h"
 
 
 void HelloWorld() {
 
-	printf("Hello, world\n");
+    double dummy = 5.7;
+
+    std::cout << "Hello, world" << std::endl;
+
+
+    std::cout << ("Hello, world," + QuantLib::DoubleFormatter::toString(dummy) + " is a number")
+        << std::endl;
 
 }
 
