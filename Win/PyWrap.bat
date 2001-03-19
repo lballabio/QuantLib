fd837f48a9588ea7b4bb8167d31c6598d12090ab
Ferdinando Ametrano <nando@ametrano.net>
2001-03-19 18:57:09 +0000

    PyQuantLib.dsp moved under Python\Win dir

--- Win/PyWrap.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Generating wrappers...
-swig -python -c++ -shadow -keyword -opt -I..\SWIG -o ..\Python\quantlib_wrap.cpp ..\SWIG\QuantLib.i
-copy QuantLib.py ..\Python\QuantLib.py
-del QuantLib.py
-
-pause
