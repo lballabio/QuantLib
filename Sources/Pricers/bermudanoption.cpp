ece5bf276277200c25381e0e768d2eeed147daf2
Marco Marchioro <marmar@users.sf.net>
2001-03-21 09:53:47 +0000

    simple BermudanOption pricer added

---
+++ Sources/Pricers/bermudanoption.cpp
@@ -0,0 +1,71 @@
+
+/*
+ * Copyright (C) 2001
+ * Ferdinando Ametrano, Luigi Ballabio, Adolfo Benin, Marco Marchioro
+ *
+ * This file is part of QuantLib.
+ * QuantLib is a C++ open source library for financial quantitative
+ * analysts and developers --- http://quantlib.sourceforge.net/
+ *
+ * QuantLib is free software and you are allowed to use, copy, modify, merge,
+ * publish, distribute, and/or sell copies of it under the conditions stated
+ * in the QuantLib License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+ *
+ * You should have received a copy of the license along with this file;
+ * if not, contact ferdinando@ametrano.net
+ *
+ * QuantLib license is also available at
+ *   http://quantlib.sourceforge.net/LICENSE.TXT
+*/
+
+/*! \file bermudanoption.cpp
+    \brief  class for finite-difference evaluation of bermudan option 
+
+    $Source$
+    $Log$
+    Revision 1.1  2001/03/21 09:52:19  marmar
+    simple BermudanOption pricer added
+
+*/
+
+#include "bermudanoption.h"
+#include "bsmeuropeanoption.h"
+
+namespace QuantLib {
+
+    namespace Pricers {
+
+        BermudanOption::BermudanOption(Type type, double underlying,
+            double strike, Rate dividendYield, Rate riskFreeRate,
+            Time residualTime, double volatility,
+            const std::vector<Time>& dates, 
+            int timeSteps, int gridPoints)
+        : MultiPeriodOption(type, underlying, strike, dividendYield, 
+          riskFreeRate, residualTime, volatility, dates, timeSteps, 
+          gridPoints) {
+            
+            analitic_ = Handle<BSMOption> (new BSMEuropeanOption (
+                            type_, underlying_, strike_, dividendYield_,
+                            riskFreeRate_, residualTime_, volatility_));
+        }
+
+        using FiniteDifferences::StandardStepCondition;
+
+        void BermudanOption::initializeStepCondition() const{
+            stepCondition_ = Handle<StandardStepCondition> ();
+        }
+
+        void BermudanOption::executeIntermediateStep(int step) const{
+            
+            int size = initialPrices_.size();
+            for(int j = 0; size; j++)
+                prices_[j] = QL_MAX(prices_[j], initialPrices_[j]);            
+        }
+
+    }
+
+}
