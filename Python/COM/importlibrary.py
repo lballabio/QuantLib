34cc4f840c3fac1fc49a3a10aac86841c1b8d328
Marco Marchioro <marmar@users.sf.net>
2001-03-30 09:56:20 +0000

    Debug is now the default, simplified and speeded up

--- Python/COM/importlibrary.py
+++ Python/COM/importlibrary.py
@@ -27,19 +27,16 @@
     
     $Source$
     $Log$
+    Revision 1.5  2001/03/30 09:56:20  marmar
+    Debug is now the default, simplified and speeded up
+
     Revision 1.4  2001/03/14 13:36:20  marmar
     Changed to accomodate some bad behaviour of VB
-
-    Revision 1.3  2001/03/12 13:09:22  marmar
-    instances of Array and Matrix can be easily accessed by COM
-
-    Revision 1.2  2001/03/09 12:51:09  marmar
-    Now python can handle COM call to _value_
-
+    
     Revision 1.1  2001/03/06 13:52:35  marmar
     Export the QuantLib functionalities to the COM world
-
-"""    
+    
+"""
 
 """
 Example:
@@ -47,16 +44,10 @@ Example:
     ' QuantLib module and uses it
 
     ' The object ImportLibrary must be initialized before any python 
-    ' module can be created. Tip: define ImportLibrary as Public
-    Public ImportLibrary As Object
-    ImportLibrary = CreateObject("Python.ImportLibrary")
-    
-    ' Import the python module QuantLib and registers it
-    Dim QuantLib As Object
-    Set QuantLib = ImportLibrary.QuantLib
+    ' module can be created. Tip: define QuantLib as Public
+    Set QuantLib = CreateObject("QuantLib.Import")
     
     ' Create a QuantLib object
-    Dim uniformDeviate As Object
     Set uniformDeviate = QuantLib.UniformRandomGenerator()
     MsgBox uniformDeviate.next()
 
@@ -77,12 +68,6 @@ from win32com.server.policy import BasicWrapPolicy
 from win32com.server.policy import DynamicPolicy
 from pythoncom import *
 
-PyIDispatchType = TypeIIDs[IID_IDispatch]
-# Set useDebug  = 0 to avoid debugging of COM,
-#                   note that any print statement will raise an exception
-# Set useDebug  = 1 to use "Python Trace Collector" to debug
-useDebug = 0 
-
 def FixArguments(args):
     """Unicode objects are converted to strings, PyIDispatch objects and
     collections are unwrapped. Does this recursively on lists
@@ -93,14 +78,6 @@ def FixArguments(args):
             arg = FixArguments(arg)
         elif type(arg) is pywintypes.UnicodeType:
             arg = str(arg)
-        elif type(arg) is PyIDispatchType:
-            try:
-                arg = win32com.server.util.unwrap(arg)
-                if string.count(repr(arg),'Collection'):
-                    arg = arg.data
-            except:
-               raise COMException("%s is not a python COM Object" %
-                                  repr(arg), 1)
         newArgs.append(arg)
     return tuple(newArgs)
 
@@ -117,17 +94,15 @@ def FixVisualBasicName(name):
         return string.lower(name[0]) + name[1:]
 
 def PrepareForReturn(object):
-   
     objectType = type(object)
-    if(objectType is types.InstanceType or
-            objectType is types.ModuleType):
-        if useDebug == 1:
-            ob = DefaultDebugDispatcher(QuitePermissivePolicy, object)
-        else:
-            ob = QuitePermissivePolicy(object)
-        wrapped = WrapObject(ob)
-        return wrapped
+    print "----> Returning", object, objectType
+    if(objectType is types.InstanceType or objectType is types.ModuleType):
+        print "Returning wrapped object"
+        return WrapObject(
+                    DefaultDebugDispatcher(
+                            QuitePermissivePolicy, object))
     else:
+        print "Returning simple object"
         return object
 
 
@@ -141,8 +116,6 @@ class QuitePermissivePolicy(DynamicPolicy):
     unexpected python error.  The advise is anyway to wrap any attribute call
     in a getAttribute method.
     """
-    _reg_clsctx_ = CLSCTX_INPROC_SERVER
-     
     def _wrap_(self, object):
         BasicWrapPolicy._wrap_(self, object)
         self._next_dynamic_ = self._min_dynamic_ = 1000
@@ -159,30 +132,19 @@ class QuitePermissivePolicy(DynamicPolicy):
         args = FixArguments(args)
         if not hasattr(self._obj_, name):
             name = FixVisualBasicName(name)
-         
+
+        print "self._obj_:",self._obj_
+        print "name:",name
+        print "args:",args
+
         if wFlags & DISPATCH_METHOD:
             if name == '_value_':
                 # VB calls to COM of the type "PyCOM_object(args)" are mapped
                 # into calls to the method "_value_".
-               
                 # When 'self._obj_' has the attribute '__call__' then it
                 #is returned 'self._obj_(args)'
                 if hasattr(self._obj_, '__call__'):
-                    return PrepareForReturn(apply(self._obj_,args))                  
-                # otherwise assume array call of the type
-                # A(i), if there is only one argument
-                elif len(args) == 1: 
-                    return PrepareForReturn(self._obj_[int(args[0])])
-                # A(i,j), if there are two arguments
-                elif len(args) == 2:
-                    return PrepareForReturn( 
-                               self._obj_[int(args[0])][int(args[1])])
-                # A(i,j,k), if there are three arguments
-                elif len(args) == 3:
-                    return PrepareForReturn(
-                          self._obj_[int(args[0])][int(args[1])][int(args[2])])
-                # Sometimes VB gives i, j, k as double so it is necessary to
-                # convert them into integers using int()
+                    return PrepareForReturn(apply(self._obj_,args))
                 else:
                     raise COMException(
                     "Cannot compute '%s %s'" % (repr(self._obj_), args),
@@ -210,52 +172,25 @@ class QuitePermissivePolicy(DynamicPolicy):
             # Calls of the type "PyCOM_object = VB_value" are 
             if len(args) == 1:
                 setattr(self._obj_, name, args)
-            # If there is more than one arguments one of the following
-            # can be assumed to be "A(i) = VB_value", if there are two arguments
-            elif len(args) == 2:
-                self._obj_[int(args[0])] = args[1]
-            # or  "A(i,j) = VB_value", if there are three arguments
-            elif len(args) == 3:
-                self._obj_[int(args[0])][int(args[1])] = args[2]
-            # or even "A(i,j,k) = VB_value", if there are four arguments
-            elif len(args) == 4:
-                self._obj_[int(args[0])][int(args[1])][int(args[2])] = args[3]
-            # Again, sometimes VB gives i, j, k as double so it is necessary to
-            # convert them into integers using int()
             else:
                 raise COMException(
                     "DISPATCH_PROPERTYPUT called with too many arguments",
                     winerror.E_INVALIDARG)
-            # When calls of the type specified above are made the index "i", is
-            # changed to be the return value of the function. Therefore we need:
-            return int(args[0])
-            
         raise COMException(scode=winerror.E_INVALIDARG, desc="invalid wFlags")
 
-
 class ComImportLibrary:
-    """ Creates a COM wrapper for any python module.
+    """ Creates a COM wrapper for QuantLib.
     """
-    _reg_clsid_ = "{13BD06C1-123F-11D5-83CD-0050DA367EDA}"
-    _reg_progid_ = "Python.ImportLibrary"
-    _reg_policy_spec_ = "DynamicPolicy"
-    _reg_clsctx_ = CLSCTX_INPROC_SERVER
-    def _dynamic_(self, name, lcid, wFlags, args):
-        name = str(name)
-        args = FixArguments(args)
-        if (not (wFlags & (DISPATCH_PROPERTYGET | DISPATCH_METHOD)) ):
-            raise COMException("Operation not valid on a python module",
-                               winerror.ERROR_ACCESS_DENIED)
-         
-        if name == 'python_empty_list':
-            return win32com.server.util.NewCollection([])
-         
+    _reg_clsid_ = "{389276A1-24EB-11D5-83D4-0050DA367EDA}"
+    _reg_progid_ = "QuantLib.Import"
+    _public_methods_ = ['QuantLib']
+    def QuantLib(self):
+        print "ComImportLibrary called"
         try :
-            exec ('import ' + name)
-            exec ('factory = PrepareForReturn(%s)' % (name,))
-            return factory 
+            import QuantLib
+            return PrepareForReturn(QuantLib)
         except:
-            raise COMException("Python module '%s' not found" % (name,),
+            raise COMException("Module QuantLib not found",
                                winerror.ERROR_FILE_NOT_FOUND)
 
 if __name__ == '__main__':
