04ca7d82789b6db97a245ccbd523474ded9cf117
Luigi Ballabio <luigi.ballabio@gmail.com>
2001-04-12 15:06:18 +0000

    *** empty log message ***

--- Python/PyWrap.bat
+++ Python/PyWrap.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Generating wrappers...
-swig -python -c++ -shadow -keyword -opt -I"%QL_DIR%\SWIG" -o quantlib_wrap.cpp "%QL_DIR%\SWIG\QuantLib.i"
+swig -python -c++ -shadow -keyword -opt -I..\SWIG -o quantlib_wrap.cpp ..\SWIG\QuantLib.i
 
 pause
