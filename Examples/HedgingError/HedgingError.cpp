07b5b4962375d6034916b4e232749ca5856a25b9
Ferdinando Ametrano <nando@ametrano.net>
2001-07-27 16:47:25 +0000

    updated in order to compile with the new GeneralMontecarlo interface.
    Added comments, renamed variables, removed tabs (4 spaces),
    style enforced.
    
    still to do:
    1) add hedgingerror.hpp
    2) avoid using namespace QuantLib in favour of
        using QuantLib::Rate

--- Examples/HedgingError/HedgingError.cpp
+++ Examples/HedgingError/HedgingError.cpp
@@ -21,12 +21,24 @@
  * available at http://quantlib.org/Authors.txt
 */
 
-/*
-    $Id$
+/*! \file HedgingError.cpp
+    \brief Example about using Montecarlo Path Pricer
+
+    \fullpath Examples/HedgingError/HedgingError.cpp
 */
 
-// $Source$
 // $Log$
+// Revision 1.5  2001/07/27 16:47:25  nando
+// updated in order to compile with the new GeneralMontecarlo interface.
+//
+// Added comments, renamed variables, removed tabs (4 spaces),
+// style enforced.
+//
+// still to do:
+// 1) add hedgingerror.hpp
+// 2) avoid using namespace QuantLib in favour of
+//     using QuantLib::Rate
+//
 // Revision 1.4  2001/07/25 15:47:27  sigmud
 // Change from quantlib.sourceforge.net to quantlib.org
 //
@@ -38,7 +50,6 @@
 //
 // Revision 1.1  2001/07/24 16:36:58  dzuki
 // Discontinuous hedging error calculation example (HedgingError)
-//
 
 
 #include "stdlib.h"
@@ -54,97 +65,133 @@ using namespace QuantLib::MonteCarlo;
 
 
 
-class PathHedgeErrorCalculator : public PathPricer
+class HedgeErrorPathPricer : public PathPricer
 {
   public:
-    PathHedgeErrorCalculator():PathPricer(){}
-    PathHedgeErrorCalculator(Option::Type type, double underlying,
-        double strike, Rate r, double maturity, double sigma);
-
-	double value(const Path &path) const;
-
-	double computePlainVanilla(Option::Type type, double price,
-								double strike, double discount) const;
-
+    HedgeErrorPathPricer():PathPricer(){}
+    HedgeErrorPathPricer(Option::Type type,
+                         double underlying,
+                         double strike,
+                         Rate r,
+                         Time maturity,
+                         double sigma);
+    double value(const Path &path) const;
+    double computePlainVanilla(Option::Type type,
+                               double price,
+                               double strike,
+                               double discount) const;
   protected:
-
-	mutable Option::Type type_;
-	mutable Rate r_;
-    mutable double underlying_, strike_, maturity_, sigma_;
-
+    Option::Type type_;
+    Rate r_;
+    double underlying_, strike_, sigma_;
+    Time maturity_;
 };
 
-PathHedgeErrorCalculator::PathHedgeErrorCalculator(Option::Type type,
-                                    double underlying,
-                                    double strike,
-                                    double r,
-                                    double maturity,
-                                    double sigma)
-            :   type_(type),underlying_(underlying),
-                strike_(strike), r_(r),
-                maturity_(maturity), sigma_(sigma)
+HedgeErrorPathPricer::HedgeErrorPathPricer(Option::Type type,
+                                           double underlying,
+                                           double strike,
+                                           double r,
+                                           Time maturity,
+                                           double sigma)
+: type_(type),underlying_(underlying), strike_(strike), r_(r),
+  maturity_(maturity), sigma_(sigma)
 {
     QL_REQUIRE(strike_ > 0.0,
-        "PathHedgeErrorCalculator: strike must be positive");
+        "HedgeErrorPathPricer: strike must be positive");
     QL_REQUIRE(underlying_ > 0.0,
-        "PathHedgeErrorCalculator: underlying must be positive");
-    QL_REQUIRE(r_ >= 0.0, "PathHedgeErrorCalculator: risk free rate (r) must"
+        "HedgeErrorPathPricer: underlying must be positive");
+    QL_REQUIRE(r_ >= 0.0, "HedgeErrorPathPricer: risk free rate (r) must"
         " be positive or zero");
    QL_REQUIRE(maturity_ > 0.0,
-        "PathHedgeErrorCalculator: maturity must be positive");
-   QL_REQUIRE(sigma_ >= 0.0, "PathHedgeErrorCalculator: volatility (sigma)"
+        "HedgeErrorPathPricer: maturity must be positive");
+   QL_REQUIRE(sigma_ >= 0.0, "HedgeErrorPathPricer: volatility (sigma)"
         " must be positive or zero");
 
    isInitialized_ = true;
 }
 
-double PathHedgeErrorCalculator::value(const Path & path) const
+double HedgeErrorPathPricer::value(const Path & path) const
 {
+    QL_REQUIRE(isInitialized_,
+        "HedgeErrorPathPricer: pricer not initialized");
+
     int n = path.size();
-
-	QL_REQUIRE(isInitialized_,
-        "PathHedgeErrorCalculator: pricer not initialized");
     QL_REQUIRE(n>0,
-        "PathHedgeErrorCalculator: the path cannot be empty");
+        "HedgeErrorPathPricer: the path cannot be empty");
 
-    double log_price = 0.0;
-	double stock = underlying_;
-	BSMEuropeanOption option = BSMEuropeanOption(type_,
-											stock,
-											strike_,
-											0.0,
-											r_, maturity_, sigma_);
+    // discrete hedging interval
+    double dt = maturity_/n;
 
-	double delta = option.delta();
-	double money_account = option.value() - delta*stock;
-        double dt = maturity_/n;
+    // initialize stock value at the start of the path
+    double stockLogGrowth = 0.0;
+    double stock = underlying_;
 
-	for(int i = 0; i < n; i++){
-        log_price += path[i];
-		money_account *= QL_EXP( r_*dt );
-		stock = underlying_*QL_EXP(log_price);
-		if(i < n-1) {
-			BSMEuropeanOption option = BSMEuropeanOption(type_, stock, strike_,
-			    0.0, r_, maturity_ - dt*(i+1), sigma_);
-			double new_delta = option.delta();
-			money_account -= (new_delta - delta)*stock;
-			delta = new_delta;
-		}
-	}
 
-    return delta*stock + money_account - computePlainVanilla(type_, stock,
-															 strike_, 1.0);
+    /************************/
+    /*** the initial deal ***/
+    /************************/
+    // option value at the start of the path
+    BSMEuropeanOption option = BSMEuropeanOption(type_, stock, strike_, 0.0, r_,
+        maturity_, sigma_);
+    // sell the option, cash in its premium
+    double money_account = option.value();
+    // compute delta
+    double delta = option.delta();
+    // delta-hedge the option
+    double stockAmount = delta;
+    money_account -= stockAmount*stock;
+
+
+
+    /**********************************/
+    /*** hedging during option life ***/
+    /**********************************/
+    for(int step = 0; step < n-1; step++){
+        // accruing on the money account
+        money_account *= QL_EXP( r_*dt );
+
+        // stock growth
+        stockLogGrowth += path[step];
+        stock = underlying_*QL_EXP(stockLogGrowth);
+
+        // recalculate option value
+        BSMEuropeanOption option = BSMEuropeanOption(type_, stock, strike_,
+            0.0, r_, maturity_ - dt*(step+1), sigma_);
+
+        // recalculate delta
+        delta = option.delta();
+
+        // re-hedging
+        money_account -= (delta - stockAmount)*stock;
+        stockAmount = delta;
+    }
+
+
+
+    /*************************/
+    /*** option expiration ***/
+    /*************************/
+    // last accrual on my money account
+    money_account *= QL_EXP( r_*dt );
+    // last stock growth
+    stockLogGrowth += path[n-1];
+    stock = underlying_*QL_EXP(stockLogGrowth);
+
+    /*** final Profit&Loss valuation ***/
+    double optionPayoff = computePlainVanilla(type_, stock, strike_, 1.0);
+    double profitLoss = stockAmount*stock + money_account - optionPayoff;
+
+    return profitLoss;
 }
 
-double PathHedgeErrorCalculator::computePlainVanilla(
-									Option::Type type,
-									double price,
-									double strike,
-									double discount) const
+double HedgeErrorPathPricer::computePlainVanilla(Option::Type type,
+                                                 double price,
+                                                 double strike,
+                                                 double discount) const
 {
     double optionPrice;
 
-	switch (type) {
+    switch (type) {
       case Option::Call:
             optionPrice = QL_MAX(price-strike,0.0);
         break;
@@ -160,27 +207,23 @@ double PathHedgeErrorCalculator::computePlainVanilla(
 class ComputeHedgingError
 {
 public:
-	ComputeHedgingError(Time maturity,
-	                     double strike,
-	                     double s0,
-	                     double sigma,
-	                     Rate r)
-	{
-		maturity_ = maturity;
-		strike_ = strike;
-		s0_ = s0;
-		sigma_ = sigma;
-		r_ = r;
+    ComputeHedgingError(Time maturity,
+                        double strike,
+                        double s0,
+                        double sigma,
+                        Rate r)
+    {
+        maturity_ = maturity;
+        strike_ = strike;
+        s0_ = s0;
+        sigma_ = sigma;
+        r_ = r;
+    }
 
-	}
-
-	void doTest(int nTimeSteps = 21, int nSamples = 50000)
-	{
-        BSMEuropeanOption option = BSMEuropeanOption(Option::Type::Call,
-											s0_,
-											strike_,
-											0.0,
-											r_, maturity_, sigma_);
+    void doTest(int nTimeSteps = 21, int nSamples = 50000)
+    {
+        BSMEuropeanOption option = BSMEuropeanOption(Option::Type::Call, s0_,
+            strike_, 0.0, r_, maturity_, sigma_);
 
         cout << "\n" << "Option value:\t " << option.value() << "\n";
 
@@ -191,42 +234,52 @@ public:
                 " computed using Derman & Kamal's formula:\t "
                 << theoretical_error_sd;
 
-		double tau = maturity_ / nTimeSteps;
-		double sigma = sigma_* sqrt(tau);
-		double mean = r_ * tau - 0.5*sigma*sigma;
-		Math::Statistics samples;
+        double tau = maturity_ / nTimeSteps;
 
-		samples = OneFactorMonteCarloOption(
-					Handle<StandardPathGenerator>(
-					    new StandardPathGenerator(nTimeSteps,
-					                              mean,
-					                              sigma*sigma)),
-					Handle<PathPricer>(new PathHedgeErrorCalculator(
-						Option::Type::Call,
-						s0_, strike_, r_, maturity_, sigma_)),
-					samples
-				).sampleAccumulator(nSamples);
+        // Black Scholes assumption
+        double sigma = sigma_* sqrt(tau);
+        double mean = r_ * tau - 0.5*sigma*sigma;
 
-		cout << "\n";
-		cout << "Mean hedging error: \t" << samples.mean() << "\n";
-		cout << "Hedging error standard deviation: \t"
-		    << samples.standardDeviation() << "\n";
-	}
+        Math::Statistics statisticAccumulator;
+
+        Handle<StandardPathGenerator> myPathGenerator(
+            new StandardPathGenerator(nTimeSteps, mean, sigma*sigma));
+
+        // casting HedgeErrorPathPricer to Handle<PathPricer>
+        Handle<PathPricer> myPathPricer =
+            Handle<PathPricer>(new HedgeErrorPathPricer(Option::Type::Call,
+            s0_, strike_, r_, maturity_, sigma_));
+
+        OneFactorMonteCarloOption myMontecarloSimulation(myPathGenerator,
+            myPathPricer, statisticAccumulator);
+
+        // simulating nSamples paths
+        myMontecarloSimulation.addSamples(nSamples);
+
+        Math::Statistics ProfitLossDistribution =
+            myMontecarloSimulation.sampleAccumulator();
+
+        cout << "\n";
+        cout << "Mean hedging error: \t" <<ProfitLossDistribution.mean()<< "\n";
+        cout << "Hedging error standard deviation: \t"
+            << ProfitLossDistribution.standardDeviation() << "\n";
+    }
 
 private:
-	double	s0_;
-	double	strike_;
-	Time	maturity_;
-	double	sigma_;
-	Rate	r_;
+    double s0_;
+    double strike_;
+    Time maturity_;
+    double sigma_;
+    Rate r_;
 
 };
 
 int main(int argc, char* argv[])
 {
-	// 1-year at the money call, volatility 20%, risk-free rate 5%
-	// underlying price 100.0, strike price 100.0
-	ComputeHedgingError test(1./12., 100., 100., 0.2, 0.05);
-	test.doTest();
-	return 0;
+    // 1-month at the money call, volatility 20%, risk-free rate 5%
+    // underlying price 100.0, strike price 100.0
+    ComputeHedgingError test(1./12., 100., 100., 0.2, 0.05);
+    test.doTest();
+    test.doTest(84);
+    return 0;
 }
