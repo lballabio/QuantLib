b65223504a3c78d5dfe35aa06816bf049ed6121e
Ferdinando Ametrano <nando@ametrano.net>
2001-09-03 11:32:18 +0000

    comments removed

--- Include/ql/DayCounters/actual365.hpp
+++ Include/ql/DayCounters/actual365.hpp
@@ -23,39 +23,13 @@
 */
 
 /*! \file actual365.hpp
+    \brief act/365 day counter
 
     \fullpath
     Include/ql/DayCounters/%actual365.hpp
-    \brief act/365 day counter
-
 */
 
 // $Id$
-// $Log$
-// Revision 1.10  2001/08/31 15:23:45  sigmud
-// refining fullpath entries for doxygen documentation
-//
-// Revision 1.9  2001/08/29 15:18:04  nando
-// _DEBUG instead of QL_DEBUG to select which lib is to link under MS VC++
-//
-// Revision 1.8  2001/08/09 14:59:46  sigmud
-// header modification
-//
-// Revision 1.7  2001/08/08 11:07:48  sigmud
-// inserting \fullpath for doxygen
-//
-// Revision 1.6  2001/08/07 11:25:53  sigmud
-// copyright header maintenance
-//
-// Revision 1.5  2001/07/25 15:47:27  sigmud
-// Change from quantlib.sourceforge.net to quantlib.org
-//
-// Revision 1.4  2001/06/22 16:38:15  lballabio
-// Improved documentation
-//
-// Revision 1.3  2001/05/24 15:38:07  nando
-// smoothing #include xx.hpp and cutting old Log messages
-//
 
 #ifndef quantlib_actual365_day_counter_h
 #define quantlib_actual365_day_counter_h
@@ -70,10 +44,10 @@ namespace QuantLib {
         class Actual365 : public DayCounter {
           public:
             std::string name() const { return std::string("act/365"); }
-            int dayCount(const Date& d1, const Date& d2) const { 
+            int dayCount(const Date& d1, const Date& d2) const {
                 return (d2-d1); }
             Time yearFraction(const Date& d1, const Date& d2,
-              const Date& refPeriodStart = Date(), 
+              const Date& refPeriodStart = Date(),
               const Date& refPeriodEnd = Date()) const {
                 return dayCount(d1,d2)/365.0;
             }
