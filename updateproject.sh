62e39cd343bae932901af4e5b3afa2b0494b0a70
Ferdinando Ametrano <nando@ametrano.net>
2001-09-03 08:00:34 +0000

    comments added

--- updateproject.sh
+++ updateproject.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
+
+# Run this to generate all the auto-generated files needed by the GNU
+# configure program
+
+# this is present in gsl
+#libtoolize --automake
+
 aclocal
+# cppunit approach
+#aclocal || \
+#    aclocal -I /usr/share/aclocal
+
+# this is absent in gsl
 autoheader
+
 automake --add-missing
+
 autoconf
 
