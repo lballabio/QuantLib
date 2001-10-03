4456ca0555bcca01176ec5ecafe5a7fa02ffa31a
Ferdinando Ametrano <nando@ametrano.net>
2001-10-03 09:56:13 +0000

    added QL_VERSION and QL_EXVERSION

--- ql/qldefines.hpp
+++ ql/qldefines.hpp
@@ -48,6 +48,12 @@
     @{
 */
 
+//! version number
+#define QL_HEXVERSION 0x000201a0
+
+//! version string
+#define QL_VERSION 0.2.1a0
+
 //! global trace level (may be superseded locally by a greater value)
 #define QL_TRACE_LEVEL 0
 
