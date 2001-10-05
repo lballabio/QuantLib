10359b404e706f75a8d7f2235b0cf75fdbb5766e
Ferdinando Ametrano <nando@ametrano.net>
2001-10-05 10:02:27 +0000

    fix for borland compiler

--- ql/date.cpp
+++ ql/date.cpp
@@ -32,11 +32,12 @@
 // $Id$
 
 #include "ql/dataformatters.hpp"
+#include <iostream>
 
 namespace QuantLib {
 
     namespace { // hide implementation in an anonymous namespace
-        
+
         // valid date interval definition
         const unsigned int MinimumSerialNumber = 367;       // Jan 1st, 1901
         const unsigned int MaximumSerialNumber = 73050;    // Dec 31st, 2099
@@ -56,11 +57,11 @@ namespace QuantLib {
         };
 
         const unsigned int MonthLength[]     = {
-            31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 
+            31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
         };
 
         const unsigned int MonthLeapLength[] = {
-            31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 
+            31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
         };
 
         // yearOffset and yearIsLeap enforce the 1900-2100 limit
@@ -109,7 +110,7 @@ namespace QuantLib {
             // 2090-2099
             69398,69763,70128,70494,70859,71224,71589,71955,72320,72685,
             // 2100
-            73050 
+            73050
         };
 
         const bool YearIsLeap[] = {
@@ -156,7 +157,7 @@ namespace QuantLib {
             // 2090-2099
             false,false, true,false,false,false, true,false,false,false,
             // 2100
-            false 
+            false
         };
 
     }
@@ -168,7 +169,7 @@ namespace QuantLib {
     Date::Date(int serialNumber)
     : serialNumber_(serialNumber) {
         #ifdef QL_DEBUG
-            QL_REQUIRE(serialNumber >= MinimumSerialNumber && 
+            QL_REQUIRE(serialNumber >= MinimumSerialNumber &&
                        serialNumber <= MaximumSerialNumber,
                 "Date " + IntegerFormatter::toString(serialNumber) +
                 "outside allowed range [" +
@@ -392,9 +393,9 @@ namespace QuantLib {
     bool operator>=(const Date& d1, const Date& d2) {
         return (d1.serialNumber() >= d2.serialNumber());
     }
+
     std::ostream& operator<< (std::ostream& stream, const Date& date) {
-        stream << DateFormatter::toString(date, true);
-        return stream;
+        return stream << DateFormatter::toString(date, true);
     }
 
 }
