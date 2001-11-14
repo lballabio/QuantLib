4a3d07466b9c07e07dff8d7d60e7ba029b9acc51
Ferdinando Ametrano <nando@ametrano.net>
2001-11-14 09:32:33 +0000

    added few developers' tools

---
+++ dev_tools/backupcvstree.py
@@ -0,0 +1,20 @@
+
+#! /usr/bin/env python
+#
+# $Id$
+#
+# test the module used to backed up cvs repository for QuantLib
+#
+
+__doc__ = """Backup for QuantLib cvs repository"""
+
+import urllib
+
+if __name__=='__main__':
+        #
+        #
+        url_name="http://cvs.sourceforge.net/cvstarballs/quantlib-cvsroot.tar.gz"
+        #
+        local_file_name="Y:/backed_up/Linux/cvs_bck/quantlib-cvsroot.tar.gz"
+        #
+        urllib.urlretrieve(url_name, local_file_name)
