b34cf04d6ee51fdd70eb451585ab5a7692bed970
Luigi Ballabio <luigi.ballabio@gmail.com>
2001-12-11 16:12:07 +0000

    Nicolas' cubic spline replaced the NR one

--- ql/Math/cubicspline.hpp
+++ ql/Math/cubicspline.hpp
@@ -35,6 +35,7 @@
 #define quantlib_cubic_spline_h
 
 #include <ql/Math/interpolation.hpp>
+#include <ql/FiniteDifferences/tridiagonaloperator.hpp>
 #include <vector>
 
 namespace QuantLib {
@@ -56,9 +57,13 @@ namespace QuantLib {
                 const RandomAccessIterator1& xEnd,
                 const RandomAccessIterator2& yBegin);
             result_type operator()(const argument_type& x) const;
-        virtual ~CubicSpline() {}
+	    virtual ~CubicSpline() {}
           private:
-            std::vector<result_type> d2y_;
+            // P[i](x) = y[i] + 
+            //           a[i]*(x-x[i]) + 
+            //           b[i]*(x-x[i])^2 + 
+            //           c[i]*(x-x[i])^3
+            std::vector<result_type> a_, b_, c_;
         };
 
 
@@ -67,30 +72,56 @@ namespace QuantLib {
         template <class I1, class I2>
         CubicSpline<I1,I2>::CubicSpline(const I1& xBegin, const I1& xEnd,
             const I2& yBegin)
-        : Interpolation<I1,I2>(xBegin,xEnd,yBegin), d2y_(xEnd-xBegin) {
-            int n = xEnd_-xBegin_;
+        : Interpolation<I1,I2>(xBegin,xEnd,yBegin), a_(xEnd-xBegin-1),
+          b_(xEnd-xBegin-1), c_(xEnd-xBegin-1) {
+            size_t n = xEnd_-xBegin_;
             #ifdef QL_DEBUG
                 QL_REQUIRE(n >= 4,
                     "not enough points for cubic spline interpolation");
             #endif
-            // calculate cubic spline coefficients
-            std::vector<result_type> u(n-1);
-            d2y_[0] = u[0] = result_type();
-            I1 xi = xBegin_+1;
-            I2 yi = yBegin_+1;
-            int i;
-            for (i=1; i<n-1; i++,xi++,yi++) {
-                double sig = double(*xi-*(xi-1))/double(*(xi+1)-*(xi-1));
-                result_type p = sig*d2y_[i-1]+2.0;
-                d2y_[i] = (sig-1)/p;
-                u[i] = (*(yi+1)-*yi)/double(*(xi+1)-*xi) -
-                    (*yi-*(yi-1))/double(*xi-*(xi-1));
-                u[i] = (6*u[i]/double(*(xi+1)-*(xi-1))-sig*u[i-1])/p;
-            }
+            FiniteDifferences::TridiagonalOperator L(n);
+            Array tmp(n);
 
-            d2y_[n-1] = result_type();
-            for (i=n-2; i>=0; i--)
-                d2y_[i] = d2y_[i]*d2y_[i+1] + u[i];
+            argument_type dx01  = xBegin_[1] - xBegin_[0],
+                          dx12  = xBegin_[2] - xBegin_[1],
+                          dx02  = xBegin_[2] - xBegin_[0];
+            result_type   dy01  = yBegin_[1] - yBegin_[0],
+                          dy12  = yBegin_[2] - yBegin_[1];
+                          
+            L.setFirstRow(dx01,dx02);
+            tmp[0] = ((dx01+2.0*dx02)*dx12*dy01/dx01 + 
+                       dx01*dx01*dy12/dx12)/dx02;
+            
+            size_t i;
+            for (i=1; i<n-1; i++) {
+                argument_type dxp = xBegin_[i+1] - xBegin_[i],
+                              dxm = xBegin_[i]   - xBegin_[i-1];
+                result_type   dyp = yBegin_[i+1] - yBegin_[i],
+                              dym = yBegin_[i]   - yBegin_[i-1];
+                L.setMidRow(i, dxp, 2.0*(dxp+dxm), dxm);
+                tmp[i] = 3.0*(dxp*dym/dxm + dxm*dyp/dxp);
+            }
+            
+            argument_type dxN32 = xBegin_[n-2] - xBegin_[n-3],
+                          dxN21 = xBegin_[n-1] - xBegin_[n-2],
+                          dxN31 = xBegin_[n-1] - xBegin_[n-3];
+            result_type   dyN32 = yBegin_[n-2] - yBegin_[n-3],
+                          dyN21 = yBegin_[n-1] - yBegin_[n-2];
+            
+            L.setLastRow(dxN31,dxN21);
+            tmp[n-1] = ((dxN21+2.0*dxN31)*dxN32*dyN21/dxN21 + 
+                         dxN21*dxN21*dyN32/dxN32)/dxN31;
+            
+            tmp = L.solveFor(tmp);
+            
+            for (i=0; i<n-1; i++) {
+                argument_type dx  = xBegin_[i+1]-xBegin_[i];
+                result_type   dy  = yBegin_[i+1]-yBegin_[i];
+
+                a_[i] = tmp[i];
+                c_[i] = (tmp[i] + tmp[i+1] - 2.0*dy/dx)/(dx*dx);
+                b_[i] = (dy/dx - tmp[i])/dx - c_[i]*dx;
+            }
         }
 
         template <class I1, class I2>
@@ -104,14 +135,10 @@ namespace QuantLib {
                     i = xEnd_-2;
                 else
                     i = Location(xBegin_,xEnd_,x);
-                I2 j = yBegin_+(i-xBegin_);
-                std::vector<double>::const_iterator k =
-                    d2y_.begin()+(i-xBegin_);
-                double h = double(*(i+1)-*i);
-                double a = double(*(i+1)-x)/h;
-                double b = 1.0-a;
-                return a*(*j) + b*(*(j+1)) +
-                    ((a*a*a-a)*(*k)+(b*b*b-b)*(*(k+1)))*(h*h)/6.0;
+
+                size_t j = i-xBegin_;
+                argument_type dx = x-xBegin_[j];
+                return yBegin_[j] + dx*(a_[j] + dx*(b_[j] + dx*c_[j]));
         }
 
     }
