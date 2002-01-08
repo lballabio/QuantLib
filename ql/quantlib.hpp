6bea236e22206536c81af914534734e53cd59e21
Sadruddin Rejeb <sadrejeb@users.sf.net>
2002-01-08 14:28:19 +0000

    Added missing file

--- ql/quantlib.hpp
+++ ql/quantlib.hpp
@@ -108,6 +108,7 @@
 #include <ql/Instruments/simpleswap.hpp>
 #include <ql/Instruments/stock.hpp>
 #include <ql/Instruments/swap.hpp>
+#include <ql/Instruments/swaption.hpp>
 
 #include <ql/InterestRateModelling/calibrationhelper.hpp>
 #include <ql/InterestRateModelling/grid.hpp>
