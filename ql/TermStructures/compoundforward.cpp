6c62398a2f77349ccc17883573935ed2d9daffb2
André Louw <andrelouw@users.sf.net>
2002-04-22 11:10:47 +0000

    Added standard QuantLib licensing comment.

--- ql/TermStructures/compoundforward.cpp
+++ ql/TermStructures/compoundforward.cpp
@@ -1,3 +1,29 @@
+
+
+/*
+ Copyright (C) 2000, 2001, 2002 RiskMap srl
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it under the
+ terms of the QuantLib license.  You should have received a copy of the
+ license along with this program; if not, please email ferdinando@ametrano.net
+ The license is also available online at http://quantlib.org/html/license.html
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+/*! \file compoundforward.cpp
+    \brief compounded forward term structure
+
+    \fullpath
+    ql/TermStructures/%compoundforward.cpp
+*/
+
+// $Id$
+
 #include <ql/TermStructures/compoundforward.hpp>
 #include <ql/dataformatters.hpp>
 
