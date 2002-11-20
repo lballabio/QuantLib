22d709b1793ed5a9f8ad9ec52a428d9b4964d1b8
Luigi Ballabio <luigi.ballabio@gmail.com>
2002-11-20 13:57:04 +0000

    Added default day counter

--- ql/TermStructures/discountcurve.hpp
+++ ql/TermStructures/discountcurve.hpp
@@ -14,11 +14,12 @@
   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
   FOR A PARTICULAR PURPOSE.  See the license for more details.
 */
-/*! \file discountcurve.hpp
-  \brief pre-bootstrapped discount factor structure
 
-  \fullpath
-  ql/TermStructures/%discountcurve.hpp
+/*! \file discountcurve.hpp
+    \brief pre-bootstrapped discount factor structure
+
+    \fullpath
+    ql/TermStructures/%discountcurve.hpp
 */
 
 // $Id$
@@ -27,78 +28,79 @@
 #define quantlib_discount_curve_h
 
 #include <ql/termstructure.hpp>
-#include <ql/dataformatters.hpp>
+#include <ql/DayCounters/actual365.hpp>
 #include <ql/Math/loglinearinterpolation.hpp>
 
-namespace QuantLib
-{
+namespace QuantLib {
 
-   namespace TermStructures
-   {
+    namespace TermStructures {
 
         //! Term structure based on loglinear interpolation of discount factors
-        /*! This term structure is based on loglinear interpolation of a grid
-            of discount factors.
-
-            Loglinear interpolation guarantees piecewise constant forward
+        /*! Loglinear interpolation guarantees piecewise constant forward
             rates.
 
             Rates are assumed to be annual continuos compounding.
         */
-        class DiscountCurve:public DiscountStructure
-        {
-         public:
-           // constructor
-           DiscountCurve(const Date& todaysDate,
-                         const std::vector < Date > &dates,
-                         const std::vector < DiscountFactor > &discounts,
-                         const DayCounter & dayCounter);
+        class DiscountCurve : public DiscountStructure {
+          public:
+            // constructor
+            DiscountCurve(const Date& todaysDate,
+                          const std::vector<Date>& dates,
+                          const std::vector<DiscountFactor>& discounts,
+                          const DayCounter& dayCounter = 
+                              DayCounters::Actual365());
 
-           DayCounter dayCounter() const;
-           Date todaysDate() const {return todaysDate_; }
-           Date settlementDate() const;
-           const std::vector<Date>& dates() const;
-           Date maxDate() const;
-           const std::vector<Time>& times() const;
-           Time maxTime() const;
-         protected:
-           DiscountFactor discountImpl(Time t, bool extrapolate = false) const;
-         private:
-           Date todaysDate_;
-           const std::vector < Date > dates_;
-           const std::vector < DiscountFactor > discounts_;
-           const DayCounter dayCounter_;
-           std::vector < Time > times_;
-           typedef Math::LogLinearInterpolation <
-              std::vector < Time >::const_iterator,
-              std::vector < double >::const_iterator > DfInterpolation;
-           Handle < DfInterpolation > interpolation_;
+            DayCounter dayCounter() const;
+            Date todaysDate() const {return todaysDate_; }
+            Date settlementDate() const;
+            const std::vector<Date>& dates() const;
+            Date maxDate() const;
+            const std::vector<Time>& times() const;
+            Time maxTime() const;
+          protected:
+            DiscountFactor discountImpl(Time t, 
+                                        bool extrapolate = false) const;
+          private:
+            Date todaysDate_;
+            std::vector<Date> dates_;
+            std::vector<DiscountFactor> discounts_;
+            DayCounter dayCounter_;
+            std::vector<Time> times_;
+            typedef Math::LogLinearInterpolation <
+                std::vector<Time>::const_iterator,
+                std::vector<double>::const_iterator > DfInterpolation;
+            Handle<DfInterpolation> interpolation_;
         };
-
+       
         // inline definitions
 
-        inline Date DiscountCurve::settlementDate() const
-        {
-           return dates_[0];
+        inline Date DiscountCurve::settlementDate() const {
+            return dates_[0];
         }
-        inline DayCounter DiscountCurve::dayCounter() const
-        {
-           return dayCounter_;
+
+        inline DayCounter DiscountCurve::dayCounter() const {
+            return dayCounter_;
         }
+
         inline const std::vector<Date>& DiscountCurve::dates() const {
             return dates_;
         }
-        inline Date DiscountCurve::maxDate() const
-        {
-           return dates_.back();
+
+        inline Date DiscountCurve::maxDate() const {
+            return dates_.back();
         }
+
         inline const std::vector<Time>& DiscountCurve::times() const {
             return times_;
         }
-        inline Time DiscountCurve::maxTime() const
-        {
-           return times_.back();
+
+        inline Time DiscountCurve::maxTime() const {
+            return times_.back();
         }
-     }
+
+    }
+
 }
+
+
 #endif
