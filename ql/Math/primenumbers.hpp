03dfa58c80769ed34783a7aca546a5ece4ed5268
Ferdinando Ametrano <nando@ametrano.net>
2003-03-17 15:35:03 +0000

    removed redundant method

--- ql/Math/primenumbers.hpp
+++ ql/Math/primenumbers.hpp
@@ -50,21 +50,33 @@ namespace QuantLib {
         */
         class PrimeNumbers {
           public:
-            PrimeNumbers() : primeNumbers_(2) {
-                primeNumbers_[0]=2;
-                primeNumbers_[1]=3;
+            PrimeNumbers() : primeNumbers_(15) {
+                // the first 2 prime numbeers are necessary
+                // for the algorithm to work
+                primeNumbers_[ 0] =  2;
+                primeNumbers_[ 1] =  3;
+                // precomputed .....
+                primeNumbers_[ 2] =  5;
+                primeNumbers_[ 3] =  7;
+                primeNumbers_[ 4] = 11;
+                primeNumbers_[ 5] = 13;
+                primeNumbers_[ 6] = 17;
+                primeNumbers_[ 7] = 19;
+                primeNumbers_[ 8] = 23;
+                primeNumbers_[ 9] = 29;
+                primeNumbers_[10] = 31;
+                primeNumbers_[11] = 37;
+                primeNumbers_[12] = 41;
+                primeNumbers_[13] = 43;
+                primeNumbers_[14] = 47;
+
             }
             //! Get and store one after another.
             Size nextPrimeNumber();
-
-            Size indexedPrimeNumber(Size absoluteIndex) {
+            Size operator[](Size absoluteIndex) {
                 while (primeNumbers_.size()<=absoluteIndex)
                     nextPrimeNumber();
                 return primeNumbers_[absoluteIndex];
-            }
-
-            Size operator[](Size absoluteIndex) {
-                return indexedPrimeNumber(absoluteIndex);
             }
           private:
             std::vector<Size> primeNumbers_;
