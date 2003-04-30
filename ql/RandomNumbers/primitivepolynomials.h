90026b34cb262b0f1826f69edf6a4c1aa31e51cd
Ferdinando Ametrano <nando@ametrano.net>
2003-04-30 14:32:04 +0000

    drop in replacement files

--- ql/RandomNumbers/primitivepolynomials.h
+++ ql/RandomNumbers/primitivepolynomials.h
@@ -1,46 +1,57 @@
 
-// $Id$
-
-// taken from
+// this file is a slightly edited version of
 // PrimitivePolynomialsModuloTwoUpToDegree27.h
-//
 // © 2002 "Monte Carlo Methods in Finance"
+// as provided ready for compilation in the directory
+// "PrimitivePolynomialsModuloTwo" on the CD accompanying the book
+// "Monte Carlo Methods in Finance" by Peter Jäckel.
 //
-//
+// the original copyright is:
 // ===========================================================================
 // Copyright (C) 2002 "Monte Carlo Methods in Finance". All rights reserved.
 //
 // Permission to use, copy, modify, and distribute this software is freely
 // granted, provided that this notice is preserved.
 // ===========================================================================
-//
-//
+
+#ifndef primitivepolynomials_h
+#define primitivepolynomials_h
+
+// $Id$
+
 // This file is provided for the use with Sobol' sequences of higher
-// dimensions. For the construction of Sobol' numbers, I recommend the
-// algorithms published in "Numerical Recipes in C", W. H. Press,
-// S. A. Teukolsky, W. t. Vetterling, B. P. Flannery, second edition,
-// section 7.7, page 309, ISBN 0-521-43108-5.
-//
-// The dimensionality of the Sobol' sequence can be extended to virtually any
-// size you ever might need by the aid of the table of primitive polynomials
-// modulo two as provided ready for compilation in the directory
-// "PrimitivePolynomialsModuloTwo" on the CD accompanying the book
-// "Monte Carlo Methods in Finance" by Peter Jäckel. You should initialise
-// them as described in the section "The initialisation of Sobol' numbers".
-// Note that you should always use Sobol' sequences in conjunction with the
-// Brownian Bridge.
-//
+// dimensions. The dimensionality of the Sobol' sequence can be extended to
+// virtually any size you ever might need by the aid of the table of
+// primitive polynomials modulo two.
+// It is up to you to define a macro PPMT_MAX_DIM to a positive integer
+// less than or equal to 8129334. If you don't define it, it will be set
+// below to N_PRIMITIVES_UP_TO_DEGREE_18 which is 21200. That's how many
+// primitive polynomials are provided by the standard primitivepolynomial.c
+// distributed with QuantLib and that will be compiled into a static array.
+// Should you need more, another version of primitivepolynomial.c is available
+// for download from quantlib.org which provide polynomials up to degree 27
+// for a grand total of 8129334 dimensions.
+// Since 8129334 longs compile into an object file of at least 32517336 byte
+// size (in fact, gcc -c -O0 PrimitivePolynomialsModuloTwoUpToDegree27.c
+// produced a file PrimitivePolynomialsModuloTwoUpToDegree27.o with 32519920
+// bytes), it is recommended to only compile as many as you may ever need.
+// Worse even than the output file size is the virtual memory requirement
+// for the compilation. For Visual C++ 6 you will need to use the /Zm compiler
+// option to set the compiler's memory allocation limit (/Zm1500 should work)
+// So really only take the maximum of what you think you might ever need.
+// After all, you can always recompile with more, should you need it.
+
 
 //	PPMT : Primitive Polynomials Modulo Two
 //
 //
 // The encoding is as follows:
 //
-//  The coefficients of each primitive polynomial are the bits of the given
-//  integer. The leading and trailing coefficients, which are 1 for all of the
-//  polynomials, have been omitted.
+// The coefficients of each primitive polynomial are the bits of the given
+// integer. The leading and trailing coefficients, which are 1 for all of the
+// polynomials, have been omitted.
 //
-//  Example: The polynomial
+// Example: The polynomial
 //
 //      4    2
 //     x  + x  + 1
@@ -60,43 +71,24 @@
 //
 // by right-shifting and losing the rightmost bit. Similarly, we have
 //
-//   5    4    3
+//   5    4    2
 //  x  + x  + x  + x + 1
 //
 // encoded as  13  [ (1)1101(1) ]  in the array for degree 5.
+
+
+// Example: replace primitivepolynomials.c provided by QuantLib standard
+// distribution with the 8129334 polinomials version and
+// comment out the line below if you want absolutely all of the
+// provided primitive polynomials modulo two.
 //
-//
-//
-// It is up to you to define a macro PPMT_MAX_DIM to a positive integer
-// less than or equal to 21200. If you don't define it, it will be set
-// below to N_PRIMITIVES_UP_TO_DEGREE_18 which is 21200. That's how many
-// primitive polynomials will be compiled into a static array. Should you
-// need more, files are available for download from quantlib.org which can 
-// be substituted for these versions of primitivepolynomial.h and 
-// primitivepolynomial.c and which provide polynomials up to degree 27 for
-// a grand total of 8129334 dimensions. Since 8129334 longs compile into 
-// an object file of at least 32517336 byte size (in fact,
-// gcc -c -O0 PrimitivePolynomialsModuloTwoUpToDegree27.c produced a the file
-// PrimitivePolynomialsModuloTwoUpToDegree27.o with 32519920 bytes), it
-// is recommended to only compile as many as you may ever need. Worse even
-// than the  output file size is the virtual memory requirement for the
-// compilation, so really only take the maximum of what you think you might
-// ever need.
-// After all, you can always recompile with more, should you need it.
-//
+// #define PPMT_MAX_DIM 8129334
+
 // Note that PPMT_MAX_DIM will be redefined to be the nearest equal or larger
 // number of polynomials up to one of the predefined macros
 // N_PRIMITIVES_UP_TO_DEGREE_XX
 // below.
-//
-//
-// Example: comment out the line below if you want all of the
-// provided primitive polynomials modulo two.
-//
-// #define PPMT_MAX_DIM 21200
 
-#ifndef quantlib_primitive_polynomials_h
-#define quantlib_primitive_polynomials_h
 
 #define N_PRIMITIVES_UP_TO_DEGREE_01         1
 #define N_PRIMITIVES_UP_TO_DEGREE_02         2
@@ -116,8 +108,17 @@
 #define N_PRIMITIVES_UP_TO_DEGREE_16      5714
 #define N_PRIMITIVES_UP_TO_DEGREE_17     13424
 #define N_PRIMITIVES_UP_TO_DEGREE_18     21200
+#define N_PRIMITIVES_UP_TO_DEGREE_19     48794
+#define N_PRIMITIVES_UP_TO_DEGREE_20     72794
+#define N_PRIMITIVES_UP_TO_DEGREE_21    157466
+#define N_PRIMITIVES_UP_TO_DEGREE_22    277498
+#define N_PRIMITIVES_UP_TO_DEGREE_23    634458
+#define N_PRIMITIVES_UP_TO_DEGREE_24    910938
+#define N_PRIMITIVES_UP_TO_DEGREE_25   2206938
+#define N_PRIMITIVES_UP_TO_DEGREE_26   3926838
+#define N_PRIMITIVES_UP_TO_DEGREE_27   8129334
 
-#define N_PRIMITIVES N_PRIMITIVES_UP_TO_DEGREE_18
+#define N_PRIMITIVES N_PRIMITIVES_UP_TO_DEGREE_27
 
 #ifndef  PPMT_MAX_DIM
 # define PPMT_MAX_DIM N_PRIMITIVES_UP_TO_DEGREE_18
@@ -193,24 +194,70 @@
 # undef  PPMT_MAX_DIM
 # define PPMT_MAX_DIM N_PRIMITIVES_UP_TO_DEGREE_17
 # define N_MAX_DEGREE 17
-#else
+#elif    PPMT_MAX_DIM <= N_PRIMITIVES_UP_TO_DEGREE_18
 # undef  PPMT_MAX_DIM
 # define PPMT_MAX_DIM N_PRIMITIVES_UP_TO_DEGREE_18
 # define N_MAX_DEGREE 18
+#elif    PPMT_MAX_DIM <= N_PRIMITIVES_UP_TO_DEGREE_19
+# undef  PPMT_MAX_DIM
+# define PPMT_MAX_DIM N_PRIMITIVES_UP_TO_DEGREE_19
+# define N_MAX_DEGREE 19
+#elif    PPMT_MAX_DIM <= N_PRIMITIVES_UP_TO_DEGREE_20
+# undef  PPMT_MAX_DIM
+# define PPMT_MAX_DIM N_PRIMITIVES_UP_TO_DEGREE_20
+# define N_MAX_DEGREE 20
+#elif    PPMT_MAX_DIM <= N_PRIMITIVES_UP_TO_DEGREE_21
+# undef  PPMT_MAX_DIM
+# define PPMT_MAX_DIM N_PRIMITIVES_UP_TO_DEGREE_21
+# define N_MAX_DEGREE 21
+#elif    PPMT_MAX_DIM <= N_PRIMITIVES_UP_TO_DEGREE_22
+# undef  PPMT_MAX_DIM
+# define PPMT_MAX_DIM N_PRIMITIVES_UP_TO_DEGREE_22
+# define N_MAX_DEGREE 22
+#elif    PPMT_MAX_DIM <= N_PRIMITIVES_UP_TO_DEGREE_23
+# undef  PPMT_MAX_DIM
+# define PPMT_MAX_DIM N_PRIMITIVES_UP_TO_DEGREE_23
+# define N_MAX_DEGREE 23
+#elif    PPMT_MAX_DIM <= N_PRIMITIVES_UP_TO_DEGREE_24
+# undef  PPMT_MAX_DIM
+# define PPMT_MAX_DIM N_PRIMITIVES_UP_TO_DEGREE_24
+# define N_MAX_DEGREE 24
+#elif    PPMT_MAX_DIM <= N_PRIMITIVES_UP_TO_DEGREE_25
+# undef  PPMT_MAX_DIM
+# define PPMT_MAX_DIM N_PRIMITIVES_UP_TO_DEGREE_25
+# define N_MAX_DEGREE 25
+#elif    PPMT_MAX_DIM <= N_PRIMITIVES_UP_TO_DEGREE_26
+# undef  PPMT_MAX_DIM
+# define PPMT_MAX_DIM N_PRIMITIVES_UP_TO_DEGREE_26
+# define N_MAX_DEGREE 26
+#else
+# undef  PPMT_MAX_DIM
+# define PPMT_MAX_DIM N_PRIMITIVES_UP_TO_DEGREE_27
+# define N_MAX_DEGREE 27
 #endif
 
+// Microsoft Visual C++ 6.0
+#if defined(_MSC_VER)
+    // disable useless warning C4049
+    // compiler limit : terminating line number emission
+    // No line number support is available for file with more
+    // than 64K source lines.
+    #pragma warning(disable: 4049)
+#endif
 
-// You can access the following array as in PrimitivePolynomials[i][j]
-// with i and j counting from 0 in C convention. PrimitivePolynomials[i][j]
-// will get you the j-th (counting from zero) primitive polynomial of degree
-// i+1. Each one-dimensional array of primitive polynomials of a given
-// degree is terminated with an entry of -1. Accessing beyond this entry
-// will result in a memory violation and must be avoided.
 
 #ifdef __cplusplus
-extern "C"
-#endif
-const long *const PrimitivePolynomials[N_MAX_DEGREE];
 
+extern "C"
+
+#endif
+
+/*! You can access the following array as in PrimitivePolynomials[i][j]
+    with i and j counting from 0 in C convention. PrimitivePolynomials[i][j]
+    will get you the j-th (counting from zero) primitive polynomial of degree
+    i+1. Each one-dimensional array of primitive polynomials of a given
+    degree is terminated with an entry of -1. Accessing beyond this entry
+    will result in a memory violation and must be avoided.  */
+const long *const PrimitivePolynomials[N_MAX_DEGREE];
 
 #endif
