60372d3b44d772ab3969ed010867e4651563053a
Ferdinando Ametrano <nando@ametrano.net>
2003-05-05 07:20:31 +0000

    more discrepancy data (final)

--- test-suite/lowdiscrepancysequences.cpp
+++ test-suite/lowdiscrepancysequences.cpp
@@ -321,41 +321,41 @@ void LDSTest::testDiscrepancy() {
     const double discrSoboDim3[]   = {1.21e-003, 6.37e-004, 3.40e-004, 1.75e-004, 9.21e-005, 4.79e-005, 2.56e-005};
     const double discrUnSoDim3[]   = {1.21e-003, 6.37e-004, 3.40e-004, 1.75e-004, 9.21e-005, 4.79e-005, 2.56e-005};
 
-    const double discrRandDim5[] = {5.15e-003, 3.64e-003, 2.57e-003, 1.82e-003, 1.29e-003, 9.10e-004, 6.43e-004};
-    const double discrMersDim5[] = {4.28e-003, 3.48e-003, 2.48e-003, 1.98e-003, 1.57e-003, 1.39e-003, 6.33e-004};
-    const double discrHaltDim5[] = {1.93e-003, 1.23e-003, 6.89e-004, 4.22e-004, 2.13e-004, 1.25e-004, 7.17e-005};
-    const double discrSoboDim5[] = {1.59e-003, 9.55e-004, 5.33e-004, 3.22e-004, 1.63e-004, 9.41e-005, 5.19e-005};
-    const double discrUnSoDim5[] = {1.85e-003, 9.39e-004, 5.19e-004, 2.99e-004, 1.75e-004, 9.51e-005, 5.55e-005};
+    const double discrRandDim5[]   = {5.15e-003, 3.64e-003, 2.57e-003, 1.82e-003, 1.29e-003, 9.10e-004, 6.43e-004};
+    const double discrMersDim5[]   = {4.28e-003, 3.48e-003, 2.48e-003, 1.98e-003, 1.57e-003, 1.39e-003, 6.33e-004};
+    const double discrHaltDim5[]   = {1.93e-003, 1.23e-003, 6.89e-004, 4.22e-004, 2.13e-004, 1.25e-004, 7.17e-005};
+    const double discrSoboDim5[]   = {1.59e-003, 9.55e-004, 5.33e-004, 3.22e-004, 1.63e-004, 9.41e-005, 5.19e-005};
+    const double discrUnSoDim5[]   = {1.85e-003, 9.39e-004, 5.19e-004, 2.99e-004, 1.75e-004, 9.51e-005, 5.55e-005};
 
-    const double discrRandDim10[] = {9.69e-004, 6.85e-004, 4.84e-004, 3.42e-004, 2.42e-004, 1.71e-004, 1.21e-004};
-    const double discrMersDim10[] = {8.83e-004, 6.56e-004, 4.87e-004, 3.37e-004, 3.06e-004, 1.73e-004, 1.43e-004};
-    const double discrHaltDim10[] = {1.23e-003, 6.89e-004, 4.03e-004, 2.83e-004, 1.61e-004, 1.08e-004, 6.69e-005};
-    const double discrSoboDim10[] = {7.08e-004, 5.31e-004, 3.60e-004, 2.18e-004, 1.57e-004, 1.12e-004, 6.39e-005};
-    const double discrUnSoDim10[] = {7.67e-004, 4.92e-004, 3.47e-004, 2.34e-004, 1.39e-004, 9.47e-005, 5.72e-005};
+    const double discrRandDim10[]  = {9.69e-004, 6.85e-004, 4.84e-004, 3.42e-004, 2.42e-004, 1.71e-004, 1.21e-004};
+    const double discrMersDim10[]  = {8.83e-004, 6.56e-004, 4.87e-004, 3.37e-004, 3.06e-004, 1.73e-004, 1.43e-004};
+    const double discrHaltDim10[]  = {1.23e-003, 6.89e-004, 4.03e-004, 2.83e-004, 1.61e-004, 1.08e-004, 6.69e-005};
+    const double discrSoboDim10[]  = {7.08e-004, 5.31e-004, 3.60e-004, 2.18e-004, 1.57e-004, 1.12e-004, 6.39e-005};
+    const double discrUnSoDim10[]  = {7.67e-004, 4.92e-004, 3.47e-004, 2.34e-004, 1.39e-004, 9.47e-005, 5.72e-005};
 
-    const double discrRandDim15[] = {1.73e-004, 1.22e-004, 8.62e-005, 6.10e-005, 4.31e-005, 3.05e-005, 2.16e-005};
-    const double discrMersDim15[] = {1.63e-004, 1.12e-004, 8.36e-005, 6.09e-005, 4.34e-005, 2.95e-005, 2.10e-005};
-    const double discrHaltDim15[] = {5.75e-004, 3.12e-004, 1.70e-004, 9.89e-005, 5.33e-005, 3.45e-005, 2.11e-005};
-    const double discrSoboDim15[] = {1.59e-004, 1.23e-004, 7.73e-005, 5.51e-005, 3.91e-005, 2.73e-005, 1.96e-005};
-    const double discrUnSoDim15[] = {2.24e-004, 1.39e-004, 9.86e-005, 6.02e-005, 4.39e-005, 3.06e-005, 2.32e-005};
+    const double discrRandDim15[]  = {1.73e-004, 1.22e-004, 8.62e-005, 6.10e-005, 4.31e-005, 3.05e-005, 2.16e-005};
+    const double discrMersDim15[]  = {1.63e-004, 1.12e-004, 8.36e-005, 6.09e-005, 4.34e-005, 2.95e-005, 2.10e-005};
+    const double discrHaltDim15[]  = {5.75e-004, 3.12e-004, 1.70e-004, 9.89e-005, 5.33e-005, 3.45e-005, 2.11e-005};
+    const double discrSoboDim15[]  = {1.59e-004, 1.23e-004, 7.73e-005, 5.51e-005, 3.91e-005, 2.73e-005, 1.96e-005};
+    const double discrUnSoDim15[]  = {2.24e-004, 1.39e-004, 9.86e-005, 6.02e-005, 4.39e-005, 3.06e-005, 2.32e-005};
 
-    const double discrRandDim30[] = {9.54e-007, 6.75e-007, 4.77e-007, 3.37e-007, 2.38e-007, 1.69e-007, 1.19e-007};
-    const double discrMersDim30[] = {4.38e-007, 3.25e-007, 4.47e-007, 2.85e-007, 2.03e-007, 1.50e-007, 1.17e-007};
-    const double discrHaltDim30[] = {4.45e-004, 2.23e-004, 1.11e-004, 5.56e-005, 2.78e-005, 1.39e-005, 6.95e-006};
-    const double discrSoboDim30[] = {6.43e-007, 5.28e-007, 3.88e-007, 2.49e-007, 2.09e-007, 1.55e-007, 1.07e-007};
-    const double discrUnSoDim30[] = {4.35e-005, 2.17e-005, 1.09e-005, 5.43e-006, 2.73e-006, 1.37e-006, 6.90e-007};
+    const double discrRandDim30[]  = {9.54e-007, 6.75e-007, 4.77e-007, 3.37e-007, 2.38e-007, 1.69e-007, 1.19e-007};
+    const double discrMersDim30[]  = {4.38e-007, 3.25e-007, 4.47e-007, 2.85e-007, 2.03e-007, 1.50e-007, 1.17e-007};
+    const double discrHaltDim30[]  = {4.45e-004, 2.23e-004, 1.11e-004, 5.56e-005, 2.78e-005, 1.39e-005, 6.95e-006};
+    const double discrSoboDim30[]  = {6.43e-007, 5.28e-007, 3.88e-007, 2.49e-007, 2.09e-007, 1.55e-007, 1.07e-007};
+    const double discrUnSoDim30[]  = {4.35e-005, 2.17e-005, 1.09e-005, 5.43e-006, 2.73e-006, 1.37e-006, 6.90e-007};
 
-    const double discrRandDim50[] = {9.32e-010, 6.59e-010, 4.66e-010, 3.29e-010, 2.33e-010, 1.65e-010, 1.16e-010};
-    const double discrMersDim50[] = {3.27e-010, 2.42e-010, 1.47e-010, 1.98e-010, 2.31e-010, 1.30e-010, 8.09e-011};
-    const double discrHaltDim50[] = {4.04e-004, 2.02e-004, 1.01e-004, 5.05e-005, 2.52e-005, 1.26e-005, 6.31e-006};
-    const double discrSoboDim50[] = {2.98e-010, 2.91e-010, 2.62e-010, 1.53e-010, 1.48e-010, 1.15e-010, 8.41e-011};
-    const double discrUnSoDim50[] = {1.63e-005, 8.14e-006, 4.07e-006, 2.04e-006, 1.02e-006, 5.09e-007, 2.54e-007};
+    const double discrRandDim50[]  = {9.32e-010, 6.59e-010, 4.66e-010, 3.29e-010, 2.33e-010, 1.65e-010, 1.16e-010};
+    const double discrMersDim50[]  = {3.27e-010, 2.42e-010, 1.47e-010, 1.98e-010, 2.31e-010, 1.30e-010, 8.09e-011};
+    const double discrHaltDim50[]  = {4.04e-004, 2.02e-004, 1.01e-004, 5.05e-005, 2.52e-005, 1.26e-005, 6.31e-006};
+    const double discrSoboDim50[]  = {2.98e-010, 2.91e-010, 2.62e-010, 1.53e-010, 1.48e-010, 1.15e-010, 8.41e-011};
+    const double discrUnSoDim50[]  = {1.63e-005, 8.14e-006, 4.07e-006, 2.04e-006, 1.02e-006, 5.09e-007, 2.54e-007};
 
     const double discrRandDim100[] = {2.78e-017, 1.96e-017, 1.39e-017, 9.81e-018, 6.94e-018, 4.91e-018, 3.47e-018};
     const double discrMersDim100[] = {5.30e-019, 7.29e-019, 3.71e-019, 3.33e-019, 1.33e-017, 6.70e-018, 3.36e-018};
     const double discrHaltDim100[] = {3.63e-004, 1.81e-004, 9.07e-005, 4.53e-005, 2.27e-005, 1.13e-005, 5.66e-006};
-    const double discrSoboDim100[] = {1.26e-018};
-    const double discrUnSoDim100[] = {4.97e-006};
+    const double discrSoboDim100[] = {1.26e-018, 1.55e-018, 8.46e-019, 4.43e-019, 4.04e-019, 2.44e-019, 4.86e-019};
+    const double discrUnSoDim100[] = {4.97e-006, 2.48e-006, 1.24e-006, 6.20e-007, 3.10e-007, 1.55e-007, 7.76e-008};
 
     const double * const discrRand[8] = { discrRandDim2,  discrRandDim3,
         discrRandDim5,  discrRandDim10, discrRandDim15, discrRandDim30,
