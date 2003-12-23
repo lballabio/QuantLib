f3d5f7a1235aea1ba58a2a64bbfc8ddb19c9ddcb
Ferdinando Ametrano <nando@ametrano.net>
2003-12-23 00:37:13 +0000

    more test added

--- test-suite/europeanoption.cpp
+++ test-suite/europeanoption.cpp
@@ -1,5 +1,6 @@
 
 /*
+ Copyright (C) 2003 Ferdinando Ametrano
  Copyright (C) 2003 RiskMap srl
 
  This file is part of QuantLib, a free-software/open-source library
@@ -20,6 +21,7 @@
 #include <ql/PricingEngines/Vanilla/mceuropeanengine.hpp>
 #include <ql/TermStructures/flatforward.hpp>
 #include <ql/Volatilities/blackconstantvol.hpp>
+#include <ql/DayCounters/Actual360.hpp>
 #include <cppunit/TestSuite.h>
 #include <cppunit/TestCaller.h>
 #include <map>
@@ -126,23 +128,25 @@ namespace {
                               engine));
     }
 
-    Handle<TermStructure> makeFlatCurve(const Handle<Quote>& forward) {
+    Handle<TermStructure> makeFlatCurve(const Handle<Quote>& forward,
+        DayCounter dc = Actual365()) {
         Date today = Date::todaysDate();
         Date reference = today;
         return Handle<TermStructure>(
             new FlatForward(today,reference,
                             RelinkableHandle<Quote>(forward),
-                            Actual365()));
+                            dc));
     }
 
     Handle<BlackVolTermStructure> makeFlatVolatility(
-                                     const Handle<Quote>& volatility) {
+                                     const Handle<Quote>& volatility,
+                                     DayCounter dc = Actual365()) {
         Date today = Date::todaysDate();
         Date reference = today;
         return Handle<BlackVolTermStructure>(
             new BlackConstantVol(reference,
                                  RelinkableHandle<Quote>(volatility),
-                                 Actual365()));
+                                 dc));
     }
 
     std::string engineTypeToString(EngineType type) {
@@ -170,13 +174,138 @@ namespace {
         }
     }
 
+
 }
 
 // tests
 
+
 void EuropeanOptionTest::testValues() {
-// all 3*3 payoffs analytical formula must be checked
+
+    struct VanillaOptionData {
+        Option::Type type;
+        double strike;
+        double s;   //spot
+        double q;   // dividend
+        double r;   // risk-free rate
+        Time t;     // time to maturity
+        double v;   // volatility
+        double result;   // result
+        double tol;   // tolerance
+    };
+
+    /* The data below are from
+       "Option pricing formulas", E.G. Haug, McGraw-Hill 1998
+    */
+    VanillaOptionData values[] = {
+        // pag 2-8
+              // type, strike,   spot,    q,    r,    t,  vol,   value,  tol
+        { Option::Call,  65.00,  60.00, 0.00, 0.08, 0.25, 0.30,  2.1334, 1e-4 },
+        { Option::Put,   95.00, 100.00, 0.05, 0.10, 0.50, 0.20,  2.4648, 1e-4 },
+        { Option::Put,   19.00,  19.00, 0.10, 0.10, 0.75, 0.28,  1.7011, 1e-4 },
+        { Option::Call,  19.00,  19.00, 0.10, 0.10, 0.75, 0.28,  1.7011, 1e-4 },
+        { Option::Call,   1.60,   1.56, 0.08, 0.06, 0.50, 0.12,  0.0291, 1e-4 },
+        { Option::Put,   70.00,  75.00, 0.05, 0.10, 0.50, 0.35,  4.0870, 1e-4 },
+        // pag 24
+        { Option::Call, 100.00,  90.00, 0.10, 0.10, 0.10, 0.15,  0.0205, 1e-4 },
+        { Option::Call, 100.00, 100.00, 0.10, 0.10, 0.10, 0.15,  1.8734, 1e-4 },
+        { Option::Call, 100.00, 110.00, 0.10, 0.10, 0.10, 0.15,  9.9413, 1e-4 },
+        { Option::Call, 100.00,  90.00, 0.10, 0.10, 0.10, 0.25,  0.3150, 1e-4 },
+        { Option::Call, 100.00, 100.00, 0.10, 0.10, 0.10, 0.25,  3.1217, 1e-4 },
+        { Option::Call, 100.00, 110.00, 0.10, 0.10, 0.10, 0.25, 10.3556, 1e-4 },
+        { Option::Call, 100.00,  90.00, 0.10, 0.10, 0.10, 0.35,  0.9474, 1e-4 },
+        { Option::Call, 100.00, 100.00, 0.10, 0.10, 0.10, 0.35,  4.3693, 1e-4 },
+        { Option::Call, 100.00, 110.00, 0.10, 0.10, 0.10, 0.35, 11.1381, 1e-4 },
+        { Option::Call, 100.00,  90.00, 0.10, 0.10, 0.50, 0.15,  0.8069, 1e-4 },
+        { Option::Call, 100.00, 100.00, 0.10, 0.10, 0.50, 0.15,  4.0232, 1e-4 },
+        { Option::Call, 100.00, 110.00, 0.10, 0.10, 0.50, 0.15, 10.5769, 1e-4 },
+        { Option::Call, 100.00,  90.00, 0.10, 0.10, 0.50, 0.25,  2.7026, 1e-4 },
+        { Option::Call, 100.00, 100.00, 0.10, 0.10, 0.50, 0.25,  6.6997, 1e-4 },
+        { Option::Call, 100.00, 110.00, 0.10, 0.10, 0.50, 0.25, 12.7857, 1e-4 },
+        { Option::Call, 100.00,  90.00, 0.10, 0.10, 0.50, 0.35,  4.9329, 1e-4 },
+        { Option::Call, 100.00, 100.00, 0.10, 0.10, 0.50, 0.35,  9.3679, 1e-4 },
+        { Option::Call, 100.00, 110.00, 0.10, 0.10, 0.50, 0.35, 15.3086, 1e-4 },
+        { Option::Put,  100.00,  90.00, 0.10, 0.10, 0.10, 0.15,  9.9210, 1e-4 },
+        { Option::Put,  100.00, 100.00, 0.10, 0.10, 0.10, 0.15,  1.8734, 1e-4 },
+        { Option::Put,  100.00, 110.00, 0.10, 0.10, 0.10, 0.15,  0.0408, 1e-4 },
+        { Option::Put,  100.00,  90.00, 0.10, 0.10, 0.10, 0.25, 10.2155, 1e-4 },
+        { Option::Put,  100.00, 100.00, 0.10, 0.10, 0.10, 0.25,  3.1217, 1e-4 },
+        { Option::Put,  100.00, 110.00, 0.10, 0.10, 0.10, 0.25,  0.4551, 1e-4 },
+        { Option::Put,  100.00,  90.00, 0.10, 0.10, 0.10, 0.35, 10.8479, 1e-4 },
+        { Option::Put,  100.00, 100.00, 0.10, 0.10, 0.10, 0.35,  4.3693, 1e-4 },
+        { Option::Put,  100.00, 110.00, 0.10, 0.10, 0.10, 0.35,  1.2376, 1e-4 },
+        { Option::Put,  100.00,  90.00, 0.10, 0.10, 0.50, 0.15, 10.3192, 1e-4 },
+        { Option::Put,  100.00, 100.00, 0.10, 0.10, 0.50, 0.15,  4.0232, 1e-4 },
+        { Option::Put,  100.00, 110.00, 0.10, 0.10, 0.50, 0.15,  1.0646, 1e-4 },
+        { Option::Put,  100.00,  90.00, 0.10, 0.10, 0.50, 0.25, 12.2149, 1e-4 },
+        { Option::Put,  100.00, 100.00, 0.10, 0.10, 0.50, 0.25,  6.6997, 1e-4 },
+        { Option::Put,  100.00, 110.00, 0.10, 0.10, 0.50, 0.25,  3.2734, 1e-4 },
+        { Option::Put,  100.00,  90.00, 0.10, 0.10, 0.50, 0.35, 14.4452, 1e-4 },
+        { Option::Put,  100.00, 100.00, 0.10, 0.10, 0.50, 0.35,  9.3679, 1e-4 },
+        { Option::Put,  100.00, 110.00, 0.10, 0.10, 0.50, 0.35,  5.7963, 1e-4 }
+
+    };
+
+
+
+    DayCounter dc = Actual360();
+    Handle<SimpleQuote> spot(new SimpleQuote(0.0));
+    Handle<SimpleQuote> qRate(new SimpleQuote(0.0));
+    Handle<TermStructure> divCurve = makeFlatCurve(qRate, dc);
+    Handle<SimpleQuote> rRate(new SimpleQuote(0.0));
+    Handle<TermStructure> rfCurve = makeFlatCurve(rRate, dc);
+    Date today = Date::todaysDate();
+    Handle<SimpleQuote> vol(new SimpleQuote(0.0));
+    Handle<BlackVolTermStructure> volCurve = makeFlatVolatility(vol, dc);
+    Handle<PricingEngine> engine(new AnalyticEuropeanEngine);
+
+    for (Size i=0; i<LENGTH(values); i++) {
+
+        Handle<StrikedTypePayoff> payoff(new
+            PlainVanillaPayoff(values[i].type, values[i].strike));
+        Date exDate = today.plusDays(values[i].t*360);
+        Handle<Exercise> exercise(new EuropeanExercise(exDate));
+
+        spot ->setValue(values[i].s);
+        qRate->setValue(values[i].q);
+        rRate->setValue(values[i].r);
+        vol  ->setValue(values[i].v);
+        VanillaOption option(payoff,
+                             RelinkableHandle<Quote>(spot),
+                             RelinkableHandle<TermStructure>(divCurve),
+                             RelinkableHandle<TermStructure>(rfCurve),
+                             exercise,
+                             RelinkableHandle<BlackVolTermStructure>(volCurve),
+                             engine);
+        double value = option.NPV();
+
+        if (QL_FABS(value-values[i].result) > values[i].tol) {
+          CPPUNIT_FAIL("European "
+              + OptionTypeFormatter::toString(values[i].type) +
+              " option :\n"
+              "    underlying value: "
+              + DoubleFormatter::toString(values[i].s) + "\n"
+              "    strike:           "
+              + DoubleFormatter::toString(values[i].strike) +"\n"
+              "    dividend yield:   "
+              + DoubleFormatter::toString(values[i].q) + "\n"
+              "    risk-free rate:   "
+              + DoubleFormatter::toString(values[i].r) + "\n"
+              "    reference date:   "
+              + DateFormatter::toString(today) + "\n"
+              "    maturity:         "
+              + DateFormatter::toString(exDate) + "\n"
+              "    volatility:       "
+              + DoubleFormatter::toString(values[i].v) + "\n\n"
+              "    tabulated value:  "
+              + DoubleFormatter::toString(values[i].result) + "\n"
+              "    result:  "
+              + DoubleFormatter::toString(value));
+        }
+    }
+
 }
+
 
 void EuropeanOptionTest::testGreeks() {
 
@@ -556,11 +685,10 @@ void EuropeanOptionTest::testMcEngines() {
 CppUnit::Test* EuropeanOptionTest::suite() {
     CppUnit::TestSuite* tests =
         new CppUnit::TestSuite("European option tests");
-/*
+
     tests->addTest(new CppUnit::TestCaller<EuropeanOptionTest>
                    ("Testing European option values",
                     &EuropeanOptionTest::testValues));
-*/
     tests->addTest(new CppUnit::TestCaller<EuropeanOptionTest>
                    ("Testing binomial European engines "
                     "against analytic results",
@@ -575,6 +703,7 @@ CppUnit::Test* EuropeanOptionTest::suite() {
                    ("Testing Monte Carlo European engines "
                     "against analytic results",
                     &EuropeanOptionTest::testMcEngines));
+
     return tests;
 }
 
