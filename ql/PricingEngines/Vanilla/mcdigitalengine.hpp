4b2341c2a8bea7b08b76744cead78d45215d5b20
Ferdinando Ametrano <nando@ametrano.net>
2003-12-28 20:58:41 +0000

    added Monte Carlo digital engine (formerly MC binary barrier engine)

---
+++ ql/PricingEngines/Vanilla/mcdigitalengine.hpp
@@ -0,0 +1,208 @@
+
+/*
+ Copyright (C) 2003 Neil Firth
+ Copyright (C) 2002, 2003 Ferdinando Ametrano
+ Copyright (C) 2002, 2003 Sad Rejeb
+ Copyright (C) 2000, 2001, 2002, 2003 RiskMap srl
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it under the
+ terms of the QuantLib license.  You should have received a copy of the
+ license along with this program; if not, please email quantlib-dev@lists.sf.net
+ The license is also available online at http://quantlib.org/html/license.html
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file mcdigitalengine.hpp
+    \brief digital option Monte Carlo engine
+
+    Based on the Vanilla Engine pattern
+*/
+
+#ifndef quantlib_digital_mc_engine_h
+#define quantlib_digital_mc_engine_h
+
+#include <ql/exercise.hpp>
+#include <ql/handle.hpp>
+#include <ql/termstructure.hpp>
+#include <ql/voltermstructure.hpp>
+#include <ql/MonteCarlo/digitalpathpricer.hpp>
+#include <ql/MonteCarlo/mctraits.hpp>
+#include <ql/PricingEngines/Vanilla/mcvanillaengine.hpp>
+
+namespace QuantLib {
+
+    //! Pricing engine for digital options using Monte Carlo simulation
+    template<class RNG = PseudoRandom, class S = Statistics>
+    class MCDigitalEngine : public MCVanillaEngine<RNG,S> {
+      public:
+        typedef typename MCVanillaEngine<RNG,S>::path_generator_type
+            path_generator_type;
+        typedef typename MCVanillaEngine<RNG,S>::path_pricer_type
+            path_pricer_type;
+        typedef typename MCVanillaEngine<RNG,S>::stats_type
+            stats_type;
+        // the uniform generator to use in path generation and
+        // path correction
+//        typedef typename RNG::ursg_type my_sequence_type;
+
+        // constructor
+        MCDigitalEngine(Size maxTimeStepsPerYear,
+                        bool antitheticVariate = false,
+                        bool controlVariate = false,
+                        Size requiredSamples = Null<int>(),
+                        double requiredTolerance = Null<double>(),
+                        Size maxSamples = Null<int>(),
+                        long seed = 0);
+
+        void calculate() const;
+      protected:
+
+        // McSimulation implementation
+        TimeGrid timeGrid() const;
+//        Handle<path_generator_type> pathGenerator() const;
+        Handle<path_pricer_type> pathPricer() const;
+
+        // data members
+        //my_sequence_type uniformGenerator_;
+//        Size maxTimeStepsPerYear_;
+//        Size requiredSamples_, maxSamples_;
+//        double requiredTolerance_;
+//        long seed_;
+    };
+
+
+    template<class RNG, class S>
+    MCDigitalEngine<RNG,S>::MCDigitalEngine(Size maxTimeStepsPerYear,
+                                          bool antitheticVariate,
+                                          bool controlVariate,
+                                          Size requiredSamples,
+                                          double requiredTolerance,
+                                          Size maxSamples,
+                                          long seed)
+    : MCVanillaEngine<RNG,S>(maxTimeStepsPerYear,
+                             antitheticVariate,
+                             controlVariate,
+                             requiredSamples,
+                             requiredTolerance,
+                             maxSamples,
+                             seed) {}
+
+/*
+    template<class RNG, class S>
+    Handle<QL_TYPENAME MCDigitalEngine<RNG,S>::path_generator_type>
+    MCDigitalEngine<RNG,S>::pathGenerator() const {
+
+        Handle<DiffusionProcess> bs(new
+            BlackScholesProcess(arguments_.riskFreeTS,
+                                arguments_.dividendTS,
+                                arguments_.volTS,
+                                arguments_.underlying));
+
+        TimeGrid grid = timeGrid();
+
+        typename RNG::rsg_type gen =
+            RNG::make_sequence_generator(grid.size()-1, seed_);
+
+        return Handle<path_generator_type>(
+            new path_generator_type(bs, grid, gen));
+
+    }
+*/
+
+    template <class RNG, class S>
+    Handle<QL_TYPENAME MCDigitalEngine<RNG,S>::path_pricer_type>
+    MCDigitalEngine<RNG,S>::pathPricer() const {
+
+        #if defined(HAVE_BOOST)
+        Handle<CashOrNothingPayoff> payoff =
+            boost::dynamic_pointer_cast<CashOrNothingPayoff>(arguments_.payoff);
+        QL_REQUIRE(payoff,
+                   "MCDigitalEngine: wrong payoff given");
+        #else
+        Handle<CashOrNothingPayoff> payoff = arguments_.payoff;
+        #endif
+
+        TimeGrid grid = timeGrid();
+        UniformRandomSequenceGenerator
+            sequenceGen(grid.size()-1, UniformRandomGenerator(76));
+
+        return Handle<MCDigitalEngine<RNG,S>::path_pricer_type>(
+            new DigitalPathPricer(arguments_.payoff,
+                                 arguments_.exercise,
+                                 arguments_.underlying,
+                                 arguments_.riskFreeTS,
+                                 Handle<DiffusionProcess>(new
+                                    BlackScholesProcess(arguments_.riskFreeTS,
+                                                        arguments_.dividendTS,
+                                                        arguments_.volTS,
+                                                        arguments_.underlying)),
+                                 sequenceGen));
+
+    }
+
+
+    template <class RNG, class S>
+    inline
+    TimeGrid MCDigitalEngine<RNG,S>::timeGrid() const {
+        Time t = arguments_.riskFreeTS->dayCounter().yearFraction(
+            arguments_.riskFreeTS->referenceDate(),
+            arguments_.exercise->lastDate());
+        return TimeGrid(t, Size(t * maxTimeStepsPerYear_));
+    }
+
+    template<class RNG, class S>
+    void MCDigitalEngine<RNG,S>::calculate() const {
+
+        QL_REQUIRE(requiredTolerance_ != Null<double>() ||
+                   int(requiredSamples_) != Null<int>(),
+                   "MCDigitalEngine::calculate: "
+                   "neither tolerance nor number of samples set");
+
+        //! Initialize the one-factor Monte Carlo
+        if (controlVariate_) {
+
+            Handle<path_pricer_type> controlPP = controlPathPricer();
+            QL_REQUIRE(!IsNull(controlPP),
+                       "MCDigitalEngine::calculate() : "
+                       "engine does not provide "
+                       "control variation path pricer");
+
+            Handle<PricingEngine> controlPE = controlPricingEngine();
+
+            QL_REQUIRE(!IsNull(controlPE),
+                       "MCDigitalEngine::calculate() : "
+                       "engine does not provide "
+                       "control variation pricing engine");
+        } else {
+            mcModel_ =
+                Handle<MonteCarloModel<SingleAsset<RNG>, S> >(
+                    new MonteCarloModel<SingleAsset<RNG>, S>(
+                        pathGenerator(), pathPricer(), S(),
+                        antitheticVariate_));
+        }
+
+        if (requiredTolerance_ != Null<double>()) {
+            if (int(maxSamples_) != Null<int>())
+                value(requiredTolerance_, maxSamples_);
+            else
+                value(requiredTolerance_);
+        } else {
+            valueWithSamples(requiredSamples_);
+        }
+
+        results_.value = mcModel_->sampleAccumulator().mean();
+        if (RNG::allowsErrorEstimate)
+            results_.errorEstimate =
+                mcModel_->sampleAccumulator().errorEstimate();
+    }
+
+}
+
+
+#endif
