12939039d9bd80414621945a85511f698a0ac400
Ferdinando Ametrano <nando@ametrano.net>
2004-01-27 14:23:40 +0000

    working on basket options

--- test-suite/basketoption.cpp
+++ test-suite/basketoption.cpp
@@ -1,5 +1,6 @@
 
 /*
+ Copyright (C) 2004 Ferdinando Ametrano
  Copyright (C) 2004 Neil Firth
  Copyright (C) 2003 RiskMap srl
 
@@ -17,9 +18,7 @@
 */
 
 #include "basketoption.hpp"
-#include <ql/Calendars/nullcalendar.hpp>
 #include <ql/DayCounters/actual360.hpp>
-#include <ql/DayCounters/simpledaycounter.hpp>
 #include <ql/Instruments/basketoption.hpp>
 #include <ql/PricingEngines/Basket/stulzengine.hpp>
 #include <ql/TermStructures/flatforward.hpp>
@@ -35,18 +34,82 @@ using namespace QuantLib;
 namespace {
 
     Handle<TermStructure> makeFlatCurve(const Handle<Quote>& forward,
-                                        DayCounter dc = SimpleDayCounter()) {
+                                        DayCounter dc) {
         Date today = Date::todaysDate();
         return Handle<TermStructure>(new
             FlatForward(today, today, RelinkableHandle<Quote>(forward), dc));
     }
 
-    Handle<BlackVolTermStructure> 
-    makeFlatVolatility(const Handle<Quote>& vol,
-                       DayCounter dc = SimpleDayCounter()) {
+    Handle<BlackVolTermStructure> makeFlatVolatility(const Handle<Quote>& vol,
+                                                     DayCounter dc) {
         Date today = Date::todaysDate();
         return Handle<BlackVolTermStructure>(new
             BlackConstantVol(today, RelinkableHandle<Quote>(vol), dc));
+    }
+
+
+
+    std::string payoffTypeToString(const Handle<Payoff>& payoff) {
+
+        // PlainVanillaPayoff?
+        Handle<PlainVanillaPayoff> pv;
+        #if defined(HAVE_BOOST)
+        pv = boost::dynamic_pointer_cast<PlainVanillaPayoff>(payoff);
+        #else
+        try {
+            pv = payoff;
+        } catch (...) {}
+        #endif
+        if (!IsNull(pv)) {
+            // ok, the payoff is PlainVanillaPayoff
+            return "PlainVanillaPayoff";
+        }
+
+        // CashOrNothingPayoff?
+        Handle<CashOrNothingPayoff> coo;
+        #if defined(HAVE_BOOST)
+        coo = boost::dynamic_pointer_cast<CashOrNothingPayoff>(payoff);
+        #else
+        try {
+            coo = payoff;
+        } catch (...) {}
+        #endif
+        if (!IsNull(coo)) {
+            // ok, the payoff is CashOrNothingPayoff
+            return "Cash ("
+                + DoubleFormatter::toString(coo->cashPayoff())
+                + ") or Nothing Payoff";
+        }
+
+        // AssetOrNothingPayoff?
+        Handle<AssetOrNothingPayoff> aoo;
+        #if defined(HAVE_BOOST)
+        aoo = boost::dynamic_pointer_cast<AssetOrNothingPayoff>(payoff);
+        #else
+        try {
+            aoo = payoff;
+        } catch (...) {}
+        #endif
+        if (!IsNull(aoo)) {
+            // ok, the payoff is AssetOrNothingPayoff
+            return "AssetOrNothingPayoff";
+        }
+
+        // SuperSharePayoff?
+        Handle<SuperSharePayoff> ss;
+        #if defined(HAVE_BOOST)
+        ss = boost::dynamic_pointer_cast<SuperSharePayoff>(payoff);
+        #else
+        try {
+            ss = payoff;
+        } catch (...) {}
+        #endif
+        if (!IsNull(ss)) {
+            // ok, the payoff is SuperSharePayoff
+            return "SuperSharePayoff";
+        }
+
+        throw Error("payoffTypeToString : unknown payoff type");
     }
 
     std::string exerciseTypeToString(const Handle<Exercise>& exercise) {
@@ -93,107 +156,181 @@ namespace {
         throw Error("exerciseTypeToString : unknown exercise type");
     }
 
+    std::string basketTypeToString(BasketOption::BasketType basketType) {
+
+        switch (basketType) {
+        case BasketOption::Min:
+            return "Basket::Min";
+        case BasketOption::Max:
+            return "Basket::Max";
+        }
+
+        throw Error("basketTypeToString : unknown basket option type");
+    }
+
+    void basketOptionTestFailed(std::string greekName,
+                                BasketOption::BasketType basketType,
+                                const Handle<StrikedTypePayoff>& payoff,
+                                const Handle<Exercise>& exercise,
+                                double s1,
+                                double s2,
+                                double q1,
+                                double q2,
+                                double r,
+                                Date today,
+                                DayCounter dc,
+                                double v1,
+                                double v2,
+                                double rho,
+                                double expected,
+                                double calculated,
+                                double error,
+                                double tolerance) {
+
+        Time t = dc.yearFraction(today, exercise->lastDate());
+
+        CPPUNIT_FAIL(exerciseTypeToString(exercise) + " "
+            + OptionTypeFormatter::toString(payoff->optionType()) +
+            " option on "
+            + basketTypeToString(basketType) +
+            " with "
+            + payoffTypeToString(payoff) + ":\n"
+            "1st underlying value: "
+            + DoubleFormatter::toString(s1) + "\n"
+            "2nd underlying value: "
+            + DoubleFormatter::toString(s2) + "\n"
+            "              strike: "
+            + DoubleFormatter::toString(payoff->strike()) +"\n"
+            "  1st dividend yield: "
+            + DoubleFormatter::toString(q1) + "\n"
+            "  2nd dividend yield: "
+            + DoubleFormatter::toString(q2) + "\n"
+            "      risk-free rate: "
+            + DoubleFormatter::toString(r) + "\n"
+            "      reference date: "
+            + DateFormatter::toString(today) + "\n"
+            "            maturity: "
+            + DateFormatter::toString(exercise->lastDate()) + "\n"
+            "      time to expiry: "
+            + DoubleFormatter::toString(t) + "\n"
+            "1st asset volatility: "
+            + DoubleFormatter::toString(v1) + "\n"
+            "2nd asset volatility: "
+            + DoubleFormatter::toString(v2) + "\n"
+            "         correlation: "
+            + DoubleFormatter::toString(rho) + "\n\n"
+            "    expected   " + greekName + ": "
+            + DoubleFormatter::toString(expected) + "\n"
+            "    calculated " + greekName + ": "
+            + DoubleFormatter::toString(calculated) + "\n"
+            "    error:            "
+            + DoubleFormatter::toString(error) + "\n"
+            "    tolerance:        "
+            + DoubleFormatter::toString(tolerance));
+    }
+
     struct BasketOptionData {
-        Option::Type type;
         BasketOption::BasketType basketType;
-        double s1_0;
-        double s2_0;
-        double rho;
-        double vol_S1;
-        double vol_S2;
+        Option::Type type;
         double strike;
-        double intRate;
-        int expiry; // months
-        double value;
+        double s1;
+        double s2;
+        double q1;
+        double q2;
+        double r;
+        Time t; // years
+        double v1;
+        double v2;
+        double rho;
+        double result;
+        double tol;
     };
 
 }
 
 void BasketOptionTest::testValues() {
 
-    double maxErrorAllowed = 1.0e-3;
-
-    double underlyingPrice = 100.0;
-    Rate r = 0.05;
-    // cannot handle dividends....
-    Rate q = 0.00;
-
     /*
-      Data from Excel spreadsheet
-      http://www.maths.ox.ac.uk/~firth/computing/excel.shtml
-        {option type, basket type, s1_0, s2_0, rho, vol_S1, vol_S2, 
-         strike, intRate, expiry, value}        
+        Data from:
+        Excel spreadsheet www.maths.ox.ac.uk/~firth/computing/excel.shtml
+        and
+        "Option pricing formulas", E.G. Haug, McGraw-Hill 1998 pag 56-58
     */
     BasketOptionData values[] = {
-        // call on the min            
-        {Option::Call, BasketOption::Min, 100.0, 100.0, 0.9, 0.3, 0.3, 100, 0.05, 12, 10.89772},
-        {Option::Call, BasketOption::Min, 100.0, 100.0, 0.7, 0.3, 0.3, 100, 0.05, 12, 8.482995},
-        {Option::Call, BasketOption::Min, 100.0, 100.0, 0.5, 0.3, 0.3, 100, 0.05, 12, 6.843896},
-        {Option::Call, BasketOption::Min, 100.0, 100.0, 0.3, 0.3, 0.3, 100, 0.05, 12, 5.530978},
-        {Option::Call, BasketOption::Min, 100.0, 100.0, 0.1, 0.3, 0.3, 100, 0.05, 12, 4.413080},
-        {Option::Call, BasketOption::Min, 100.0, 100.0, 0.0, 0.5, 0.7, 100, 0.05, 12, 4.981078},
-        {Option::Call, BasketOption::Min, 100.0, 100.0, 0.0, 0.5, 0.3, 100, 0.05, 12, 4.158957},
-        {Option::Call, BasketOption::Min, 100.0, 100.0, 0.0, 0.5, 0.1, 100, 0.05, 12, 2.597341},
-        {Option::Call, BasketOption::Min, 100.0, 100.0, 0.5, 0.5, 0.1, 100, 0.05, 12, 4.029683},
-        // call on the max
-        {Option::Call, BasketOption::Max, 100.0, 100.0, 0.9, 0.3, 0.3, 100, 0.05, 12, 17.56477},
-        {Option::Call, BasketOption::Max, 100.0, 100.0, 0.7, 0.3, 0.3, 100, 0.05, 12, 19.97949},
-        {Option::Call, BasketOption::Max, 100.0, 100.0, 0.5, 0.3, 0.3, 100, 0.05, 12, 21.61859},
-        {Option::Call, BasketOption::Max, 100.0, 100.0, 0.3, 0.3, 0.3, 100, 0.05, 12, 22.93151},
-        {Option::Call, BasketOption::Max, 100.0, 100.0, 0.1, 0.3, 0.3, 100, 0.05, 12, 24.04941},
-        {Option::Call, BasketOption::Max, 80.0,  100.0, 0.3, 0.3, 0.3, 100, 0.05, 12, 16.5077},
-        {Option::Call, BasketOption::Max, 80.0,  80.0,  0.3, 0.3, 0.3, 100, 0.05, 12, 8.049205},
-        {Option::Call, BasketOption::Max, 80.0,  120.0, 0.3, 0.3, 0.3, 100, 0.05, 12, 30.14043},
-        {Option::Call, BasketOption::Max, 120.0, 120.0, 0.3, 0.3, 0.3, 100, 0.05, 12, 42.88935},
-        // put on the min
-        {Option::Put, BasketOption::Min, 100.0, 100.0, 0.9, 0.3, 0.3, 100, 0.05, 12, 11.36904},
-        {Option::Put, BasketOption::Min, 100.0, 100.0, 0.7, 0.3, 0.3, 100, 0.05, 12, 12.85571},
-        {Option::Put, BasketOption::Min, 100.0, 100.0, 0.5, 0.3, 0.3, 100, 0.05, 12, 13.89038},
-        {Option::Put, BasketOption::Min, 100.0, 100.0, 0.3, 0.3, 0.3, 100, 0.05, 12, 14.74097},
-        {Option::Put, BasketOption::Min, 100.0, 100.0, 0.1, 0.3, 0.3, 100, 0.05, 12, 15.4854},
-        {Option::Put, BasketOption::Min, 100.0, 100.0, 0.1, 0.3, 0.3, 100, 0.05, 6, 11.89271},
-        {Option::Put, BasketOption::Min, 100.0, 100.0, 0.1, 0.3, 0.3, 100, 0.05, 3, 8.880667},
-        {Option::Put, BasketOption::Min, 100.0, 100.0, 0.1, 0.3, 0.3, 100, 0.05, 24, 19.26801},
-        // put on the max
-        {Option::Put, BasketOption::Max, 100.0, 100.0, 0.9, 0.3, 0.3, 100, 0.05, 12, 7.339334},
-        {Option::Put, BasketOption::Max, 100.0, 100.0, 0.7, 0.3, 0.3, 100, 0.05, 12, 5.852663},
-        {Option::Put, BasketOption::Max, 100.0, 100.0, 0.5, 0.3, 0.3, 100, 0.05, 12, 4.817994},
-        {Option::Put, BasketOption::Max, 100.0, 100.0, 0.3, 0.3, 0.3, 100, 0.05, 12, 3.967405},
-        {Option::Put, BasketOption::Max, 100.0, 100.0, 0.1, 0.3, 0.3, 100, 0.05, 12, 3.222970}
+        //      basketType,   optionType, strike,    s1,    s2,   q1,   q2,    r,    t,   v1,   v2,  rho,   result, tol
+
+        // data from http://www.maths.ox.ac.uk/~firth/computing/excel.shtml
+        {BasketOption::Min, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.90, 10.898, 1.0e-3},
+        {BasketOption::Min, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.70,  8.483, 1.0e-3},
+        {BasketOption::Min, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.50,  6.844, 1.0e-3},
+        {BasketOption::Min, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.30,  5.531, 1.0e-3},
+        {BasketOption::Min, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.10,  4.413, 1.0e-3},
+        {BasketOption::Min, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.50, 0.70, 0.00,  4.981, 1.0e-3},
+        {BasketOption::Min, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.50, 0.30, 0.00,  4.159, 1.0e-3},
+        {BasketOption::Min, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.50, 0.10, 0.00,  2.597, 1.0e-3},
+        {BasketOption::Min, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.50, 0.10, 0.50,  4.030, 1.0e-3},
+
+        {BasketOption::Max, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.90, 17.565, 1.0e-3},
+        {BasketOption::Max, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.70, 19.980, 1.0e-3},
+        {BasketOption::Max, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.50, 21.619, 1.0e-3},
+        {BasketOption::Max, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.30, 22.932, 1.0e-3},
+        {BasketOption::Max, Option::Call,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.10, 24.049, 1.0e-3},
+        {BasketOption::Max, Option::Call,  100.0,  80.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.30, 16.508, 1.0e-3},
+        {BasketOption::Max, Option::Call,  100.0,  80.0,  80.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.30,  8.049, 1.0e-3},
+        {BasketOption::Max, Option::Call,  100.0,  80.0, 120.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.30, 30.141, 1.0e-3},
+        {BasketOption::Max, Option::Call,  100.0, 120.0, 120.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.30, 42.889, 1.0e-3},
+
+        {BasketOption::Min,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.90, 11.369, 1.0e-3},
+        {BasketOption::Min,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.70, 12.856, 1.0e-3},
+        {BasketOption::Min,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.50, 13.890, 1.0e-3},
+        {BasketOption::Min,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.30, 14.741, 1.0e-3},
+        {BasketOption::Min,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.10, 15.485, 1.0e-3},
+        {BasketOption::Min,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 0.50, 0.30, 0.30, 0.10, 11.893, 1.0e-3},
+        {BasketOption::Min,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 0.25, 0.30, 0.30, 0.10,  8.881, 1.0e-3},
+        {BasketOption::Min,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 2.00, 0.30, 0.30, 0.10, 19.268, 1.0e-3},
+
+        {BasketOption::Max,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.90,  7.339, 1.0e-3},
+        {BasketOption::Max,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.70,  5.853, 1.0e-3},
+        {BasketOption::Max,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.50,  4.818, 1.0e-3},
+        {BasketOption::Max,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.30,  3.967, 1.0e-3},
+        {BasketOption::Max,  Option::Put,  100.0, 100.0, 100.0, 0.00, 0.00, 0.05, 1.00, 0.30, 0.30, 0.10,  3.223, 1.0e-3},
+
+        // data from "Option pricing formulas", E.G. Haug, McGraw-Hill 1998 pag 58
+//        {BasketOption::Max,  Option::Put,   98.0, 100.0, 105.0, 0.00, 0.00, 0.05, 0.50, 0.11, 0.16, 0.63,   1.2181, 1.0e-4}
     };
 
-    Handle<SimpleQuote> underlying1(new SimpleQuote(underlyingPrice));
-    Handle<SimpleQuote> underlying2(new SimpleQuote(underlyingPrice));
+    DayCounter dc = Actual360();
+    Handle<SimpleQuote> underlying1(new SimpleQuote(0.0));
+    Handle<SimpleQuote> underlying2(new SimpleQuote(0.0));
 
     // cannot handle dividends
-    Handle<SimpleQuote> qH_SME(new SimpleQuote(q));
-    Handle<TermStructure> qTS = makeFlatCurve(qH_SME);
+    Handle<SimpleQuote> qH_SME(new SimpleQuote(0.0));
+    Handle<TermStructure> qTS = makeFlatCurve(qH_SME, dc);
 
-    Handle<SimpleQuote> rH_SME(new SimpleQuote(r));
-    Handle<TermStructure> rTS = makeFlatCurve(rH_SME);
+    Handle<SimpleQuote> rH_SME(new SimpleQuote(0.0));
+    Handle<TermStructure> rTS = makeFlatCurve(rH_SME, dc);
 
-    Handle<SimpleQuote> volatility1(new SimpleQuote(0.10));
-    Handle<BlackVolTermStructure> volTS1 = makeFlatVolatility(volatility1);
-    Handle<SimpleQuote> volatility2(new SimpleQuote(0.10));
-    Handle<BlackVolTermStructure> volTS2 = makeFlatVolatility(volatility2);
+    Handle<SimpleQuote> volatility1(new SimpleQuote(0.0));
+    Handle<BlackVolTermStructure> volTS1 = makeFlatVolatility(volatility1, dc);
+    Handle<SimpleQuote> volatility2(new SimpleQuote(0.0));
+    Handle<BlackVolTermStructure> volTS2 = makeFlatVolatility(volatility2, dc);
 
 
     Handle<PricingEngine> engine(new StulzEngine);
 
     Date today = Date::todaysDate();
-    Calendar calendar = NullCalendar();
 
     for (Size i=0; i<LENGTH(values); i++) {
-        Date exDate = calendar.advance(today,values[i].expiry,Months);
+        Date exDate = today.plusDays(int(values[i].t*360+0.5));
         Handle<Exercise> exercise(new EuropeanExercise(exDate));
 
-        rH_SME->setValue(values[i].intRate);
+        rH_SME->setValue(values[i].r);
 
-        underlying1->setValue(values[i].s1_0);
-        underlying2->setValue(values[i].s2_0);
+        underlying1->setValue(values[i].s1);
+        underlying2->setValue(values[i].s2);
 
-        volatility1->setValue(values[i].vol_S1);
-        volatility2->setValue(values[i].vol_S2);
+        volatility1->setValue(values[i].v1);
+        volatility2->setValue(values[i].v2);
 
         Handle<PlainVanillaPayoff> payoff(new
             PlainVanillaPayoff(values[i].type, values[i].strike));
@@ -214,7 +351,7 @@ void BasketOptionTest::testValues() {
                 RelinkableHandle<TermStructure>(rTS),
                 RelinkableHandle<BlackVolTermStructure>(volTS2)));
 
-        std::vector<Handle<BlackScholesStochasticProcess> > procs = 
+        std::vector<Handle<BlackScholesStochasticProcess> > procs =
             std::vector<Handle<BlackScholesStochasticProcess> >();
         procs.push_back(stochProcess1);
         procs.push_back(stochProcess2);
@@ -226,15 +363,14 @@ void BasketOptionTest::testValues() {
                                   exercise, rho, engine);
 
         double calculated = basketOption.NPV();
-        double expected = values[i].value;
-        if (QL_FABS(calculated-expected) > maxErrorAllowed) {
-            CPPUNIT_FAIL(
-                "Data at index " + IntegerFormatter::toString(i) + ", "
-                "Basket option:\n"
-                    "    value:    " +
-                    DoubleFormatter::toString(calculated) + "\n"
-                    "    expected: " +
-                    DoubleFormatter::toString(expected));
+        double expected = values[i].result;
+        double error = QL_FABS(calculated-expected);
+        if (error > values[i].tol) {
+            basketOptionTestFailed("value",
+                values[i].basketType, payoff, exercise, values[i].s1, values[i].s2,
+                values[i].q1, values[i].q2, values[i].r,
+                today, dc, values[i].v1, values[i].v2, values[i].rho,
+                values[i].result, calculated, error, values[i].tol);
         }
 
     }
