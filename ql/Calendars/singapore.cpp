68a9f31a641a1b320f8ff533f7f97c1336b236e7
Luigi Ballabio <luigi.ballabio@gmail.com>
2004-04-27 09:49:59 +0000

    Singapore calendar added (thanks to Xavier Abulker)

---
+++ ql/Calendars/singapore.cpp
@@ -0,0 +1,79 @@
+
+/*
+ Copyright (C) 2004 FIMAT Group
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it under the
+ terms of the QuantLib license.  You should have received a copy of the
+ license along with this program; if not, please email quantlib-dev@lists.sf.net
+ The license is also available online at http://quantlib.org/html/license.html
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+#include <ql/Calendars/singapore.hpp>
+
+namespace QuantLib {
+
+    Singapore::Singapore() {
+        // all calendar instances share the same implementation instance
+        static boost::shared_ptr<Calendar::Impl> impl(new Singapore::Impl);
+        impl_ = impl;
+    }
+
+    bool Singapore::Impl::isBusinessDay(const Date& date) const {
+        Weekday w = date.weekday();
+        Day d = date.dayOfMonth(), dd = date.dayOfYear();
+        Month m = date.month();
+        Year y = date.year();
+        Day em = easterMonday(y);
+
+        if ((w == Saturday || w == Sunday)
+            // New Year's Day
+            || (d == 1 && m == January)
+            // Good Friday
+            || (dd == em-3)
+            // Labor Day
+            || (d == 1 && m == May)
+            // National Day
+            || (d == 9 && m == August)
+            // Christmas Day
+            || (d == 25 && m == December)
+            // Boxing Day
+            || (d == 26 && m == December)
+
+            // Chinese New Year 2004
+            || ((d==22 || d==23) && m == January && y==2004)
+            // Chinese New Year 2005
+            || ((d==9 || d==10) && m == February && y==2005)
+
+            // Hari Raya Haji 2004
+            || ((d==1 || d==2) && m == February && y==2004)
+            // Hari Raya Haji 2005
+            || ((d==21) && m == January && y==2005)
+
+            // Vesak Poya Day 2004
+            || ((d==2) && m == June && y==2004)
+            // Vesak Poya Day 2005
+            || ((d==22) && m == May && y==2005)
+
+            // Deepavali
+            || ((d==11) && m == November && y==2004)
+            // Diwali
+            || ((d==1) && m == November && y==2005)
+
+            // Hari Raya Puasa 2004
+            || ((d==14 || d==15) && m == November && y==2004)
+            // Hari Raya Puasa 2005
+            || (d==3 && m == November && y==2005)
+            )
+            return false;
+        return true;
+    }
+
+}
+
---
+++ ql/Calendars/singapore.hpp
@@ -0,0 +1,71 @@
+
+/*
+ Copyright (C) 2004 FIMAT Group
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it under the
+ terms of the QuantLib license.  You should have received a copy of the
+ license along with this program; if not, please email quantlib-dev@lists.sf.net
+ The license is also available online at http://quantlib.org/html/license.html
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file singapore.hpp
+    \brief Singapore calendar
+*/
+
+#ifndef quantlib_singapore_calendar_h
+#define quantlib_singapore_calendar_h
+
+#include <ql/calendar.hpp>
+
+namespace QuantLib {
+
+    //! %Singapore calendar
+    /*! Holidays:
+        <ul>
+        <li>Saturdays</li>
+        <li>Sundays</li>
+        <li>New Year's day, January 1st</li>
+        <li>Good Friday</li>
+        <li>Labour Day, May 1st</li>
+        <li>National Day, August 9th</li>
+        <li>Christmas, December 25th </li>
+        <li>Boxing Day, December 26th </li>
+        </ul>
+
+        Other holidays for which no rule is given 
+        (data available for 2004-2005 only:)
+        <ul>
+        <li>Chinese New Year</li>
+        <li>Hari Raya Haji</li>
+        <li>Vesak Poya Day</li>
+        <li>Deepavali</li>
+        <li>Diwali</li>
+        <li>Hari Raya Puasa</li>
+        </ul>
+
+        Data from http://www.asx.com.au and http://www.ses.com.sg
+
+        \ingroup calendars
+    */
+    class Singapore : public Calendar {
+      private:
+        class Impl : public Calendar::WesternImpl {
+          public:
+            std::string name() const { return "Singapore"; }
+            bool isBusinessDay(const Date&) const;
+        };
+      public:
+        Singapore();
+    };
+
+}
+
+
+#endif
