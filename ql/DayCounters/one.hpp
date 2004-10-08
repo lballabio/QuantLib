4be4569e302a35a2700fbc680bb8241bf0887d79
Ferdinando Ametrano <nando@ametrano.net>
2004-10-08 16:21:13 +0000

    bug fix (?)

--- ql/DayCounters/one.hpp
+++ ql/DayCounters/one.hpp
@@ -35,7 +35,7 @@ namespace QuantLib {
             std::string name() const { return std::string("1/1"); }
             BigInteger dayCount(const Date& d1, const Date& d2) const {
                 // the sign is all we need
-                return (d2 > d1 ? 1 : -1);
+                return (d2 >= d1 ? 1 : -1);
             };
             Time yearFraction(const Date& d1, const Date& d2,
                               const Date&, const Date&) const {
