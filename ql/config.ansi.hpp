219c73c9b6bb839d0528456393a9281c0bd240e9
Ferdinando Ametrano <nando@ametrano.net>
2004-10-20 12:11:48 +0000

    using mingw32 as ansi proxy (ansi is used with Dev-C++ without mingw with cygwin)

--- ql/config.ansi.hpp
+++ ql/config.ansi.hpp
@@ -25,44 +25,8 @@
    do not modify the following definitions.
  *******************************************/
 
-#define HAVE_CSTDLIB 1
-#define QL_SIZE_T    std::size_t
-#define QL_ATOI      std::atoi
-
-#define HAVE_CMATH   1
-#define QL_SQRT      std::sqrt
-#define QL_FABS      std::fabs
-#define QL_EXP       std::exp
-#define QL_LOG       std::log
-#define QL_SIN       std::sin
-#define QL_COS       std::cos
-#define QL_SINH      std::sinh
-#define QL_COSH      std::cosh
-#define QL_POW       std::pow
-#define QL_MODF      std::modf
-#define QL_FLOOR     std::floor
-
-#define HAVE_LIMITS  1
-
-#define HAVE_CTIME   1
-#define QL_TIME_T    std::time_t
-#define QL_TM        std::tm
-#define QL_CLOCK     std::clock
-#define QL_TIME      std::time
-#define QL_GMTIME    std::gmtime
-
-#define HAVE_CCTYPE  1
-#define QL_TOLOWER   std::tolower
-#define QL_TOUPPER   std::toupper
-
-#define HAVE_CSTRING 1
-#define QL_STRLEN    std::strlen
-
-#define QL_MIN       std::min
-#define QL_MAX       std::max
-
-#define QL_ITERATOR         std::iterator
-#define QL_ITERATOR_TRAITS  std::iterator_traits
+// use mingw as proxy for ansi compliant compiler
+#include <ql/config.mingw.hpp>
 
 
 #endif
