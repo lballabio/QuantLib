802d89bb9d558391055111202f67d448abfea3cb
Ferdinando Ametrano <nando@ametrano.net>
2004-10-20 13:43:25 +0000

    no message

--- ql/config.ansi.hpp
+++ ql/config.ansi.hpp
@@ -1,5 +1,6 @@
 
 /*
+ Copyright (C) 2004 Ferdinando Ametrano
  Copyright (C) 2000, 2001, 2002, 2003 RiskMap srl
 
  This file is part of QuantLib, a free-software/open-source library
@@ -25,8 +26,48 @@
    do not modify the following definitions.
  *******************************************/
 
-// use mingw as proxy for ansi compliant compiler
-#include <ql/config.mingw.hpp>
+#define HAVE_CSTDLIB 1
+#define QL_SIZE_T    std::size_t
+#define QL_ATOI      std::atoi
+
+#define HAVE_CMATH   1
+#define QL_SQRT      std::sqrt
+#define QL_FABS      std::fabs
+#define QL_EXP       std::exp
+#define QL_LOG       std::log
+#define QL_SIN       std::sin
+#define QL_COS       std::cos
+#define QL_SINH      std::sinh
+#define QL_COSH      std::cosh
+#define QL_POW       std::pow
+#define QL_MODF      std::modf
+#define QL_FLOOR     std::floor
+
+#define M_SQRT_2    0.7071067811865475244008443621048490392848359376887
+#define M_1_SQRTPI  0.564189583547756286948
+#define M_SQRTPI    1.77245385090551602792981
+
+#define HAVE_LIMITS  1
+
+#define HAVE_CTIME   1
+#define QL_TIME_T    std::time_t
+#define QL_TM        std::tm
+#define QL_CLOCK     std::clock
+#define QL_TIME      std::time
+#define QL_GMTIME    std::gmtime
+
+#define HAVE_CCTYPE  1
+#define QL_TOLOWER   std::tolower
+#define QL_TOUPPER   std::toupper
+
+#define HAVE_CSTRING 1
+#define QL_STRLEN    std::strlen
+
+#define QL_MIN       std::min
+#define QL_MAX       std::max
+
+#define QL_ITERATOR         std::iterator
+#define QL_ITERATOR_TRAITS  std::iterator_traits
 
 
 #endif
