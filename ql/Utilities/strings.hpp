d4b23f27f555c49adac9bbb8f2ad1876660eeb63
Luigi Ballabio <luigi.ballabio@gmail.com>
2005-02-01 15:15:57 +0000

    *** empty log message ***

---
+++ ql/Utilities/strings.hpp
@@ -0,0 +1,51 @@
+
+/*
+ Copyright (C) 2005 StatPro Italia srl
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it under the
+ terms of the QuantLib license.  You should have received a copy of the
+ license along with this program; if not, please email quantlib-dev@lists.sf.net
+ The license is also available online at http://quantlib.org/html/license.html
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file strings.hpp
+    \brief string utilities
+*/
+
+#ifndef quantlib_strings_hpp
+#define quantlib_strings_hpp
+
+namespace QuantLib {
+
+    std::string lowercase(const std::string&);
+
+    std::string uppercase(const std::string&);
+
+
+    // inline definitions
+
+    inline std::string lowercase(const std::string& s) {
+        std::string output = s;
+        for (std::string::iterator i=output.begin(); i!=output.end(); i++)
+            *i = std::tolower(*i);
+        return output;
+    }
+
+    inline std::string uppercase(const std::string& s) {
+        std::string output = s;
+        for (std::string::iterator i=output.begin(); i!=output.end(); i++)
+            *i = std::toupper(*i);
+        return output;
+    }
+
+}
+
+
+#endif
