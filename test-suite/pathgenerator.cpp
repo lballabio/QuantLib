b3c59f09b13d72f664a60ff57e36a2c04bbcfd86
Luigi Ballabio <luigi.ballabio@gmail.com>
2005-05-17 10:43:17 +0000

    Improved test

--- test-suite/pathgenerator.cpp
+++ test-suite/pathgenerator.cpp
@@ -36,7 +36,8 @@ void teardown() {
 }
 
 void testSingle(const boost::shared_ptr<StochasticProcess1D>& process,
-                const std::string& tag, bool brownianBridge, Real expected) {
+                const std::string& tag, bool brownianBridge,
+                Real expected, Real antithetic) {
     typedef PseudoRandom::rsg_type rsg_type;
     typedef PathGenerator<rsg_type>::sample_type sample_type;
 
@@ -46,10 +47,14 @@ void testSingle(const boost::shared_ptr<StochasticProcess1D>& process,
     rsg_type rsg = PseudoRandom::make_sequence_generator(timeSteps, seed);
     PathGenerator<rsg_type> generator(process, length, timeSteps,
                                       rsg, brownianBridge);
-    for (Size i=0; i<100; i++)
+    Size i;
+    for (i=0; i<100; i++)
         generator.next();
+
     sample_type sample = generator.next();
-    Real calculated = sample.value[sample.value.size()-1];
+    Real calculated = process->x0();
+    for (i=0; i<sample.value.size(); i++)
+        calculated = process->apply(calculated,sample.value[i]);
     if (std::fabs(calculated-expected) > 1.0e-10) {
         BOOST_ERROR("using " << tag << " process "
                     << (brownianBridge ? "with " : "without ")
@@ -58,10 +63,25 @@ void testSingle(const boost::shared_ptr<StochasticProcess1D>& process,
                     << "    calculated: " << calculated << "\n"
                     << "    expected:   " << expected);
     }
+
+    sample = generator.antithetic();
+    calculated = process->x0();
+    for (i=0; i<sample.value.size(); i++)
+        calculated = process->apply(calculated,sample.value[i]);
+    if (std::fabs(calculated-antithetic) > 1.0e-10) {
+        BOOST_ERROR("using " << tag << " process "
+                    << (brownianBridge ? "with " : "without ")
+                    << "brownian bridge:\n"
+                    << "antithetic sample:\n"
+                    << std::setprecision(13)
+                    << "    calculated: " << calculated << "\n"
+                    << "    expected:   " << antithetic);
+    }
+
 }
 
 void testMultiple(const boost::shared_ptr<GenericStochasticProcess>& process,
-                  const std::string& tag, Real expected[]) {
+                  const std::string& tag, Real expected[], Real antithetic[]) {
     typedef PseudoRandom::rsg_type rsg_type;
     typedef MultiPathGenerator<rsg_type>::sample_type sample_type;
 
@@ -74,17 +94,45 @@ void testMultiple(const boost::shared_ptr<GenericStochasticProcess>& process,
     MultiPathGenerator<rsg_type> generator(process,
                                            TimeGrid(length, timeSteps),
                                            rsg, false);
-    for (Size i=0; i<100; i++)
+    Size i;
+    for (i=0; i<100; i++)
         generator.next();
+
     sample_type sample = generator.next();
-    for (Size j=0; j<assets; j++) {
-        Real calculated = sample.value[j][sample.value.pathSize()-1];
-        if (std::fabs(calculated-expected[j]) > 1.0e-10) {
+    Array calculated = process->initialValues();
+    for (i=0; i<sample.value.pathSize(); i++) {
+        Array change(assets);
+        for (Size j=0; j<assets; j++)
+            change[j] = sample.value[j][i];
+        calculated = process->apply(calculated,change);
+    }
+    Size j;
+    for (j=0; j<assets; j++) {
+        if (std::fabs(calculated[j]-expected[j]) > 1.0e-10) {
             BOOST_ERROR("using " << tag << " process "
-                       << "(" << io::ordinal(j+1) << " asset:)\n"
-                       << std::setprecision(13)
-                       << "    calculated: " << calculated << "\n"
-                       << "    expected:   " << expected[j]);
+                        << "(" << io::ordinal(j+1) << " asset:)\n"
+                        << std::setprecision(13)
+                        << "    calculated: " << calculated[j] << "\n"
+                        << "    expected:   " << expected[j]);
+        }
+    }
+
+    sample = generator.antithetic();
+    calculated = process->initialValues();
+    for (i=0; i<sample.value.pathSize(); i++) {
+        Array change(assets);
+        for (Size j=0; j<assets; j++)
+            change[j] = sample.value[j][i];
+        calculated = process->apply(calculated,change);
+    }
+    for (j=0; j<assets; j++) {
+        if (std::fabs(calculated[j]-antithetic[j]) > 1.0e-10) {
+            BOOST_ERROR("using " << tag << " process "
+                        << "(" << io::ordinal(j+1) << " asset:)\n"
+                        << "antithetic sample:\n"
+                        << std::setprecision(13)
+                        << "    calculated: " << calculated[j] << "\n"
+                        << "    expected:   " << antithetic[j]);
         }
     }
 }
@@ -106,31 +154,31 @@ void PathGeneratorTest::testPathGenerator() {
     Handle<BlackVolTermStructure> sigma(flatVol(0.20, Actual360()));
     testSingle(boost::shared_ptr<StochasticProcess1D>(
                                        new BlackScholesProcess(x0,q,r,sigma)),
-               "Black-Scholes", false, -0.05907199908012);
+               "Black-Scholes", false, 26.13784357783, 467.2928562519);
     testSingle(boost::shared_ptr<StochasticProcess1D>(
                                        new BlackScholesProcess(x0,q,r,sigma)),
-               "Black-Scholes", true, 0.04657002649305);
+               "Black-Scholes", true, 60.28215551021, 202.6143139437);
 
     testSingle(boost::shared_ptr<StochasticProcess1D>(
                        new GeometricBrownianMotionProcess(100.0, 0.03, 0.20)),
-               "geometric Brownian", false, -1.223200366087);
+               "geometric Brownian", false, 27.62223714065, 483.602651493);
     /*testSingle(boost::shared_ptr<StochasticProcess1D>(
                        new GeometricBrownianMotionProcess(100.0, 0.03, 0.20)),
-               "geometric Brownian", true, 0.7287922656694);*/
+               "geometric Brownian", true, 133.0182971308, 134.3990344294);*/
 
     testSingle(boost::shared_ptr<StochasticProcess1D>(
                                      new OrnsteinUhlenbeckProcess(0.1, 0.20)),
-               "Ornstein-Uhlenbeck", false, 0.003963152890092);
+               "Ornstein-Uhlenbeck", false, -0.8198975077064, 0.8198975077064);
     /*testSingle(boost::shared_ptr<StochasticProcess1D>(
                                      new OrnsteinUhlenbeckProcess(0.1, 0.20)),
-               "Ornstein-Uhlenbeck", true, 0.09162840758277);*/
+               "Ornstein-Uhlenbeck", true, -0.295462017761, 0.295462017761);*/
 
     testSingle(boost::shared_ptr<StochasticProcess1D>(
                                  new SquareRootProcess(0.1, 0.1, 0.20, 10.0)),
-               "square-root", false, -0.2488203640255);
+               "square-root", false, 1.70608664108, 6.024200546031);
     /*testSingle(boost::shared_ptr<StochasticProcess1D>(
                                  new SquareRootProcess(0.1, 0.1, 0.20, 10.0)),
-               "square-root", true, -0.09560382006972);*/
+               "square-root", true, 2.657244730273, 3.495860834833);*/
 
     QL_TEST_TEARDOWN
 }
@@ -166,10 +214,14 @@ void PathGeneratorTest::testMultiPathGenerator() {
     process = boost::shared_ptr<GenericStochasticProcess>(
                            new StochasticProcessArray(processes,correlation));
     Real result1[] = {
-        0.1428138446634,
-        0.2092744336095,
-        0.04057136028272 };
-    testMultiple(process, "Black-Scholes", result1);
+        188.2235869273,
+        270.6713071508,
+        113.0431145652 };
+    Real result1a[] = {
+        64.89105739157,
+        45.12494401537,
+        108.0475146914 };
+    testMultiple(process, "Black-Scholes", result1, result1a);
 
     processes[0] = boost::shared_ptr<StochasticProcess1D>(
                        new GeometricBrownianMotionProcess(100.0, 0.03, 0.20));
@@ -180,10 +232,14 @@ void PathGeneratorTest::testMultiPathGenerator() {
     process = boost::shared_ptr<GenericStochasticProcess>(
                            new StochasticProcessArray(processes,correlation));
     Real result2[] = {
-        24.04649099339,
-        43.72875185354,
-        6.44889193814 };
-    testMultiple(process, "geometric Brownian", result2);
+        174.8266132344,
+        237.2692444869,
+        119.1168555605 };
+    Real result2a[] = {
+        57.69082387657,
+        38.50016858691,
+        116.4056510107 };
+    testMultiple(process, "geometric Brownian", result2, result2a);
 
     processes[0] = boost::shared_ptr<StochasticProcess1D>(
                                      new OrnsteinUhlenbeckProcess(0.1, 0.20));
@@ -194,10 +250,14 @@ void PathGeneratorTest::testMultiPathGenerator() {
     process = boost::shared_ptr<GenericStochasticProcess>(
                            new StochasticProcessArray(processes,correlation));
     Real result3[] = {
-        0.1145529276342,
-        0.1609358646784,
-        0.03128651475229 };
-    testMultiple(process, "Ornstein-Uhlenbeck", result3);
+        0.2887210915189,
+        0.543931060928,
+        0.02713159568959 };
+    Real result3a[] = {
+        -0.2887210915189,
+        -0.543931060928,
+        -0.02713159568959 };
+    testMultiple(process, "Ornstein-Uhlenbeck", result3, result3a);
 
     processes[0] = boost::shared_ptr<StochasticProcess1D>(
                                  new SquareRootProcess(0.1, 0.1, 0.20, 10.0));
@@ -208,10 +268,14 @@ void PathGeneratorTest::testMultiPathGenerator() {
     process = boost::shared_ptr<GenericStochasticProcess>(
                            new StochasticProcessArray(processes,correlation));
     Real result4[] = {
-        -0.07385744304226,
-        0.04484225895393,
-        -0.2484464926277 };
-    testMultiple(process, "square-root", result4);
+        4.279510844897,
+        4.943783503533,
+        3.590930385958 };
+    Real result4a[] = {
+        2.763967737724,
+        2.226487196647,
+        3.503859264341 };
+    testMultiple(process, "square-root", result4, result4a);
 
     QL_TEST_TEARDOWN
 }
