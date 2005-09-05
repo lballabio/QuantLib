80b6ee3665c86d5e2129f4d853549041bf942b46
Luigi Ballabio <luigi.ballabio@gmail.com>
2005-09-05 10:05:42 +0000

    *** empty log message ***

--- test-suite/bonds.cpp
+++ test-suite/bonds.cpp
@@ -22,7 +22,6 @@
 #include <ql/Instruments/fixedcouponbond.hpp>
 #include <ql/Instruments/floatingratebond.hpp>
 #include <ql/Instruments/zerocouponbond.hpp>
-#include <ql/Instruments/convertiblebond.hpp>
 #include <ql/Calendars/target.hpp>
 #include <ql/Calendars/unitedstates.hpp>
 #include <ql/Calendars/nullcalendar.hpp>
