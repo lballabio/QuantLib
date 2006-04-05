f2dbaddbaeeba24af85f6880fb56f85b7f5d3521
Joseph Wang <drjoe@users.sf.net>
2006-04-05 16:25:34 +0000

    Need to check in

---
+++ ql/VolatilityModels/garch.hpp
@@ -0,0 +1,47 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2002, 2003 Ferdinando Ametrano
+ Copyright (C) 2003, 2004, 2005 StatPro Italia srl
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/reference/license.html>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file volatilitymodel.hpp
+    \brief Volatility term structures
+*/
+
+#ifndef quantlib_volatility_model_hpp
+#define quantlib_volatility_model_hpp
+
+#include <vector>
+#include <ql/types.hpp>
+#include <ql/timeseries.hpp>
+
+namespace QuantLib {
+    /*! This abstract class defines the interface of concrete
+        volatility model
+
+        Volatilities are assumed to be expressed on an annual basis.
+    */
+    class Garch11 : public VolatilityModel {
+      public:
+        TimeSeries<Volatility> 
+        calculate(const TimeSeries<Real> &quoteSeries) {};
+        void calibrate(const TimeSeries<Real> &quoteSeries) {};
+    };
+}
+
+
+#endif
