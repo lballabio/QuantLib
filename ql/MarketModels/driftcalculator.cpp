ea9ae740d77a5184bf44a55a3f6a35491610798b
Silvia Frasson <s_frasson@users.sf.net>
2006-07-05 15:19:41 +0000

    fixed bug

--- ql/MarketModels/driftcalculator.cpp
+++ ql/MarketModels/driftcalculator.cpp
@@ -61,7 +61,7 @@ namespace QuantLib {
             drifts[i]=0.;
             const Size down = std::min(i+1, numeraire_), up = std::max(i+1, numeraire_) - 1;
             const int sign = i+1>numeraire_ ? +1 : -1;
-            for(Size k=down; k<up; ++k) {
+            for(Size k=down; k<=up; ++k) {
                 const double A = (taus_[k]*(forwards[k]+displacements_[k]) / (1.+taus_[k]*forwards[k]));
                 drifts[i] += sign * A * C_[k][i];
             }
