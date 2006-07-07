b8aec34cfd5c1d973e5f4102a9ba3b06689a43f8
Luigi Ballabio <luigi.ballabio@gmail.com>
2006-07-07 20:49:00 +0000

    Updated Makefiles

---
+++ ql/MarketModels/core.hpp
@@ -0,0 +1,33 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2006 StatPro Italia srl
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/reference/license.html>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+#ifndef quantlib_market_models_core_hpp
+#define quantlib_market_models_core_hpp
+
+#include <ql/qldefines.hpp>
+#include <ql/MarketModels/accountingengine.hpp>
+#include <ql/MarketModels/browniangenerator.hpp>
+#include <ql/MarketModels/curvestate.hpp>
+#include <ql/MarketModels/evolutiondescription.hpp>
+#include <ql/MarketModels/marketmodelevolver.hpp>
+#include <ql/MarketModels/marketmodelproduct.hpp>
+#include <ql/MarketModels/pseudoroot.hpp>
+
+
+#endif
