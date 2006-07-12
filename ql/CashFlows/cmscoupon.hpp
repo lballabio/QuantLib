d4a31b9ebd5569fd82c9cb27195ab2847a195719
Mario Pucci <mariopucci@users.sf.net>
2006-07-12 13:51:45 +0000

    license amendment

--- ql/CashFlows/cmscoupon.hpp
+++ ql/CashFlows/cmscoupon.hpp
@@ -1,19 +1,19 @@
 /*
- * Copyright (C) 2004, 2005 StatPro Italia s.r.l. Milano
- *
- * StatPro Italia
- * Via G. B. Vico 4
- * I-20123 Milano
- * ITALY
- *
- * phone: +39 02 43317510
- * fax: +39 02 43911424
- *
- * email: info@riskmap.net
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the license for more details. */
+ Copyright (C) 2006 StatPro Italia srl
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/reference/license.html>.
+
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ or FITNESS FOR A PARTICULAR PURPOSE. See the license for more details. */
 
 /*! \file cmscoupon.hpp
     \brief CMS coupon
