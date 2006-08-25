e553c44fc5f72647fe8e56fd410822d1ef7e1152
Giorgio Facchinetti <giorfa@users.sf.net>
2006-08-25 14:26:40 +0000

    *** empty log message ***

---
+++ ql/MarketModels/Products/marketmodelcoterminalswapsonestep.hpp
@@ -0,0 +1,83 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2006 Giorgio Facchinetti
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/reference/license.html>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+
+#ifndef quantlib_MarketModelCoterminalSwapsOneStep_hpp
+#define quantlib_MarketModelCoterminalSwapsOneStep_hpp
+
+#include <ql/MarketModels/marketmodelproduct.hpp>
+
+namespace QuantLib {
+    class MarketModelCoterminalSwapsOneStep : public MarketModelProduct
+    {
+      public:
+        MarketModelCoterminalSwapsOneStep(const std::vector<Time>& rateTimes,
+                           const std::vector<Real>& fixedAccruals,
+                           const std::vector<Real>& floatingAccruals,
+                           const std::vector<Time>& paymentTimes,
+                           double fixedRate);
+        //! for initializing other objects
+        virtual EvolutionDescription suggestedEvolution() const;
+        virtual std::vector<Time> possibleCashFlowTimes() const;
+        virtual Size numberOfProducts() const;
+        virtual Size maxNumberOfCashFlowsPerProductPerStep() const;
+
+        //!during simulation
+        //!put product at start of path
+        virtual void reset(); 
+        //! bool return indicates whether path is finished, true means done
+        virtual bool nextTimeStep(const CurveState& currentState, 
+            std::vector<Size>& numberCashFlowsThisStep, //! one int for each product 
+            std::vector<std::vector<CashFlow> >& cashFlowsGenerated); //! the cash flows
+      private:
+        std::vector<Time> rateTimes_;
+        std::vector<Real> fixedAccruals_, floatingAccruals_;
+        std::vector<Time> paymentTimes_;
+        double fixedRate_;
+
+        Size lastIndex_;
+
+        // things that vary in a path
+        Size currentIndex_;
+        
+    };
+
+    // inline 
+
+    inline std::vector<Time>
+    MarketModelCoterminalSwapsOneStep::possibleCashFlowTimes() const {
+        return paymentTimes_;
+    }
+
+    inline Size MarketModelCoterminalSwapsOneStep::numberOfProducts() const {
+        return lastIndex_;    
+    }
+
+    inline Size
+    MarketModelCoterminalSwapsOneStep::maxNumberOfCashFlowsPerProductPerStep() const {
+        return 2*lastIndex_;
+    }
+
+    inline void MarketModelCoterminalSwapsOneStep::reset() {
+       // nothing to do 
+    }
+       
+}
+
+#endif
