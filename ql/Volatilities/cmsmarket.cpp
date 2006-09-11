a6673b97e6159b5bb8876e00b567a19703cf58ee
Giorgio Facchinetti <giorfa@users.sf.net>
2006-09-11 15:51:52 +0000

    work in progress ...

---
+++ ql/Volatilities/cmsmarket.cpp
@@ -0,0 +1,195 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2006 Giorgio Facchinetti
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/reference/license.html>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+
+#include <ql/Volatilities/cmsmarket.hpp>
+#include <ql/Volatilities/swaptionvolcube.hpp>
+#include <ql/Indexes/euriborswapfixa.hpp>
+
+#include <fstream>
+#include <string>
+             
+namespace QuantLib {
+
+    //===========================================================================//
+    //                                CmsMarket                                  //
+    //===========================================================================//
+
+    CmsMarket::CmsMarket(
+        const std::vector<Period>& expiries,
+        const std::vector<Period>& lengths,
+        const Matrix& bidsAsks,
+        const Matrix& meanReversions,
+        const boost::shared_ptr<VanillaCMSCouponPricer>& pricer,
+        const Handle<YieldTermStructure>& yieldTermStructure,
+        const Handle<SwaptionVolatilityStructure>& volStructure):
+    expiries_(expiries),
+    lengths_(lengths),
+    volStructure_(volStructure),
+    meanReversions_(meanReversions),
+    pricer_(pricer),
+    yieldTermStructure_(yieldTermStructure),
+    calendar_(TARGET()),
+    frequency_(Quarterly),
+    bdc_(Unadjusted),
+    stubDate_(0),
+    startFromEnd_(true), 
+    longFinal_(false),
+    dayCounter_(),
+    floatingIndex_(boost::shared_ptr<Xibor>(new Euribor3M(yieldTermStructure_))){
+
+        referenceDate_ = yieldTermStructure_->referenceDate();
+        effectiveDate_ = calendar_.advance(referenceDate_,2,Days,Following); //FIXME
+
+        nExercise_ = expiries_.size();
+        nLengths_ = lengths_.size();
+
+        QL_REQUIRE(2*nLengths_==bidsAsks.columns(),
+                   "2*nLengths_!=bidsAsks.columns()");
+        QL_REQUIRE(nExercise_==bidsAsks.rows(),
+                   "nExercise_==bidsAsks.rows()");
+
+        bids_ = Matrix(nExercise_, nLengths_, 0.);
+        asks_ = Matrix(nExercise_, nLengths_, 0.);
+        mids_ = Matrix(nExercise_, nLengths_, 0.);
+        impliedCmsSpreads_ = Matrix(nExercise_, nLengths_, 0.);
+
+        swapIndices_.push_back(boost::shared_ptr<SwapIndex>(new EuriborSwapFixA2Y(yieldTermStructure_)));
+        swapIndices_.push_back(boost::shared_ptr<SwapIndex>(new EuriborSwapFixA5Y(yieldTermStructure_)));
+        swapIndices_.push_back(boost::shared_ptr<SwapIndex>(new EuriborSwapFixA10Y(yieldTermStructure_)));
+        swapIndices_.push_back(boost::shared_ptr<SwapIndex>(new EuriborSwapFixA20Y(yieldTermStructure_)));
+        swapIndices_.push_back(boost::shared_ptr<SwapIndex>(new EuriborSwapFixA30Y(yieldTermStructure_)));
+
+        for (Size i=0; i<nExercise_; i++) {
+            exerciseDates_.push_back(calendar_.advance(referenceDate_,expiries[i],bdc_));//FIXME
+            schedules_.push_back( boost::shared_ptr<Schedule>(
+                new Schedule(calendar_, effectiveDate_, exerciseDates_[i], frequency_, 
+                            bdc_,stubDate_, startFromEnd_, longFinal_))
+            );
+            std::vector<Leg> cmsTmp;
+            std::vector<Leg> floatingTmp;
+            std::vector< boost::shared_ptr<Swap> > swapTmp;
+            for (Size j=0; j<nLengths_ ; j++) {
+                bids_[i][j] = bidsAsks[i][j*2];
+                asks_[i][j] = bidsAsks[i][j*2+1];
+                mids_[i][j] = (bids_[i][j]+asks_[i][j])/2;
+                Size nCoupons = schedules_[i]->size();
+                cmsTmp.push_back(
+                    CMSCouponVector(*(schedules_[i].get()),
+                                    bdc_, 
+                                    std::vector<double>(nCoupons,1.), 
+                                    swapIndices_[j], 
+                                    2, 
+                                    dayCounter_,
+                                    std::vector<double>(nCoupons, 0.),
+                                    std::vector<double>(nCoupons, 1.),
+                                    std::vector<double>(nCoupons, 1.),
+                                    std::vector<double>(nCoupons, 0.),
+                                    std::vector<double>(nCoupons, meanReversions_[i][j]),
+                                    pricer_,
+                                    volStructure_ )
+                );
+                floatingTmp.push_back(
+                    FloatingRateCouponVector(*(schedules_[i].get()),
+                                    floatingIndex_->businessDayConvention(),
+                                    std::vector<double>(nCoupons, 1.),
+                                    floatingIndex_->settlementDays(),
+                                    floatingIndex_,
+                                    std::vector<double>(nCoupons, 1.),
+                                    std::vector<double>(nCoupons, 0.),
+                                    floatingIndex_->dayCounter())
+                );
+                swapTmp.push_back(
+                    boost::shared_ptr<Swap>(new Swap(yieldTermStructure_, cmsTmp.back(), floatingTmp.back()))
+                );
+                impliedCmsSpreads_[i][j] = -swapTmp.back()->NPV()/swapTmp.back()->legBPS(1);
+                spreadErrors_[i][j] = impliedCmsSpreads_[i][j]-mids_[i][j];
+            }
+            cmsLegs_.push_back(cmsTmp);
+            floatingLegs_.push_back(floatingTmp);
+            swaps_.push_back(swapTmp);
+        }
+     }
+
+    Real CmsMarket::weightedError(const Matrix& weights){
+ 	    Real error=0.;
+	    Size count=0;
+	    for(Size i=0;i<nExercise_;i++){
+		    for(Size j=0;j<nLengths_;j++){
+			    count++;
+			    error+=weights[i][j]*spreadErrors_[i][j]*spreadErrors_[i][j];
+		    }
+	    }
+        error=std::sqrt(error/count);
+   	    return error;           
+    }
+
+
+
+    //===========================================================================//
+    //                       SmileAndCmsCalibrationBySabr                        //
+    //===========================================================================//
+
+    SmileAndCmsCalibrationBySabr::SmileAndCmsCalibrationBySabr(
+        SwaptionVolatilityCubeBySabr& volCube,
+        CmsMarket& cmsMarket,
+        const Matrix& weights):
+    volCube_(volCube),
+    cmsMarket_(cmsMarket),
+    weights_(weights){
+        tranformation_ = boost::shared_ptr<Transformation>(new transformationBeta);
+    }
+
+    Real SmileAndCmsCalibrationBySabr::calibration(){
+
+        NoConstraint constraint;
+        ObjectiveFunctionJustBeta costFunction(this);
+
+        boost::shared_ptr<LineSearch> lineSearch(
+            new ArmijoLineSearch(1e-12, 0.15, 0.55));
+        boost::shared_ptr<OptimizationMethod> method = 
+            boost::shared_ptr<OptimizationMethod>(new ConjugateGradient(lineSearch));
+
+        method->setEndCriteria(EndCriteria(10000, 1e-12));
+
+        Array guess(1);
+        guess[0] = .7;  
+
+        guess = tranformation_->inverse(guess);
+        method->setInitialValue(guess);
+
+        Problem problem(costFunction, constraint, *method);
+        problem.minimize();
+        Array result = problem.minimumValue();
+
+        Array y = tranformation_->direct(result);
+
+        return y[0];
+    }
+    //===========================================================================//
+    //       SmileAndCmsCalibrationBySabr::ObjectiveFunctionJustBeta             //
+    //===========================================================================//
+    Real SmileAndCmsCalibrationBySabr::ObjectiveFunctionJustBeta::value(const Array& x) const {
+
+
+        return cmsMarket_.weightedError(weights_);
+    }
+
+
+}
---
+++ ql/Volatilities/cmsmarket.hpp
@@ -0,0 +1,157 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2006 Giorgio Facchinetti
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/reference/license.html>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file cmsmarket.hpp
+*/
+
+#ifndef quantlib_cms_market_h
+#define quantlib_cms_market_h
+
+#include <ql/Volatilities/swaptionvolmatrix.hpp>
+#include <ql/Volatilities/swaptionvolcube.hpp>
+#include <ql/Volatilities/swaptionvolcubebysabr.hpp>
+#include <ql/CashFlows/cmscoupon.hpp>
+#include <ql/Indexes/euribor.hpp>
+#include <ql/Optimization/method.hpp>
+#include <ql/Optimization/problem.hpp>
+#include <ql/Optimization/conjugategradient.hpp>
+#include <ql/Optimization/simplex.hpp>
+
+namespace QuantLib {
+
+     typedef std::vector<boost::shared_ptr<CashFlow> > Leg;
+
+     class CmsMarket{
+
+      public:
+
+        CmsMarket(
+            const std::vector<Period>& expiries,
+            const std::vector<Period>& lengths,
+            const Matrix& bidsAsks,
+            const Matrix& meanReversions,
+            const boost::shared_ptr<VanillaCMSCouponPricer>& pricer,
+            const Handle<YieldTermStructure>& yieldTermStructure,
+            const Handle<SwaptionVolatilityStructure>& volStructure);
+
+        Matrix impliedCmsSpreads(){return impliedCmsSpreads_;};
+        Matrix spreadErrors(){return spreadErrors_;};
+        Real weightedError(const Matrix& weights);
+ 
+      private:
+         
+        std::vector<Period> expiries_;
+        std::vector<Period> lengths_;
+        Size nExercise_;
+        Size nLengths_;
+
+        Matrix bids_;
+        Matrix asks_;
+        Matrix mids_;
+        Matrix impliedCmsSpreads_;
+        Matrix spreadErrors_;
+
+        Date referenceDate_; 
+        Date effectiveDate_;
+        std::vector<Date> exerciseDates_;
+
+        Calendar calendar_;
+        Frequency frequency_;
+        BusinessDayConvention bdc_;
+        Date stubDate_;
+        bool startFromEnd_; 
+        bool longFinal_;
+        
+        DayCounter dayCounter_;
+        Matrix meanReversions_;
+        boost::shared_ptr<VanillaCMSCouponPricer> pricer_;
+
+        std::vector< boost::shared_ptr<const Schedule> > schedules_;
+        std::vector< boost::shared_ptr<SwapIndex> > swapIndices_;
+        boost::shared_ptr<Xibor> floatingIndex_;
+
+        std::vector< std::vector< Leg > > cmsLegs_;
+        std::vector< std::vector< Leg > > floatingLegs_;
+        std::vector< std::vector< boost::shared_ptr<Swap> > > swaps_;
+
+        Handle<SwaptionVolatilityStructure> volStructure_;
+        Handle<YieldTermStructure> yieldTermStructure_;
+
+    };
+
+     class SmileAndCmsCalibrationBySabr{
+        
+        class Transformation {
+          public:
+            virtual ~Transformation() {}
+            virtual Array direct(const Array& x) const = 0;
+            virtual Array inverse(const Array& x) const = 0;
+        };
+
+      public:
+
+        SmileAndCmsCalibrationBySabr(
+            SwaptionVolatilityCubeBySabr& volCube,
+            CmsMarket& cmsMarket,
+            const Matrix& weights);
+        
+        SwaptionVolatilityCubeBySabr& volCube_;
+        CmsMarket& cmsMarket_;
+        Matrix weights_;
+        boost::shared_ptr<Transformation> tranformation_;
+
+        Real calibration();
+
+      private:
+
+        class transformationBeta : public Transformation {
+             mutable Array y_;
+          public:
+
+             transformationBeta() : y_(Array(1)){ }
+
+             Array direct(const Array& x) const {
+                    y_[0] = std::exp(-(x[0]*x[0]));
+                    return y_;
+                }
+             Array inverse(const Array& x) const {
+                    y_[0] = std::sqrt(-std::log(x[1]));
+                       return y_;
+                }
+        };
+          
+        class ObjectiveFunctionJustBeta : public CostFunction {
+          public:
+            ObjectiveFunctionJustBeta(SmileAndCmsCalibrationBySabr* smileAndCms)
+                :volCube_(smileAndCms->volCube_),
+                cmsMarket_(smileAndCms->cmsMarket_),
+                weights_(smileAndCms->weights_){};
+
+                Real value(const Array& x) const;
+          private:
+            SwaptionVolatilityCubeBySabr& volCube_;
+            CmsMarket& cmsMarket_;
+            Matrix weights_;
+        };
+  
+    };
+
+}
+
+#endif
