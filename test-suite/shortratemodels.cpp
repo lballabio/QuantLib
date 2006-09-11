0e90fc4fd8ce0cca81996e4e049a361786687c15
Cristina Dominuco <cduminuco@users.sf.net>
2006-09-11 12:13:32 +0000

    using new tenor-based Schedule

--- test-suite/shortratemodels.cpp
+++ test-suite/shortratemodels.cpp
@@ -194,11 +194,12 @@ void ShortRateModelTest::testSwaps() {
         for (Size j=0; j<LENGTH(length); j++) {
 
             Date maturity = calendar.advance(startDate,length[i],Years);
-            Schedule fixedSchedule(calendar,startDate,maturity,
-                                   Annual,Unadjusted);
-            Schedule floatSchedule(calendar,startDate,maturity,
-                                   Semiannual,Following);
-
+            Schedule fixedSchedule(startDate,maturity,Period(Annual),
+                                   calendar,Unadjusted,Unadjusted,
+                                   false,false);
+            Schedule floatSchedule(startDate,maturity,Period(Semiannual),
+                                   calendar,Following,Following,
+                                   false,false);                     
             for (Size k=0; k<LENGTH(rates); k++) {
 
                 VanillaSwap swap(true, 1000000.0,
