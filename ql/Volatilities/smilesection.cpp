1f89f18ba46f4b90f2c6143b381cd0fb603e1398
Ferdinando Ametrano <nando@ametrano.net>
2006-10-25 07:37:12 +0000

    checks added

--- ql/Volatilities/smilesection.cpp
+++ ql/Volatilities/smilesection.cpp
@@ -49,7 +49,7 @@ namespace QuantLib {
     }
 
     SabrSmileSection::SabrSmileSection(const std::vector<Real>& sabrParameters,
-                            const Time timeToExpiry)
+                                       const Time timeToExpiry)
     : timeToExpiry_(timeToExpiry) {
 
         alpha_ = sabrParameters[0];
@@ -57,6 +57,19 @@ namespace QuantLib {
         nu_ = sabrParameters[2];
         rho_ = sabrParameters[3];
         forward_ = sabrParameters[4];
+
+        QL_REQUIRE(forward_>0.0, "forward must be positive: "
+                                << io::rate(forward_) << " not allowed");
+        QL_REQUIRE(timeToExpiry_>0.0, "expiry time must be positive: "
+                                   << timeToExpiry_ << " not allowed");
+        QL_REQUIRE(alpha_>0.0, "alpha must be positive: "
+                              << alpha_ << " not allowed");
+        QL_REQUIRE(beta_>=0.0 && beta_<=1.0, "beta must be in [0.0, 1.0]: "
+                                           << beta_ << " not allowed");
+        QL_REQUIRE(nu_>=0.0, "nu must be non negative: "
+                            << nu_ << " not allowed");
+        QL_REQUIRE(rho_*rho_<=1.0, "rho square must be not greater than one: "
+                                 << rho_ << " not allowed");
 
     }
 
