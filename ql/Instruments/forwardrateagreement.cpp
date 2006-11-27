92d28c1061d6267afc9ff703204682a4f17b0ee5
Marco Bianchetti <marcobianchetti@users.sf.net>
2006-11-27 15:09:24 +0000

    Exported method qlFRAisExpired

--- ql/Instruments/forwardrateagreement.cpp
+++ ql/Instruments/forwardrateagreement.cpp
@@ -7,7 +7,7 @@
  for financial quantitative analysts and developers - http://quantlib.org/
 
  QuantLib is free software: you can redistribute it and/or modify it
- under the terms of the QuantLib license.  You should have received a
+ under the terms of the QuantLib license. You should have received a
  copy of the license along with this program; if not, please email
  <quantlib-dev@lists.sf.net>. The license is also available online at
  <http://quantlib.org/reference/license.html>.
@@ -60,7 +60,6 @@ namespace QuantLib {
         underlyingIncome_ = 0.0;
         registerWith(index_);
     }
-
 
     Date ForwardRateAgreement::settlementDate() const {
         return calendar_.advance(Settings::instance().evaluationDate(),
