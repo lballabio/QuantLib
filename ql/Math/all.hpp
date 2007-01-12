c87799e89727b6bd62c3daec7f45f331e6eeb553
Ferdinando Ametrano <nando@ametrano.net>
2007-01-12 09:49:22 +0000

    *** empty log message ***

--- ql/Math/all.hpp
+++ ql/Math/all.hpp
@@ -24,7 +24,7 @@
 
 #include <ql/Math/core.hpp>
 
-#include <ql/Math/abcdflatinterpolation.hpp>
+//#include <ql/Math/abcdflatinterpolation.hpp>
 #include <ql/Math/backwardflatinterpolation.hpp>
 #include <ql/Math/beta.hpp>
 #include <ql/Math/bicubicsplineinterpolation.hpp>
