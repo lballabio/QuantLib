51f65e3a75b105ef9506006f3da0eb89ed415299
Chiara Fornarola <chiforna@users.sf.net>
2007-02-05 18:33:41 +0000

    created a new instrument: cappedflooredcouponbond to contruct reversefloater bond  objects there's still a bug to be fixed concerning day counter: cappedfloatercouponbond accepts  a daycounter in input bond when performing bond flow analysis it returns the daycount  of the underlying index, despite of the input value passed for day counter. this will be fixed with cristina in cappendflooredcoupon.

---
+++ ql/Instruments/cappedflooredcouponbond.cpp
@@ -0,0 +1,84 @@
+//* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+//
+//*
+// Copyright (C) 2007 Chiara Fornarola
+//
+// This file is part of QuantLib, a free-software/open-source library
+// for financial quantitative analysts and developers - http://quantlib.org/
+//
+// QuantLib is free software: you can redistribute it and/or modify it
+// under the terms of the QuantLib license.  You should have received a
+// copy of the license along with this program; if not, please email
+// <quantlib-dev@lists.sf.net>. The license is also available online at
+// <http://quantlib.org/reference/license.html>.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the license for more details.
+//*/
+
+#include <ql/Instruments/Bonds/cappedflooredcouponbond.hpp>
+#include <ql/CashFlows/capflooredcoupon.hpp>
+#include <ql/CashFlows/cashflowvectors.hpp>
+#include <ql/CashFlows/simplecashflow.hpp>
+
+namespace QuantLib {
+
+                CappedflooredCouponBond::CappedflooredCouponBond(
+                Real faceAmount,
+                const Date& issueDate,
+                const Date& datedDate,
+                const Date& maturityDate,
+                Integer settlementDays,
+                const boost::shared_ptr<IborIndex>& index,
+                Integer fixingDays,
+                const std::vector<Real>& gearings,
+                const std::vector<Spread>& spreads,
+                Frequency couponFrequency,
+                const Calendar& calendar,
+                const DayCounter& dayCounter,
+                const std::vector<Rate>& caps,
+                const std::vector<Rate>& floors,
+                BusinessDayConvention accrualConvention,
+                BusinessDayConvention paymentConvention,
+                Real redemption,
+                const Handle<CapletVolatilityStructure>& vol,
+                const Handle<YieldTermStructure>& discountCurve,
+                const Date& stub, bool fromEnd)
+    : Bond(faceAmount, dayCounter, calendar, accrualConvention,
+           paymentConvention, settlementDays, discountCurve) {
+
+        issueDate_ = issueDate;
+        datedDate_ = datedDate;
+        maturityDate_ = maturityDate;
+        frequency_ = couponFrequency;
+
+        Date firstDate = (fromEnd ? Date() : stub);
+        Date nextToLastDate = (fromEnd ? stub : Date());
+        Schedule schedule(datedDate, maturityDate, Period(couponFrequency), calendar,
+                          accrualConvention, accrualConvention, fromEnd, false, 
+                          firstDate, nextToLastDate);
+
+        // !!!
+        cashflows_ = CappedFlooredFloatingRateLeg(schedule,
+                                     std::vector<Real>(1, faceAmount_),
+                                     index,
+                                     dayCounter,
+                                     fixingDays,
+                                     paymentConvention,
+                                     gearings,
+                                     spreads,
+                                     caps,
+                                     floors,
+                                     vol);
+        // redemption
+        // !!!
+        Date redemptionDate =
+            calendar.adjust(maturityDate, paymentConvention);
+        cashflows_.push_back(boost::shared_ptr<CashFlow>(new
+            SimpleCashFlow(faceAmount_*redemption/100.0, redemptionDate)));
+
+        registerWith(index);
+    }
+
+}
---
+++ ql/Instruments/cappedflooredcouponbond.hpp
@@ -0,0 +1,69 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2007 Chiara Fornarola
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/reference/license.html>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file cappedflooredcouponbond.hpp
+    \brief reverse floater capped-floored coupon bond
+*/
+
+#ifndef quantlib_cappedfloored_coupon_bond_hpp
+#define quantlib_cappedfloored_coupon_bond_hpp
+
+#include <ql/Instruments/bond.hpp>
+#include <ql/Indexes/iborindex.hpp>
+#include <ql/CashFlows/capfloorlet.hpp>
+#include <ql/PricingEngines/core.hpp>
+
+namespace QuantLib {
+
+    //! capped & floored coupon bond
+    /*! \ingroup instruments
+
+        \test calculations are tested by checking results against
+              cached values.
+    */
+    class CappedFlooredCouponBond : public Bond {
+      public:
+          CappedFlooredCouponBond(Real faceAmount,
+                         const Date& issueDate,
+                         const Date& datedDate,
+                         const Date& maturityDate,
+                         Integer settlementDays,
+                         const boost::shared_ptr<IborIndex>& index,
+                         Integer fixingDays,
+                         const std::vector<Real>& gearings,
+                         const std::vector<Spread>& spreads,
+                         Frequency couponFrequency,
+                         const Calendar& calendar,
+                         const DayCounter& dayCounter,
+                         const std::vector<Rate>& caps,
+                         const std::vector<Rate>& floors,
+                         BusinessDayConvention accrualConvention = Following,
+                         BusinessDayConvention paymentConvention = Following,
+                         Real redemption = 100.0,
+                         const Handle<CapletVolatilityStructure>& vol =
+                            Handle<CapletVolatilityStructure>(),
+                         const Handle<YieldTermStructure>& discountCurve
+                                              = Handle<YieldTermStructure>(),
+                         const Date& stub = Date(),
+                         bool fromEnd = true);
+    };
+
+}
+
+#endif
