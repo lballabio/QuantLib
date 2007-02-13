6565b53e3fedaf5bcdc8ea36ffc9edc083e19e25
Chiara Fornarola <chiforna@users.sf.net>
2007-02-13 16:44:28 +0000

    computing drift under assumption of normal evolution of foward rates. an exercise  on the general methodology for computing drifts for rates

---
+++ ql/MarketModels/DriftComputation/lmmnormaldriftcalculator.hpp
@@ -0,0 +1,77 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2007 Giorgio Facchinetti
+ Copyright (C) 2007 Chiara Fornarola
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/reference/license.html>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file driftcalculator.hpp
+    \brief Drift computation for Libor Market Model with forwards evolving normally
+*/
+
+#ifndef quantlib_lmm_normal_drift_calculator_hpp
+#define quantlib_lmm_normal_drift_calculator_hpp
+
+#include <ql/Math/matrix.hpp>
+#include <vector>
+
+namespace QuantLib {
+
+    //! Drift computation for Libor Market Models
+    /*! \ingroup Market Models */
+
+    class LMMNormalDriftCalculator {
+      public:
+        /*! Returns the drift \f$ \mu \Delta t \f$.
+            See Mark Joshi, "\it {Rapid Computation of Drifts in a 
+            Reduced Factor Libor Market Model}", Wilmott Magazine, 
+            May 2003.
+        */
+        LMMNormalDriftCalculator(const Matrix& pseudo,
+                       const std::vector<Spread>& displacements,
+                       const std::vector<Time>& taus,
+                       Size numeraire,
+                       Size alive);
+        //! Computes the drifts
+        void compute(const std::vector<Rate>& forwards,
+                     std::vector<Real>& drifts) const;
+
+        /*! Computes the drifts without factor reduction as in 
+            eqs. 2, 4 of ref. [1] (uses the covariance matrix directly). */
+        void computePlain(const std::vector<Rate>& forwards,
+                          std::vector<Real>& drifts) const;
+
+        /*! Computes the drifts with factor reduction as in eq. 7 of ref. [1]
+            (uses pseudo square root of the covariance matrix). */
+        void computeReduced(const std::vector<Rate>& forwards,
+                            std::vector<Real>& drifts) const;
+
+      private:
+        Size dim_, factors_;
+        bool isFullFactor_;
+        Size numeraire_, alive_;
+        std::vector<Spread> displacements_;
+        std::vector<Real> oneOverTaus_;
+        Matrix C_, pseudo_;
+        // temporary variables to be added later
+        mutable std::vector<Real> tmp_;
+        mutable Matrix e_;
+        std::vector<Size> downs_, ups_;
+    };
+
+}
+
+#endif
---
+++ ql/MarketModels/Evolvers/forwardratenormalpcevolver.hpp
@@ -0,0 +1,70 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2007 Giorgio Facchinetti
+ Copyright (C) 2007 Chiara Fornarola
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/reference/license.html>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+
+#ifndef quantlib_forward_rate_normal_pc_evolver_hpp
+#define quantlib_forward_rate_normal_pc_evolver_hpp
+
+#include <ql/MarketModels/marketmodelevolver.hpp>
+#include <ql/MarketModels/marketmodel.hpp>
+#include <ql/MarketModels/evolutiondescription.hpp>
+#include <ql/MarketModels/browniangenerator.hpp>
+#include <ql/MarketModels/DriftComputation/lmmnormaldriftcalculator.hpp>
+
+namespace QuantLib {
+
+    //! Predictor-Corrector
+    class ForwardRateNormalPcEvolver : public MarketModelEvolver {
+      public:
+        ForwardRateNormalPcEvolver(const boost::shared_ptr<MarketModel>&,
+                             const BrownianGeneratorFactory&,
+                             const std::vector<Size>& numeraires,
+                             Size initialStep = 0);
+        //! \name MarketModelEvolver interface
+        //@{
+        const std::vector<Size>& numeraires() const;
+        Real startNewPath();
+        Real advanceStep();
+        Size currentStep() const;
+        const CurveState& currentState() const;
+        void setInitialState(const CurveState&);
+        //@}
+      private:
+        void setForwards(const std::vector<Real>& forwards);
+        // inputs
+        boost::shared_ptr<MarketModel> marketModel_;
+        std::vector<Size> numeraires_;
+        Size initialStep_;
+        boost::shared_ptr<BrownianGenerator> generator_;
+         // working variables
+        Size n_, F_;
+        CurveState curveState_;
+        Size currentStep_;
+        std::vector<Rate> forwards_, displacements_;
+        std::vector<Real> drifts1_, drifts2_, initialDrifts_;
+        Array brownians_, correlatedBrownians_;
+        std::vector<Size> alive_;
+        // helper classes
+        std::vector<LMMNormalDriftCalculator> calculators_;
+    };
+
+}
+
+#endif
