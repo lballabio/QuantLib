9e8e9a122537ae79f8ad14c3e540c07191c7c7cd
Ferdinando Ametrano <nando@ametrano.net>
2007-02-16 15:50:21 +0000

    formatting

--- ql/MarketModels/Evolvers/forwardratepcevolver.cpp
+++ ql/MarketModels/Evolvers/forwardratepcevolver.cpp
@@ -80,8 +80,8 @@ namespace QuantLib {
         QL_REQUIRE(forwards.size()==n_,
                    "mismatch between forwards and rateTimes");
         for (Size i=0; i<n_; ++i) 
-            initialLogForwards_[i] = std::log(forwards[i] +
-                                              displacements_[i]);
+             initialLogForwards_[i] = std::log(forwards[i] +
+                                               displacements_[i]);
         calculators_[initialStep_].compute(forwards, initialDrifts_);
     }
 
