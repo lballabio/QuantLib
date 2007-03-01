5004bd7921be71de2a18088d0ef1cd60d9c1f72c
Luigi Ballabio <luigi.ballabio@gmail.com>
2007-03-01 14:04:13 +0000

    *** empty log message ***

--- ql/Instruments/cappedflooredcouponbond.cpp
+++ ql/Instruments/cappedflooredcouponbond.cpp
@@ -1,21 +1,21 @@
-//* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-//
-//*
-// Copyright (C) 2007 Chiara Fornarola
-//
-// This file is part of QuantLib, a free-software/open-source library
-// for financial quantitative analysts and developers - http://quantlib.org/
-//
-// QuantLib is free software: you can redistribute it and/or modify it
-// under the terms of the QuantLib license.  You should have received a
-// copy of the license along with this program; if not, please email
-// <quantlib-dev@lists.sf.net>. The license is also available online at
-// <http://quantlib.org/reference/license.html>.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE.  See the license for more details.
-//*/
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2007 Chiara Fornarola
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/reference/license.html>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
 
 #include <ql/Instruments/cappedflooredcouponbond.hpp>
 #include <ql/CashFlows/capflooredcoupon.hpp>
@@ -67,7 +67,7 @@ namespace QuantLib {
         Date firstDate = (fromEnd ? Date() : stub);
         Date nextToLastDate = (fromEnd ? stub : Date());
         Schedule schedule(datedDate, maturityDate, Period(couponFrequency), calendar,
-                          accrualConvention, accrualConvention, fromEnd, false, 
+                          accrualConvention, accrualConvention, fromEnd, false,
                           firstDate, nextToLastDate);
 
         cashflows_ = IborLeg(schedule,
