a083f1ddcaebf6ef89028058be606326e5e20805
Ferdinando Ametrano <nando@ametrano.net>
2007-03-01 16:22:53 +0000

    copyright attributions

--- ql/Instruments/cmscouponbond.cpp
+++ ql/Instruments/cmscouponbond.cpp
@@ -1,7 +1,7 @@
 /* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
- Copyright (C) 2006 Chiara Fornarola
+ Copyright (C) 2006, 2007 Chiara Fornarola
  Copyright (C) 2007 StatPro Italia srl
 
  This file is part of QuantLib, a free-software/open-source library
