598ed578c9746186b9d5527c5f16d05286d03f76
Ferdinando Ametrano <nando@ametrano.net>
2007-03-06 13:46:51 +0000

    Weekday enumeration in its own file

--- ql/date.hpp
+++ ql/date.hpp
@@ -29,75 +29,12 @@
 #define quantlib_date_hpp
 
 #include <ql/period.hpp>
+#include <ql/weekday.hpp>
 #include <ql/types.hpp>
 #include <utility>
 #include <functional>
 
 namespace QuantLib {
-
-    /*! Day's serial number MOD 7;
-        WEEKDAY Excel function is the same except for Sunday = 7.
-
-        \ingroup datetime
-    */
-    enum Weekday { Sunday    = 1,
-                   Monday    = 2,
-                   Tuesday   = 3,
-                   Wednesday = 4,
-                   Thursday  = 5,
-                   Friday    = 6,
-                   Saturday  = 7,
-                   Sun = 1,
-                   Mon = 2,
-                   Tue = 3,
-                   Wed = 4,
-                   Thu = 5,
-                   Fri = 6,
-                   Sat = 7
-    };
-
-    /*! \relates Weekday */
-    std::ostream& operator<<(std::ostream&, const Weekday&);
-
-    namespace detail {
-
-        struct long_weekday_holder {
-            long_weekday_holder(Weekday d) : d(d) {}
-            Weekday d;
-        };
-        std::ostream& operator<<(std::ostream&, const long_weekday_holder&);
-
-        struct short_weekday_holder {
-            short_weekday_holder(Weekday d) : d(d) {}
-            Weekday d;
-        };
-        std::ostream& operator<<(std::ostream&, const short_weekday_holder&);
-
-        struct shortest_weekday_holder {
-            shortest_weekday_holder(Weekday d) : d(d) {}
-            Weekday d;
-        };
-        std::ostream& operator<<(std::ostream&,
-                                 const shortest_weekday_holder&);
-
-    }
-
-    namespace io {
-
-        //! output weekdays in long format
-        /*! \ingroup manips */
-        detail::long_weekday_holder long_weekday(Weekday);
-
-        //! output weekdays in short format (three letters)
-        /*! \ingroup manips */
-        detail::short_weekday_holder short_weekday(Weekday);
-
-        //! output weekdays in shortest format (two letters)
-        /*! \ingroup manips */
-        detail::shortest_weekday_holder shortest_weekday(Weekday);
-
-    }
-
 
     //! Month names
     /*! \ingroup datetime */
