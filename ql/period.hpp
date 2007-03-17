385fbb1d1cf42a818c50a2bed1d507936542cbe0
Joseph Wang <drjoe@users.sf.net>
2007-03-17 05:18:09 +0000

    changed the calling arguments to operator * so that there is one call to integer * Period and one call to unsigned * Period.  This removes the call to size_t.  Hopefully, this will compile on all compilers.  Let me know if it doesn't.

--- ql/period.hpp
+++ ql/period.hpp
@@ -53,7 +53,6 @@ namespace QuantLib {
 
     /*! \relates Period */
     Period operator*(Integer n, TimeUnit units);
-    Period operator*(Size n, TimeUnit units);
     Period operator*(Natural n, TimeUnit units);
     /*! \relates Period */
     Period operator*(TimeUnit units, Integer n);
@@ -114,12 +113,10 @@ namespace QuantLib {
         return Period(n,units);
     }
 
-    inline Period operator*(Size n, TimeUnit units) {
+    inline Period operator*(Natural n, TimeUnit units) {
         return Period(static_cast<Integer>(n),units);
     }
-   /* inline Period operator*(Natural n, TimeUnit units) {
-        return Period(static_cast<Integer>(n),units);
-    }*/
+
     inline Period operator*(TimeUnit units, Integer n) {
         return Period(n,units);
     }
