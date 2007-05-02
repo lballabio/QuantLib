6e68b2546e28992858ddcbe25b1c533ad88d79fe
Ferdinando Ametrano <nando@ametrano.net>
2007-05-02 18:07:48 +0000

    updating copyright attributions

--- ql/index.hpp
+++ ql/index.hpp
@@ -2,6 +2,7 @@
 
 /*
  Copyright (C) 2007 Ferdinando Ametrano
+ Copyright (C) 2007 Chiara Fornarola
  Copyright (C) 2000, 2001, 2002, 2003 RiskMap srl
  Copyright (C) 2003, 2004, 2005, 2006 StatPro Italia srl
 
