ba6171ce2e26b7e03f4fa1b8a626171104ab41de
Mark Joshi <markjoshi@users.sf.net>
2007-06-01 07:19:33 +0000

    code to  turn incomplete ordered basis into orthonormal basis, i.e. GramSchmidt

---
+++ ql/math/matrixutilities/basisincompletedordered.cpp
@@ -0,0 +1,86 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2007 Mark Joshi
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+
+#include <ql/math/matrixutilities/basisincompleteordered.hpp>
+#include <algorithm>
+
+namespace QuantLib {
+
+	basisincompleteordered::basisincompleteordered(Size euclideanDimension)
+		: euclideanDimension_(euclideanDimension)
+	{
+	}
+
+	bool basisincompleteordered::addVector(const Array& newVector1)
+	{
+			  QL_REQUIRE(newVector.size() == euclideanDimension_, 
+				"missized vector passed to basisincompleteordered::addVector");
+
+			  newVector = newVector1;
+    
+			  if (currentBasis_.size()==euclideanDimension_)
+				  return false;
+			 
+			  for (Size j=0; j < currentBasis_.size(); ++j)
+				{
+					Real innerProd = std::inner_product(newVector.begin(),newVector.end(), currentBasis_[j].begin(), 0.0);
+
+					for (Size k=0; k < euclideanDimension_; ++k)
+						newVector[k] -=innerProd*currentBasis_[j][k];
+
+				}
+
+			  	Real norm =sqrt(std::inner_product(newVector.begin(), newVector.end(), newVector.begin(),0.0));
+
+				if (norm ==0.0) // maybe this should be a tolerance
+					return false; 
+
+				for (Size l=0; l < euclideanDimension_; ++l)
+					newVector[l]/=norm;
+
+				currentBasis_.push_back(newVector);
+
+				return true;
+	}
+
+	Size basisincompleteordered::basisSize() const
+	{
+		   return currentBasis_.size();
+	}
+
+	Size basisincompleteordered::euclideanDimension() const
+	{
+			return euclideanDimension_;
+	}
+
+	
+	Matrix basisincompleteordered::getBasisAsRowsInMatrix() const
+	{
+		Matrix basis(currentBasis_.size(), euclideanDimension_);
+		for (Size i=0; i < basis.rows(); ++i)
+			for (Size j=0; j < basis.columns(); ++j)
+				basis[i][j] = currentBasis_[i][j];
+		
+		return basis;
+     
+	}
+
+}
+
---
+++ ql/math/matrixutilities/basisincompleteordered.hpp
@@ -0,0 +1,50 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2007 Mark Joshi
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+
+#ifndef quantlib_basis_incompleted_ordered_hpp
+#define quantlib_basis_incompleted_ordered_hpp
+
+#include <ql/math/matrix.hpp>
+
+namespace QuantLib {
+
+	class basisincompleteordered
+	{
+	public:
+		basisincompleteordered(Size euclideanDimension);
+		bool addVector(const Array& newVector); // return value indicates if the vector was linearly independent
+
+		Size basisSize() const; 
+		Size euclideanDimension() const;
+
+		Matrix getBasisAsRowsInMatrix() const;
+
+	private:
+		
+		std::vector<Array> currentBasis_;
+		Size euclideanDimension_;
+		Array newVector;
+
+	};
+
+}
+
+
+#endif
