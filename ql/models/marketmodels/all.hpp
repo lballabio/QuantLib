cbfc37d821fe56ad8ffda39dd8c229d3c002f66e
Luigi Ballabio <luigi.ballabio@gmail.com>
2007-06-12 09:18:26 +0000

    Updated gcc build

--- ql/models/marketmodels/all.hpp
+++ ql/models/marketmodels/all.hpp
@@ -9,7 +9,9 @@
 #include <ql/models/marketmodels/duffsdeviceinnerproduct.hpp>
 #include <ql/models/marketmodels/evolutiondescription.hpp>
 #include <ql/models/marketmodels/evolver.hpp>
+#include <ql/models/marketmodels/historicalcorrelation.hpp>
 #include <ql/models/marketmodels/marketmodel.hpp>
+#include <ql/models/marketmodels/marketmodeldifferences.hpp>
 #include <ql/models/marketmodels/multiproduct.hpp>
 #include <ql/models/marketmodels/piecewiseconstantcorrelation.hpp>
 #include <ql/models/marketmodels/proxygreekengine.hpp>
