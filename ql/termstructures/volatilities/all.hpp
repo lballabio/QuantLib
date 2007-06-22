1b11d49cadfc360fc4b6974f20831c21afb2ccb7
Luigi Ballabio <luigi.ballabio@gmail.com>
2007-06-22 13:49:15 +0000

    Added new files to gcc build

--- ql/termstructures/volatilities/all.hpp
+++ ql/termstructures/volatilities/all.hpp
@@ -2,6 +2,7 @@
 /* Add the files to be included into Makefile.am instead. */
 
 #include <ql/termstructures/volatilities/abcd.hpp>
+#include <ql/termstructures/volatilities/abcdcalibration.hpp>
 #include <ql/termstructures/volatilities/interpolatedsmilesection.hpp>
 #include <ql/termstructures/volatilities/sabr.hpp>
 #include <ql/termstructures/volatilities/sabrinterpolatedsmilesection.hpp>
