9767aef80369ce0e004902485afa7c44bfb44c0d
Klaus Spanderen <klaus@spanderen.de>
2007-07-31 22:00:52 +0000

    added AnalyticHestonHullWhiteEngine

---
+++ ql/pricingengines/vanilla/analytichestonhullwhiteengine.cpp
@@ -0,0 +1,68 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2007 Klaus Spanderen
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+#include <ql/pricingengines/vanilla/analytichestonhullwhiteengine.hpp>
+
+namespace QuantLib {
+ 
+    AnalyticHestonHullWhiteEngine::AnalyticHestonHullWhiteEngine(
+        const boost::shared_ptr<HestonModel> & hestonModel,
+        const boost::shared_ptr<HullWhite> & hullWhiteModel,
+        Size integrationOrder)
+    : AnalyticHestonEngine(hestonModel, integrationOrder),
+      hullWhiteModel_(hullWhiteModel) {
+
+        update();
+        registerWith(hullWhiteModel_);
+    }
+
+    void AnalyticHestonHullWhiteEngine::update() {
+        a_ = hullWhiteModel_->params()[0];
+        sigma_ = hullWhiteModel_->params()[1];
+
+        AnalyticHestonEngine::update();
+    }
+
+    void AnalyticHestonHullWhiteEngine::calculate() const {
+        // do some pre calculation
+        boost::shared_ptr<HestonProcess> process =
+            boost::dynamic_pointer_cast<HestonProcess>(
+                                                arguments_.stochasticProcess);
+
+        QL_REQUIRE(process, "A Heston process is required");
+
+        const Real t = process->time(arguments_.exercise->lastDate());
+        if (a_*t > std::pow(QL_EPSILON, 0.25)) {
+            m_ = sigma_*sigma_/(2*a_*a_)
+                *(t+2/a_*std::exp(-a_*t)-1/(2*a_)*std::exp(-2*a_*t)-3/(2*a_));
+        }
+        else {
+            // low-a algebraic limit
+            m_ = 0.5*sigma_*sigma_*t*t*t*(1/3.0-0.25*a_*t+7/60.0*a_*a_*t*t);
+        }
+
+        AnalyticHestonEngine::calculate();
+    }
+
+    std::complex<Real> AnalyticHestonHullWhiteEngine::addOnTerm(
+        Real u, Time t, Size j) const {
+
+        return std::complex<Real>(-m_*u*u, u*(m_-2*m_*(j-1)));
+    }
+}
---
+++ ql/pricingengines/vanilla/analytichestonhullwhiteengine.hpp
@@ -0,0 +1,85 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2007 Klaus Spanderen
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file analytichestonhullwhiteengine.hpp
+    \brief analytic heston engine incl. stochastic interest rates
+*/
+
+#ifndef quantlib_analytic_heston_hull_white_engine_hpp
+#define quantlib_analytic_heston_hull_white_engine_hpp
+
+#include <ql/models/equity/hestonmodel.hpp>
+#include <ql/models/shortrate/onefactormodels/hullwhite.hpp>
+#include <ql/pricingengines/vanilla/analytichestonengine.hpp>
+
+namespace QuantLib {
+
+    //! Analytic Heston engine incl. stochastic interest rates
+    /*! this class prices a european options under the following processes
+
+        \f[
+        \begin{array}{rcl}
+        dS(t, S)  &=& (r-d-\lambda m) S dt +\sqrt{v} S dW_1 + (e^J - 1) S dN \\
+        dv(t, S)  &=& \kappa (\theta - v) dt + \sigma \sqrt{v} dW_2 \\
+        dr(t)     &=& (\theta(t) - a r) dt + \eta dW_3 \\
+        dW_1 dW_2 &=& \rho dt \\
+        dW_1 dW_3 &=& 0 \\
+        dW_2 dW_3 &=& 0 \\
+        \end{array}
+        \f]
+
+        References:
+
+        Karel in't Hout, Joris Bierkens, Antoine von der Ploeg, 
+        Joe in't Panhuis, A Semi closed-from analytic pricing formula for
+        call options in a hybrid Heston-Hull-White Model.
+
+        A. Sepp, Pricing European-Style Options under Jump Diffusion
+        Processes with Stochastic Volatility: Applications of Fourier
+        Transform (<http://math.ut.ee/~spartak/papers/stochjumpvols.pdf>)
+
+        \ingroup vanillaengines
+
+        \test the correctness of the returned value is tested by
+              reproducing results available in web/literature, testing
+              against QuantLib's analytic Heston and 
+              Black-Scholes-Merton Hull-White engine
+    */
+    class AnalyticHestonHullWhiteEngine : public AnalyticHestonEngine {
+      public:
+        AnalyticHestonHullWhiteEngine(
+            const boost::shared_ptr<HestonModel> & hestonModel,
+            const boost::shared_ptr<HullWhite> & hullWhiteModel,
+            Size integrationOrder = 64);
+
+        void update();
+        void calculate() const; 
+
+      protected:
+        std::complex<Real> addOnTerm(Real phi, Time t, Size j) const;
+
+      private:
+        const boost::shared_ptr<HullWhite> hullWhiteModel_;
+
+        mutable Real m_;
+        mutable Real a_, sigma_;
+    };
+}
+
+#endif
