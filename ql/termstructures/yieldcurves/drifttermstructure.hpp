dd0612f3ec27bac411767642d6501c230bf49680
Joseph Wang <drjoe@users.sf.net>
2007-08-06 02:05:11 +0000

    Modify to deal with the renaming of some fo the termstructure header files

--- ql/termstructures/yieldcurves/drifttermstructure.hpp
+++ ql/termstructures/yieldcurves/drifttermstructure.hpp
@@ -25,7 +25,7 @@
 #define quantlib_drift_term_structure_hpp
 
 #include <ql/termstructures/yieldcurves/zeroyieldstructure.hpp>
-#include <ql/termstructures/voltermstructure.hpp>
+#include <ql/termstructures/blackvoltermstructure.hpp>
 
 namespace QuantLib {
 
