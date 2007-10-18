8086a3c181375243280833146c84aa20ba0a617b
Luigi Ballabio <luigi.ballabio@gmail.com>
2007-10-18 13:53:30 +0000

    Added histogram class (thanks to Gang Liang)

--- ql/math/statistics/all.hpp
+++ ql/math/statistics/all.hpp
@@ -5,6 +5,7 @@
 #include <ql/math/statistics/discrepancystatistics.hpp>
 #include <ql/math/statistics/gaussianstatistics.hpp>
 #include <ql/math/statistics/generalstatistics.hpp>
+#include <ql/math/statistics/histogram.hpp>
 #include <ql/math/statistics/incrementalstatistics.hpp>
 #include <ql/math/statistics/riskstatistics.hpp>
 #include <ql/math/statistics/sequencestatistics.hpp>
