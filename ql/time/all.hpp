cf4487db32f1a2e7881678d13a80164913f668a9
Luigi Ballabio <luigi.ballabio@gmail.com>
2007-11-23 17:09:41 +0000

    Added new files to gcc build

--- ql/time/all.hpp
+++ ql/time/all.hpp
@@ -4,6 +4,7 @@
 #include <ql/time/businessdayconvention.hpp>
 #include <ql/time/calendar.hpp>
 #include <ql/time/date.hpp>
+#include <ql/time/dategenerationrule.hpp>
 #include <ql/time/daycounter.hpp>
 #include <ql/time/frequency.hpp>
 #include <ql/time/imm.hpp>
