a4939ff3113881b9265fc4dd35545120ef6d68d3
Luigi Ballabio <luigi.ballabio@gmail.com>
2007-11-27 10:52:25 +0000

    Added new files to gcc build

--- ql/quotes/all.hpp
+++ ql/quotes/all.hpp
@@ -4,6 +4,7 @@
 #include <ql/quotes/compositequote.hpp>
 #include <ql/quotes/derivedquote.hpp>
 #include <ql/quotes/eurodollarfuturesquote.hpp>
+#include <ql/quotes/forwardswapquote.hpp>
 #include <ql/quotes/forwardvaluequote.hpp>
 #include <ql/quotes/futuresconvadjustmentquote.hpp>
 #include <ql/quotes/impliedstddevquote.hpp>
