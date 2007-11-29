7a38c78c44f4c306cd3c8e9f9df7a9094fa14337
Ferdinando Ametrano <nando@ametrano.net>
2007-11-29 13:35:33 +0000

    gcc and VC7 catching up (DevCpp not updated...)

--- ql/termstructures/volatility/optionlet/all.hpp
+++ ql/termstructures/volatility/optionlet/all.hpp
@@ -6,6 +6,7 @@
 #include <ql/termstructures/volatility/optionlet/constantoptionletvol.hpp>
 #include <ql/termstructures/volatility/optionlet/strippedoptionletadapter.hpp>
 #include <ql/termstructures/volatility/optionlet/optionletstripper.hpp>
+#include <ql/termstructures/volatility/optionlet/strippedoptionlet.hpp>
 #include <ql/termstructures/volatility/optionlet/strippedoptionletbase.hpp>
 #include <ql/termstructures/volatility/optionlet/optionletstripper1.hpp>
 #include <ql/termstructures/volatility/optionlet/optionletstripper2.hpp>
