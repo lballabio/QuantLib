4435d92e7b1a1cfc4e7d18a290759875f0ba7010
Luigi Ballabio <luigi.ballabio@gmail.com>
2008-01-04 17:08:54 +0000

    Compilation fix for gcc

--- ql/termstructures/volatility/swaption/swaptionvolstructure.hpp
+++ ql/termstructures/volatility/swaption/swaptionvolstructure.hpp
@@ -1,9 +1,9 @@
 /* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
- Copyright (C) 2006 Ferdinando Ametrano
  Copyright (C) 2002, 2003 RiskMap srl
  Copyright (C) 2003, 2004, 2005, 2006 StatPro Italia srl
+ Copyright (C) 2006 Ferdinando Ametrano
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
@@ -227,7 +227,7 @@ namespace QuantLib {
         checkSwapTenor(swapLength, extrapolate);
         checkRange(optionTime, extrapolate);
         return smileSectionImpl(optionTime, swapLength);
-    };
+    }
 
     // 4. default implementation of Date-based xxxImpl methods
     //    relying on the equivalent Time-based methods
