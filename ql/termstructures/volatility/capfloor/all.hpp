e0727911910568a0a09fb8ac5a97c36fa51f742b
Luigi Ballabio <luigi.ballabio@gmail.com>
2008-01-09 10:09:51 +0000

    Added new files to gcc build

--- ql/termstructures/volatility/capfloor/all.hpp
+++ ql/termstructures/volatility/capfloor/all.hpp
@@ -4,4 +4,5 @@
 #include <ql/termstructures/volatility/capfloor/capfloortermvolatilitystructure.hpp>
 #include <ql/termstructures/volatility/capfloor/capfloortermvolcurve.hpp>
 #include <ql/termstructures/volatility/capfloor/capfloortermvolsurface.hpp>
+#include <ql/termstructures/volatility/capfloor/constantcapfloortermvol.hpp>
 
