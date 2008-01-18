0d66df4e5c49d632413b5109de38f87f4fea918b
Luigi Ballabio <luigi.ballabio@gmail.com>
2008-01-18 12:56:21 +0000

    Increased version number

--- ql/qldefines.hpp
+++ ql/qldefines.hpp
@@ -57,15 +57,15 @@
 
 //! version string
 #ifdef QL_DEBUG
-    #define QL_VERSION "0.9.0-debug"
+    #define QL_VERSION "0.9.5-debug"
 #else
-    #define QL_VERSION "0.9.0"
+    #define QL_VERSION "0.9.5"
 #endif
 
 //! version hexadecimal number
-#define QL_HEX_VERSION 0x000900f0
+#define QL_HEX_VERSION 0x000905f0
 //! version string for output lib name
-#define QL_LIB_VERSION "0_9_0"
+#define QL_LIB_VERSION "0_9_5"
 
 #if   defined(HAVE_CONFIG_H)    // Dynamically created by configure
    #include <ql/config.hpp>
