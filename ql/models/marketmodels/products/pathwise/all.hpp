973bd7050a2d766930eec326496cad0212c10f07
Luigi Ballabio <luigi.ballabio@gmail.com>
2008-08-19 08:39:29 +0000

    Added new files to autotools build

--- ql/models/marketmodels/products/pathwise/all.hpp
+++ ql/models/marketmodels/products/pathwise/all.hpp
@@ -2,4 +2,5 @@
 /* Add the files to be included into Makefile.am instead. */
 
 #include <ql/models/marketmodels/products/pathwise/pathwiseproductcaplet.hpp>
+#include <ql/models/marketmodels/products/pathwise/pathwiseproductswaption.hpp>
 
