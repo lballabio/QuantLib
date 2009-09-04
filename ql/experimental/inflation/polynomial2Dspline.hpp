936a563e67b5fbf088d9a2c669bc188ad6a8f094
Luigi Ballabio <luigi.ballabio@gmail.com>
2009-09-04 12:40:54 +0000

    Fixed misspelled name.

--- ql/experimental/inflation/polynomial2Dspline.hpp
+++ ql/experimental/inflation/polynomial2Dspline.hpp
@@ -3,7 +3,7 @@
 /*
  Copyright (C) 2003 Ferdinando Ametrano
  Copyright (C) 2004 StatPro Italia srl
- Copyright (C) 2009 Berndt Engelmann
+ Copyright (C) 2009 Bernd Engelmann
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
