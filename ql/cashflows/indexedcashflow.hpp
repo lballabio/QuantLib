8a46d90590d61253ffeb841371525bd30780342f
Luigi Ballabio <luigi.ballabio@gmail.com>
2009-10-19 14:52:43 +0000

    Renamed variables for consistency with the rest of the library.

--- ql/cashflows/indexedcashflow.hpp
+++ ql/cashflows/indexedcashflow.hpp
@@ -2,16 +2,16 @@
 
 /*
  Copyright (C) 2009 Chris Kenyon
- 
+
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
- 
+
  QuantLib is free software: you can redistribute it and/or modify it
  under the terms of the QuantLib license.  You should have received a
  copy of the license along with this program; if not, please email
  <quantlib-dev@lists.sf.net>. The license is also available online at
  <http://quantlib.org/license.shtml>.
- 
+
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the license for more details.
@@ -27,53 +27,57 @@
 #include <ql/cashflow.hpp>
 
 namespace QuantLib {
-	class Index;
-	
+
+    class Index;
+
     //! Cash flow dependent on an index ratio (NOT a coupon, i.e. no accruals)
-    /*! Cash flow dependent on an index ratio (NOT a coupon, i.e. no accruals).
-		We expect this to be used inside an istrument that does all the date
-		adjustment etc., so this takes just dates and does not change them.
-	 */
+    /*! We expect this to be used inside an istrument that does all the date
+        adjustment etc., so this takes just dates and does not change them.
+     */
     class IndexedCashFlow : public CashFlow {
-	public:
-        IndexedCashFlow(Real notional, const boost::shared_ptr<Index> &index,
-                        const Date& baseDate, const Date& fixDate, const Date& payDate)
-        : notional_(notional), index_(index), baseDate_(baseDate), fixDate_(fixDate), 
-		  payDate_(payDate) {}
+      public:
+        IndexedCashFlow(Real notional,
+                        const boost::shared_ptr<Index> &index,
+                        const Date& baseDate,
+                        const Date& fixingDate,
+                        const Date& paymentDate)
+        : notional_(notional), index_(index),
+          baseDate_(baseDate), fixingDate_(fixingDate),
+          paymentDate_(paymentDate) {}
         //! \name Event interface
         //@{
-        Date date() const { return payDate_; }
+        Date date() const { return paymentDate_; }
         //@}
-		virtual Real notional() const { return notional_; }
-		virtual Date baseDate() const { return baseDate_; }
-		virtual Date fixDate() const { return fixDate_; }
-		virtual boost::shared_ptr<Index> index() const { return index_; }
+        virtual Real notional() const { return notional_; }
+        virtual Date baseDate() const { return baseDate_; }
+        virtual Date fixingDate() const { return fixingDate_; }
+        virtual boost::shared_ptr<Index> index() const { return index_; }
         //! \name CashFlow interface
         //@{
-        Real amount() const;	// already virtual
+        Real amount() const;    // already virtual
         //@}
         //! \name Visitability
         //@{
         virtual void accept(AcyclicVisitor&);
         //@}
-	private:
+    private:
         Real notional_;
-		boost::shared_ptr<Index> index_;
-        Date baseDate_, fixDate_, payDate_;
+        boost::shared_ptr<Index> index_;
+        Date baseDate_, fixingDate_, paymentDate_;
     };
-	
-	
+
+
     // inline definitions
-	
+
     inline void IndexedCashFlow::accept(AcyclicVisitor& v) {
         Visitor<IndexedCashFlow>* v1 =
-		dynamic_cast<Visitor<IndexedCashFlow>*>(&v);
+        dynamic_cast<Visitor<IndexedCashFlow>*>(&v);
         if (v1 != 0)
             v1->visit(*this);
         else
             CashFlow::accept(v);
     }
-	
+
 
 }
 
