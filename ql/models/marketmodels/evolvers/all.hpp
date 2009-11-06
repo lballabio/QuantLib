f9f8038a67611ffb9e2df470faef6f7f5d0ad1d2
Luigi Ballabio <luigi.ballabio@gmail.com>
2009-11-06 08:48:29 +0000

    Added new files to autotools build.

--- ql/models/marketmodels/evolvers/all.hpp
+++ ql/models/marketmodels/evolvers/all.hpp
@@ -6,6 +6,7 @@
 #include <ql/models/marketmodels/evolvers/lognormalfwdrateballand.hpp>
 #include <ql/models/marketmodels/evolvers/lognormalfwdrateeuler.hpp>
 #include <ql/models/marketmodels/evolvers/lognormalfwdrateeulerconstrained.hpp>
+#include <ql/models/marketmodels/evolvers/lognormalfwdrateiballand.hpp>
 #include <ql/models/marketmodels/evolvers/lognormalfwdrateipc.hpp>
 #include <ql/models/marketmodels/evolvers/lognormalfwdratepc.hpp>
 #include <ql/models/marketmodels/evolvers/marketmodelvolprocess.hpp>
