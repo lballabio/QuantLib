84c4d577368b0141b3fbffac0a0cf6e08a2292e7
Luigi Ballabio <luigi.ballabio@gmail.com>
2009-11-24 11:19:14 +0000

    Reverted change to avoid binary dependency on Boost.Date.

--- ql/time/date.cpp
+++ ql/time/date.cpp
@@ -24,12 +24,8 @@
 #include <ql/time/date.hpp>
 #include <ql/utilities/dataformatters.hpp>
 #include <ql/errors.hpp>
-
-#include <boost/date_time/gregorian/gregorian.hpp>
-
 #include <iomanip>
 #include <ctime>
-
 #if defined(BOOST_NO_STDC_NAMESPACE)
     namespace std { using ::time; using ::time_t; using ::tm;
                     using ::gmtime; using ::localtime; }
@@ -201,7 +197,74 @@ namespace QuantLib {
     }
 
     bool Date::isLeap(Year y) {
-        return boost::gregorian::gregorian_calendar::is_leap_year(y);
+        static const bool YearIsLeap[] = {
+            // 1900 is leap in agreement with Excel's bug
+            // 1900 is out of valid date range anyway
+            // 1900-1909
+             true,false,false,false, true,false,false,false, true,false,
+            // 1910-1919
+            false,false, true,false,false,false, true,false,false,false,
+            // 1920-1929
+             true,false,false,false, true,false,false,false, true,false,
+            // 1930-1939
+            false,false, true,false,false,false, true,false,false,false,
+            // 1940-1949
+             true,false,false,false, true,false,false,false, true,false,
+            // 1950-1959
+            false,false, true,false,false,false, true,false,false,false,
+            // 1960-1969
+             true,false,false,false, true,false,false,false, true,false,
+            // 1970-1979
+            false,false, true,false,false,false, true,false,false,false,
+            // 1980-1989
+             true,false,false,false, true,false,false,false, true,false,
+            // 1990-1999
+            false,false, true,false,false,false, true,false,false,false,
+            // 2000-2009
+             true,false,false,false, true,false,false,false, true,false,
+            // 2010-2019
+            false,false, true,false,false,false, true,false,false,false,
+            // 2020-2029
+             true,false,false,false, true,false,false,false, true,false,
+            // 2030-2039
+            false,false, true,false,false,false, true,false,false,false,
+            // 2040-2049
+             true,false,false,false, true,false,false,false, true,false,
+            // 2050-2059
+            false,false, true,false,false,false, true,false,false,false,
+            // 2060-2069
+             true,false,false,false, true,false,false,false, true,false,
+            // 2070-2079
+            false,false, true,false,false,false, true,false,false,false,
+            // 2080-2089
+             true,false,false,false, true,false,false,false, true,false,
+            // 2090-2099
+            false,false, true,false,false,false, true,false,false,false,
+            // 2100-2109
+            false,false,false,false, true,false,false,false, true,false,
+            // 2110-2119
+            false,false, true,false,false,false, true,false,false,false,
+            // 2120-2129
+             true,false,false,false, true,false,false,false, true,false,
+            // 2130-2139
+            false,false, true,false,false,false, true,false,false,false,
+            // 2140-2149
+             true,false,false,false, true,false,false,false, true,false,
+            // 2150-2159
+            false,false, true,false,false,false, true,false,false,false,
+            // 2160-2169
+             true,false,false,false, true,false,false,false, true,false,
+            // 2170-2179
+            false,false, true,false,false,false, true,false,false,false,
+            // 2180-2189
+             true,false,false,false, true,false,false,false, true,false,
+            // 2190-2199
+            false,false, true,false,false,false, true,false,false,false,
+            // 2200
+            false
+        };
+        QL_REQUIRE(y>=1900 && y<=2200, "year outside valid range");
+        return YearIsLeap[y-1900];
     }
 
 
