3dc10f60dbb5c1a20076cf118a585f43886f5606
Luigi Ballabio <luigi.ballabio@gmail.com>
2010-11-29 14:55:37 +0000

    Regenerated all.hpp file with ordered headers.

--- ql/termstructures/volatility/optionlet/all.hpp
+++ ql/termstructures/volatility/optionlet/all.hpp
@@ -4,12 +4,11 @@
 #include <ql/termstructures/volatility/optionlet/capletvariancecurve.hpp>
 #include <ql/termstructures/volatility/optionlet/constantoptionletvol.hpp>
 #include <ql/termstructures/volatility/optionlet/optionletstripper.hpp>
-#include <ql/termstructures/volatility/optionlet/strippedoptionlet.hpp>
-#include <ql/termstructures/volatility/optionlet/strippedoptionletbase.hpp>
 #include <ql/termstructures/volatility/optionlet/optionletstripper1.hpp>
 #include <ql/termstructures/volatility/optionlet/optionletstripper2.hpp>
 #include <ql/termstructures/volatility/optionlet/optionletvolatilitystructure.hpp>
 #include <ql/termstructures/volatility/optionlet/spreadedoptionletvol.hpp>
+#include <ql/termstructures/volatility/optionlet/strippedoptionlet.hpp>
 #include <ql/termstructures/volatility/optionlet/strippedoptionletadapter.hpp>
 #include <ql/termstructures/volatility/optionlet/strippedoptionletbase.hpp>
 
