eec3cd0becd27ed4a7ebda385dae75a54db972ef
Luigi Ballabio <luigi.ballabio@gmail.com>
2013-06-14 11:20:41 +0200

    Updated list of copyright holders.
    Also, a few copyright attributions were cleaned up of typos
    and alternative spellings.

--- ql/termstructures/yield/oisratehelper.cpp
+++ ql/termstructures/yield/oisratehelper.cpp
@@ -2,7 +2,7 @@
 
 /*
  Copyright (C) 2009, 2012 Roland Lichters
- Copyright (C) 2009. 2012 Ferdinando Ametrano
+ Copyright (C) 2009, 2012 Ferdinando Ametrano
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
