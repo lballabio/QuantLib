6c6720b05b6097c9f3ce0b97ad8f68164a104a39
klausspanderen <klaus@spanderen.de>
2013-09-14 16:51:39 +0200

    merge from trunk

--- ql/experimental/processes/gemanroncoroniprocess.hpp
+++ ql/experimental/processes/gemanroncoroniprocess.hpp
@@ -61,9 +61,13 @@ namespace QuantLib {
         Real evolve(Time t0, Real x0, Time dt, Real dw) const;
 
 #ifdef __clang__
+#pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Woverloaded-virtual"
 #endif
         Real evolve(Time t0, Real x0, Time dt, Real dw, const Array& du) const;
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
         
     private:
         const Real x0_;
