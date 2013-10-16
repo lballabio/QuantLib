88b4bc02399dfc741f058b0831a1f48f1465b9df
Peter Caspers <pcaspers1973@gmail.com>
2013-10-16 09:54:40 +0200

    allow for negative strikes (down to -displacement)

--- ql/pricingengines/blackformula.cpp
+++ ql/pricingengines/blackformula.cpp
@@ -32,15 +32,16 @@ namespace {
                          QuantLib::Real forward,
                          QuantLib::Real displacement)
     {
-        QL_REQUIRE(strike>=0.0,
-                   "strike (" << strike << ") must be non-negative");
+        QL_REQUIRE(displacement >= 0.0, "displacement ("
+                                            << displacement
+                                            << ") must be non-negative");
+        QL_REQUIRE(strike + displacement >= 0.0,
+                   "strike + displacement (" << strike << " + " << displacement
+                                             << ") must be non-negative");
         QL_REQUIRE(forward + displacement > 0.0, "forward + displacement ("
                                                      << forward << " + "
                                                      << displacement
                                                      << ") must be positive");
-        QL_REQUIRE(displacement >= 0.0, "displacement ("
-                                            << displacement
-                                            << ") must be non-negative");
     }
 }
 
