74739f5901df5e393bc895c51d1a21008dad3266
klausspanderen <klaus@spanderen.de>
2013-12-16 23:10:44 +0100

    added b=0 case to Levy engine for Asian options improved case runningAvg >> forward for FDM Asian engine

--- ql/experimental/exoticoptions/continuousarithmeticasianlevyengine.cpp
+++ ql/experimental/exoticoptions/continuousarithmeticasianlevyengine.cpp
@@ -72,9 +72,10 @@ namespace QuantLib {
         Rate dividendYield = process_->dividendYield()->
             zeroRate(maturity, divdc, Continuous, NoFrequency);
         Real b = riskFreeRate - dividendYield;
-        QL_REQUIRE(b != 0.0, "null cost of carry not allowed by Levy engine");
 
-        Real Se = (spot/(T*b))*(exp((b-riskFreeRate)*T2)-exp(-riskFreeRate*T2));
+        Real Se = (std::fabs(b) > 1000*QL_EPSILON) 
+			? (spot/(T*b))*(exp((b-riskFreeRate)*T2)-exp(-riskFreeRate*T2))
+			: spot*T2/T * std::exp(-riskFreeRate*T2);
 
         Real X;
         if (T2 < T) {
@@ -85,9 +86,11 @@ namespace QuantLib {
             X = strike;
         }
 
+		Real m = (std::fabs(b) > 1000*QL_EPSILON) ? ((exp(b*T2)-1)/b) : T2;
+
         Real M = (2*spot*spot/(b+volatility*volatility)) *
             (((exp((2*b+volatility*volatility)*T2)-1)
-              / (2*b+volatility*volatility))-((exp(b*T2)-1)/b));
+              / (2*b+volatility*volatility))-m);
 
         Real D = M/(T*T);
 
