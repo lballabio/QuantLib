fb9f2c2dd1ff9658b01bcb04ed897d78e25ceeb9
Luigi Ballabio <luigi.ballabio@gmail.com>
2014-02-02 17:47:23 +0100

    Update list of copyright holders.

--- test-suite/piecewisezerospreadedtermstructure.cpp
+++ test-suite/piecewisezerospreadedtermstructure.cpp
@@ -1,6 +1,6 @@
 
 /*
- Copyright (C) 2014 RiskMap srl
+ Copyright (C) 2014 StatPro Italia srl
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
