9c209158f25d32a9be76cc201fb2eaadb53ef14f
Pepe <japari@free.fr>
2014-03-01 22:23:01 +0100

    missing clean up

--- ql/experimental/math/multidimquadrature.cpp
+++ ql/experimental/math/multidimquadrature.cpp
@@ -21,19 +21,12 @@
 
 namespace QuantLib {
 
-    /* a higher than this number of dimension would presumably be impractical
-       and another integration algorithm (MC) should be considered.
-       Is this a library limit to define at library configuration?
-    */
-    const Size GaussianQuadMultidimIntegrator::maxNumFactors_ = 30; // move to header
-
     GaussianQuadMultidimIntegrator::GaussianQuadMultidimIntegrator(
         Size dimension, Size quadOrder, Real mu) 
         : integral_(quadOrder, mu),
           integralV_(quadOrder, mu),
           integrationEntries_(maxNumFactors_),
-  /////        integrationEntriesV_(maxNumFactors_),
-integrationEntriesVR_(maxNumFactors_),
+          integrationEntriesVR_(maxNumFactors_),
           dimension_(dimension),
           integrandFctrs(dimension_, 0.)
         {
