895e3057bf64c765f2ab50168ec5063898e0eda1
Peter Caspers <pcaspers1973@gmail.com>
2014-05-25 16:16:17 +0200

    fix refresh of reversion zero indicator

--- ql/experimental/models/gsrprocess.hpp
+++ ql/experimental/models/gsrprocess.hpp
@@ -81,7 +81,7 @@ namespace QuantLib {
         mutable std::map<std::pair<Real, Real>, Real> cache1_, cache2_, cache3_,
             cache5_;
         mutable std::map<Real, Real> cache4_;
-        std::vector<bool> revZero_;
+        mutable std::vector<bool> revZero_;
     };
 }
 
