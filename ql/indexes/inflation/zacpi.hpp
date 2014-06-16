ed29c6e938eb80fd0ef206927c2b49d28f10491c
U-MUD\FrancoisBo <FrancoisBo@DST00964.mud.internal.co.za>
2014-06-16 17:01:22 +0200

    Add ZACPI inflation index

---
+++ ql/indexes/inflation/zacpi.hpp
@@ -0,0 +1,87 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2007, 2009 Chris Kenyon
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file ukrpi.hpp
+    \brief %ZACPI index
+*/
+
+#ifndef quantlib_zacpi_hpp
+#define quantlib_zacpi_hpp
+
+#include <ql/indexes/inflationindex.hpp>
+#include <ql/currencies/africa.hpp>
+
+namespace QuantLib {
+
+    //! UK Retail Price Inflation Index
+    class ZACPI : public ZeroInflationIndex {
+      public:
+        ZACPI(bool interpolated,
+              const Handle<ZeroInflationTermStructure>& ts =
+                    Handle<ZeroInflationTermStructure>())
+        : ZeroInflationIndex("CPI",
+                             ZARegion(),
+                             false,
+                             interpolated,
+                             Monthly,
+                             Period(1, Months),
+                             ZARCurrency(),
+                             ts) {}
+    };
+
+
+    //! Genuine year-on-year UK RPI (i.e. not a ratio of UK RPI)
+    class YYUKRPI : public YoYInflationIndex {
+      public:
+        YYUKRPI(bool interpolated,
+                const Handle<YoYInflationTermStructure>& ts =
+                        Handle<YoYInflationTermStructure>())
+        : YoYInflationIndex("YY_CPI",
+                            ZARegion(),
+                            false,
+                            interpolated,
+                            false,
+                            Monthly,
+                            Period(1, Months),
+                            ZARCurrency(),
+                            ts) {}
+    };
+
+
+    //! Fake year-on-year UK RPI (i.e. a ratio of UK RPI)
+    class YYUKRPIr : public YoYInflationIndex {
+      public:
+        YYUKRPIr(bool interpolated,
+                 const Handle<YoYInflationTermStructure>& ts =
+                        Handle<YoYInflationTermStructure>())
+        : YoYInflationIndex("YYR_CPI",
+                            ZARegion(),
+                            false,
+                            interpolated,
+                            true,
+                            Monthly,
+                            Period(1, Months),
+                            ZARCurrency(),
+                            ts) {}
+    };
+
+}
+
+
+#endif
