cf21e3865713c4c2e0a78a92c946bdf7e52c17b0
rglarix <rghetta@larix.it>
2014-09-22 17:21:51 +0200

    binary barrier (non digital) options

--- ql/pricingengines/barrier/all.hpp
+++ ql/pricingengines/barrier/all.hpp
@@ -2,6 +2,7 @@
 /* Add the files to be included into Makefile.am instead. */
 
 #include <ql/pricingengines/barrier/analyticbarrierengine.hpp>
+#include <ql/pricingengines/barrier/analyticbinarybarrierengine.hpp>
 #include <ql/pricingengines/barrier/fdblackscholesbarrierengine.hpp>
 #include <ql/pricingengines/barrier/fdblackscholesrebateengine.hpp>
 #include <ql/pricingengines/barrier/fdhestonbarrierengine.hpp>
