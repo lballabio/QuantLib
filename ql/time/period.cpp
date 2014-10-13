6dbfd5b525b69c4a025eaff740e48b377fb10570
Paolo Mazzocchi <mazzocchip@live.it>
2014-10-13 18:11:05 +0200

    bug fix: 7 business days are not equal to 1 week

--- ql/time/period.cpp
+++ ql/time/period.cpp
@@ -1,7 +1,7 @@
 /* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
- Copyright (C) 2004, 2005, 2006, 2007, 2008 Ferdinando Ametrano
+ Copyright (C) 2004, 2005, 2006, 2007, 2008, 2014 Ferdinando Ametrano
  Copyright (C) 2006 Katiuscia Manzoni
  Copyright (C) 2000, 2001, 2002, 2003 RiskMap srl
  Copyright (C) 2003, 2004, 2005, 2006, 2008 StatPro Italia srl
@@ -107,18 +107,14 @@ namespace QuantLib {
     void Period::normalize() {
         if (length_!=0)
             switch (units_) {
-              case Days:
-                if (!(length_%7)) {
-                    length_/=7;
-                    units_ = Weeks;
-                }
-                break;
+              // bug fix: 7 business days are not equal to 1 week
               case Months:
                 if (!(length_%12)) {
                     length_/=12;
                     units_ = Years;
                 }
                 break;
+              case Days:
               case Weeks:
               case Years:
                 break;
