527071bb1fe2eb323650f082687756901870a68a
Luigi Ballabio <luigi.ballabio@gmail.com>
2015-03-25 14:34:58 +0100

    Increase tolerance in test.
    The previous value was slightly exceeded on March 25th, 2015.

--- test-suite/digitalcoupon.cpp
+++ test-suite/digitalcoupon.cpp
@@ -345,7 +345,7 @@ void DigitalCouponTest::testAssetOrNothingDeepInTheMoney() {
         targetPrice = underlying->price(vars.termStructure) + targetOptionPrice ;
         digitalPrice = digitalFlooredCoupon.price(vars.termStructure);
         error = std::fabs(targetPrice - digitalPrice);
-        tolerance = 2.0e-06;
+        tolerance = 2.5e-06;
         if (error>tolerance)
             BOOST_ERROR("\nFloating Coupon + Digital Put Option:" <<
                         "\nVolatility = " << io::rate(capletVolatility) <<
@@ -359,7 +359,7 @@ void DigitalCouponTest::testAssetOrNothingDeepInTheMoney() {
         replicationOptionPrice = digitalFlooredCoupon.putOptionRate() *
                                  vars.nominal * accrualPeriod * discount;
         error = std::abs(targetOptionPrice - replicationOptionPrice);
-        optionTolerance = 2.0e-06;
+        optionTolerance = 2.5e-06;
         if (error>optionTolerance)
             BOOST_ERROR("\nDigital Put Option:" <<
                         "\nVolatility = " << io::rate(capletVolatility) <<
