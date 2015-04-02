6fc42a7ecc468664749a576baf73498a68af3cca
Ferdinando Ametrano <nando@ametrano.net>
2015-04-02 18:48:53 +0200

    updated copyright

--- ql/time/period.cpp
+++ ql/time/period.cpp
@@ -5,6 +5,7 @@
  Copyright (C) 2006 Katiuscia Manzoni
  Copyright (C) 2000, 2001, 2002, 2003 RiskMap srl
  Copyright (C) 2003, 2004, 2005, 2006, 2008 StatPro Italia srl
+ Copyright (C) 2014 Paolo Mazzocchi
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
