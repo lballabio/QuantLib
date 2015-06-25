6379c55035aee5840c95df5ecdf7c97b49d32dd1
Paolo Mazzocchi <mazzocchip@live.it>
2015-06-25 11:20:52 +0200

    added copyright

--- ql/termstructures/volatility/abcdcalibration.hpp
+++ ql/termstructures/volatility/abcdcalibration.hpp
@@ -1,7 +1,7 @@
 /* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
- Copyright (C) 2006, 2007 Ferdinando Ametrano
+ Copyright (C) 2006, 2007, 2015 Ferdinando Ametrano
  Copyright (C) 2006 Cristina Duminuco
  Copyright (C) 2007 Giorgio Facchinetti
  Copyright (C) 2015 Paolo Mazzocchi
