37941d371f2ba316f4e74b1e4392c28007cbc072
Javier G. Sogo <jgsogo@gmail.com>
2016-10-10 11:49:38 +0200

    compile using conan-package-tools

---
+++ build.py
@@ -0,0 +1,11 @@
+from conan.packager import ConanMultiPackager
+import os
+
+
+username = os.getenv("CONAN_USERNAME", "jgsogo")
+
+
+if __name__ == "__main__":
+    builder = ConanMultiPackager(username=username, stable_branch_pattern='1.9')
+    builder.add_common_builds()
+    builder.run()
--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class QuantLibConan(ConanFile):
     name = "QuantLib"
     settings = "os", "compiler", "build_type", "arch"
     version = get_version()
-    exports = "*"
+    exports = ["cmake/*", "ql/*", "CMakeLists.txt", "Examples/*", "test-suite/*",]
     requires = "Boost/1.60.0@lasote/stable"
     generators = "cmake"
 
@@ -19,9 +19,13 @@ class QuantLibConan(ConanFile):
         self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", dst="include")
+        self.copy("cmake/FindQuantlib.cmake", ".", ".")
+        
+        self.copy(pattern="*", dst="ql", src="ql")
         self.copy("*.lib", dst="lib", src="lib")
         self.copy("*.a", dst="lib", src="lib")
+        self.copy("*.bin", dst="bin", src="bin")
+        self.copy("*.so", dst="bin", src="bin")
 
     def package_info(self):
         self.cpp_info.libs = ["QuantLib"]
