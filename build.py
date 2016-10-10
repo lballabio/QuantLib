047d59d80637faa33f0321c90640544c279e510b
Javier G. Sogo <jgsogo@gmail.com>
2016-10-10 21:43:17 +0200

    do nos use vs10

--- build.py
+++ build.py
@@ -6,6 +6,9 @@ username = os.getenv("CONAN_USERNAME", "jgsogo")
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username=username, stable_branch_pattern='1.9')
+    builder = ConanMultiPackager(username=username, 
+                                 stable_branch_pattern='1.9',
+                                 visual_versions=["12", "14", "15"],
+                                 gcc_versions=["4.8", "4.9", "5.2", "5.3"])
     builder.add_common_builds()
     builder.run()
