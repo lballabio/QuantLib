55a1a6b285518b2ff812c2630a51b4777cec1cc9
Javier G. Sogo <jgsogo@gmail.com>
2016-10-12 18:29:20 +0200

    do not build tests or examples when packaging

--- conanfile.py
+++ conanfile.py
@@ -9,14 +9,19 @@ class QuantLibConan(ConanFile):
     name = "QuantLib"
     settings = "os", "compiler", "build_type", "arch"
     version = get_version()
-    exports = "*"
+    exports = "CMakeLists.txt", "cmake/*", "ql/*", "Examples/*", "test-suite/*", "*.TXT", "README.md"
     requires = "Boost/1.60.0@lasote/stable"
     generators = "cmake"
 
+    options = {"examples": [True, False], "tests": [True, False]}
+    default_options = "examples=False", "tests=False"
+
     def build(self):
         cmake = CMake(self.settings)
-        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        build_examples = "-DBUILD_EXAMPLES:BOOL={}".format("ON" if self.options.examples else "OFF")
+        build_tests = "-DBUILD_TESTS:BOOL={}".format("ON" if self.options.tests else "OFF")
+        self.run('cmake {} {} {} {}'.format(self.conanfile_directory, cmake.command_line, build_examples, build_tests))
+        self.run("cmake --build . {}".format(cmake.build_config))
 
     def package(self):
         self.copy(pattern="*.cmake", src="cmake", dst=".")
