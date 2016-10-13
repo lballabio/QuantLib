b9c288779bc0927ab98348b917868c9a8175296e
Javier G. Sogo <jgsogo@gmail.com>
2016-10-13 23:10:52 +0200

    use QuantLib genuine example to test package

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -1,7 +1,7 @@
 
-#include <iostream>
 
-int main(void) {
-    std::cout << "Hello QuantLib" << std::endl;
-}
+// Use an existing example from QuantLib/Examples directory to test
+// that Conan is able to find the library and link to it.
+.
+#include "../Examples/Repo/Repo.cpp"
 
