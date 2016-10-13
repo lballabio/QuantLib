24481bbad76225b7696c2abc9401790ffd7de49b
Javier G. Sogo <jgsogo@gmail.com>
2016-10-13 12:20:25 +0200

    only enable MT (fix prev commit)

--- build.py
+++ build.py
@@ -8,6 +8,6 @@ username = os.getenv("CONAN_USERNAME", "jgsogo")
 if __name__ == "__main__":
     builder = ConanMultiPackager(username=username, 
                                  stable_branch_pattern='1.9',
-                                 visual_runtimes=["MD", "MDd"])
+                                 visual_runtimes=["MT", "MTd"])
     builder.add_common_builds()
     builder.run()
