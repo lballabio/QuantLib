c41b59f48aeb41a286b0a7a2ddcc6b056e04c64c
Javier G. Sogo <jgsogo@gmail.com>
2016-10-13 23:25:08 +0200

    get version and name in a centrlized way

--- build.py
+++ build.py
@@ -1,12 +1,15 @@
-from conan.packager import ConanMultiPackager
 import os
+from conan.packager import ConanMultiPackager
+from conanfile import QuantLibConan
 
 
+reference = "{}/{}".format(QuantLibConan.name, QuantLibConan.version)
 username = os.getenv("CONAN_USERNAME", "jgsogo")
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username=username, 
+    builder = ConanMultiPackager(username=username,
+                                 reference=reference,
                                  stable_branch_pattern='1.9',
                                  visual_runtimes=["MT", "MTd"])
     builder.add_common_builds()
