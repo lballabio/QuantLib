4f0d92dfb1f400aace70a52bcbc4302ac705ca0d
klausspanderen <klaus@spanderen.de>
2017-01-14 17:12:25 +0100

    added GMRES iterative solver for large linear systems

--- ql/math/matrixutilities/gmres.hpp
+++ ql/math/matrixutilities/gmres.hpp
@@ -1,8 +1,7 @@
 /* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
- Copyright (C) 2009 Ralph Schreyer
- Copyright (C) 2009 Klaus Spanderen
+ Copyright (C) 2017 Klaus Spanderen
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
@@ -18,37 +17,57 @@
  FOR A PARTICULAR PURPOSE.  See the license for more details.
 */
 
-/*! \file bicgstab.hpp
-    \brief bi-conjugated gradient stableized algorithm
+/*! \file gmres.hpp
+    \brief generalized minimal residual method
 */
 
-#ifndef quantlib_bicgstab_hpp
-#define quantlib_bicgstab_hpp
+#ifndef quantlib_gmres_hpp
+#define quantlib_gmres_hpp
 
 #include <ql/math/array.hpp>
 #include <boost/function.hpp>
 
+#include <list>
+
 namespace QuantLib {
 
-    struct BiCGStabResult {
-        Size iterations;
-        Real error;
+    /*! References:
+        Saad, Yousef. 1996, Iterative methods for sparse linear systems,
+        http://www-users.cs.umn.edu/~saad/books.html
+
+        Dongarra et al. 1994,
+        Templates for the Solution of Linear Systems: Building Blocks
+        for Iterative Methods, 2nd Edition, SIAM, Philadelphia
+        http://http://www.netlib.org/templates/templates.pdf
+
+        Christian Kanzow
+        Numerik linearer Gleichungssysteme (German)
+        Chapter 6: GMRES und verwandte Verfahren
+        http://bilder.buecher.de/zusatz/12/12950/12950560_lese_1.pdf
+    */
+
+    struct GMRESResult {
+        std::list<Real> errors;
         Array x;
     };
 
-    class BiCGstab  {
+    class GMRES  {
       public:
         typedef boost::function1<Disposable<Array> , const Array& > MatrixMult;
-        
-        BiCGstab(const MatrixMult& A, Size maxIter, Real relTol,
+
+        GMRES(const MatrixMult& A, Size maxIter, Real relTol,
                  const MatrixMult& preConditioner = MatrixMult());
-        
-        BiCGStabResult solve(const Array& b, const Array& x0 = Array()) const;
-        
+
+        GMRESResult solve(const Array& b, const Array& x0 = Array()) const;
+        GMRESResult solveWithRestart(
+            Size restart, const Array& b, const Array& x0 = Array()) const;
+
       protected:
+        GMRESResult solveImpl(const Array& b, const Array& x0) const;
+
         const MatrixMult A_, M_;
         const Size maxIter_;
-        const Real relTol_;  
+        const Real relTol_;
 
         static Real norm2(const Array& a);
     };
