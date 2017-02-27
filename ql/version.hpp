73bbaca9a87514a1d16ebfb233968ffdfb58a627
Luigi Ballabio <luigi.ballabio@gmail.com>
2017-02-27 14:41:17 +0100

    Bump version number to 1.9.3.

--- ql/version.hpp
+++ ql/version.hpp
@@ -31,15 +31,15 @@
 
 //! version string
 #ifdef QL_DEBUG
-    #define QL_VERSION "1.9.2-debug"
+    #define QL_VERSION "1.9.3-debug"
 #else
-    #define QL_VERSION "1.9.2"
+    #define QL_VERSION "1.9.3"
 #endif
 
 //! version hexadecimal number
-#define QL_HEX_VERSION 0x010902f0
+#define QL_HEX_VERSION 0x010903f0
 //! version string for output lib name
-#define QL_LIB_VERSION "1_9_2"
+#define QL_LIB_VERSION "1_9_3"
 
 /*! @}  */
 
