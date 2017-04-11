0e85f03841068bc5e58bc7ec56f03613231ecf09
Luigi Ballabio <luigi.ballabio@gmail.com>
2017-04-11 12:13:12 +0200

    Update copyrights.

--- ql/pricingengines/swaption/blackswaptionengine.cpp
+++ ql/pricingengines/swaption/blackswaptionengine.cpp
@@ -6,7 +6,8 @@
  Copyright (C) 2001, 2002, 2003 Sadruddin Rejeb
  Copyright (C) 2006, 2007 StatPro Italia srl
  Copyright (C) 2015, 2016, 2017 Peter Caspers
- Copyright (C) 2017 Paul Giltinan, Werner Kuerzinger
+ Copyright (C) 2017 Paul Giltinan
+ Copyright (C) 2017 Werner Kuerzinger
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
