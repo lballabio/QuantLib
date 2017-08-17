e11d679b8bd78dfbb6c0cadbe662c8ca4f451d48
alas <alix.lassauzet@derivexperts.com>
2017-08-17 18:03:28 +0200

    merge with Nicholas works

--- ql/time/calendars/hongkong.cpp
+++ ql/time/calendars/hongkong.cpp
@@ -284,18 +284,46 @@ namespace QuantLib {
 
         if (y == 2018) {
             if (// Lunar New Year
-                ((d == 16 && m == February) || (d == 19 && m == February))
-                // Ching Ming Festival
-                || (d == 5 && m == April)
-                // Buddha's birthday
-                || (d == 22 && m == May)
-                // Tuen NG festival
-                || (d == 18 && m == June)
-                // Mid-autumn festival
-                || (d == 25 && m == September)
-                // Chung Yeung festival
-                || (d == 17 && m == October))
+                    ((d == 16 && m == February) || (d == 19 && m == February))
+                    // Ching Ming Festival
+                    || (d == 5 && m == April)
+                    // Buddha's birthday
+                    || (d == 22 && m == May)
+                    // Tuen NG festival
+                    || (d == 18 && m == June)
+                    // Mid-autumn festival
+                    || (d == 25 && m == September)
+                    // Chung Yeung festival
+                    || (d == 17 && m == October))
                 return false;
+        }
+
+        if (y == 2019) {
+           if (// Lunar New Year
+              ((d == 5 && m == February) || (d == 6 && m == February) || (d == 7 && m == February))
+              // Ching Ming Festival
+              || (d == 5 && m == April)
+              // Tuen Ng festival
+              || (d == 7 && m == June)
+              // Chung Yeung festival
+              || (d == 7 && m == October))
+              return false;
+        }
+
+        if (y == 2020) {
+           if (// Lunar New Year
+              ((d == 27 && m == January) || (d == 28 && m == January))
+              // Ching Ming Festival
+              || (d == 4 && m == April)
+              // Buddha's birthday
+              || (d == 30 && m == April)
+              // Tuen Ng festival
+              || (d == 25 && m == June)
+              // Mid-autumn festival
+              || (d == 2 && m == October)
+              // Chung Yeung festival
+              || (d == 26 && m == October))
+              return false;
         }
 
         return true;
