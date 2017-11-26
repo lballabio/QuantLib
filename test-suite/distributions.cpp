2ff095315c32152dd20558ef47f7bfa50067df8b
klausspanderen <klaus@spanderen.de>
2017-11-26 01:25:16 +0100

    adapt parallel unit test runner for the upcoming boost 1.66 release

--- test-suite/distributions.cpp
+++ test-suite/distributions.cpp
@@ -33,7 +33,14 @@
 #include <ql/math/comparison.hpp>
 #include <ql/math/functional.hpp>
 
+#if defined(__GNUC__) && !defined(__clang__) && BOOST_VERSION > 106300
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
 #include <boost/math/distributions/non_central_chi_squared.hpp>
+#if defined(__GNUC__) && !defined(__clang__) && BOOST_VERSION > 106300
+#pragma GCC diagnostic pop
+#endif
 
 using namespace QuantLib;
 using namespace boost::unit_test_framework;
