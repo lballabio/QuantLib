f062230c67cf7e2995ab549466da44356526f2c1
Sebastian Schlenkrich <sebastian.schlenkrich@d-fine.de>
2018-12-06 16:31:47 +0100

    more fixes for makefile

--- ql/experimental/all.hpp
+++ ql/experimental/all.hpp
@@ -24,6 +24,7 @@
 #include <ql/experimental/shortrate/all.hpp>
 #include <ql/experimental/swaptions/all.hpp>
 #include <ql/experimental/termstructures/all.hpp>
+#include <ql/experimental/vanillalocalvolmodel/all.hpp>
 #include <ql/experimental/variancegamma/all.hpp>
 #include <ql/experimental/varianceoption/all.hpp>
 #include <ql/experimental/volatility/all.hpp>
