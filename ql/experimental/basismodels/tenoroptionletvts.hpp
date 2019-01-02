cb1019cf0b5feca906a3fb21d9991c0d09ee0941
Luigi Ballabio <luigi.ballabio@gmail.com>
2019-01-02 14:38:45 +0100

    Run clang-format on new files.

--- ql/experimental/basismodels/tenoroptionletvts.hpp
+++ ql/experimental/basismodels/tenoroptionletvts.hpp
@@ -24,109 +24,114 @@ FOR A PARTICULAR PURPOSE.  See the license for more details.
 #ifndef quantlib_tenoroptionletvts_hpp
 #define quantlib_tenoroptionletvts_hpp
 
-
-#include <ql/termstructures/volatility/optionlet/optionletvolatilitystructure.hpp>
-#include <ql/termstructures/volatility/smilesection.hpp>
 #include <ql/indexes/iborindex.hpp>
 #include <ql/math/interpolation.hpp>
+#include <ql/termstructures/volatility/optionlet/optionletvolatilitystructure.hpp>
+#include <ql/termstructures/volatility/smilesection.hpp>
 #include <ql/time/dategenerationrule.hpp>
 
 
 namespace QuantLib {
 
-	class TenorOptionletVTS : public OptionletVolatilityStructure {
+    class TenorOptionletVTS : public OptionletVolatilityStructure {
 
-	public: class CorrelationStructure;  // declaration below
+      public:
+        class CorrelationStructure; // declaration below
 
-	protected:
-
-		class TenorOptionletSmileSection : public SmileSection {
-		protected:
-			ext::shared_ptr<CorrelationStructure>         correlation_;
-		    std::vector<ext::shared_ptr<SmileSection> >   baseSmileSection_;
-			std::vector<Time>                             startTimeBase_;  // for correlation parametrisation
-			std::vector<Real>                             fraRateBase_;
-			Real                                          fraRateTarg_;
-			std::vector<Real>                             v_;
-			// implement transformation formula
+      protected:
+        class TenorOptionletSmileSection : public SmileSection {
+          protected:
+            ext::shared_ptr<CorrelationStructure> correlation_;
+            std::vector<ext::shared_ptr<SmileSection> > baseSmileSection_;
+            std::vector<Time> startTimeBase_; // for correlation parametrisation
+            std::vector<Real> fraRateBase_;
+            Real fraRateTarg_;
+            std::vector<Real> v_;
+            // implement transformation formula
             virtual Volatility volatilityImpl(Rate strike) const;
-		public:
-			// constructor includes actual transformation details
-			TenorOptionletSmileSection( const TenorOptionletVTS& volTS,
-				                        const Time               optionTime );
 
-		    // further SmileSection interface methods
-			virtual Real minStrike() const { return baseSmileSection_[0]->minStrike()+fraRateTarg_-fraRateBase_[0]; }
-			virtual Real maxStrike() const { return baseSmileSection_[0]->maxStrike()+fraRateTarg_-fraRateBase_[0]; }
-			virtual Real atmLevel() const  { return fraRateTarg_;  }
-		};
+          public:
+            // constructor includes actual transformation details
+            TenorOptionletSmileSection(const TenorOptionletVTS& volTS, const Time optionTime);
 
-		Handle<OptionletVolatilityStructure>          baseVTS_;
-		ext::shared_ptr<IborIndex>                    baseIndex_;
-		ext::shared_ptr<IborIndex>                    targIndex_;
-		ext::shared_ptr<CorrelationStructure>         correlation_;
+            // further SmileSection interface methods
+            virtual Real minStrike() const {
+                return baseSmileSection_[0]->minStrike() + fraRateTarg_ - fraRateBase_[0];
+            }
+            virtual Real maxStrike() const {
+                return baseSmileSection_[0]->maxStrike() + fraRateTarg_ - fraRateBase_[0];
+            }
+            virtual Real atmLevel() const { return fraRateTarg_; }
+        };
 
-	public:
+        Handle<OptionletVolatilityStructure> baseVTS_;
+        ext::shared_ptr<IborIndex> baseIndex_;
+        ext::shared_ptr<IborIndex> targIndex_;
+        ext::shared_ptr<CorrelationStructure> correlation_;
 
-		// functor interface for parametric correlation
-		class CorrelationStructure {
-		public:
-			// return the correlation between two FRA rates starting at start1 and start2
-			virtual Real operator() (const Time& start1, const Time& start2) const = 0;
-			virtual ~CorrelationStructure() {};
-		};
+      public:
+        // functor interface for parametric correlation
+        class CorrelationStructure {
+          public:
+            // return the correlation between two FRA rates starting at start1 and start2
+            virtual Real operator()(const Time& start1, const Time& start2) const = 0;
+            virtual ~CorrelationStructure(){};
+        };
 
-		// very basic choice for correlation structure
-		class TwoParameterCorrelation : public CorrelationStructure {
-		protected:
-			ext::shared_ptr<Interpolation> rhoInf_;
-			ext::shared_ptr<Interpolation> beta_;
-		public:
-			TwoParameterCorrelation ( const ext::shared_ptr<Interpolation>& rhoInf,
-				                      const ext::shared_ptr<Interpolation>& beta) {
-				rhoInf_ = rhoInf;
-				beta_   = beta;
-			}
-			Real operator() (const Time& start1, const Time& start2) const {
-				Real rhoInf = (*rhoInf_)(start1);
-				Real beta   = (*beta_)(start1);
-				Real rho = rhoInf + (1.0 - rhoInf)*exp(-beta*fabs(start2-start1));
-				return rho;
-			}
-		};
+        // very basic choice for correlation structure
+        class TwoParameterCorrelation : public CorrelationStructure {
+          protected:
+            ext::shared_ptr<Interpolation> rhoInf_;
+            ext::shared_ptr<Interpolation> beta_;
 
-		// constructor
-		TenorOptionletVTS( const Handle<OptionletVolatilityStructure>&          baseVTS,
-			               const ext::shared_ptr<IborIndex>&                    baseIndex,
-		                   const ext::shared_ptr<IborIndex>&                    targIndex,
-						   const ext::shared_ptr<CorrelationStructure>&         correlation);  
+          public:
+            TwoParameterCorrelation(const ext::shared_ptr<Interpolation>& rhoInf,
+                                    const ext::shared_ptr<Interpolation>& beta) {
+                rhoInf_ = rhoInf;
+                beta_ = beta;
+            }
+            Real operator()(const Time& start1, const Time& start2) const {
+                Real rhoInf = (*rhoInf_)(start1);
+                Real beta = (*beta_)(start1);
+                Real rho = rhoInf + (1.0 - rhoInf) * exp(-beta * fabs(start2 - start1));
+                return rho;
+            }
+        };
 
-		// Termstructure interface
+        // constructor
+        TenorOptionletVTS(const Handle<OptionletVolatilityStructure>& baseVTS,
+                          const ext::shared_ptr<IborIndex>& baseIndex,
+                          const ext::shared_ptr<IborIndex>& targIndex,
+                          const ext::shared_ptr<CorrelationStructure>& correlation);
 
-		//! the latest date for which the curve can return values
-		virtual Date maxDate() const { return baseVTS_->maxDate(); }
-		
-		// VolatilityTermstructure interface
+        // Termstructure interface
+
+        //! the latest date for which the curve can return values
+        virtual Date maxDate() const { return baseVTS_->maxDate(); }
+
+        // VolatilityTermstructure interface
 
         //! implements the actual smile calculation in derived classes
-		virtual ext::shared_ptr<SmileSection> smileSectionImpl(Time optionTime) const { return ext::shared_ptr<SmileSection>(new TenorOptionletSmileSection(*this,optionTime)); }
+        virtual ext::shared_ptr<SmileSection> smileSectionImpl(Time optionTime) const {
+            return ext::shared_ptr<SmileSection>(new TenorOptionletSmileSection(*this, optionTime));
+        }
         //! implements the actual volatility calculation in derived classes
-		virtual Volatility volatilityImpl(Time optionTime,Rate strike) const { return smileSection(optionTime)->volatility(strike); }
+        virtual Volatility volatilityImpl(Time optionTime, Rate strike) const {
+            return smileSection(optionTime)->volatility(strike);
+        }
 
 
-		//! the minimum strike for which the term structure can return vols
-		virtual Rate minStrike() const { return baseVTS_->minStrike(); }
+        //! the minimum strike for which the term structure can return vols
+        virtual Rate minStrike() const { return baseVTS_->minStrike(); }
         //! the maximum strike for which the term structure can return vols
-        virtual Rate maxStrike() const {return baseVTS_->maxStrike(); }
+        virtual Rate maxStrike() const { return baseVTS_->maxStrike(); }
 
-		// the methodology is designed for normal volatilities
-		VolatilityType volatilityType() const { return Normal; }
+        // the methodology is designed for normal volatilities
+        VolatilityType volatilityType() const { return Normal; }
+    };
 
-
-	};
-
-	typedef TenorOptionletVTS::CorrelationStructure TenorOptionletVTSCorrelationStructure; 
+    typedef TenorOptionletVTS::CorrelationStructure TenorOptionletVTSCorrelationStructure;
 
 }
 
-#endif /* #ifndef quantlib_tenoroptionletvts_hpp */
+#endif
--- ql/experimental/basismodels/tenorswaptionvts.hpp
+++ ql/experimental/basismodels/tenorswaptionvts.hpp
@@ -25,94 +25,103 @@ FOR A PARTICULAR PURPOSE.  See the license for more details.
 #define quantlib_tenorswaptionvts_hpp
 
 #include <ql/instruments/swaption.hpp>
-#include <ql/termstructures/yieldtermstructure.hpp>
+#include <ql/option.hpp>
 #include <ql/termstructures/volatility/smilesection.hpp>
 #include <ql/termstructures/volatility/swaption/swaptionvolstructure.hpp>
+#include <ql/termstructures/yieldtermstructure.hpp>
 #include <ql/time/date.hpp>
-#include <ql/option.hpp>
 
 namespace QuantLib {
 
     class TenorSwaptionVTS : public SwaptionVolatilityStructure {
-    protected:
-
-		class TenorSwaptionSmileSection : public SmileSection {
-        protected:
+      protected:
+        class TenorSwaptionSmileSection : public SmileSection {
+          protected:
             ext::shared_ptr<SmileSection> baseSmileSection_;
-			Real    swapRateBase_;
-			Real    swapRateTarg_;
-			Real    swapRateFinl_;
-			Real    lambda_;
-			Real    annuityScaling_;
-			// implement transformation formula
+            Real swapRateBase_;
+            Real swapRateTarg_;
+            Real swapRateFinl_;
+            Real lambda_;
+            Real annuityScaling_;
+            // implement transformation formula
             virtual Volatility volatilityImpl(Rate strike) const;
-		public:
-			// constructor includes actual transformation details
-			TenorSwaptionSmileSection ( const TenorSwaptionVTS& volTS,
-				                        Time                    optionTime,
-                                        Time                    swapLength);
 
-		    // further SmileSection interface methods
-			virtual Real minStrike() const { return baseSmileSection_->minStrike()+swapRateTarg_-swapRateBase_; }
-			virtual Real maxStrike() const { return baseSmileSection_->maxStrike()+swapRateTarg_-swapRateBase_; }
-			virtual Real atmLevel() const  { return swapRateFinl_;  }
-		};
+          public:
+            // constructor includes actual transformation details
+            TenorSwaptionSmileSection(const TenorSwaptionVTS& volTS,
+                                      Time optionTime,
+                                      Time swapLength);
 
-		Handle<SwaptionVolatilityStructure>    baseVTS_;
-		Handle<YieldTermStructure>             discountCurve_;
+            // further SmileSection interface methods
+            virtual Real minStrike() const {
+                return baseSmileSection_->minStrike() + swapRateTarg_ - swapRateBase_;
+            }
+            virtual Real maxStrike() const {
+                return baseSmileSection_->maxStrike() + swapRateTarg_ - swapRateBase_;
+            }
+            virtual Real atmLevel() const { return swapRateFinl_; }
+        };
 
-		ext::shared_ptr<IborIndex>             baseIndex_;
-		ext::shared_ptr<IborIndex>             targIndex_;
-		Period                                 baseFixedFreq_;
-		Period                                 targFixedFreq_;
-		DayCounter                             baseFixedDC_;
-		DayCounter                             targFixedDC_;
+        Handle<SwaptionVolatilityStructure> baseVTS_;
+        Handle<YieldTermStructure> discountCurve_;
 
-	public:
-		// constructor
-		TenorSwaptionVTS( const Handle<SwaptionVolatilityStructure>&    baseVTS,
-		                  const Handle<YieldTermStructure>&             discountCurve,
-		                  const ext::shared_ptr<IborIndex>&             baseIndex,
-		                  const ext::shared_ptr<IborIndex>&             targIndex,
-		                  const Period&                                 baseFixedFreq,
-		                  const Period&                                 targFixedFreq,
-		                  const DayCounter&                             baseFixedDC,
-		                  const DayCounter&                             targFixedDC)
-						  : SwaptionVolatilityStructure(baseVTS->referenceDate(),baseVTS->calendar(),baseVTS->businessDayConvention(),baseVTS->dayCounter()),
-		                  baseVTS_(baseVTS), discountCurve_(discountCurve), baseIndex_(baseIndex), targIndex_(targIndex),
-			              baseFixedFreq_(baseFixedFreq), targFixedFreq_(targFixedFreq), baseFixedDC_(baseFixedDC), targFixedDC_(targFixedDC) { }
+        ext::shared_ptr<IborIndex> baseIndex_;
+        ext::shared_ptr<IborIndex> targIndex_;
+        Period baseFixedFreq_;
+        Period targFixedFreq_;
+        DayCounter baseFixedDC_;
+        DayCounter targFixedDC_;
 
-		// Termstructure interface
+      public:
+        // constructor
+        TenorSwaptionVTS(const Handle<SwaptionVolatilityStructure>& baseVTS,
+                         const Handle<YieldTermStructure>& discountCurve,
+                         const ext::shared_ptr<IborIndex>& baseIndex,
+                         const ext::shared_ptr<IborIndex>& targIndex,
+                         const Period& baseFixedFreq,
+                         const Period& targFixedFreq,
+                         const DayCounter& baseFixedDC,
+                         const DayCounter& targFixedDC)
+        : SwaptionVolatilityStructure(baseVTS->referenceDate(),
+                                      baseVTS->calendar(),
+                                      baseVTS->businessDayConvention(),
+                                      baseVTS->dayCounter()),
+          baseVTS_(baseVTS), discountCurve_(discountCurve), baseIndex_(baseIndex),
+          targIndex_(targIndex), baseFixedFreq_(baseFixedFreq), targFixedFreq_(targFixedFreq),
+          baseFixedDC_(baseFixedDC), targFixedDC_(targFixedDC) {}
 
-		//! the latest date for which the curve can return values
-		virtual Date maxDate() const { return baseVTS_->maxDate(); }
+        // Termstructure interface
 
-		// SwaptionVolatility interface
+        //! the latest date for which the curve can return values
+        virtual Date maxDate() const { return baseVTS_->maxDate(); }
 
-		//! the minimum strike for which the term structure can return vols
-		virtual Rate minStrike() const { return baseVTS_->minStrike(); }
+        // SwaptionVolatility interface
+
+        //! the minimum strike for which the term structure can return vols
+        virtual Rate minStrike() const { return baseVTS_->minStrike(); }
         //! the maximum strike for which the term structure can return vols
-        virtual Rate maxStrike() const {return baseVTS_->maxStrike(); }
+        virtual Rate maxStrike() const { return baseVTS_->maxStrike(); }
 
 
-		// SwaptionVolatilityStructure interface
+        // SwaptionVolatilityStructure interface
 
-		//! the largest length for which the term structure can return vols
-		virtual const Period& maxSwapTenor() const { return baseVTS_->maxSwapTenor(); }
+        //! the largest length for which the term structure can return vols
+        virtual const Period& maxSwapTenor() const { return baseVTS_->maxSwapTenor(); }
 
-        virtual ext::shared_ptr<SmileSection> smileSectionImpl(
-                                                Time optionTime,
-												Time swapLength) const { return ext::shared_ptr<SmileSection>(new TenorSwaptionSmileSection(*this,optionTime,swapLength) ); }
+        virtual ext::shared_ptr<SmileSection> smileSectionImpl(Time optionTime,
+                                                               Time swapLength) const {
+            return ext::shared_ptr<SmileSection>(
+                new TenorSwaptionSmileSection(*this, optionTime, swapLength));
+        }
 
-        virtual Volatility volatilityImpl(Time optionTime,
-                                          Time swapLength,
-										  Rate strike) const { return smileSectionImpl(optionTime,swapLength)->volatility(strike,Normal,0.0); }
+        virtual Volatility volatilityImpl(Time optionTime, Time swapLength, Rate strike) const {
+            return smileSectionImpl(optionTime, swapLength)->volatility(strike, Normal, 0.0);
+        }
 
-		// the methodology is designed for normal volatilities
-		VolatilityType volatilityType() const { return Normal; }
-
+        // the methodology is designed for normal volatilities
+        VolatilityType volatilityType() const { return Normal; }
     };
 
 }
 
-#endif /* #ifndef quantlib_tenorswaptionvts_hpp */
+#endif
