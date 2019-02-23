d30cf3f92fb2afd2539b7d93acac9dafe409db6b
Peter Caspers <peter.caspers@quaternion.com>
2019-02-23 20:30:28 +0100

    require size, replace try/catch by macro

--- test-suite/schedule.cpp
+++ test-suite/schedule.cpp
@@ -362,63 +362,21 @@ void ScheduleTest::testFourWeeksTenor() {
 
 void ScheduleTest::testShortEomSchedule() {
     BOOST_TEST_MESSAGE("Testing short eom schedule...");
-
-    try {
-        // seg-faults in 1.15
-        Schedule s = MakeSchedule()
-                         .from(Date(21, Feb, 2019))
-                         .to(Date(28, Feb, 2019))
-                         .withCalendar(TARGET())
-                         .withTenor(1 * Years)
-                         .withConvention(ModifiedFollowing)
-                         .withTerminationDateConvention(ModifiedFollowing)
-                         .backwards()
-                         .endOfMonth(true);
-        BOOST_CHECK(s.size() == 2);
-        BOOST_CHECK(s[0] == Date(21, Feb, 2019));
-        BOOST_CHECK(s[1] == Date(28, Feb, 2019));
-    } catch (std::exception& e) {
-        BOOST_ERROR(
-            "Short eom schedule construction caused an exception: " << e.what());
-    }
+    Schedule s;
+    // seg-faults in 1.15
+    BOOST_REQUIRE_NO_THROW(s = MakeSchedule()
+                                   .from(Date(21, Feb, 2019))
+                                   .to(Date(28, Feb, 2019))
+                                   .withCalendar(TARGET())
+                                   .withTenor(1 * Years)
+                                   .withConvention(ModifiedFollowing)
+                                   .withTerminationDateConvention(ModifiedFollowing)
+                                   .backwards()
+                                   .endOfMonth(true));
+    BOOST_REQUIRE(s.size() == 2);
+    BOOST_CHECK(s[0] == Date(21, Feb, 2019));
+    BOOST_CHECK(s[1] == Date(28, Feb, 2019));
 }
-
-void ScheduleTest::testScheduleAlwaysHasAStartDate() {
-    BOOST_TEST_MESSAGE("Testing that variations of MakeSchedule "
-                       "always produce a schedule with a start date...");
-    // Attempt to establish whether the first coupoun payment date is
-    // always the second element of the constructor.
-    Calendar calendar = UnitedStates();
-    Schedule schedule = MakeSchedule()
-        .from(Date(10, January, 2017))
-        .withFirstDate(Date(31, August, 2017))
-        .to(Date(28, February, 2026))
-        .withFrequency(Semiannual)
-        .withCalendar(calendar)
-        .withConvention(Unadjusted)
-        .backwards().endOfMonth(false);
-    QL_ASSERT(schedule.date(0) == Date(10, January, 2017),
-              "The first element should always be the start date");
-    schedule = MakeSchedule()
-        .from(Date(10, January, 2017))
-        .to(Date(28, February, 2026))
-        .withFrequency(Semiannual)
-        .withCalendar(calendar)
-        .withConvention(Unadjusted)
-        .backwards().endOfMonth(false);
-    QL_ASSERT(schedule.date(0) == Date(10, January, 2017),
-              "The first element should always be the start date");
-    schedule = MakeSchedule()
-        .from(Date(31, August, 2017))
-        .to(Date(28, February, 2026))
-        .withFrequency(Semiannual)
-        .withCalendar(calendar)
-        .withConvention(Unadjusted)
-        .backwards().endOfMonth(false);
-    QL_ASSERT(schedule.date(0) == Date(31, August, 2017),
-              "The first element should always be the start date");
-}
-
 
 test_suite* ScheduleTest::suite() {
     test_suite* suite = BOOST_TEST_SUITE("Schedule tests");
@@ -437,8 +395,6 @@ test_suite* ScheduleTest::suite() {
     suite->add(QUANTLIB_TEST_CASE(&ScheduleTest::testCDS2015Convention));
     suite->add(QUANTLIB_TEST_CASE(&ScheduleTest::testDateConstructor));
     suite->add(QUANTLIB_TEST_CASE(&ScheduleTest::testFourWeeksTenor));
-    suite->add(QUANTLIB_TEST_CASE(
-                           &ScheduleTest::testScheduleAlwaysHasAStartDate));
     suite->add(QUANTLIB_TEST_CASE(&ScheduleTest::testShortEomSchedule));
     return suite;
 }
