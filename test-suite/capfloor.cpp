9f270cb6a74242bae58b72c1a80606cbdc340872
Wojciech Ślusarski <wojciech.slusarski@gmail.com>
2019-08-18 20:43:50 +0200

    Adding copyright information

--- test-suite/capfloor.cpp
+++ test-suite/capfloor.cpp
@@ -3,6 +3,7 @@
 /*
  Copyright (C) 2003 RiskMap srl
  Copyright (C) 2004, 2005, 2006, 2007, 2008 StatPro Italia srl
+ Copyright (C) 2019 Wojciech Ślusarski
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
