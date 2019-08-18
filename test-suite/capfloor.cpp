b9c59b92ba9318e241988255f48b4c068703a9c2
Wojciech Ślusarski <wojciech.slusarski@gmail.com>
2019-08-18 23:38:51 +0200

    Fixing errors found in CI builds

--- test-suite/capfloor.cpp
+++ test-suite/capfloor.cpp
@@ -635,18 +635,13 @@ void CapFloorTest::testCachedValueFromOptionLets() {
     // par coupon price
     Real cachedCapNPV   = 6.87570026732,
          cachedFloorNPV = 2.65812927959;
-    Real calculatedCapNPV   = 0.0,
-         calculatedFloorNPV = 0.0,   
-         calculatedCapletsNPV = 0.0,
+    Real calculatedCapletsNPV = 0.0,
          calculatedFloorletsNPV = 0.0;
-
 #else
     // index fixing price
     Real cachedCapNPV   = 6.87630307745,
          cachedFloorNPV = 2.65796764715;
-    Real calculatedCapNPV   = 0.0,
-         calculatedFloorNPV = 0.0,   
-         calculatedCapletsNPV = 0.0,
+    Real calculatedCapletsNPV = 0.0,
          calculatedFloorletsNPV = 0.0;
 #endif
     // test Black floor price against cached value
