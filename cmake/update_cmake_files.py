78315fe6f7e7cb4cd7bb4f3db223a6d791aa379b
Peter Caspers <peter.caspers@quaternion.com>
2019-11-16 21:42:19 +0100

    use explicit file lists in cmake

---
+++ cmake/update_cmake_files.py
@@ -0,0 +1,41 @@
+# update cpp and hpp file-lists in CMakeLists.txt files
+
+import glob
+
+def updateFileLists(directory, lists):
+    # read CMakeLists.txt
+    inputFile = open (directory + "/CMakeLists.txt", "r")
+    text = inputFile.read()
+    inputFile.close()
+    # update lists
+    for list in lists:
+        filelist = glob.glob(directory + "/**/" + list[1], recursive=True)
+        filelist.sort()
+        filelistStr = ""
+        firstFile = True
+        for l in filelist:
+            isException = False
+            for e in list[2]:
+                isException |= l.find("/" + e) != -1
+            if not isException:
+                if firstFile:
+                    firstFile = False
+                else:
+                    filelistStr += "\n"
+                filelistStr += l[len(directory)+1:]
+        index = text.find("set(" + list[0])
+        if index == -1:
+            print("Error: set(" + list[0] + "... in " + directory + "/CMakeLists.txt not found")
+            return
+        indexEnd = text.find(")", index)
+        if index == -1:
+            print("Error: set" + list[0] + "... in " + directory + "/CMakeLists.txt: no closing bracket found")
+        text = text[:index] + "set(" + list[0] + " " + filelistStr + text[indexEnd:]
+    # write CMakeLists.txt
+    outputFile = open (directory + "/CMakeLists.txt", "w")
+    outputFile.write(text)
+    outputFile.close()
+
+updateFileLists("ql", [("QuantLib_SRC", "*.cpp", []),
+                                 ("QuantLib_HDR", "*.hpp", ["quantlib.hpp", "all.hpp", "config.hpp"])])
+updateFileLists("test-suite", [("QuantLib-Test_SRC", "*.cpp", ["quantlibbenchmark.cpp"])])
