aba6791c29a44ede86237a379c84a80654cd3e51
Peter Caspers <peter.caspers@quaternion.com>
2020-01-22 21:15:47 +0100

    ensure non-empty index estimation period

--- ql/cashflows/iborcoupon.cpp
+++ ql/cashflows/iborcoupon.cpp
@@ -66,6 +66,8 @@ namespace QuantLib {
                 accrualEndDate_, -static_cast<Integer>(fixingDays_), Days);
             fixingEndDate_ = fixingCalendar.advance(
                 nextFixingDate, indexFixingDays, Days);
+            // make sure the estimation period contains at least one day
+            fixingEndDate_ = std::max(fixingEndDate_, fixingValueDate_ + 1);
         }
         #endif
 
