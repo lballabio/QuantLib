7e2bbc55bf8fc40a61e4f6adb551626c2ff8966c
Klaus Spanderen <klaus@spanderen.de>
2020-04-05 09:17:48 +0200

    initial implementation

--- test-suite/hestonmodel.hpp
+++ test-suite/hestonmodel.hpp
@@ -54,6 +54,7 @@ class HestonModelTest {
     static void testPiecewiseTimeDependentChFvsHestonChF();
     static void testPiecewiseTimeDependentComparison();
     static void testPiecewiseTimeDependentChFAsymtotic();
+    static void testExponentialFittingHestonEngine();
 
     static boost::unit_test_framework::test_suite* suite(SpeedLevel);
     static boost::unit_test_framework::test_suite* experimental();
