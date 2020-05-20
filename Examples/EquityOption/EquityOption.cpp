74e1c2c24afd05b22bc614ac4cd358f6d6fc3482
w31ha0 <lewweihao93@hotmail.com>
2020-05-19 19:57:48 -0700

    Fix build

--- Examples/EquityOption/EquityOption.cpp
+++ Examples/EquityOption/EquityOption.cpp
@@ -32,11 +32,9 @@
 #include <ql/pricingengines/vanilla/fdblackscholesvanillaengine.hpp>
 #include <ql/pricingengines/vanilla/mceuropeanengine.hpp>
 #include <ql/pricingengines/vanilla/mcamericanengine.hpp>
-#include <ql/pricingengines/vanilla/analyticeuropeanvasicekengine.hpp>
-#include <ql/pricingengines/vanilla/fdhestonvanillaengine.hpp>
 #include <ql/time/calendars/target.hpp>
 #include <ql/utilities/dataformatters.hpp>
-#include <ql/models/shortrate/onefactormodels/vasicek.hpp>
+
 
 #include <iostream>
 #include <iomanip>
@@ -148,24 +146,6 @@ int main(int, char* []) {
                   << std::setw(widths[3]) << std::left << "N/A"
                   << std::endl;
 
-        //Vasicek rates model for European
-        method = "Black Vasicek Model";
-        Real r0 = riskFreeRate;
-        Real a = 0.3;
-        Real b = 0.3;
-        Real sigma_r = 0.15;
-        Real riskPremium = 0.0;
-        Real correlation = 0.5;
-        ext::shared_ptr<Vasicek> vasicekProcess(new Vasicek(r0, a, b, sigma_r, riskPremium));
-        europeanOption.setPricingEngine(ext::shared_ptr<PricingEngine>(
-                new AnalyticBlackVasicekEngine(bsmProcess, vasicekProcess, correlation)));
-        std::cout << std::setw(widths[0]) << std::left << method
-                  << std::fixed
-                  << std::setw(widths[1]) << std::left << europeanOption.NPV()
-                  << std::setw(widths[2]) << std::left << "N/A"
-                  << std::setw(widths[3]) << std::left << "N/A"
-                  << std::endl;
-
         // semi-analytic Heston for European
         method = "Heston semi-analytic";
         ext::shared_ptr<HestonProcess> hestonProcess(
@@ -176,21 +156,6 @@ int main(int, char* []) {
                                               new HestonModel(hestonProcess));
         europeanOption.setPricingEngine(ext::shared_ptr<PricingEngine>(
                                      new AnalyticHestonEngine(hestonModel)));
-        std::cout << std::setw(widths[0]) << std::left << method
-                  << std::fixed
-                  << std::setw(widths[1]) << std::left << europeanOption.NPV()
-                  << std::setw(widths[2]) << std::left << "N/A"
-                  << std::setw(widths[3]) << std::left << "N/A"
-                  << std::endl;
-
-        method = "Heston FD mixingFactor 0.5";
-        europeanOption.setPricingEngine(ext::shared_ptr<PricingEngine>(
-            new FdHestonVanillaEngine(ext::make_shared<HestonModel>(
-                hestonProcess), 100, 400, 3, 0,
-                                           FdmSchemeDesc::Hundsdorfer(),
-                                           ext::shared_ptr<LocalVolTermStructure>(),
-                                               0.5)));
-
         std::cout << std::setw(widths[0]) << std::left << method
                   << std::fixed
                   << std::setw(widths[1]) << std::left << europeanOption.NPV()
