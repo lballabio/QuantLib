d953c3703f79d76e5f7c62eb9769b5b67524282c
Luigi Ballabio <luigi.ballabio@gmail.com>
2020-07-21 12:45:18 +0200

    Remove harmful virtual method declaration.

--- ql/experimental/callablebonds/callablebond.cpp
+++ ql/experimental/callablebonds/callablebond.cpp
@@ -426,7 +426,8 @@ namespace QuantLib {
     void CallableFixedRateBond::setupArguments(
                                        PricingEngine::arguments* args) const {
 
-        QuantLib::CallableBond::setupArguments(args);
+        CallableBond::setupArguments(args);
+
         CallableBond::arguments* arguments =
             dynamic_cast<CallableBond::arguments*>(args);
 
