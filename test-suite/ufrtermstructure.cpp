273fab63aca7ee94cdf2a8a9fce5616e17f6ff01
Marcin <mrybacki12@gmail.com>
2020-10-11 14:55:41 +0200

    Replaced vector with raw arrays.]

--- test-suite/ufrtermstructure.cpp
+++ test-suite/ufrtermstructure.cpp
@@ -44,13 +44,6 @@ namespace ufr_term_structure_test {
         Real weight;
     };
 
-    LLFRWeight initLlfrWeight(Time ttm, Real w) {
-        LLFRWeight d;
-        d.ttm = ttm;
-        d.weight = w;
-        return d;
-    }
-
     struct CommonVars {
         // global data
         Date today, settlement;
@@ -82,25 +75,13 @@ namespace ufr_term_structure_test {
                 new IborIndex("FTK_IDX", floatingTenor, settlementDays, ccy, calendar,
                               businessConvention, false, dayCount, ftkTermStructureHandle));
 
-            std::vector<Datum> swapData;
             // Data source: https://fred.stlouisfed.org/
-            swapData.push_back({1, Years, -0.00315});
-            swapData.push_back({2, Years, -0.00205});
-            swapData.push_back({3, Years, -0.00144});
-            swapData.push_back({4, Years, -0.00068});
-            swapData.push_back({5, Years, 0.00014});
-            swapData.push_back({6, Years, 0.00103});
-            swapData.push_back({7, Years, 0.00194});
-            swapData.push_back({8, Years, 0.00288});
-            swapData.push_back({9, Years, 0.00381});
-            swapData.push_back({10, Years, 0.00471});
-            swapData.push_back({12, Years, 0.0063});
-            swapData.push_back({15, Years, 0.00808});
-            swapData.push_back({20, Years, 0.00973});
-            swapData.push_back({25, Years, 0.01035});
-            swapData.push_back({30, Years, 0.01055});
-            swapData.push_back({40, Years, 0.0103});
-            swapData.push_back({50, Years, 0.0103});
+            Datum swapData[] = {{1, Years, -0.00315}, {2, Years, -0.00205}, {3, Years, -0.00144},
+                                {4, Years, -0.00068}, {5, Years, 0.00014},  {6, Years, 0.00103},
+                                {7, Years, 0.00194},  {8, Years, 0.00288},  {9, Years, 0.00381},
+                                {10, Years, 0.00471}, {12, Years, 0.0063},  {15, Years, 0.00808},
+                                {20, Years, 0.00973}, {25, Years, 0.01035}, {30, Years, 0.01055},
+                                {40, Years, 0.0103},  {50, Years, 0.0103}};
 
             InterestRate ufr(0.023, dayCount, Compounded, Annual);
             ufrRate = ext::shared_ptr<Quote>(
@@ -110,12 +91,12 @@ namespace ufr_term_structure_test {
             Settings::instance().evaluationDate() = today;
             settlement = calendar.advance(today, settlementDays, Days);
 
-            Size swaps = LENGTH(swapData);
-            std::vector<ext::shared_ptr<RateHelper> > instruments;
-            for (std::vector<Datum>::iterator it = swapData.begin(); it != swapData.end(); ++it) {
-                instruments.push_back(ext::shared_ptr<RateHelper>(
-                    new SwapRateHelper((*it).rate, Period((*it).n, (*it).units), calendar,
-                                       fixedFrequency, businessConvention, dayCount, index)));
+            Size nInstruments = LENGTH(swapData);
+            std::vector<ext::shared_ptr<RateHelper> > instruments(nInstruments);
+            for (Size i = 0; i < nInstruments; i++) {
+                instruments[i] = ext::shared_ptr<RateHelper>(new SwapRateHelper(
+                    swapData[i].rate, Period(swapData[i].n, swapData[i].units), calendar,
+                    fixedFrequency, businessConvention, dayCount, index));
             }
 
             ext::shared_ptr<YieldTermStructure> ftkTermStructure(
@@ -125,16 +106,17 @@ namespace ufr_term_structure_test {
         }
     };
 
-    ext::shared_ptr<Quote> calculateLLFR(const Handle<YieldTermStructure>& ts,
-                                         Time fsp,
-                                         const std::vector<LLFRWeight>& weights,
-                                         Real omega) {
-        Rate llfr = 0.0;
+    ext::shared_ptr<Quote> calculateLLFR(const Handle<YieldTermStructure>& ts, Time fsp) {
         DayCounter dc = ts->dayCounter();
         Date ref = ts->referenceDate();
+        Real omega = 8.0 / 15.0;
 
-        for (Size i = 0; i < weights.size(); i++) {
-            LLFRWeight w = weights[i];
+        LLFRWeight llfrWeights[] = {{25.0, 1.0}, {30.0, 0.5}, {40.0, 0.25}, {50.0, 0.125}};
+        Size nWeights = LENGTH(llfrWeights);
+
+        Rate llfr = 0.0;
+        for (Size j = 0; j < nWeights; j++) {
+            LLFRWeight w = llfrWeights[j];
             llfr += w.weight * ts->forwardRate(fsp, w.ttm, Continuous, NoFrequency, true);
         }
         return ext::shared_ptr<Quote>(new SimpleQuote(omega * llfr));
@@ -148,48 +130,34 @@ void UFRTermStructureTest::testDnbReplication() {
 
     CommonVars vars;
 
-    std::vector<LLFRWeight> llfrWeights;
-    llfrWeights.push_back(initLlfrWeight(25.0, 1.0));
-    llfrWeights.push_back(initLlfrWeight(30.0, 0.5));
-    llfrWeights.push_back(initLlfrWeight(40.0, 0.25));
-    llfrWeights.push_back(initLlfrWeight(50.0, 0.125));
-
-    Real omega = 8.0 / 15.0;
     Time fsp = 20.0;
     Real alpha = 0.1;
 
-    ext::shared_ptr<Quote> llfr =
-        calculateLLFR(vars.ftkTermStructureHandle, fsp, llfrWeights, omega);
+    ext::shared_ptr<Quote> llfr = calculateLLFR(vars.ftkTermStructureHandle, fsp);
 
     ext::shared_ptr<YieldTermStructure> ufrTs(new UFRTermStructure(
         vars.ftkTermStructureHandle, Handle<Quote>(llfr), Handle<Quote>(vars.ufrRate), fsp, alpha));
 
-    std::vector<Datum> expZeroes;
-    expZeroes.push_back({10, Years, 0.00477});
-    expZeroes.push_back({20, Years, 0.01004});
-    expZeroes.push_back({30, Years, 0.01223});
-    expZeroes.push_back({40, Years, 0.01433});
-    expZeroes.push_back({50, Years, 0.01589});
-    expZeroes.push_back({60, Years, 0.01702});
-    expZeroes.push_back({70, Years, 0.01785});
-    expZeroes.push_back({80, Years, 0.01849});
-    expZeroes.push_back({90, Years, 0.01899});
-    expZeroes.push_back({100, Years, 0.01939});
+    Datum expectedZeroes[] = {{10, Years, 0.00477}, {20, Years, 0.01004}, {30, Years, 0.01223},
+                         {40, Years, 0.01433}, {50, Years, 0.01589}, {60, Years, 0.01702},
+                         {70, Years, 0.01785}, {80, Years, 0.01849}, {90, Years, 0.01899},
+                         {100, Years, 0.01939}};
 
     Real tolerance = 1.0e-4;
+    Size nRates = LENGTH(expectedZeroes);
 
-    for (Size i = 0; i < expZeroes.size(); ++i) {
-        Period p = expZeroes[i].n * expZeroes[i].units;
-
+    for (Size k = 0; k < nRates; ++k) {
+        Period p = expectedZeroes[k].n * expectedZeroes[k].units;
         Date maturity = vars.settlement + p;
+        
         Rate actual = ufrTs->zeroRate(maturity, vars.dayCount, Compounded, Annual).rate();
-        Rate expected = expZeroes[i].rate;
+        Rate expected = expectedZeroes[k].rate;
 
         if (std::fabs(actual - expected) > tolerance)
-        BOOST_ERROR("unable to reproduce zero yield rate from the UFR curve\n"
-                    << std::setprecision(10) << "    calculated: " << actual << "\n"
-                    << "    expected:   " << expected << "\n"
-                    << "    tenor:       " << p << "\n");
+            BOOST_ERROR("unable to reproduce zero yield rate from the UFR curve\n"
+                        << std::setprecision(10) << "    calculated: " << actual << "\n"
+                        << "    expected:   " << expected << "\n"
+                        << "    tenor:       " << p << "\n");
     }
 }
 
