a0cec8c5ff10123a7efd534ee319cfbca13cb520
francis <francis.duffy@quaternionrisk.com>
2020-11-17 21:20:30 +0000

    GH-727 Forgot this one.

--- ql/termstructures/credit/defaultprobabilityhelpers.cpp
+++ ql/termstructures/credit/defaultprobabilityhelpers.cpp
@@ -263,12 +263,12 @@ namespace QuantLib {
     }
 
     void UpfrontCdsHelper::resetEngine() {
-        swap_ = ext::make_shared<CreditDefaultSwap>(
+        swap_ = ext::shared_ptr<CreditDefaultSwap>(new CreditDefaultSwap(
             Protection::Buyer, 100.0, 0.01, runningSpread_, schedule_,
             paymentConvention_, dayCounter_, settlesAccrual_,
             paysAtDefaultTime_, protectionStart_, upfrontDate_,
             ext::shared_ptr<Claim>(), lastPeriodDC_, rebatesAccrual_,
-            evaluationDate_);
+            evaluationDate_));
 
         switch (model_) {
           case CreditDefaultSwap::ISDA:
