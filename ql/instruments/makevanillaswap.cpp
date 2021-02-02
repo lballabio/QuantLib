e3eba10d37d377e6dde9574814d1fa3cdb576794
RalfKonrad <github@eckel-consulting.de>
2021-02-02 17:30:05 +0100

    Avoid DivByZero when nominal_==0.0 by providing an explicit nominal.

--- ql/instruments/makevanillaswap.cpp
+++ ql/instruments/makevanillaswap.cpp
@@ -156,8 +156,7 @@ namespace QuantLib {
 
         Rate usedFixedRate = fixedRate_;
         if (fixedRate_ == Null<Rate>()) {
-            QL_REQUIRE(nominal_ != 0.0, "Cannot determine fair rate for zero nominal.");
-            VanillaSwap temp(type_, nominal_,
+            VanillaSwap temp(type_, 100.00,
                              fixedSchedule,
                              0.0, // fixed rate
                              fixedDayCount,
