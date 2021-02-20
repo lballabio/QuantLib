2180a56f72f66d57ce0e175406a861e08a040b33
RalfKonrad <github@eckel-consulting.de>
2021-02-20 19:47:09 +0100

    Wrong copyright year

--- ql/indexes/inflationindex.hpp
+++ ql/indexes/inflationindex.hpp
@@ -2,7 +2,7 @@
 
 /*
  Copyright (C) 2007 Chris Kenyon
- Copyright (C) 2007 Ralf Konrad Eckel
+ Copyright (C) 2021 Ralf Konrad Eckel
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
