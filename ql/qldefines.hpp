8e42359a5bb43b1e5cf1f1c93f243b825cd172cf
RalfKonrad <github@eckel-consulting.de>
2021-02-23 10:42:55 +0100

    Added macro to disable and enable deprecated warnings

--- ql/qldefines.hpp
+++ ql/qldefines.hpp
@@ -194,6 +194,38 @@
 #define QL_DEPRECATED
 #endif
 
+// skip warnings temporarily when using deprecated features
+// clang-format off
+#if defined(BOOST_MSVC)
+#    define QL_DEPRECATED_DISABLE_WARNING \
+        __pragma(warning(push))           \
+        __pragma(warning(disable : 4996))
+
+#    define QL_DEPRECATED_ENABLE_WARNING \
+        __pragma(warning(pop))
+
+#elif defined(__GNUC__)
+#    define QL_DEPRECATED_DISABLE_WARNING                               \
+        _Pragma("GCC diagnostic push")                                  \
+        _Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"")
+
+#    define QL_DEPRECATED_ENABLE_WARNING \
+        _Pragma("GCC diagnostic pop")
+
+#elif defined(__clang__)
+#    define QL_DEPRECATED_DISABLE_WARNING                                 \
+        _Pragma("clang diagnostic push")                                  \
+        _Pragma("clang diagnostic ignored \"-Wdeprecated-declarations\"")
+
+#    define QL_DEPRECATED_ENABLE_WARNING \
+        _Pragma("clang diagnostic pop")
+#else
+// we don't know how to enable it, just define the macro away
+#   define QL_DEPRECATED_DISABLE_WARNING
+#   define QL_DEPRECATED_ENABLE_WARNING
+#endif
+// clang-format on
+
 
 // until we stop supporting Visual C++ 2013
 #if defined(QL_PATCH_MSVC_2013)
