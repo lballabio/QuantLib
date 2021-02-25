bab8c96830fc28f17263f5194ce7aaeb07956f07
Ralf Konrad <42419984+ralfkonrad@users.noreply.github.com>
2021-02-25 22:12:19 +0100

    bugfix

--- test-suite/inflationcpicapfloor.cpp
+++ test-suite/inflationcpicapfloor.cpp
@@ -63,14 +63,14 @@ namespace inflation_cpi_capfloor_test {
 
     template <class T, class U, class I>
     std::vector<ext::shared_ptr<BootstrapHelper<T> > > makeHelpers(
-        QL_DEPRECATED_DISABLE_WARNING
-        
         Datum iiData[], Size N,
         const ext::shared_ptr<I> &ii, const Period &observationLag,
         const Calendar &calendar,
         const BusinessDayConvention &bdc,
         const DayCounter &dc,
         const Handle<YieldTermStructure>& discountCurve) {
+
+        QL_DEPRECATED_DISABLE_WARNING
 
         std::vector<ext::shared_ptr<BootstrapHelper<T> > > instruments;
         for (Size i=0; i<N; i++) {
