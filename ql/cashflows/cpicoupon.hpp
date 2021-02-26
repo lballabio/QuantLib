edf12b298fc9d32d4e677b4c748d8449f417b11e
RalfKonrad <github@eckel-consulting.de>
2021-02-26 16:34:35 +0100

    Added copyrights

--- ql/cashflows/cpicoupon.hpp
+++ ql/cashflows/cpicoupon.hpp
@@ -2,6 +2,7 @@
 
 /*
  Copyright (C) 2011 Chris Kenyon
+ Copyright (C) 2021 Ralf Konrad Eckel
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
--- ql/termstructures/inflation/inflationhelpers.hpp
+++ ql/termstructures/inflation/inflationhelpers.hpp
@@ -3,6 +3,7 @@
 /*
  Copyright (C) 2007, 2009 Chris Kenyon
  Copyright (C) 2007 StatPro Italia srl
+ Copyright (C) 2021 Ralf Konrad Eckel
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
