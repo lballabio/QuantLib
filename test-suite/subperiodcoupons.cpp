80f36c1cca92009d233c6bef67355d2988c193ac
Marcin Rybacki <mrybacki12@gmail.com>
2021-03-14 12:55:45 +0100

    Adding unit tests for sub-period coupons.

--- test-suite/subperiodcoupons.cpp
+++ test-suite/subperiodcoupons.cpp
@@ -52,7 +52,7 @@ namespace subperiodcoupons_test {
 
             euribor = ext::shared_ptr<IborIndex>(new Euribor6M(euriborHandle));
 
-            today = calendar.adjust(Date::todaysDate());
+            today = calendar.adjust(Date(15, March, 2021));
             Settings::instance().evaluationDate() = today;
             settlement = calendar.advance(today, settlementDays, Days);
 
@@ -83,6 +83,14 @@ namespace subperiodcoupons_test {
             .withFixingDays(vars.settlementDays);
     }
 
+    Real sumLegPayments(const Leg& leg)
+    {
+        Real payments = 0.0;
+        std::for_each(leg.begin(), leg.end(),
+                      [&](const ext::shared_ptr<CashFlow>& cf) { payments += cf->amount(); });
+        return payments;
+    }
+
     ext::shared_ptr<CashFlow> createSubPeriodsCoupon(const Date& start,
                                                     const Date& end,
                                                     bool isInArrears = false,
@@ -90,17 +98,39 @@ namespace subperiodcoupons_test {
         CommonVars vars;
         Date paymentDate = vars.calendar.advance(end, 1 * Days, vars.businessConvention);
         Date exCouponDate = vars.calendar.advance(paymentDate, -2 * Days, vars.businessConvention);
-        return ext::shared_ptr<CashFlow>(new SubPeriodsCoupon(
+        ext::shared_ptr<FloatingRateCoupon> cpn(new SubPeriodsCoupon(
             paymentDate, 1.0, start, end, vars.settlementDays, vars.euribor, 1.0, spread, 0.0,
             Date(), Date(), vars.dayCount, isInArrears, exCouponDate));
+        cpn->setPricer(ext::shared_ptr<FloatingRateCouponPricer>(new CompoundingRatePricer()));
+        return cpn;
     }
 }
 
 void SubPeriodsCouponTest::test() {
     BOOST_TEST_MESSAGE("...");
 
-}
+    using namespace subperiodcoupons_test;
 
+    Date start(15, April, 2021);
+    Date end(15, September, 2021);
+
+    Leg iborLeg = createIborLeg(start, end);
+    ext::shared_ptr<CashFlow> subPeriodCpn = createSubPeriodsCoupon(start, end);
+
+    Real tolerance = 1.0e-10;
+
+    Real actualPayment = subPeriodCpn->amount();
+    Real expectedPayment = sumLegPayments(iborLeg);
+
+    if (std::fabs(actualPayment - expectedPayment) > tolerance)
+        BOOST_ERROR("unable to replicate sub-periods coupon payment\n"
+                    << std::setprecision(5) << "    calculated:    " << actualPayment << "\n"
+                    << "    expected:    " << expectedPayment << "\n"
+                    << "    start:    " << start << "\n"
+                    << "    end:    " << end << "\n");
+
+
+}
 
 test_suite* SubPeriodsCouponTest::suite() {
     auto* suite = BOOST_TEST_SUITE("Sub-period coupons tests");
