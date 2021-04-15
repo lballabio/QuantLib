806e455b0d1486e0e3da3192ab00831f1de3648c
Luigi Ballabio <luigi.ballabio@gmail.com>
2021-04-15 16:25:45 +0200

    Set version to 1.34-dev.

--- ql/version.hpp
+++ ql/version.hpp
@@ -31,10 +31,10 @@
 /*! @{ */
 
 //! version string
-#define QL_VERSION "1.33"
+#define QL_VERSION "1.34-dev"
 
 //! version hexadecimal number
-#define QL_HEX_VERSION 0x013300f0
+#define QL_HEX_VERSION 0x01340000
 
 /*! @}  */
 
