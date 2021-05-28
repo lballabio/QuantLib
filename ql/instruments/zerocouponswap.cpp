f5afe6079fbd93534b210413802339670b436981
Marcin Rybacki <52628342+marcin-rybacki@users.noreply.github.com>
2021-05-28 13:22:34 +0200

    Update ql/instruments/zerocouponswap.cpp
    Co-authored-by: Luigi Ballabio <luigi.ballabio@gmail.com>

--- ql/instruments/zerocouponswap.cpp
+++ ql/instruments/zerocouponswap.cpp
@@ -106,7 +106,7 @@ namespace QuantLib {
                                    Real baseNominal,
                                    const Date& startDate,
                                    const Date& maturityDate,
-                                   Real fixedRate,
+                                   Rate fixedRate,
                                    const DayCounter& fixedDayCounter,
                                    ext::shared_ptr<IborIndex> iborIndex,
                                    const Calendar& paymentCalendar,
