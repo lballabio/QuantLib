c1a2aa91f34bf4ad051d656aede3d0aca41a3b4d
Luigi Ballabio <luigi.ballabio@gmail.com>
2021-05-31 11:24:10 +0200

    Add correct implementation of 30/360 US

--- ql/time/daycounters/thirty360.hpp
+++ ql/time/daycounters/thirty360.hpp
@@ -33,20 +33,21 @@ namespace QuantLib {
     /*! The 30/360 day count can be calculated according to a
         number of conventions.
 
-        US (NASD) convention: if the starting date is the 31st of a
-        month, it becomes equal to the 30th of the same month.
-        If the ending date is the 31st of a month and the starting
-        date is earlier than the 30th of a month, the ending date
-        becomes equal to the 1st of the next month, otherwise the
-        ending date becomes equal to the 30th of the same month.
-        Also known as "30/360", "360/360", or "Bond Basis".
+        US convention: if the starting date is the 31st of a month or
+        the last day of February, it becomes equal to the 30th of the
+        same month.  If the ending date is the 31st of a month and the
+        starting date is the 30th or 31th of a month, the ending date
+        becomes equal to the 30th.  If the ending date is the last of
+        February and the starting date is also the last of February,
+        the ending date becomes equal to the 30th.
+        Also known as "30/360" or "360/360".
 
-        US (ISMA) convention: if the starting date is the 31st of a
+        Bond Basis convention: if the starting date is the 31st of a
         month, it becomes equal to the 30th of the same month.
         If the ending date is the 31st of a month and the starting
         date is the 30th or 31th of a month, the ending date
         also becomes equal to the 30th of the month.
-        Also known as "Bond Basis".
+        Also known as "US (ISMA)".
 
         European convention: starting dates or ending dates that
         occur on the 31st of a month become equal to the 30th of the
@@ -57,10 +58,18 @@ namespace QuantLib {
         occur on February and are greater than 27 become equal to 30
         for computational sake.
 
-        ISDA convention: starting dates or ending dates that
-        occur on the last day of February become equal to 30
-        for computational sake, except for the termination date.
-        Also known as "30E/360 ISDA", "30/360 ISDA", or "30/360 German".
+        ISDA convention: starting or ending dates on the 31st of the
+        month become equal to 30; starting dates or ending dates that
+        occur on the last day of February also become equal to 30,
+        except for the termination date.  Also known as "30E/360
+        ISDA", "30/360 ISDA", or "30/360 German".
+
+        NASD convention: if the starting date is the 31st of a
+        month, it becomes equal to the 30th of the same month.
+        If the ending date is the 31st of a month and the starting
+        date is earlier than the 30th of a month, the ending date
+        becomes equal to the 1st of the next month, otherwise the
+        ending date becomes equal to the 30th of the same month.
 
         \ingroup daycounters
     */
@@ -87,7 +96,7 @@ namespace QuantLib {
         };
         class US_Impl : public Thirty360_Impl {
           public:
-            std::string name() const override { return std::string("30/360 (NASD)"); }
+            std::string name() const override { return std::string("30/360 (US)"); }
             Date::serial_type dayCount(const Date& d1, const Date& d2) const override;
         };
         class ISMA_Impl : public Thirty360_Impl {
@@ -114,6 +123,11 @@ namespace QuantLib {
           private:
             Date terminationDate_;
             bool isLastPeriod_;
+        };
+        class NASD_Impl : public Thirty360_Impl {
+          public:
+            std::string name() const override { return std::string("30/360 (NASD)"); }
+            Date::serial_type dayCount(const Date& d1, const Date& d2) const override;
         };
         static ext::shared_ptr<DayCounter::Impl>
         implementation(Convention c, const Date& terminationDate, bool isLastPeriod);
