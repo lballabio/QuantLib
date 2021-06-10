383f2603861960cfd64b9a8658f5234d7f16b8c3
mmencke <57640398+mmencke@users.noreply.github.com>
2021-06-10 23:53:54 +0200

    Improve CIR Discretization
    Using the Quadratic Exponential scheme of Leif Andersen to avoid "explosions" in the short-rate when the square root process gets close to zero.

--- ql/models/shortrate/onefactormodels/coxingersollross.hpp
+++ ql/models/shortrate/onefactormodels/coxingersollross.hpp
@@ -25,8 +25,7 @@
 #define quantlib_cox_ingersoll_ross_hpp
 
 #include <ql/models/shortrate/onefactormodel.hpp>
-#include <ql/stochasticprocess.hpp>
-#include <ql/processes/eulerdiscretization.hpp>
+#include <ql/processes/coxingersollrossprocess.hpp>
 
 namespace QuantLib {
 
@@ -58,6 +57,7 @@ namespace QuantLib {
 
         ext::shared_ptr<Lattice> tree(const TimeGrid& grid) const override;
 
+
         class Dynamics;
       protected:
         Real A(Time t, Time T) const override;
@@ -70,7 +70,6 @@ namespace QuantLib {
 
       private:
         class VolatilityConstraint;
-        class HelperProcess;
 
         Parameter& theta_;
         Parameter& k_;
@@ -78,33 +77,10 @@ namespace QuantLib {
         Parameter& r0_;
     };
 
-    class CoxIngersollRoss::HelperProcess : public StochasticProcess1D {
-      public:
-        HelperProcess(Real theta, Real k, Real sigma, Real y0)
-        : y0_(y0), theta_(theta), k_(k), sigma_(sigma) {
-            discretization_ =
-                ext::shared_ptr<discretization>(new EulerDiscretization);
-        }
-
-        Real x0() const override { return y0_; }
-        Real drift(Time, Real y) const override {
-            return (0.5*theta_*k_ - 0.125*sigma_*sigma_)/y
-                - 0.5*k_*y;
-        }
-        Real diffusion(Time, Real) const override { return 0.5 * sigma_; }
-
-      private:
-        Real y0_, theta_, k_, sigma_;
-    };
-
     //! %Dynamics of the short-rate under the Cox-Ingersoll-Ross model
-    /*! The state variable \f$ y_t \f$ will here be the square-root of the
-        short-rate. It satisfies the following stochastic equation
-        \f[
-            dy_t=\left[
-                    (\frac{k\theta }{2}+\frac{\sigma ^2}{8})\frac{1}{y_t}-
-                    \frac{k}{2}y_t \right] d_t+ \frac{\sigma }{2}dW_{t}
-        \f].
+    /*! The short-rate is simulated directly using the Quadratic Exponential
+        discretization scheme as described in Leif Andersen,
+        Efficient Simulation of the Heston Stochastic Volatility Model.
     */
     class CoxIngersollRoss::Dynamics :
         public OneFactorModel::ShortRateDynamics {
@@ -114,14 +90,13 @@ namespace QuantLib {
                  Real sigma,
                  Real x0)
         : ShortRateDynamics(ext::shared_ptr<StochasticProcess1D>(
-                        new HelperProcess(theta, k, sigma, std::sqrt(x0)))) {}
+                        new CoxIngersollRossProcess(k, sigma, x0, theta))) {}
 
-        Real variable(Time, Rate r) const override { return std::sqrt(r); }
-        Real shortRate(Time, Real y) const override { return y * y; }
+        Real variable(Time, Rate r) const override { return r; }
+        Real shortRate(Time, Real y) const override { return y; }
     };
 
 }
 
 
 #endif
-
