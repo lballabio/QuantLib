6fb51a741aefcc7317d9f3f414d41c7cd447d9f4
Luigi Ballabio <luigi.ballabio@gmail.com>
2021-07-12 09:46:54 +0200

    Set version to 1.24 final.

--- ql/version.hpp
+++ ql/version.hpp
@@ -31,10 +31,10 @@
 /*! @{ */
 
 //! version string
-#define QL_VERSION "1.24-rc"
+#define QL_VERSION "1.24"
 
 //! version hexadecimal number
-#define QL_HEX_VERSION 0x012400c0
+#define QL_HEX_VERSION 0x012400f0
 
 /*! @}  */
 
