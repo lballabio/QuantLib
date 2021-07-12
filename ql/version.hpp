62661ecb9fa15ac98d9e9c37de87bba825d0247c
Luigi Ballabio <luigi.ballabio@gmail.com>
2021-07-12 09:46:54 +0200

    Set version to 1.31 final.

--- ql/version.hpp
+++ ql/version.hpp
@@ -31,10 +31,10 @@
 /*! @{ */
 
 //! version string
-#define QL_VERSION "1.31rc2"
+#define QL_VERSION "1.31"
 
 //! version hexadecimal number
-#define QL_HEX_VERSION 0x013100c2
+#define QL_HEX_VERSION 0x013100f0
 
 /*! @}  */
 
