d228b6bcbc28b6c33f6b5d08395981f3174b0d68
Luigi Ballabio <luigi.ballabio@gmail.com>
2021-08-12 10:42:32 +0200

    Update docs

--- ql/userconfig.hpp
+++ ql/userconfig.hpp
@@ -62,7 +62,9 @@
 /* Define this to have singletons return different instances for
    different sessions. You will have to provide and link with the
    library a sessionId() function in namespace QuantLib, returning a
-   different session id for each session.*/
+   different session id for each session.
+   This also implies thread-safe Singleton initialization.
+*/
 #ifndef QL_ENABLE_SESSIONS
 //#   define QL_ENABLE_SESSIONS
 #endif
@@ -117,8 +119,8 @@
 #endif
 
 /* Define this to make Singleton initialization thread-safe.
-   Note: QL_ENABLE_SESSIONS implies thread safe initialization no matter
-         if defined or not.
+   Note that defining QL_ENABLE_SESSIONS implies thread-safe
+   initialization, whether this is defined or not.
 */
 #ifndef QL_ENABLE_SINGLETON_THREAD_SAFE_INIT
 //#   define QL_ENABLE_SINGLETON_THREAD_SAFE_INIT
