fd29f9a8542ee0d71eeeac2fc9c28b765a173276
lewwe <lewweihao93@hotmail.com>
2021-08-15 14:58:04 +0800

    Revert

--- ql/experimental/credit/riskybond.cpp
+++ ql/experimental/credit/riskybond.cpp
@@ -40,15 +40,19 @@ namespace QuantLib {
     : name_(std::move(name)), ccy_(std::move(ccy)), recoveryRate_(recoveryRate),
       defaultTS_(std::move(defaultTS)), yieldTS_(std::move(yieldTS)),
       settlementDays_(settlementDays), calendar_(std::move(calendar)) {
-        registerWith(yieldTS_);
-        registerWith(defaultTS_);
-        // the two above might not be registered with evalDate
+        registerWith (yieldTS_);
+        registerWith (defaultTS_);
+        //the two above might not be registered with evalDate
         registerWith(Settings::instance().evaluationDate());
     }
 
-    bool RiskyBond::isExpired() const { return detail::simple_event(maturityDate()).hasOccurred(); }
+    bool RiskyBond::isExpired() const {
+        return detail::simple_event(maturityDate()).hasOccurred();
+    }
 
-    void RiskyBond::setupExpired() const { Instrument::setupExpired(); }
+    void RiskyBond::setupExpired() const {
+        Instrument::setupExpired();
+    }
 
     void RiskyBond::performCalculations() const {
         NPV_ = 0;
@@ -59,13 +63,13 @@ namespace QuantLib {
         for (auto& i : cf) {
             Date d2 = i->date();
             if (d2 > npvDate) {
-                d1 = max(npvDate, d1);
-                Date defaultDate = d1 + (d2 - d1) / 2;
+                d1 = max(npvDate , d1);
+                Date defaultDate = d1 + (d2-d1)/2;
 
                 Real coupon = i->amount() * defaultTS_->survivalProbability(d2);
-                Real recovery =
-                    notional(defaultDate) * recoveryRate_ *
-                    (defaultTS_->survivalProbability(d1) - defaultTS_->survivalProbability(d2));
+                Real recovery = notional(defaultDate) * recoveryRate_
+                    * (defaultTS_->survivalProbability(d1)
+                       -defaultTS_->survivalProbability(d2));
                 NPV_ += coupon * yieldTS()->discount(d2);
                 NPV_ += recovery * yieldTS()->discount(defaultDate);
             }
@@ -109,16 +113,18 @@ namespace QuantLib {
             Date d2 = i->date();
             if (d2 > npvDate) {
                 d1 = max(npvDate, d1);
-                Date defaultDate = d1 + (d2 - d1) / 2;
+                Date defaultDate = d1 + (d2-d1)/2;
 
                 Real coupon = i->amount() * defaultTS_->survivalProbability(d2);
-                Real recovery =
-                    notional(defaultDate) * recoveryRate_ *
-                    (defaultTS_->survivalProbability(d1) - defaultTS_->survivalProbability(d2));
-                ext::shared_ptr<CashFlow> flow1(new SimpleCashFlow(coupon, d2));
+                Real recovery = notional(defaultDate) * recoveryRate_
+                    * (defaultTS_->survivalProbability(d1)
+                       -defaultTS_->survivalProbability(d2));
+                ext::shared_ptr<CashFlow>
+                    flow1(new SimpleCashFlow(coupon, d2));
                 expected.push_back(flow1);
 
-                ext::shared_ptr<CashFlow> flow2(new SimpleCashFlow(recovery, defaultDate));
+                ext::shared_ptr<CashFlow>
+                    flow2(new SimpleCashFlow(recovery, defaultDate));
                 expected.push_back(flow2);
             }
             d1 = d2;
@@ -146,32 +152,37 @@ namespace QuantLib {
         std::vector<Date> dates = schedule_.dates();
         Real previousNotional = notionals_.front();
         for (Size i = 1; i < dates.size(); i++) {
-            Real currentNotional = (i < notionals_.size() ? notionals_[i] : notionals_.back());
-            ext::shared_ptr<CashFlow> interest(new FixedRateCoupon(
-                dates[i], previousNotional, rate_, dayCounter_, dates[i - 1], dates[i]));
-            ext::shared_ptr<CashFlow> amortization(
-                new AmortizingPayment(previousNotional - currentNotional, dates[i]));
+            Real currentNotional = (i < notionals_.size() ?
+                             notionals_[i] :
+                             notionals_.back());
+            ext::shared_ptr<CashFlow> interest (new
+                   FixedRateCoupon(dates[i], previousNotional,
+                                   rate_, dayCounter_, dates[i-1], dates[i]));
+            ext::shared_ptr<CashFlow> amortization(new
+                 AmortizingPayment(previousNotional - currentNotional, dates[i]));
             previousNotional = currentNotional;
 
             leg_.push_back(interest);
             interestLeg_.push_back(interest);
-            if (amortization->amount() != 0) {
+            if (amortization->amount() != 0){
                 leg_.push_back(amortization);
                 redemptionLeg_.push_back(amortization);
             }
         }
 
-        ext::shared_ptr<CashFlow> redemption(
-            new Redemption(previousNotional, schedule_.dates().back()));
+        ext::shared_ptr<CashFlow> redemption(new
+                 Redemption(previousNotional, schedule_.dates().back()));
         leg_.push_back(redemption);
         redemptionLeg_.push_back(redemption);
     }
 
-    std::vector<ext::shared_ptr<CashFlow> > RiskyFixedBond::cashflows() const { return leg_; }
-    std::vector<ext::shared_ptr<CashFlow> > RiskyFixedBond::interestFlows() const {
+    std::vector<ext::shared_ptr<CashFlow> > RiskyFixedBond::cashflows() const{
+        return leg_;
+    }
+    std::vector<ext::shared_ptr<CashFlow> > RiskyFixedBond::interestFlows() const{
         return interestLeg_;
     }
-    std::vector<ext::shared_ptr<CashFlow> > RiskyFixedBond::notionalFlows() const {
+    std::vector<ext::shared_ptr<CashFlow> > RiskyFixedBond::notionalFlows() const{
         return redemptionLeg_;
     }
 
@@ -188,9 +199,13 @@ namespace QuantLib {
         return ntl;
     }
 
-    Date RiskyFixedBond::effectiveDate() const { return schedule_.dates().front(); }
+    Date RiskyFixedBond::effectiveDate() const {
+        return schedule_.dates().front();
+    }
 
-    Date RiskyFixedBond::maturityDate() const { return schedule_.dates().back(); }
+    Date RiskyFixedBond::maturityDate() const {
+        return schedule_.dates().back();
+    }
 
     //------------------------------------------------------------------------
     RiskyFloatingBond::RiskyFloatingBond(const std::string& name,
@@ -212,39 +227,47 @@ namespace QuantLib {
         std::vector<Date> dates = schedule_.dates();
         Real previousNotional = notionals_.front();
         for (Size i = 1; i < dates.size(); i++) {
-            Real currentNotional = (i < notionals_.size() ? notionals_[i] : notionals_.back());
-            ext::shared_ptr<CashFlow> interest(new IborCoupon(dates[i], previousNotional,
-                                                              dates[i - 1], dates[i], fixingDays_,
-                                                              index_, 1.0, spread_));
-            ext::shared_ptr<CashFlow> amortization(
-                new AmortizingPayment(previousNotional - currentNotional, dates[i]));
+            Real currentNotional = (i < notionals_.size() ?
+                             notionals_[i] :
+                             notionals_.back());
+            ext::shared_ptr<CashFlow> interest (new
+                   IborCoupon(dates[i], previousNotional, dates[i-1], dates[i],
+                              fixingDays_, index_, 1.0, spread_));
+            ext::shared_ptr<CashFlow> amortization(new
+                 AmortizingPayment(previousNotional - currentNotional, dates[i]));
             previousNotional = currentNotional;
 
             leg_.push_back(interest);
             interestLeg_.push_back(interest);
-            if (amortization->amount() != 0) {
+            if (amortization->amount() != 0){
                 leg_.push_back(amortization);
                 redemptionLeg_.push_back(amortization);
             }
         }
 
-        ext::shared_ptr<CashFlow> redemption(
-            new Redemption(previousNotional, schedule_.dates().back()));
+        ext::shared_ptr<CashFlow> redemption(new
+                 Redemption(previousNotional, schedule_.dates().back()));
         leg_.push_back(redemption);
         redemptionLeg_.push_back(redemption);
 
-        ext::shared_ptr<IborCouponPricer> fictitiousPricer(
-            new BlackIborCouponPricer(Handle<OptionletVolatilityStructure>()));
-        setCouponPricer(leg_, fictitiousPricer);
+        ext::shared_ptr<IborCouponPricer>
+            fictitiousPricer(new
+                BlackIborCouponPricer(Handle<OptionletVolatilityStructure>()));
+        setCouponPricer(leg_,fictitiousPricer);
     }
 
-    std::vector<ext::shared_ptr<CashFlow> > RiskyFloatingBond::cashflows() const { return leg_; }
+    std::vector<ext::shared_ptr<CashFlow> > RiskyFloatingBond::cashflows()
+        const {
+        return leg_;
+    }
 
-    std::vector<ext::shared_ptr<CashFlow> > RiskyFloatingBond::interestFlows() const {
+    std::vector<ext::shared_ptr<CashFlow> > RiskyFloatingBond::interestFlows()
+    const {
         return interestLeg_;
     }
 
-    std::vector<ext::shared_ptr<CashFlow> > RiskyFloatingBond::notionalFlows() const {
+    std::vector<ext::shared_ptr<CashFlow> > RiskyFloatingBond::notionalFlows()
+    const {
         return redemptionLeg_;
     }
 
@@ -261,8 +284,13 @@ namespace QuantLib {
         return ntl;
     }
 
-    Date RiskyFloatingBond::effectiveDate() const { return schedule_.dates().front(); }
+    Date RiskyFloatingBond::effectiveDate() const {
+        return schedule_.dates().front();
+    }
 
-    Date RiskyFloatingBond::maturityDate() const { return schedule_.dates().back(); }
+    Date RiskyFloatingBond::maturityDate() const {
+        return schedule_.dates().back();
+    }
 
 }
+
