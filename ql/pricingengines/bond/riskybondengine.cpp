1b1a79fcc7c203140c601a6fd81da88ebcf222c3
Luigi Ballabio <luigi.ballabio@gmail.com>
2021-08-24 12:25:02 +0200

    Fix recovery dates in some cases

--- ql/pricingengines/bond/riskybondengine.cpp
+++ ql/pricingengines/bond/riskybondengine.cpp
@@ -38,26 +38,26 @@ namespace QuantLib {
         Real NPV_ = 0;
         Date npvDate = arguments_.settlementDate;
         std::vector<ext::shared_ptr<CashFlow> > cf = arguments_.cashflows;
-        Date d1 = CashFlows::startDate(arguments_.cashflows);
+        Date startDate = CashFlows::startDate(arguments_.cashflows);
+        Date d1 = std::max(npvDate, startDate);
+
         for (auto& i : cf) {
             Date d2 = i->date();
             if (d2 > npvDate) {
-                d1 = std::max(npvDate, d1);
-                Date defaultDate = d1 + (d2 - d1) / 2;
 
                 Real couponAmount = i->amount() * defaultTS()->survivalProbability(d2);
+                NPV_ += couponAmount * yieldTS()->discount(d2);
 
                 auto coupon = ext::dynamic_pointer_cast<Coupon>(i);
                 if (coupon != nullptr) {
+                    Date defaultDate = d1 + (d2 - d1) / 2;
                     Real recovery = coupon->nominal() * recoveryRate() *
                                     (defaultTS()->survivalProbability(d1) -
                                      defaultTS()->survivalProbability(d2)); 
                     NPV_ += recovery * yieldTS()->discount(defaultDate);
+                    d1 = d2;
                 }
-                
-                NPV_ += couponAmount * yieldTS()->discount(d2);
             }
-            d1 = d2;
         }
 
         results_.value = NPV_;
