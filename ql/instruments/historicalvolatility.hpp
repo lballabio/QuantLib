40db15b30a42e7e42258ed2aac02dc07949e7d18
laaouini <laaouini_anas@yahoo.fr>
2021-09-09 18:24:20 +0200

    Update historicalvolatility.hpp

--- ql/instruments/historicalvolatility.hpp
+++ ql/instruments/historicalvolatility.hpp
@@ -11,41 +11,43 @@ namespace QuantLib {
     class HistoricalVolatility {
       public:
         static Volatility calculateHistoricalVolatility(vector<double>& stockPrices,
-                                                 double lengthOfTimeIntervalInYears) {
-            QL_ENSURE(3 <= stockPrices.size(),"the stockPrice vector must have a size at least equal to 3");
-            double result = 0;
+          double lengthOfTimeIntervalInYears) {
+          QL_ENSURE(3 <= stockPrices.size(), "the stockPrice vector must have a size at least equal to 3");
+          double result = 0;
 
-            double currentStock;
-            double previousStock;
+          double currentStock;
+          double previousStock;
 
-            double logreturn = 0;
-            vector<double> logreturns;
-            double meanlogreturn = 0;
-            for (int i = 0; i < stockPrices.size(); i++) {
-                currentStock = stockPrices[i];
-                if (0 < i) {
-                    logreturn = log(currentStock / previousStock);
+          double logreturn = 0;
+          vector<double> logreturns;
+          double meanlogreturn = 0;
+          int size = stockPrices.size();
+          for (int i = 0; i < size; i++) {
+              currentStock = stockPrices[i];
+              if (0 < i) {
+                  logreturn = log(currentStock / previousStock);
 
-                    logreturns.push_back(logreturn);
-                    meanlogreturn = meanlogreturn + logreturn;
-                }
-                previousStock = currentStock;
-            }
-            meanlogreturn = meanlogreturn / (logreturns.size());
+                  logreturns.push_back(logreturn);
+                  meanlogreturn = meanlogreturn + logreturn;
+              }
+              previousStock = currentStock;
+          }
+          meanlogreturn = meanlogreturn / (double)(size - 1);
 
-            double standarddeviation = 0;
-            double element = 0;
-            double element2 = 0;
-            for (int j = 0; j < logreturns.size(); j++) {
-                element = meanlogreturn - logreturns[j];
-                element2 = element * element;
-                standarddeviation = standarddeviation + element2;
-            }
-            standarddeviation = standarddeviation / (logreturns.size() - 1);
-            standarddeviation = sqrt(standarddeviation);
-            result = standarddeviation / sqrt(lengthOfTimeIntervalInYears);
-            return result;
-        }
+          double standarddeviation = 0;
+          double element = 0;
+          double element2 = 0;
+          for (int j = 0; j < size - 1; j++) {
+              element = meanlogreturn - logreturns[j];
+              element2 = element * element;
+              standarddeviation = standarddeviation + element2;
+          }
+          standarddeviation = standarddeviation / (double)(size - 2);
+          standarddeviation = sqrt(standarddeviation);
+          result = standarddeviation / sqrt(lengthOfTimeIntervalInYears);
+          return result;
+      }
+
     };
 
 
