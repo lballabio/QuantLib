a50b4249d31cca14fd0034f851e7fd67fe5820d8
Luigi Ballabio <luigi.ballabio@gmail.com>
2021-09-15 18:12:59 +0200

    Restore filelist check on CMakeLists

--- tools/check_filelists.sh
+++ tools/check_filelists.sh
@@ -50,8 +50,8 @@ grep -o -E 'Include=".*\.[hc]p*"' test-suite/testsuite.vcxproj.filters \
 # same with CMakelists
 
 # reference files above align only to autotools build system
-#grep -o -E '[a-zA-Z0-9_/\.]*\.[hc]p*' ql/CMakeLists.txt \
-#| sed -e 's|^|ql/|' | sort > ql.cmake.files
+grep -o -E '[a-zA-Z0-9_/\.]*\.[hc]p*' ql/CMakeLists.txt \
+| grep -v '/cmake/' | grep -v 'ql/config\.hpp' | sed -e 's|/ql/||' | sed -e 's|^|ql/|' | sort > ql.cmake.files
 
 grep -o -E '[a-zA-Z0-9_/\.]*\.[hc]pp' test-suite/CMakeLists.txt \
 | grep -v 'quantlibbenchmark' | grep -v 'main\.cpp' \
