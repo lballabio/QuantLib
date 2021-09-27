4578bdccc0a60e38e7a001254b5e0318d56cc255
Luigi Ballabio <luigi.ballabio@gmail.com>
2021-09-27 11:52:15 +0200

    Output new rule to iostreams

--- ql/time/dategenerationrule.hpp
+++ ql/time/dategenerationrule.hpp
@@ -46,10 +46,10 @@ namespace QuantLib {
                                  and termination date. */
             ThirdWednesday,  /*!< All dates but effective date and termination
                                   date are taken to be on the third wednesday
-                                  of their month (with forward calculation. ) */
+                                  of their month (with forward calculation.) */
             ThirdWednesdayInclusive, /*!< All dates including effective date and termination
-                                  date are taken to be on the third wednesday
-                                  of their month (with forward calculation) */
+                                          date are taken to be on the third wednesday
+                                          of their month (with forward calculation.) */
             Twentieth,      /*!< All dates but the effective date are
                                  taken to be the twentieth of their
                                  month (used for CDS schedules in
