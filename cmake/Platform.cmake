235bf8655332d0b27171263379fccda9b19b7e52
RalfKonrad <github@eckel-consulting.de>
2021-10-05 15:48:08 +0200

    Make warnings as errors configurable via QL_TREAT_WARNINGS_AS_ERRORS with default OFF

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -26,5 +26,15 @@ if (MSVC)
 else()
     # warning level in line with the .github/worksflows/linux.yml compile options
     # for gcc and clang
-    add_compile_options(-Wall -Wno-unknown-pragmas -Werror)
+    add_compile_options(-Wall -Wno-unknown-pragmas)
+endif()
+
+# Add compiler option to cause the build to fail 
+# when a warning is emitteg
+if(QL_TREAT_WARNINGS_AS_ERRORS)
+    if(MSVC)
+        add_compile_options(/WX)
+    else()
+        add_compile_options(-Werror)
+    endif(MSVC)
 endif()
