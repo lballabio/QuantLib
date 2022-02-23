e4d2580b7460867e78c279af1c128caf39209ab7
3332620 <jonghee.lee@miraeasset.com>
2022-02-23 17:17:33 +0900

    edit copyright

--- ql/indexes/ibor/tona.hpp
+++ ql/indexes/ibor/tona.hpp
@@ -1,7 +1,7 @@
 /* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
-Copyright (C) 2016 Fabrice Lecuyer
+Copyright (C) 2022 Jonghee Lee
 
 This file is part of QuantLib, a free-software/open-source library
 for financial quantitative analysts and developers - http://quantlib.org/
