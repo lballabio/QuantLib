b47371f4925d817fce1ded5d057bf998ed43260b
Jonathan Sweemer <sweemer@gmail.com>
2022-04-16 10:51:45 +0900

    Deprecate public typedefs in functional

--- ql/math/functional.hpp
+++ ql/math/functional.hpp
@@ -36,7 +36,9 @@ namespace QuantLib {
     template <class T, class U>
     class constant {
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef U result_type;
         explicit constant(const U& u) : u_(u) {}
         U operator()(const T&) const { return u_; }
@@ -47,7 +49,9 @@ namespace QuantLib {
     template <class T>
     class identity {
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef T result_type;
         T operator()(const T& t) const { return t; }
     };
@@ -55,7 +59,9 @@ namespace QuantLib {
     template <class T>
     class square {
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef T result_type;
         T operator()(const T& t) const { return t*t; }
     };
@@ -63,7 +69,9 @@ namespace QuantLib {
     template <class T>
     class cube {
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef T result_type;
         T operator()(const T& t) const { return t*t*t; }
     };
@@ -71,7 +79,9 @@ namespace QuantLib {
     template <class T>
     class fourth_power {
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef T result_type;
         T operator()(const T& t) const { T t2 = t*t; return t2*t2; }
     };
@@ -82,9 +92,10 @@ namespace QuantLib {
     class add {
         T y;
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef Real result_type;
-
         explicit add(Real y) : y(y) {}
         Real operator()(T x) const { return x + y; }
     };
@@ -93,9 +104,10 @@ namespace QuantLib {
     class subtract {
         T y;
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef Real result_type;
-
         explicit subtract(Real y) : y(y) {}
         Real operator()(T x) const { return x - y; }
     };
@@ -104,9 +116,10 @@ namespace QuantLib {
     class subtract_from {
         T y;
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef Real result_type;
-
         explicit subtract_from(Real y) : y(y) {}
         Real operator()(T x) const { return y - x; }
     };
@@ -115,9 +128,10 @@ namespace QuantLib {
     class multiply_by {
         T y;
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef Real result_type;
-
         explicit multiply_by(Real y) : y(y) {}
         Real operator()(T x) const { return x * y; }
     };
@@ -126,9 +140,10 @@ namespace QuantLib {
     class divide {
         T y;
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef Real result_type;
-
         explicit divide(Real y) : y(y) {}
         Real operator()(T x) const { return y / x; }
     };
@@ -137,9 +152,10 @@ namespace QuantLib {
     class divide_by {
         T y;
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef Real result_type;
-
         explicit divide_by(Real y) : y(y) {}
         Real operator()(T x) const { return x / y; }
     };
@@ -148,9 +164,10 @@ namespace QuantLib {
     class less_than {
         T y;
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef bool result_type;
-
         explicit less_than(Real y) : y(y) {}
         bool operator()(T x) const { return x < y; }
     };
@@ -159,9 +176,10 @@ namespace QuantLib {
     class greater_than {
         T y;
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef bool result_type;
-
         explicit greater_than(Real y) : y(y) {}
         bool operator()(T x) const { return x > y; }
     };
@@ -170,9 +188,10 @@ namespace QuantLib {
     class greater_or_equal_to {
         T y;
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef bool result_type;
-
         explicit greater_or_equal_to(Real y) : y(y) {}
         bool operator()(T x) const { return x >= y; }
     };
@@ -180,7 +199,9 @@ namespace QuantLib {
     template <class T>
     class not_zero {
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef bool result_type;
         bool operator()(T x) const { return x != T(); }
     };
@@ -189,9 +210,10 @@ namespace QuantLib {
     class not_null {
         T null;
       public:
+        QL_DEPRECATED
         typedef T argument_type;
+        QL_DEPRECATED
         typedef bool result_type;
-
         not_null() : null(Null<T>()) {}
         bool operator()(T x) const { return x != null; }
     };
@@ -211,10 +233,12 @@ namespace QuantLib {
     template <class T>
     class equal_within {
       public:
+        QL_DEPRECATED
         typedef T first_argument_type;
+        QL_DEPRECATED
         typedef T second_argument_type;
+        QL_DEPRECATED
         typedef bool result_type;
-
         explicit equal_within(const T& eps) : eps_(eps) {}
         bool operator()(const T& a, const T& b) const {
             return std::fabs(a-b) <= eps_;
@@ -227,12 +251,17 @@ namespace QuantLib {
     template <class F, class R>
     class clipped_function {
       public:
+        QL_DEPRECATED
         typedef typename F::argument_type argument_type;
+        QL_DEPRECATED
         typedef typename F::result_type result_type;
+        QL_DEPRECATED
         clipped_function(const F& f, const R& r) : f_(f), r_(r) {}
+        QL_DEPRECATED_DISABLE_WARNING
         result_type operator()(const argument_type& x) const {
             return r_(x) ? f_(x) : result_type();
         }
+        QL_DEPRECATED_ENABLE_WARNING
       private:
         F f_;
         R r_;
@@ -244,21 +273,28 @@ namespace QuantLib {
         return [f_ = std::forward<F>(f), r_ = std::forward<R>(r)](const auto& x) { return r_(x) ? f_(x) : decltype(f_(x)){}; };
     }
 #else
+    QL_DEPRECATED_DISABLE_WARNING
     template <class F, class R>
     clipped_function<F,R> clip(const F& f, const R& r) {
         return clipped_function<F,R>(f,r);
     }
+    QL_DEPRECATED_ENABLE_WARNING
 #endif
 
     template <class F, class G>
     class composed_function {
       public:
+        QL_DEPRECATED
         typedef typename G::argument_type argument_type;
+        QL_DEPRECATED
         typedef typename F::result_type result_type;
+        QL_DEPRECATED
         composed_function(const F& f, G g) : f_(f), g_(std::move(g)) {}
+        QL_DEPRECATED_DISABLE_WARNING
         result_type operator()(const argument_type& x) const {
             return f_(g_(x));
         }
+        QL_DEPRECATED_ENABLE_WARNING
       private:
         F f_;
         G g_;
@@ -270,26 +306,32 @@ namespace QuantLib {
         return [f_ = std::forward<F>(f), g_ = std::forward<G>(g)](const auto& x) { return f_(g_(x)); };
     }
 #else
+    QL_DEPRECATED_DISABLE_WARNING
     template <class F, class G>
     composed_function<F,G> compose(const F& f, const G& g) {
         return composed_function<F,G>(f,g);
     }
+    QL_DEPRECATED_ENABLE_WARNING
 #endif
 
     template <class F, class G, class H>
     class binary_compose3_function {
       public:
+        QL_DEPRECATED
         typedef typename G::argument_type first_argument_type;
+        QL_DEPRECATED
         typedef typename H::argument_type second_argument_type;
+        QL_DEPRECATED
         typedef typename F::result_type result_type;
-
+        QL_DEPRECATED
         binary_compose3_function(const F& f, const G& g, const H& h)
         : f_(f), g_(g), h_(h) {}
-
+        QL_DEPRECATED_DISABLE_WARNING
         result_type operator()(const first_argument_type&  x,
                                const second_argument_type& y) const {
             return f_(g_(x), h_(y));
         }
+        QL_DEPRECATED_ENABLE_WARNING
 
       private:
         F f_;
@@ -303,10 +345,12 @@ namespace QuantLib {
         return [f_ = std::forward<F>(f), g_ = std::forward<G>(g), h_ = std::forward<H>(h)](const auto& x, const auto& y) { return f_(g_(x), h_(y)); };
     }
 #else
+    QL_DEPRECATED_DISABLE_WARNING
     template <class F, class G, class H> binary_compose3_function<F, G, H>
     compose3(const F& f, const G& g, const H& h) {
         return binary_compose3_function<F, G, H>(f, g, h);
     }
+    QL_DEPRECATED_ENABLE_WARNING
 #endif
 }
 
