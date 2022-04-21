465f0e8fc3f17322c1d9a93eed879edc1d53ac9e
Fredrik Gerdin Börjesson <fredrik.gerdin.borjesson@hotmail.com>
2022-04-21 17:43:01 +0200

    Add docstrings to SviSmileSection constructors

--- ql/experimental/volatility/svismilesection.hpp
+++ ql/experimental/volatility/svismilesection.hpp
@@ -33,11 +33,23 @@ namespace QuantLib {
 class SviSmileSection : public SmileSection {
 
   public:
+    //! \name Constructors
+    //@{
+    /*! @param timeToExpiry Time to expiry
+        @param forward Forward price corresponding to the expiry date
+        @param sviParameters Expects SVI parameters as a vector composed of a, b, sigma, rho, s, m
+    */
     SviSmileSection(Time timeToExpiry, Rate forward, std::vector<Real> sviParameters);
+    /*! @param d Date of expiry
+        @param forward Forward price corresponding to the expiry date
+        @param sviParameters Expects SVI parameters as a vector composed of a, b, sigma, rho, s, m
+        @param dc Day count method used to compute the time to expiry
+    */
     SviSmileSection(const Date& d,
                     Rate forward,
                     std::vector<Real> sviParameters,
                     const DayCounter& dc = Actual365Fixed());
+    //@}
     Real minStrike() const override { return 0.0; }
     Real maxStrike() const override { return QL_MAX_REAL; }
     Real atmLevel() const override { return forward_; }
