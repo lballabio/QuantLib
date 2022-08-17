b7ec573f861a8cab778b5008fc80cd37ce90c0a8
Ignacio Anguita <i.anguita13@gmail.com>
2022-08-17 10:01:43 +0200

    Adding Feedback

--- ql/time/daycounters/actual36525.hpp
+++ ql/time/daycounters/actual36525.hpp
@@ -1,14 +1,17 @@
 /* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
- Copyright (C) 2000, 2001, 2002, 2003 RiskMap srl
+ Copyright (C) 2022 Ignacio Anguita
+ 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
+ 
  QuantLib is free software: you can redistribute it and/or modify it
  under the terms of the QuantLib license.  You should have received a
  copy of the license along with this program; if not, please email
  <quantlib-dev@lists.sf.net>. The license is also available online at
  <http://quantlib.org/license.shtml>.
+ 
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the license for more details.
@@ -48,8 +51,7 @@ namespace QuantLib {
             }
             Time
             yearFraction(const Date& d1, const Date& d2, const Date&, const Date&) const override {
-                return (daysBetween(d1,d2)
-                        + (includeLastDay_ ? 1.0 : 0.0))/365.25;
+                return dayCount(d1,d2)/365.25;
             }
         };
       public:
