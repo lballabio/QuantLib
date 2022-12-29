5faade57eb0b1f5e1d6f404f6291189b5e98d34a
Luigi Ballabio <luigi.ballabio@gmail.com>
2022-12-29 13:17:23 +0100

    Use both processors for CI header check

--- tools/check_header.py
+++ tools/check_header.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python -u
 
+import multiprocessing as mp
 import os
 import sys
 
-errors = 0
 
-for line in sys.stdin.readlines():
-    header = line.strip()
-
-    print("Checking %s" % header)
-    command = "g++ -c -Wno-unknown-pragmas -Wall -Werror -I. %s -o /dev/null" % header
+def check(header):
+    print("Checking %s" % header, file=sys.stderr)
+    command = "g++ -std=c++14 -c -Wno-unknown-pragmas -Wall -Werror -I. %s -o /dev/null" % header
     code = os.system(command)
     if code != 0:
-        print("Errors while checking %s" % header)
-        errors += 1
+        print("Errors while checking %s" % header, file=sys.stderr)
+        return 1
+    return 0
 
-sys.exit(errors)
+
+if __name__ == "__main__":
+    headers = [line.strip() for line in sys.stdin.readlines()]
+    with mp.Pool(processes=mp.cpu_count()) as pool:
+        errors = sum(pool.map(check, headers))
+    sys.exit(errors)
