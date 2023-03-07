70ec6d5ee84926f3a65c4ed31f8b421d47c85cc1
Peter Caspers <peter.caspers@acadia.inc>
2023-03-07 15:08:58 +0100

    Revert "fix warning c4250"
    This reverts commit 31bd92d6be307462701ef81288cb30b011058a2a.

--- ql/termstructures/yield/fittedbonddiscountcurve.hpp
+++ ql/termstructures/yield/fittedbonddiscountcurve.hpp
@@ -120,8 +120,6 @@ namespace QuantLib {
         //! \name Observer interface
         //@{
         void update() override;
-        std::pair<Observer::iterator, bool>
-        registerWith(const ext::shared_ptr<Observable>& o) override;
         //@}
 
       private:
@@ -338,11 +336,6 @@ namespace QuantLib {
         } else {
             return discountFunction(x, t);
         }
-    }
-
-    inline std::pair<Observer::iterator, bool>
-    FittedBondDiscountCurve::registerWith(const ext::shared_ptr<Observable>& o) {
-        return LazyObject::registerWith(o);
     }
 }
 
