464800bf0d26b7e4cac27eb876310cec3094f643
Peter Caspers <peter.caspers@acadia.inc>
2023-03-07 15:08:45 +0100

    Revert "avoid c4250"
    This reverts commit 64a72ead06acc925e3f448881f63a78a2a9fb808.

--- ql/termstructures/inflation/piecewiseyoyinflationcurve.hpp
+++ ql/termstructures/inflation/piecewiseyoyinflationcurve.hpp
@@ -88,8 +88,6 @@ namespace QuantLib {
         //! \name Observer interface
         //@{
         void update() override;
-        std::pair<Observer::iterator, bool>
-        registerWith(const ext::shared_ptr<Observable>& o) override;
         //@}
       private:
         // methods
@@ -154,11 +152,6 @@ namespace QuantLib {
         LazyObject::update();
     }
 
-    template <class I, template <class> class B, class T>
-    std::pair<Observer::iterator, bool>
-    PiecewiseYoYInflationCurve<I,B,T>::registerWith(const ext::shared_ptr<Observable>& o) {
-        return LazyObject::registerWith(o);
-    }
 }
 
 #endif
