1ca515fd766afec155e4c97a4c24e2574d2c9f50
Peter Caspers <pcaspers1973@gmail.com>
2023-03-18 18:02:05 +0100

    Update test-suite/piecewiseyieldcurve.cpp
    Co-authored-by: Luigi Ballabio <luigi.ballabio@gmail.com>

--- test-suite/piecewiseyieldcurve.cpp
+++ test-suite/piecewiseyieldcurve.cpp
@@ -825,7 +825,7 @@ void PiecewiseYieldCurveTest::testObservability() {
                                                    vars.instruments,
                                                    Actual360()));
 
-    boost::dynamic_pointer_cast<LazyObject>(vars.termStructure)->forwardFirstNotificationOnly();
+    ext::dynamic_pointer_cast<LazyObject>(vars.termStructure)->forwardFirstNotificationOnly();
 
     Flag f;
     f.registerWith(vars.termStructure);
