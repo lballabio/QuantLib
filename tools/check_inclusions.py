d9caed0bcefd9b360e9f006a90c975b2caf49a75
Luigi Ballabio <luigi.ballabio@gmail.com>
2023-04-11 15:17:42 +0200

    Convert tool to Python 3 syntax

--- tools/check_inclusions.py
+++ tools/check_inclusions.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys, re
 
 if len(sys.argv) != 2:
-    print 'Usage: %s <file>' % sys.argv[0]
+    print(f"Usage: {sys.argv[0]} <file>")
     sys.exit()
 
 filename = sys.argv[1]
-print 'Checking %s' % filename
+print(f"Checking {filename}")
 
-regex = re.compile('^#include.*<(.*)>')
+regex = re.compile("^#include.*<(.*)>")
 ql_headers = []
 boost_headers = []
 std_headers = []
@@ -18,54 +18,59 @@ source = open(filename)
 for n, line in enumerate(source):
     match = regex.search(line)
     if match:
-        header, = match.groups()
-        line_num = n+1
-        if header.startswith('ql/'):
-            ql_headers.append((header,line_num))
-            if header.startswith('ql/experimental') and 'internal' not in line:
-                experimental_headers.append((header,line_num))
-        elif header.startswith('boost/'):
-            boost_headers.append((header,line_num))
+        (header,) = match.groups()
+        line_num = n + 1
+        if header.startswith("ql/"):
+            ql_headers.append((header, line_num))
+            if header.startswith("ql/experimental") and "internal" not in line:
+                experimental_headers.append((header, line_num))
+        elif header.startswith("boost/"):
+            boost_headers.append((header, line_num))
         else:
-            std_headers.append((header,line_num))
+            std_headers.append((header, line_num))
 source.close()
 
 # At least one QuantLib header muct be included (which, hopefully,
 # ultimately leads to including ql/qldefines.hpp)
 if not ql_headers:
-    print "./%s:1: error: no QuantLib header included" % filename
+    print(f"./{filename}:1: error: no QuantLib header included")
     sys.exit(1)
 
-if 'ql/experimental' not in filename:
+if "ql/experimental" not in filename:
     # files in core library can't include stuff in experimental
     if experimental_headers:
-        for f,n in experimental_headers:
-            print "./%s:%d: error: experimental header '%s' included" % (filename, n, f)
+        for f, n in experimental_headers:
+            print(f"./{filename}:{n}: error: experimental header '{f}' included")
         sys.exit(1)
 
-for f,n in ql_headers:
-    if f.endswith('/all.hpp'):
-        print "./%s:%d: error: generated header '%s' included" % (filename, n, f)
+for f, n in ql_headers:
+    if f.endswith("/all.hpp"):
+        print(f"./{filename}:{n}: error: generated header '{f}' included")
         sys.exit(1)
 
 # All Boost headers must be included after QuantLib ones
-last_ql_header = max([ n for _,n in ql_headers])
+last_ql_header = max([n for _, n in ql_headers])
 
-for _,n in boost_headers:
+for _, n in boost_headers:
     if n < last_ql_header:
-        print "./%s:%d: error: Boost header included before last QuantLib header" % (filename, n)
+        print(
+            f"./{filename}:{n}: error: Boost header included before last QuantLib header"
+        )
         sys.exit(1)
 
 
 # All standard headers must be included after QuantLib and Boost ones
 if boost_headers:
-    last_boost_header = max([ n for _,n in boost_headers])
+    last_boost_header = max([n for _, n in boost_headers])
 
-for _,n in std_headers:
+for _, n in std_headers:
     if n < last_ql_header:
-        print "./%s:%d: error: standard header included before last QuantLib header" % (filename, n)
+        print(
+            f"./{filename}:{n}: error: standard header included before last QuantLib header"
+        )
         sys.exit(1)
     if boost_headers and n < last_boost_header:
-        print "./%s:%d: error: standard header included before last Boost header" % (filename, n)
+        print(
+            f"./{filename}:{n}: error: standard header included before last Boost header"
+        )
         sys.exit(1)
-
