39d1be9e76cc131d189e3c910e5f88a754fa292a
amiemiec <133022689+amiemiec@users.noreply.github.com>
2023-05-11 11:55:38 +0200

    Enable normal volatilities in Hagan pricer for CMS coupons
    Added the fallback to the forwardingTS if the discountingTS is a null pointer

--- ql/cashflows/conundrumpricer.cpp
+++ ql/cashflows/conundrumpricer.cpp
@@ -95,7 +95,7 @@ namespace QuantLib {
         fixingDate_ = coupon_->fixingDate();
         paymentDate_ = coupon_->date();
         const ext::shared_ptr<SwapIndex>& swapIndex = coupon_->swapIndex();
-        rateCurve_ = *(swapIndex->discountingTermStructure());
+        rateCurve_ = *(swapIndex->discountingTermStructure()) ? *(swapIndex->discountingTermStructure()) : *(swapIndex->forwardingTermStructure());
 
         Date today = Settings::instance().evaluationDate();
 
