c26490f58f0549cc3e08e0d423a6cd12fcbfb464
jonghee.lee <jonghee.lee@miraeasset.com>
2023-06-20 17:22:12 +0900

    add copyright

--- ql/time/calendars/southkorea.cpp
+++ ql/time/calendars/southkorea.cpp
@@ -7,6 +7,7 @@
  Copyright (C) 2015 Riccardo Barone
  Copyright (C) 2015 Paolo Mazzocchi
  Copyright (C) 2023 Skandinaviska Enskilda Banken AB (publ)
+ Copyright (C) 2023 Jonghee Lee
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
