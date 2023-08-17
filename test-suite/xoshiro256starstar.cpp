32181a1bc8d9ac1807035249fe00b9a4ab4ae0cd
ralfeckel <ralf.eckel@smarttra.de>
2023-08-17 12:26:20 +0200

    Inline xoshiro256starstar.c

--- test-suite/xoshiro256starstar.cpp
+++ test-suite/xoshiro256starstar.cpp
@@ -23,8 +23,9 @@
 #include <numeric>
 
 // we do not want to change the original xoshiro256starstar.c implementation. Therefore, we suppress
-// any warnings from this file.
+// any warnings from this file and also prevent linting and formatting.
 // clang-format off
+// NOLINTBEGIN
 #if defined(__GNUC__)
     _Pragma("GCC diagnostic push")
     _Pragma("GCC diagnostic ignored \"-Wsign-compare\"")
@@ -33,8 +34,114 @@
     _Pragma("clang diagnostic ignored \"-Wsign-compare\"")
 #endif
 
+// The block inside extern "C" { ... } inlines the reference implementation
+// from https://prng.di.unimi.it/xoshiro256starstar.c
+
 extern "C" {
-#include "xoshiro256starstar.c"
+    /*  Written in 2018 by David Blackman and Sebastiano Vigna (vigna@acm.org)
+
+    To the extent possible under law, the author has dedicated all copyright
+    and related and neighboring rights to this software to the public domain
+    worldwide. This software is distributed without any warranty.
+
+    See <http://creativecommons.org/publicdomain/zero/1.0/>. */
+
+    #include <stdint.h>
+
+    /* This is xoshiro256** 1.0, one of our all-purpose, rock-solid
+       generators. It has excellent (sub-ns) speed, a state (256 bits) that is
+       large enough for any parallel application, and it passes all tests we
+       are aware of.
+
+       For generating just floating-point numbers, xoshiro256+ is even faster.
+
+       The state must be seeded so that it is not everywhere zero. If you have
+       a 64-bit seed, we suggest to seed a splitmix64 generator and use its
+       output to fill s. */
+
+    static inline uint64_t rotl(const uint64_t x, int k) {
+            return (x << k) | (x >> (64 - k));
+    }
+
+
+    static uint64_t s[4];
+
+    uint64_t next(void) {
+            const uint64_t result = rotl(s[1] * 5, 7) * 9;
+
+            const uint64_t t = s[1] << 17;
+
+            s[2] ^= s[0];
+            s[3] ^= s[1];
+            s[1] ^= s[2];
+            s[0] ^= s[3];
+
+            s[2] ^= t;
+
+            s[3] = rotl(s[3], 45);
+
+            return result;
+    }
+
+
+    /* This is the jump function for the generator. It is equivalent
+       to 2^128 calls to next(); it can be used to generate 2^128
+       non-overlapping subsequences for parallel computations. */
+
+    void jump(void) {
+            static const uint64_t JUMP[] = { 0x180ec6d33cfd0aba, 0xd5a61266f0c9392c, 0xa9582618e03fc9aa, 0x39abdc4529b1661c };
+
+            uint64_t s0 = 0;
+            uint64_t s1 = 0;
+            uint64_t s2 = 0;
+            uint64_t s3 = 0;
+            for(int i = 0; i < sizeof JUMP / sizeof *JUMP; i++)
+                    for(int b = 0; b < 64; b++) {
+                            if (JUMP[i] & UINT64_C(1) << b) {
+                                    s0 ^= s[0];
+                                    s1 ^= s[1];
+                                    s2 ^= s[2];
+                                    s3 ^= s[3];
+                            }
+                            next();
+                    }
+
+            s[0] = s0;
+            s[1] = s1;
+            s[2] = s2;
+            s[3] = s3;
+    }
+
+
+
+    /* This is the long-jump function for the generator. It is equivalent to
+       2^192 calls to next(); it can be used to generate 2^64 starting points,
+       from each of which jump() will generate 2^64 non-overlapping
+       subsequences for parallel distributed computations. */
+
+    void long_jump(void) {
+            static const uint64_t LONG_JUMP[] = { 0x76e15d3efefdcbbf, 0xc5004e441c522fb3, 0x77710069854ee241, 0x39109bb02acbe635 };
+
+            uint64_t s0 = 0;
+            uint64_t s1 = 0;
+            uint64_t s2 = 0;
+            uint64_t s3 = 0;
+            for(int i = 0; i < sizeof LONG_JUMP / sizeof *LONG_JUMP; i++)
+                    for(int b = 0; b < 64; b++) {
+                            if (LONG_JUMP[i] & UINT64_C(1) << b) {
+                                    s0 ^= s[0];
+                                    s1 ^= s[1];
+                                    s2 ^= s[2];
+                                    s3 ^= s[3];
+                            }
+                            next();
+                    }
+
+            s[0] = s0;
+            s[1] = s1;
+            s[2] = s2;
+            s[3] = s3;
+    }
 }
 
 #if defined(__GNUC__)
@@ -42,9 +149,10 @@ extern "C" {
 #elif defined(__clang__)
     _Pragma("clang diagnostic pop")
 #endif
-    // clang-format on
+// NOLINTEND
+// clang-format on
 
-    using QuantLib::Real;
+using QuantLib::Real;
 using QuantLib::Xoshiro256StarStarUniformRng;
 
 void Xoshiro256StarStarTest::testMeanAndStdDevOfNextReal() {
@@ -55,7 +163,7 @@ void Xoshiro256StarStarTest::testMeanAndStdDevOfNextReal() {
     const auto iterations = 10'000'000;
     auto randoms = std::vector<Real>();
     randoms.reserve(iterations);
-    for (int j = 0; j < iterations; ++j) {
+    for (auto j = 0; j < iterations; ++j) {
         auto next = random.nextReal();
         if (next <= 0.0 || 1.0 <= next) {
             BOOST_FAIL("next " << next << " not in range");
@@ -93,7 +201,7 @@ void Xoshiro256StarStarTest::testAgainstReferenceImplementationInC() {
     s[1] = s1;
     s[2] = s2;
     s[3] = s3;
-    for (int i = 0; i < 1'000; ++i) {
+    for (auto i = 0; i < 1'000; ++i) {
         next();
     }
 
