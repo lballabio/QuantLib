b584e2c271119f9f0ab9509712d123206f57cb9a
Gyan Sinha <gsinha@godolphincapital.com>
2023-08-20 20:30:47 -0400

    The existing function signatures for amortizing bond classes have been extended by adding a redemptions vector argument that is passed as an argument to the addRedemptionsToCashflows function. This is a vector with values between 0 and 100 that represents the proportion of principal payments which would be calculated by taking the difference between notionals for each payment period. This allows these functions to be used for pools of loans where a certain proportion of the underlying loans are subject to defaults and losses.

---
+++ conftest.cpp
@@ -0,0 +1,30 @@
+/* confdefs.h */
+#define PACKAGE_NAME "QuantLib"
+#define PACKAGE_TARNAME "QuantLib"
+#define PACKAGE_VERSION "1.32-dev"
+#define PACKAGE_STRING "QuantLib 1.32-dev"
+#define PACKAGE_BUGREPORT "quantlib-dev@lists.sourceforge.net"
+#define PACKAGE_URL ""
+#define PACKAGE "QuantLib"
+#define VERSION "1.32-dev"
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define STDC_HEADERS 1
+#define HAVE_DLFCN_H 1
+#define LT_OBJDIR ".libs/"
+/* end confdefs.h.  */
+#include <boost/test/included/unit_test.hpp>
+          using namespace boost::unit_test_framework;
+          test_suite*
+          init_unit_test_suite(int argc, char** argv)
+          {
+              return (test_suite*) 0;
+          }
+
