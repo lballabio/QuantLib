144ca0b70bc684f7532ffb326c4a0f5b1f81447d
Peter Caspers <peter.caspers@acadia.inc>
2023-09-22 19:44:41 +0200

    move the thread-safe part in observable

--- ql/patterns/observable.cpp
+++ ql/patterns/observable.cpp
@@ -243,6 +243,31 @@ namespace QuantLib {
 
     }
 
+    Observer::Proxy::update() const {
+        std::lock_guard<std::recursive_mutex> lock(mutex_);
+        if (active_) {
+            // c++17 is required if used with std::shared_ptr<T>
+            const ext::weak_ptr<Observer> o = observer_->weak_from_this();
+
+            // check for empty weak reference
+            // https://stackoverflow.com/questions/45507041/how-to-check-if-weak-ptr-is-empty-non-assigned
+            const ext::weak_ptr<Observer> empty;
+            if (o.owner_before(empty) || empty.owner_before(o)) {
+                const ext::shared_ptr<Observer> obs(o.lock());
+                if (obs)
+                    obs->update();
+            } else {
+                observer_->update();
+            }
+        }
+    }
+
+    void Observer::Proxy::deactivate() {
+        std::lock_guard<std::recursive_mutex> lock(mutex_);
+        active_ = false;
+    }
+
+
     void Observable::registerObserver(const ext::shared_ptr<Observer::Proxy>& observerProxy) {
         {
             std::lock_guard<std::recursive_mutex> lock(mutex_);
@@ -300,6 +325,150 @@ namespace QuantLib {
     : sig_(new detail::Signal()) {
         // the observer set is not copied; no observer asked to
         // register with this object
+    }
+
+    void ObservableSettings::registerDeferredObservers(const Observable::set_type& observers) {
+        deferredObservers_.insert(observers.begin(), observers.end());
+    }
+
+    void ObservableSettings::unregisterDeferredObserver(
+        const ext::shared_ptr<Observer::Proxy>& o) {
+        deferredObservers_.erase(o);
+    }
+
+    void ObservableSettings::enableUpdates() {
+        std::lock_guard<std::mutex> lock(mutex_);
+
+        // if there are outstanding deferred updates, do the notification
+        updatesType_ = UpdatesEnabled;
+
+        if (deferredObservers_.size()) {
+            bool successful = true;
+            std::string errMsg;
+
+            for (auto i=deferredObservers_.begin();
+                i!=deferredObservers_.end(); ++i) {
+                try {
+                    const ext::shared_ptr<Observer::Proxy> proxy = i->lock();
+                    if (proxy)
+                        proxy->update();
+                } catch (std::exception& e) {
+                    successful = false;
+                    errMsg = e.what();
+                } catch (...) {
+                    successful = false;
+                }
+            }
+
+            deferredObservers_.clear();
+
+            QL_ENSURE(successful,
+                  "could not notify one or more observers: " << errMsg);
+        }
+    }
+
+    /*! \warning notification is sent before the copy constructor has
+             a chance of actually change the data
+             members. Therefore, observers whose update() method
+             tries to use their observables will not see the
+             updated values. It is suggested that the update()
+             method just raise a flag in order to trigger
+            a later recalculation.
+    */
+    Observable& Observable::operator=(const Observable& o) {
+        // as above, the observer set is not copied. Moreover,
+        // observers of this object must be notified of the change
+        if (&o != this)
+            notifyObservers();
+        return *this;
+    }
+
+    Observer::Observer(const Observer& o) {
+        proxy_.reset(new Proxy(this));
+
+        {
+             std::lock_guard<std::recursive_mutex> lock(o.mutex_);
+             observables_ = o.observables_;
+        }
+
+        for (const auto& observable : observables_)
+            observable->registerObserver(proxy_);
+    }
+
+    Observer& Observer::operator=(const Observer& o) {
+        std::lock_guard<std::recursive_mutex> lock(mutex_);
+        if (!proxy_) {
+            proxy_.reset(new Proxy(this));
+        }
+
+        for (const auto& observable : observables_)
+            observable->unregisterObserver(proxy_, true);
+
+        {
+            std::lock_guard<std::recursive_mutex> lock(o.mutex_);
+            observables_ = o.observables_;
+        }
+        for (const auto& observable : observables_)
+            observable->registerObserver(proxy_);
+
+        return *this;
+    }
+
+    Observer::~Observer() {
+        std::lock_guard<std::recursive_mutex> lock(mutex_);
+        if (proxy_)
+            proxy_->deactivate();
+
+        for (const auto& observable : observables_)
+            observable->unregisterObserver(proxy_, false);
+    }
+
+    std::pair<Observer::iterator, bool>
+    Observer::registerWith(const ext::shared_ptr<Observable>& h) {
+        std::lock_guard<std::recursive_mutex> lock(mutex_);
+        if (!proxy_) {
+            proxy_.reset(new Proxy(this));
+        }
+
+        if (h) {
+            h->registerObserver(proxy_);
+            return observables_.insert(h);
+        }
+        return std::make_pair(observables_.end(), false);
+    }
+
+    void
+    Observer::registerWithObservables(const ext::shared_ptr<Observer>& o) {
+        if (o) {
+            std::lock_guard<std::recursive_mutex> lock(o->mutex_);
+
+            for (const auto& observable : o->observables_)
+                registerWith(observable);
+        }
+    }
+
+    inline
+    Size Observer::unregisterWith(const ext::shared_ptr<Observable>& h) {
+        std::lock_guard<std::recursive_mutex> lock(mutex_);
+
+        if (h && proxy_)  {
+            h->unregisterObserver(proxy_, true);
+        }
+
+        return observables_.erase(h);
+    }
+
+    void Observer::unregisterWithAll() {
+        std::lock_guard<std::recursive_mutex> lock(mutex_);
+
+        for (const auto& observable : observables_)
+            observable->unregisterObserver(proxy_, true);
+
+        observables_.clear();
+    }
+
+    void Observer::deepUpdate() {
+        update();
     }
 
 }
