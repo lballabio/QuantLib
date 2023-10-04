4cf029617169cfc1985527a5187113ba105655a2
Luigi Ballabio <luigi.ballabio@gmail.com>
2023-10-04 11:52:15 +0200

    Set version to 1.36-rc

--- ql/version.hpp
+++ ql/version.hpp
@@ -31,10 +31,10 @@
 /*! @{ */
 
 //! version string
-#define QL_VERSION "1.36-dev"
+#define QL_VERSION "1.36-rc"
 
 //! version hexadecimal number
-#define QL_HEX_VERSION 0x01360000
+#define QL_HEX_VERSION 0x013600c0
 
 /*! @}  */
 
