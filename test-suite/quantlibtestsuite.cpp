9be368b8fd1302795c4dc50bc1bfb7fd952a0b3a
siddharthmehrotra <siddharthmehrotra1234@gmail.com>
2023-10-26 17:19:36 +0800

    Migrated covariance.cpp

--- test-suite/quantlibtestsuite.cpp
+++ test-suite/quantlibtestsuite.cpp
@@ -16,7 +16,6 @@
 #  include <ql/auto_link.hpp>
 #endif
 
-#include "covariance.hpp"
 #include "creditdefaultswap.hpp"
 #include "creditriskplus.hpp"
 #include "crosscurrencyratehelpers.hpp"
@@ -183,7 +182,6 @@ test_suite* init_unit_test_suite(int, char* []) {
 
     auto* test = BOOST_TEST_SUITE("QuantLib test suite");
 
-    test->add(CovarianceTest::suite());
     test->add(CPISwapTest::suite());
     test->add(CreditDefaultSwapTest::suite());
     test->add(CrossCurrencyRateHelpersTest::suite());
