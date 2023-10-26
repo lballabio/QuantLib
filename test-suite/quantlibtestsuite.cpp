dde5d43b63916d2643530a9e9a8e784eb7369384
siddharthmehrotra <siddharthmehrotra1234@gmail.com>
2023-10-26 16:49:40 +0800

    Migrated chooseroption.cpp

--- test-suite/quantlibtestsuite.cpp
+++ test-suite/quantlibtestsuite.cpp
@@ -16,7 +16,6 @@
 #  include <ql/auto_link.hpp>
 #endif
 
-#include "chooseroption.hpp"
 #include "cliquetoption.hpp"
 #include "cms.hpp"
 #include "cms_normal.hpp"
@@ -192,7 +191,6 @@ test_suite* init_unit_test_suite(int, char* []) {
 
     auto* test = BOOST_TEST_SUITE("QuantLib test suite");
 
-    test->add(ChooserOptionTest::suite());
     test->add(CliquetOptionTest::suite());
     test->add(CmsTest::suite());
     test->add(CmsNormalTest::suite());
