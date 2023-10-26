5370fbd96530362541a2a18d9b82036ae35a0108
siddharthmehrotra <siddharthmehrotra1234@gmail.com>
2023-10-26 17:47:48 +0800

    Migrated curvestates.cpp

--- test-suite/quantlibtestsuite.cpp
+++ test-suite/quantlibtestsuite.cpp
@@ -16,7 +16,6 @@
 #  include <ql/auto_link.hpp>
 #endif
 
-#include "curvestates.hpp"
 #include "dates.hpp"
 #include "daycounters.hpp"
 #include "defaultprobabilitycurves.hpp"
@@ -179,7 +178,6 @@ test_suite* init_unit_test_suite(int, char* []) {
     auto* test = BOOST_TEST_SUITE("QuantLib test suite");
 
     test->add(CPISwapTest::suite());
-    test->add(CurveStatesTest::suite());
     test->add(DateTest::suite(speed));
     test->add(DayCounterTest::suite());
     test->add(DefaultProbabilityCurveTest::suite());
