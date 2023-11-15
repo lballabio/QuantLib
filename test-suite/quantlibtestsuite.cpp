778b758dc0a95459188e7290982b28935327ffd2
siddharthmehrotra <siddharthmehrotra1234@gmail.com>
2023-11-15 11:42:32 +0800

    Migrated quotes.cpp

--- test-suite/quantlibtestsuite.cpp
+++ test-suite/quantlibtestsuite.cpp
@@ -36,7 +36,6 @@
 
 #include "marketmodel.hpp"
 #include "quantlibglobalfixture.hpp"
-#include "quotes.hpp"
 #include "rangeaccrual.hpp"
 #include "riskneutraldensitycalculator.hpp"
 #include "riskstats.hpp"
@@ -88,7 +87,6 @@ test_suite* init_unit_test_suite(int, char* []) {
     auto* test = BOOST_TEST_SUITE("QuantLib test suite");
 
     test->add(MarketModelTest::suite(speed));
-    test->add(QuoteTest::suite());
     test->add(RangeAccrualTest::suite());
     test->add(RiskStatisticsTest::suite());
     test->add(RngTraitsTest::suite());
