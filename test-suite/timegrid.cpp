6abf054209bea0281d9ff41167c8cac8bc7a63a4
Paul <d.goldbauer@protonmail.com>
2023-12-09 22:25:50 +0100

    better test message

--- test-suite/timegrid.cpp
+++ test-suite/timegrid.cpp
@@ -72,13 +72,13 @@ BOOST_AUTO_TEST_CASE(testConstructorAdditionalStepsAutomatically)
 BOOST_AUTO_TEST_CASE(testConstructorEvenSteps)
 {
     BOOST_TEST_MESSAGE("Testing TimeGrid construction with n evenly spaced points...");
-    
+
     Time end_time = 10;
     Size steps = 5;
     const TimeGrid tg(end_time, steps);
 
     std::vector<Time> expected_times = {0.0, 2.0, 4.0, 6.0, 8.0, 10.0};
-    
+
     BOOST_CHECK_EQUAL_COLLECTIONS(
         tg.begin(), tg.end(), expected_times.begin(), expected_times.end()
     );
@@ -89,7 +89,7 @@ BOOST_AUTO_TEST_CASE(testConstructorEmptyIterator)
     BOOST_TEST_MESSAGE(
         "Testing that the TimeGrid constructor raises an error for empty iterators..."
     );
-    
+
     const std::vector<Time> times;
     BOOST_CHECK_THROW(const TimeGrid tg(times.begin(), times.end()), Error);
 }
@@ -97,14 +97,15 @@ BOOST_AUTO_TEST_CASE(testConstructorEmptyIterator)
 BOOST_AUTO_TEST_CASE(testConstructorNegativeValuesInIterator)
 {
     BOOST_TEST_MESSAGE("Testing that the TimeGrid constructor raises an error for negative time values...");
-    
+
     std::vector<Time> times = {-3.0, 1.0, 4.0, 5.0};
     BOOST_CHECK_THROW(const TimeGrid tg(times.begin(), times.end()), Error);
 }
 
 BOOST_AUTO_TEST_CASE(testIndex)
 {
-    BOOST_TEST_MESSAGE("Testing that the supplied time is very close to a time grid point...");
+    BOOST_TEST_MESSAGE("Testing that querying an index by floating-point time works for exact time nodes and "
+                       "throws otherwise...");
 
     // will automatically insert additional point at t=0
     const TimeGrid tg = {1.0, 2.0, 5.0};
@@ -130,7 +131,7 @@ BOOST_AUTO_TEST_CASE(testClosestIndex)
 
     const TimeGrid tg = {1.0, 2.0, 5.0};
     const Size expected_index = 3;
-    
+
     QL_ASSERT(tg.closestIndex(4) == expected_index,
               "Expected index: " << expected_index << ", which does not match " <<
               "the returned index: " << tg.closestIndex(4));
@@ -141,7 +142,7 @@ BOOST_AUTO_TEST_CASE(testClosestTime)
     BOOST_TEST_MESSAGE("Testing that the returned time matches the requested index...");
     const TimeGrid tg = {1.0, 2.0, 5.0};
     const Size expected_time = 5;
-    
+
     QL_ASSERT(tg.closestTime(4) == expected_time,
               "Expected time of: " << expected_time << ", which does not match " <<
               "the returned time: " << tg.closestTime(4));
@@ -152,7 +153,7 @@ BOOST_AUTO_TEST_CASE(testMandatoryTimes)
     BOOST_TEST_MESSAGE("Testing that mandatory times are recalled correctly...");
     std::vector<Time> test_times = {1.0, 2.0, 4.0};
     const TimeGrid tg(test_times.begin(), test_times.end(), 8);
-    
+
     // Mandatory times are those provided by the original iterator.
     const std::vector<Time>& tg_mandatory_times = tg.mandatoryTimes();
     BOOST_CHECK_EQUAL_COLLECTIONS(
