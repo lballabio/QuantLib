5df997add0404e933cd91cbd871798d6bb1fa149
Paul <d.goldbauer@protonmail.com>
2023-12-12 16:31:57 +0100

    cleanup unit test

--- test-suite/dates.cpp
+++ test-suite/dates.cpp
@@ -162,45 +162,39 @@ BOOST_AUTO_TEST_CASE(asxDates) {
         "F9", "G9", "H9", "J9", "K9", "M9", "N9", "Q9", "U9", "V9", "X9", "Z9"
     };
 
-    Date counter = { 1, January, 2000 };
-    Date last = { 1, January, 2040 };
-    Date asx;
-
-    while (counter <= last) {
-        asx = ASX::nextDate(counter, false);
+    for (Date counter(1, January, 2000); counter <= Date(1,January, 2040); ++counter) {
+        const Date asx = ASX::nextDate(counter, false);
 
         // check that asx is greater than counter
-        if (asx <= counter)
-            BOOST_FAIL(asx.weekday() << " " << asx
+        BOOST_TEST(asx > counter,
+                       asx.weekday() << " " << asx
                        << " is not greater than "
                        << counter.weekday() << " " << counter);
 
         // check that asx is an ASX date
-        if (!ASX::isASXdate(asx, false))
-            BOOST_FAIL(asx.weekday() << " " << asx
+        BOOST_TEST(ASX::isASXdate(asx, false),
+                       asx.weekday() << " " << asx
                        << " is not an ASX date (calculated from "
                        << counter.weekday() << " " << counter << ")");
 
         // check that asx is <= to the next ASX date in the main cycle
-        if (asx>ASX::nextDate(counter, true))
-            BOOST_FAIL(asx.weekday() << " " << asx
+        BOOST_TEST(asx <= ASX::nextDate(counter, true),
+                       asx.weekday() << " " << asx
                        << " is not less than or equal to the next future in the main cycle "
                        << ASX::nextDate(counter, true));
 
         // check that for every date ASXdate is the inverse of ASXcode
-        if (ASX::date(ASX::code(asx), counter) != asx)
-            BOOST_FAIL(ASX::code(asx)
+        BOOST_TEST(ASX::date(ASX::code(asx), counter) == asx,
+                       ASX::code(asx)
                        << " at calendar day " << counter
                        << " is not the ASX code matching " << asx);
 
         // check that for every date the 120 ASX codes refer to future dates
         for (const auto& ASXcode : ASXcodes) {
-            if (ASX::date(ASXcode, counter) < counter)
-                BOOST_FAIL(ASX::date(ASXcode, counter) << " is wrong for " << ASXcode
+            BOOST_TEST(ASX::date(ASXcode, counter) >= counter,
+                           ASX::date(ASXcode, counter) << " is wrong for " << ASXcode
                            << " at reference date " << counter);
         }
-
-        counter = counter + 1;
     }
 }
 
