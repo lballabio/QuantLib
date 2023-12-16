dfff9140712d729b83866df418ed0503c09076d5
Paul <d.goldbauer@protonmail.com>
2023-12-16 13:51:33 +0100

    cleanup: format array of dates; include archive.org link to data

--- ql/time/ecb.cpp
+++ ql/time/ecb.cpp
@@ -39,52 +39,63 @@ namespace QuantLib {
     }
 
     namespace detail {
+        //clang-format off
+        // Start of maintenance period
+        // source: https://web.archive.org/web/20230610050642/https://www.ecb.europa.eu/press/calendars/reserve/html/index.en.html
         static std::set<Date> ecbKnownDateSet = {
+            // 2005
             Date(38371), Date(38391), Date(38420), Date(38455), Date(38483), Date(38511),
-            Date(38546), Date(38574), Date(38602), Date(38637), Date(38665),
-            Date(38692), // 2005
+            Date(38546), Date(38574), Date(38602), Date(38637), Date(38665), Date(38692),
+
+            // 2006
             Date(38735), Date(38756), Date(38784), Date(38819), Date(38847), Date(38883),
-            Date(38910), Date(38938), Date(38966), Date(39001), Date(39029),
-            Date(39064), // 2006
+            Date(38910), Date(38938), Date(38966), Date(39001), Date(39029), Date(39064),
+
+            // 2007
             Date(39099), Date(39127), Date(39155), Date(39190), Date(39217), Date(39246),
-            Date(39274), Date(39302), Date(39337), Date(39365), Date(39400),
-            Date(39428), // 2007
+            Date(39274), Date(39302), Date(39337), Date(39365), Date(39400), Date(39428),
+
+            // 2008
             Date(39463), Date(39491), Date(39519), Date(39554), Date(39582), Date(39610),
-            Date(39638), Date(39673), Date(39701), Date(39729), Date(39764),
-            Date(39792), // 2008
+            Date(39638), Date(39673), Date(39701), Date(39729), Date(39764), Date(39792),
+
+            // 2009
             Date(39834), Date(39855), Date(39883), Date(39911), Date(39946), Date(39974),
-            Date(40002), Date(40037), Date(40065), Date(40100), Date(40128),
-            Date(40155), // 2009
+            Date(40002), Date(40037), Date(40065), Date(40100), Date(40128), Date(40155),
+
+            // 2010
             Date(40198), Date(40219), Date(40247), Date(40282), Date(40310), Date(40345),
-            Date(40373), Date(40401), Date(40429), Date(40464), Date(40492),
-            Date(40520), // 2010
+            Date(40373), Date(40401), Date(40429), Date(40464), Date(40492), Date(40520),
+
+            // 2011
             Date(40562), Date(40583), Date(40611), Date(40646), Date(40674), Date(40709),
-            Date(40737), Date(40765), Date(40800), Date(40828), Date(40856),
-            Date(40891), // 2011
-                         // http://www.ecb.europa.eu/press/pr/date/2011/html/pr110520.en.html
+            Date(40737), Date(40765), Date(40800), Date(40828), Date(40856), Date(40891),
+
+            // 2012
             Date(40926), Date(40954), Date(40982), Date(41010), Date(41038), Date(41073),
-            Date(41101), Date(41129), Date(41164), Date(41192), Date(41227),
-            Date(41255), // 2012
+            Date(41101), Date(41129), Date(41164), Date(41192), Date(41227), Date(41255),
+
+            // 2013
             Date(41290), Date(41318), Date(41346), Date(41374), Date(41402), Date(41437),
-            Date(41465), Date(41493), Date(41528), Date(41556), Date(41591),
-            Date(41619), // 2013
-                         // http://www.ecb.europa.eu/press/pr/date/2013/html/pr130610.en.html
+            Date(41465), Date(41493), Date(41528), Date(41556), Date(41591), Date(41619),
+
+            // 2014
             Date(41654), Date(41682), Date(41710), Date(41738), Date(41773), Date(41801),
-            Date(41829), Date(41864), Date(41892), Date(41920), Date(41955),
-            Date(41983), // 2014
-                         // http://www.ecb.europa.eu/press/pr/date/2014/html/pr140717_1.en.html
+            Date(41829), Date(41864), Date(41892), Date(41920), Date(41955), Date(41983),
+
+            // 2015
             Date(42032), Date(42074), Date(42116), Date(42165), Date(42207), Date(42256),
-            Date(42305),
-            Date(42347), // 2015
-                         // https://www.ecb.europa.eu/press/pr/date/2015/html/pr150622.en.html
+            Date(42305), Date(42347),
+
+            // 2016
             Date(42396), Date(42445), Date(42487), Date(42529), Date(42578), Date(42627),
-            Date(42669),
-            Date(42718), // 2016
-                         // https://www.ecb.europa.eu/press/calendars/reserve/html/index.en.html
+            Date(42669), Date(42718),
+
+            // 2017
             Date(42760), Date(42809), Date(42858), Date(42900), Date(42942), Date(42991),
-            Date(43040),
-            Date(43089) // 2017
+            Date(43040), Date(43089)
         };
+        //clang-format on
     }
 
     const std::set<Date>& ECB::knownDates() {
