15ba7960e87c22649b21e24bc0b0cc1f3e7a81f4
Christian Köhnenkamp <cvk5@me.com>
2024-02-27 12:41:55 +0100

    Update inflation.py
    Add YoY benchmarks

--- Examples/Inflation/inflation.py
+++ Examples/Inflation/inflation.py
@@ -9,6 +9,11 @@ convention = ql.ModifiedFollowing
 daycounter = ql.Actual360()
 dc360 = ql.Actual360()
 
+kpi_data = [338.09, 339.01, 339.54, 340.37, 341.04, 341.32, 342.23, 343.99, 345.74, 346.44,
+            348.03, 352.47, 350.56, 353.56, 359.80, 362.02, 365.82, 370.95, 371.28, 377.81,
+            383.21, 384.04, 387.93, 395.96, 391.50, 395.82, 398.08, 399.93, 401.19, 405.49,
+            405.67, 405.97, 408.05, 409.07, 410.35, 413.34, 412.74]
+
 
 class Coupon:
     def __init__(self, date, rate):
@@ -49,12 +54,34 @@ def create_zero_coupon_inflation_swap_helpers(coupons, index):
     return helpers
 
 
-def get_kpi_index() -> ql.ZeroInflationIndex:
-    kpi_index = ql.ZeroInflationIndex("KPI", ql.CustomRegion("Europe", "EU"), False, ql.Monthly, ql.Period(1, ql.Days), ql.SEKCurrency())
-    kpi_data = [338.09, 339.01, 339.54, 340.37, 341.04, 341.32, 342.23, 343.99, 345.74, 346.44,
-            348.03, 352.47, 350.56, 353.56, 359.80, 362.02, 365.82, 370.95, 371.28, 377.81,
-            383.21, 384.04, 387.93, 395.96, 391.50, 395.82, 398.08, 399.93, 401.19, 405.49,
-            405.67, 405.97, 408.05, 409.07, 410.35, 413.34, 412.74]
+def create_yoy_coupon_inflation_swap_helpers(coupons, index):
+    helpers = []
+
+    for coupon in coupons:
+        quote = ql.SimpleQuote(coupon.rate / 100.0)
+        quote_handle = ql.QuoteHandle(quote)
+        
+        nts = nominal_term_structure()
+        observation_lag =ql.Period(1, ql.Months)
+        
+        helper = ql.YearOnYearInflationSwapHelper(
+            quote_handle,
+            observation_lag,
+            coupon.maturity,
+            calendar,
+            convention,
+            daycounter,
+            index,
+            nts
+        )
+        
+        helpers.append(helper)
+
+    return helpers
+
+
+def get_zero_index() -> ql.ZeroInflationIndex:
+    zero_index = ql.ZeroInflationIndex("KPI", ql.CustomRegion("Europe", "EU"), False, ql.Monthly, ql.Period(1, ql.Days), ql.SEKCurrency())
     from_date = ql.Date(1, 1, 2021)
     to_date = ql.Date(1, 1, 2024)
     kpi_schedule = ql.Schedule(from_date, 
@@ -65,11 +92,20 @@ def get_kpi_index() -> ql.ZeroInflationIndex:
                                ql.Unadjusted,
             ql.DateGeneration.Backward,False)
     for i in range(len(kpi_data)):
-        kpi_index.addFixing(kpi_schedule[i], kpi_data[i])
-    return kpi_index
+        zero_index.addFixing(kpi_schedule[i], kpi_data[i])
+    return zero_index
     
 
-zcSwap = [
+def get_yoy_index() -> ql.YoYInflationIndex:
+    zero_index = get_zero_index()
+    is_interpolated = False
+    yoy_index = ql.YoYInflationIndex(zero_index, is_interpolated)
+    return yoy_index
+
+
+
+############## ZC benchmarchs ############
+zc_swap = [
     Coupon(ql.Date(1, 1, 2026), 3.145),
     Coupon(ql.Date(1, 1, 2027), 3.145),
     Coupon(ql.Date(1, 1, 2029), 3.145),
@@ -77,25 +113,70 @@ zcSwap = [
     Coupon(ql.Date(1, 1, 2034), 3.145)
 ]
 
-#########################
-inflation_index = get_kpi_index()
-helper = create_zero_coupon_inflation_swap_helpers(zcSwap, inflation_index)
+
+zero_inflation_index = get_zero_index()
+zero_helpers = create_zero_coupon_inflation_swap_helpers(zc_swap, zero_inflation_index)
 
 frequency = ql.Monthly
 base_rate = 0.0
 period_lag = ql.Period("1M")
 
-bootstrap_curve = ql.PiecewiseZeroInflation(
+zero_bootstrap_curve = ql.PiecewiseZeroInflation(
     study_date,
     calendar,
     daycounter,
     period_lag,
     frequency,
     base_rate,
-    helper
+    zero_helpers
 )
 
-## Plot curve
+############## YoY benchmarchs ############
+
+yoy_swap = [
+    Coupon(ql.Date(1, 2, 2024), 5.45),
+    Coupon(ql.Date(1, 3, 2024), 5.35),
+    Coupon(ql.Date(1, 4, 2024), 5.23),
+    Coupon(ql.Date(1, 5, 2024), 5.21),
+    Coupon(ql.Date(1, 6, 2024), 5.14),
+    Coupon(ql.Date(1, 7, 2024), 5.00),
+    Coupon(ql.Date(1, 8, 2024), 4.80),
+    Coupon(ql.Date(1, 9, 2024), 4.55),
+    Coupon(ql.Date(1, 10, 2024), 4.45),
+    Coupon(ql.Date(1, 11, 2024), 4.37),
+    Coupon(ql.Date(1, 12, 2024), 4.25),
+    Coupon(ql.Date(1, 1, 2025), 3.135),
+    Coupon(ql.Date(1, 2, 2025), 3.155),
+    Coupon(ql.Date(1, 3, 2025), 3.145),
+    Coupon(ql.Date(1, 4, 2025), 3.145),
+    Coupon(ql.Date(1, 5, 2025), 3.145),
+    Coupon(ql.Date(1, 6, 2025), 3.145),
+    Coupon(ql.Date(1, 7, 2025), 3.145),
+    Coupon(ql.Date(1, 8, 2025), 3.145),
+    Coupon(ql.Date(1, 9, 2025), 3.145),
+    Coupon(ql.Date(1, 10, 2025), 3.145),
+    Coupon(ql.Date(1, 11, 2025), 3.145),
+    Coupon(ql.Date(1, 12, 2025), 3.145),
+]
+
+
+yoy_inflation_index = get_yoy_index()
+
+yoy_helpers = create_yoy_coupon_inflation_swap_helpers(yoy_swap, yoy_inflation_index)
+
+index_is_interpolated = True
+yoy_bootstrap_curve = ql.PiecewiseYoYInflation(
+    study_date,
+    calendar,
+    daycounter,
+    period_lag,
+    frequency,
+    index_is_interpolated,
+    base_rate,
+    yoy_helpers
+)
+
+############## Plot curve
 start_date = ql.Date(1, 1, 2025)
 end_date = ql.Date(1, 2, 2033)
 calendar = ql. Sweden()
@@ -108,7 +189,7 @@ y = []
 
 for day in  schedule:
     print(day)
-    _y =bootstrap_curve.zeroRate(day)
+    _y = zero_bootstrap_curve.zeroRate(day)
     print(_y)
     y.append(_y)
     year_fraction = dc360.yearFraction(start_date, day)
