06f617279e12394da5e37d604f42e996fa3de97d
Luigi Ballabio <luigi.ballabio@gmail.com>
2024-11-12 15:51:26 +0100

    Update copyright list in license

--- ql/pricingengines/basket/operatorsplittingspreadengine.cpp
+++ ql/pricingengines/basket/operatorsplittingspreadengine.cpp
@@ -1,7 +1,7 @@
 /* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
- Copyright (C) 2024 klaus Spanderen
+ Copyright (C) 2024 Klaus Spanderen
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
