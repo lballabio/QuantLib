f17a411f1a8c12ff90a8ce41b2748131f15a68c9
paolodelia99 <paolo.delia99@gmail.com>
2025-01-05 21:40:57 +0100

    #1986: added tests for put-call symmetry

--- test-suite/partialtimebarrieroption.cpp
+++ test-suite/partialtimebarrieroption.cpp
@@ -207,6 +207,91 @@ BOOST_AUTO_TEST_CASE(testAnalyticEnginePutOption) {
     }
 }
 
+BOOST_AUTO_TEST_CASE(testPutCallSymmetry) {
+    BOOST_TEST_MESSAGE(
+        "Testing put-call symmetry for the partial-time barrier option...");
+
+    Date today = Settings::instance().evaluationDate();
+
+    struct PutCallSymmetryTestCase {
+        Real call_strike;
+        Real call_barrier;
+        PartialBarrier::Type call_type;
+        Real put_strike;
+        Real put_barrier;
+        Integer days;
+        PartialBarrier::Type put_type;
+    };
+
+    PutCallSymmetryTestCase cases[] = {
+        { 105.2631, 95.2380, PartialBarrier::DownOut, 95.0, 105.0, 1, PartialBarrier::UpOut },
+        { 105.2631, 95.2380, PartialBarrier::DownOut, 95.0, 105.0, 90, PartialBarrier::UpOut },
+        { 105.2631, 95.2380, PartialBarrier::DownOut, 95.0, 105.0, 180, PartialBarrier::UpOut },
+        { 105.2631, 95.2380, PartialBarrier::DownOut, 95.0, 105.0, 270, PartialBarrier::UpOut },
+        { 105.2631, 95.2380, PartialBarrier::DownOut, 95.0, 105.0, 359, PartialBarrier::UpOut },
+
+        { 110.0, 120.0, PartialBarrier::UpOut, 90.9090, 83.3333, 1, PartialBarrier::DownOut },
+        { 110.0, 120.0, PartialBarrier::UpOut, 90.9090, 83.3333, 90, PartialBarrier::DownOut },
+        { 110.0, 120.0, PartialBarrier::UpOut, 90.9090, 83.3333, 180, PartialBarrier::DownOut },
+        { 110.0, 120.0, PartialBarrier::UpOut, 90.9090, 83.3333, 270, PartialBarrier::DownOut },
+        { 110.0, 120.0, PartialBarrier::UpOut, 90.9090, 83.3333, 359, PartialBarrier::DownOut },
+    };
+
+    DayCounter dc = Actual360();
+    Date maturity = today + 360;
+    ext::shared_ptr<Exercise> exercise =
+        ext::make_shared<EuropeanExercise>(maturity);
+    Real rebate = 0.0;
+
+    ext::shared_ptr<SimpleQuote> spot = ext::make_shared<SimpleQuote>();
+    ext::shared_ptr<SimpleQuote> qRate = ext::make_shared<SimpleQuote>(0.0);
+    ext::shared_ptr<SimpleQuote> rRate = ext::make_shared<SimpleQuote>(0.01);
+    ext::shared_ptr<SimpleQuote> vol = ext::make_shared<SimpleQuote>(0.25);
+
+    Handle<Quote> underlying(spot);
+    Handle<YieldTermStructure> dividendTS(flatRate(today, qRate, dc));
+    Handle<YieldTermStructure> riskFreeTS(flatRate(today, rRate, dc));
+    Handle<BlackVolTermStructure> blackVolTS(flatVol(today, vol, dc));
+
+    const ext::shared_ptr<BlackScholesMertonProcess> process =
+        ext::make_shared<BlackScholesMertonProcess>(underlying,
+                                                      dividendTS,
+                                                      riskFreeTS,
+                                                      blackVolTS);
+    ext::shared_ptr<PricingEngine> engine =
+        ext::make_shared<AnalyticPartialTimeBarrierOptionEngine>(process);
+
+    for (auto& i : cases) {
+        Date coverEventDate = today + i.days;
+        ext::shared_ptr<StrikedTypePayoff> put_payoff =
+            ext::make_shared<PlainVanillaPayoff>(Option::Put, i.put_strike);
+        ext::shared_ptr<StrikedTypePayoff> call_payoff =
+            ext::make_shared<PlainVanillaPayoff>(Option::Call, i.call_strike);
+        PartialTimeBarrierOption put_option(i.put_type,
+                                        PartialBarrier::EndB1,
+                                        i.put_barrier, rebate,
+                                        coverEventDate,
+                                        put_payoff, exercise);
+        put_option.setPricingEngine(engine);
+        PartialTimeBarrierOption call_option(i.call_type,
+                                        PartialBarrier::EndB1,
+                                        i.call_barrier, rebate,
+                                        coverEventDate,
+                                        call_payoff, exercise);
+        call_option.setPricingEngine(engine);
+
+        spot->setValue(100.0);
+        Real put_value = put_option.NPV();
+        Real call_value = call_option.NPV();
+        Real call_amount = (i.put_strike / 100.0);
+        Real error = std::fabs(put_value - call_amount * call_value);
+        Real tolerance = 1e-4;
+        if (error > tolerance)
+            BOOST_ERROR("Failed to reproduce the put-call symmetry for the partial-time barrier options "
+                        << "\n    error:      " << error);
+    }
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE_END()
