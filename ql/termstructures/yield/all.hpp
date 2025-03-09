dc9cb4c5ca06efd6f5e7383fc6c2823f97e02566
paolodelia99 <paolo.delia99@gmail.com>
2025-03-09 14:19:20 +0100

    Changed position of includes in the header

--- ql/termstructures/yield/all.hpp
+++ ql/termstructures/yield/all.hpp
@@ -11,18 +11,17 @@
 #include <ql/termstructures/yield/forwardspreadedtermstructure.hpp>
 #include <ql/termstructures/yield/forwardstructure.hpp>
 #include <ql/termstructures/yield/impliedtermstructure.hpp>
+#include <ql/termstructures/yield/interpolatedpiecewisespreadcurve.hpp>
 #include <ql/termstructures/yield/interpolatedsimplezerocurve.hpp>
 #include <ql/termstructures/yield/nonlinearfittingmethods.hpp>
 #include <ql/termstructures/yield/oisratehelper.hpp>
 #include <ql/termstructures/yield/overnightindexfutureratehelper.hpp>
 #include <ql/termstructures/yield/piecewiseyieldcurve.hpp>
 #include <ql/termstructures/yield/piecewisezerospreadedtermstructure.hpp>
+#include <ql/termstructures/yield/piecewiseforwardspreadedtermstructure.hpp>
 #include <ql/termstructures/yield/quantotermstructure.hpp>
 #include <ql/termstructures/yield/ratehelpers.hpp>
 #include <ql/termstructures/yield/ultimateforwardtermstructure.hpp>
 #include <ql/termstructures/yield/zerocurve.hpp>
 #include <ql/termstructures/yield/zerospreadedtermstructure.hpp>
 #include <ql/termstructures/yield/zeroyieldstructure.hpp>
-#include <ql/termstructures/yield/piecewiseforwardspreadedtermstructure.hpp>
-#include <ql/termstructures/yield/interpolatedpiecewisespreadcurve.hpp>
-
