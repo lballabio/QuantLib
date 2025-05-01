0ca6ff031fce7bab0b45ade7ab01ea8f44cfca9a
paolodelia99 <paolo.delia99@gmail.com>
2025-05-01 13:57:29 +0200

    small change in test copyright

--- test-suite/overnightindex.cpp
+++ test-suite/overnightindex.cpp
@@ -1,7 +1,7 @@
 /* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
- Copyright (C) 2025 StatPro Italia srl
+ Copyright (C) 2025 Paolo D'Elia
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
