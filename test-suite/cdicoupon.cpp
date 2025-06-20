0a51a994cae2ac310783409e2b0f96b2ef1a31a2
Sebastian Bohlen <bohlensebastian@aol.de>
2025-06-20 22:37:37 +0200

    add coupon calculation test

--- test-suite/cdicoupon.cpp
+++ test-suite/cdicoupon.cpp
@@ -22,6 +22,7 @@
 #include <ql/cashflows/overnightindexedcoupon.hpp>
 #include <ql/indexes/ibor/cdi.hpp>
 #include <ql/settings.hpp>
+#include <ql/termstructures/yield/zerocurve.hpp>
 #include <ql/time/daycounters/business252.hpp>
 #include <iomanip>
 
@@ -64,7 +65,10 @@ namespace CdiTestData {
         return 14.65;
     }
 
-    void addFixings(Cdi& index, const Date& first, const Date& last, const Calendar& cal) {
+    void addFixings(const ext::shared_ptr<OvernightIndex>& index,
+                    const Date& first,
+                    const Date& last,
+                    const Calendar& cal) {
         Schedule s = MakeSchedule()
                          .from(first)
                          .to(last)
@@ -74,14 +78,57 @@ namespace CdiTestData {
                          .forwards()
                          .endOfMonth(false);
         for (const auto& d : s) {
-            index.addFixing(d, fixing(d) / 100);
+            index->addFixing(d, fixing(d) / 100);
         }
+    }
+
+    std::vector<Date> curveDates(const Date& today) {
+        return {today,
+                Date(23, June, 2025),
+                Date(1, July, 2025),
+                Date(1, August, 2025),
+                Date(1, September, 2025),
+                Date(1, October, 2025),
+                Date(3, November, 2025),
+                Date(1, December, 2025),
+                Date(2, January, 2026),
+                Date(2, February, 2026),
+                Date(2, March, 2026),
+                Date(1, April, 2026),
+                Date(4, May, 2026),
+                Date(1, June, 2026),
+                Date(1, July, 2026),
+                Date(1, October, 2026),
+                Date(4, January, 2027),
+                Date(1, April, 2027),
+                Date(1, July, 2027)};
+    }
+
+    std::vector<Rate>
+    curveRates(const Date& today, const Date& first, const Date& second, const DayCounter& dc) {
+        // some randomized rates with linear extrapolation for today's rate
+        const Real r_1 = 0.14;
+        const Real r_2 = 0.145;
+        const Real r_0 =
+            r_1 - dc.yearFraction(today, first) * (r_2 - r_1) / dc.yearFraction(first, second);
+        return {r_0,     r_1,     r_2,     0.14512, 0.14683, 0.14614, 0.14707,
+                0.14762, 0.14886, 0.15101, 0.14961, 0.14958, 0.15110, 0.14881,
+                0.14942, 0.14782, 0.14641, 0.14573, 0.14293};
+    }
+
+    ext::shared_ptr<ZeroCurve> makeCurve(const Date& today) {
+        const auto dc = Business252();
+        const auto dates = curveDates(today);
+        const auto rates = curveRates(today, dates[0], dates[1], dc);
+        return ext::make_shared<InterpolatedZeroCurve<Linear>>(dates, rates, dc, Calendar(),
+                                                               Linear(), Compounded, Annual);
     }
 
 }
 
 struct CommonVars {
     const Calendar calendar = Brazil();
+    const DayCounter dc = Business252();
     const Date today = Date(19, June, 2025); // holiday
     const Date start = Date(23, June, 2023);
     const Real notional = 10000000.0;
@@ -99,9 +146,8 @@ struct CommonVars {
     CommonVars() {
         Settings::instance().evaluationDate() = today;
         cdi = ext::make_shared<Cdi>(forecastCurve);
-
         Date lastFixingDate = calendar.adjust(today, Preceding);
-        CdiTestData::addFixings(*cdi, start, lastFixingDate, calendar);
+        CdiTestData::addFixings(cdi, start, lastFixingDate, calendar);
     }
 };
 
@@ -114,7 +160,7 @@ struct CommonVars {
                     << std::fabs(calculated - expected));                          \
     }
 
-BOOST_AUTO_TEST_CASE(testPastCouponRate) {
+BOOST_AUTO_TEST_CASE(testPastCoupon) {
     BOOST_TEST_MESSAGE("Testing rate for cdi-indexed coupon in the past...");
 
     CommonVars vars;
@@ -142,31 +188,47 @@ BOOST_AUTO_TEST_CASE(testPastCouponRate) {
                                 1e-5);
 }
 
-// BOOST_AUTO_TEST_CASE(testCurrentCouponRate) {
-//     BOOST_TEST_MESSAGE("Testing rate for current overnight-indexed coupon...");
-//
-//     CommonVars vars;
-//
-//     vars.forecastCurve.linkTo(flatRate(0.0010, Actual360()));
-//
-//     // coupon partly in the past, today not fixed
-//
-//     auto currentCoupon = vars.makeCoupon(Date(10, November, 2021), Date(10, December, 2021));
-//
-//     Rate expectedRate = 0.000926701551;
-//     Real expectedAmount = vars.notional * expectedRate * 30.0 / 360;
-//     CHECK_CDI_OIS_COUPON_RESULT("coupon rate", currentCoupon->rate(), expectedRate, 1e-12);
-//     CHECK_CDI_OIS_COUPON_RESULT("coupon amount", currentCoupon->amount(), expectedAmount, 1e-8);
-//
-//     // coupon partly in the past, today fixed
-//
-//     vars.cdi->addFixing(Date(23, November, 2021), 0.0007);
-//
-//     expectedRate = 0.000916700760;
-//     expectedAmount = vars.notional * expectedRate * 30.0 / 360;
-//     CHECK_CDI_OIS_COUPON_RESULT("coupon rate", currentCoupon->rate(), expectedRate, 1e-12);
-//     CHECK_CDI_OIS_COUPON_RESULT("coupon amount", currentCoupon->amount(), expectedAmount, 1e-8);
-// }
+BOOST_AUTO_TEST_CASE(testCurrentCoupon) {
+    BOOST_TEST_MESSAGE("Testing rate for cdi-indexed coupon...");
+
+    CommonVars vars;
+
+    const auto curve = CdiTestData::makeCurve(vars.today);
+    vars.forecastCurve.linkTo(curve);
+
+    // coupon partly in the past, today not fixed
+
+    const Date end = Date(23, June, 2027);
+
+    auto coupon1 = vars.makeCoupon(vars.start, end);
+    auto coupon2 = vars.makeCoupon(vars.start, end, 1.1, 0.005);
+    auto coupon3 = vars.makeCoupon(vars.start, end, 0.6, -0.003);
+
+    // expected values here come from manual calculations while looking at BBG SWPM
+    Real expAccrued1 = 2513886.62007;
+    Real expAccrued2 = 2924630.86799;
+    Real expAccrued3 = 1372523.67713;
+
+    Real expAmount1 = 6345689.77964;
+    // here we can use the telescope formula
+    Real compFactor = (expAccrued1 / vars.notional + 1) / vars.forecastCurve->discount(end);
+    Rate expRate1 = (compFactor - 1) / vars.dc.yearFraction(vars.start, end);
+
+    Real expAmount2 = 7512591.22333;
+    Real expAmount3 = 3269861.76315;
+
+    auto d = vars.today;
+
+    CHECK_CDI_OIS_COUPON_RESULT("accrued amount", coupon1->accruedAmount(d), expAccrued1, 1e-5);
+    CHECK_CDI_OIS_COUPON_RESULT("accrued amount", coupon2->accruedAmount(d), expAccrued2, 1e-5);
+    CHECK_CDI_OIS_COUPON_RESULT("accrued amount", coupon3->accruedAmount(d), expAccrued3, 1e-5);
+
+    CHECK_CDI_OIS_COUPON_RESULT("coupon rate", coupon1->rate(), expRate1, 1e-12);
+
+     CHECK_CDI_OIS_COUPON_RESULT("coupon amount", coupon1->amount(), expAmount1, 1e-5)
+     CHECK_CDI_OIS_COUPON_RESULT("coupon amount", coupon2->amount(), expAmount2, 1e-5);
+     CHECK_CDI_OIS_COUPON_RESULT("coupon amount", coupon3->amount(), expAmount3, 1e-5);
+}
 //
 // BOOST_AUTO_TEST_CASE(testFutureCouponRate) {
 //     BOOST_TEST_MESSAGE("Testing rate for future overnight-indexed coupon...");
