a89c92db188584e3aa24703829f131b8048d555e
Vladimir Polin <Vladimir.Polin@intel.com>
2025-07-28 09:24:53 -0700

    supported rounding range added to doxygen, comments refactored

--- ql/math/rounding.cpp
+++ ql/math/rounding.cpp
@@ -26,15 +26,15 @@
 
 namespace QuantLib {
 
-    static inline Real fast_pow10(Integer n) {
-        // supporting precision up to double 1e-16 (15.9 digits)
+    static inline Real fast_pow10(Integer precision) {
+        // Providing support for truncating up to 16 decimal places after dot
         constexpr static Real pow10_lut[0x20] = {
             1.0E0,  1.0E1,  1.0E2,  1.0E3,  1.0E4,  1.0E5,
             1.0E6,  1.0E7,  1.0E8,  1.0E9,  1.0E10, 1.0E11,
             1.0E12, 1.0E13, 1.0E14, 1.0E15, 1.0E16
             /*the rest of the numbers are zeros*/};
-        // not checking precision input values but not crashing
-        return pow10_lut[n&0x1F];
+        // Skipping precision input value checks without causing a crash
+        return pow10_lut[precision & 0x1F];
     }
 
     Decimal Rounding::operator()(Decimal value) const {
