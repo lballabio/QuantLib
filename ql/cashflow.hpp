5b05868ee2e52e3339ad0eca45e99aadaf7f000b
Luigi Ballabio <luigi.ballabio@gmail.com>
2025-09-17 23:03:31 +0200

    Trigger automated PRs

--- ql/cashflow.hpp
+++ ql/cashflow.hpp
@@ -3,6 +3,7 @@
 /*
  Copyright (C) 2000, 2001, 2002, 2003 RiskMap srl
  Copyright (C) 2003, 2004, 2005, 2006, 2007, 2009 StatPro Italia srl
+ Copyright (C) 2025 Test User
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
@@ -11,7 +12,7 @@
  under the terms of the QuantLib license.  You should have received a
  copy of the license along with this program; if not, please email
  <quantlib-dev@lists.sf.net>. The license is also available online at
- <https://www.quantlib.org/license.shtml>.
+ <http://quantlib.org/license.shtml>.
 
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
