a1b5160688c91a586eaafaefd57ab41f3bb0d3ce
Peter Caspers <pcaspers1973@gmail.com>
2026-01-04 13:21:05 +0100

    avoid storing references, add setters

--- ql/processes/gsrprocess.hpp
+++ ql/processes/gsrprocess.hpp
@@ -39,9 +39,9 @@ namespace QuantLib {
     /*! \ingroup processes */
     class GsrProcess : public ForwardMeasureProcess1D {
       public:
-        GsrProcess(const Array& times,
-                   const Array& vols,
-                   const Array& reversions,
+        GsrProcess(Array times,
+                   Array vols,
+                   Array reversions,
                    Real T = 60.0,
                    const Date& referenceDate = Date(),
                    DayCounter dc = DayCounter());
@@ -56,19 +56,28 @@ namespace QuantLib {
         Real time(const Date& d) const override;
         //@}
         //! \name ForwardMeasureProcess1D interface
+        //@{
         void setForwardMeasureTime(Time t) override;
         //@}
         //! additional inspectors
+        //@{
         Real sigma(Time t) const;
         Real reversion(Time t) const;
         Real y(Time t) const;
         Real G(Time t, Time T, Real x) const;
+        //@}
         //! reset cache
         void flushCache() const;
+        //! additional setters
+        //@{
+        void setTimes(Array times) { core_.setTimes(times); }
+        void setVols(Array vols) { core_.setVols(vols); }
+        void setReversions(Array reversions) { core_.setReversions(reversions); }
+        //@}
 
       private:
         void checkT(Time t) const;
-        const detail::GsrProcessCore core_;
+        detail::GsrProcessCore core_;
         Date referenceDate_;
         DayCounter dc_;
     };
