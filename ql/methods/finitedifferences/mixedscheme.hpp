4b71c3ba7200b8d31b3879804d208c318751a1b3
Luigi Ballabio <luigi.ballabio@gmail.com>
2026-01-08 11:19:19 +0100

    Avoid warning

--- ql/methods/finitedifferences/mixedscheme.hpp
+++ ql/methods/finitedifferences/mixedscheme.hpp
@@ -37,18 +37,45 @@ namespace QuantLib {
     template <class Operator>
     class [[deprecated("Part of the old FD framework; copy this function in your codebase if needed")]] MixedScheme  {
       public:
-        // typedefs
         typedef OperatorTraits<Operator> traits;
         typedef typename traits::operator_type operator_type;
         typedef typename traits::array_type array_type;
         typedef typename traits::bc_set bc_set;
         typedef typename traits::condition_type condition_type;
-        // constructors
+
         MixedScheme(const operator_type& L, Real theta, bc_set bcs)
         : L_(L), I_(operator_type::identity(L.size())), dt_(0.0), theta_(theta),
           bcs_(std::move(bcs)) {}
+
         void step(array_type& a,
-                  Time t);
+                  Time t) {
+            Size i;
+            for (i=0; i<bcs_.size(); i++)
+                bcs_[i]->setTime(t);
+            if (theta_!=1.0) { // there is an explicit part
+                if (L_.isTimeDependent()) {
+                    L_.setTime(t);
+                    explicitPart_ = I_-((1.0-theta_) * dt_)*L_;
+                }
+                for (i=0; i<bcs_.size(); i++)
+                    bcs_[i]->applyBeforeApplying(explicitPart_);
+                a = explicitPart_.applyTo(a);
+                for (i=0; i<bcs_.size(); i++)
+                    bcs_[i]->applyAfterApplying(a);
+            }
+            if (theta_!=0.0) { // there is an implicit part
+                if (L_.isTimeDependent()) {
+                    L_.setTime(t-dt_);
+                    implicitPart_ = I_+(theta_ * dt_)*L_;
+                }
+                for (i=0; i<bcs_.size(); i++)
+                    bcs_[i]->applyBeforeSolving(implicitPart_,a);
+                implicitPart_.solveFor(a, a);
+                for (i=0; i<bcs_.size(); i++)
+                    bcs_[i]->applyAfterSolving(a);
+            }
+        }
+
         void setStep(Time dt) {
             dt_ = dt;
             if (theta_!=1.0) // there is an explicit part
@@ -56,44 +83,13 @@ namespace QuantLib {
             if (theta_!=0.0) // there is an implicit part
                 implicitPart_ = I_+(theta_ * dt_)*L_;
         }
+
       protected:
         operator_type L_, I_, explicitPart_, implicitPart_;
         Time dt_;
         Real theta_;
         bc_set bcs_;
     };
-
-
-    // inline definitions
-
-    template <class Operator>
-    inline void MixedScheme<Operator>::step(array_type& a, Time t) {
-        Size i;
-        for (i=0; i<bcs_.size(); i++)
-            bcs_[i]->setTime(t);
-        if (theta_!=1.0) { // there is an explicit part
-            if (L_.isTimeDependent()) {
-                L_.setTime(t);
-                explicitPart_ = I_-((1.0-theta_) * dt_)*L_;
-            }
-            for (i=0; i<bcs_.size(); i++)
-                bcs_[i]->applyBeforeApplying(explicitPart_);
-            a = explicitPart_.applyTo(a);
-            for (i=0; i<bcs_.size(); i++)
-                bcs_[i]->applyAfterApplying(a);
-        }
-        if (theta_!=0.0) { // there is an implicit part
-            if (L_.isTimeDependent()) {
-                L_.setTime(t-dt_);
-                implicitPart_ = I_+(theta_ * dt_)*L_;
-            }
-            for (i=0; i<bcs_.size(); i++)
-                bcs_[i]->applyBeforeSolving(implicitPart_,a);
-            implicitPart_.solveFor(a, a);
-            for (i=0; i<bcs_.size(); i++)
-                bcs_[i]->applyAfterSolving(a);
-        }
-    }
 
 }
 
