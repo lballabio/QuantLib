b1aafc14c571dd271e19e05d2396a3857e3cc959
Eugene Toder <eltoder@twosigma.com>
2026-03-24 21:29:44 -0400

    Replace dynamic_cast with static_cast in interpolations
    dynamic_cast is an expensive operations that involves class hierarchy
    traversal at runtime. On Windows it usually uses strcmp to search for
    the target type_info. On the other hand, static_cast is a pointer shift
    by a compile-time constant, which is usually 0, so it often has 0 cost
    at runtime.

--- ql/math/interpolations/cubicinterpolation.hpp
+++ ql/math/interpolations/cubicinterpolation.hpp
@@ -39,16 +39,15 @@ namespace QuantLib {
 
     namespace detail {
 
-        class CubicInterpolationBaseImpl {
+        class CubicInterpolationBaseImpl : public Interpolation::Impl {
           public:
             CubicInterpolationBaseImpl(Size n,
                                        Real leftConditionValue,
                                        Real rightConditionValue)
-            : n_(n), primitiveConst_(n-1), a_(n-1), b_(n-1), c_(n-1),
+            : primitiveConst_(n-1), a_(n-1), b_(n-1), c_(n-1),
               monotonicityAdjustments_(n),
               leftValue_(leftConditionValue), rightValue_(rightConditionValue) {}
-            virtual ~CubicInterpolationBaseImpl() = default;
-            Size n_;
+
             // P[i](x) = y[i] +
             //           a[i]*(x-x[i]) +
             //           b[i]*(x-x[i])^2 +
@@ -196,7 +195,7 @@ namespace QuantLib {
         }
       private:
         detail::CubicInterpolationBaseImpl& baseImpl() const {
-            return *dynamic_cast<detail::CubicInterpolationBaseImpl*>(impl_.get());
+            return *static_cast<detail::CubicInterpolationBaseImpl*>(impl_.get());
         }
     };
 
@@ -371,8 +370,8 @@ namespace QuantLib {
     namespace detail {
 
         template <class I1, class I2>
-        class CubicInterpolationImpl final : public CubicInterpolationBaseImpl,
-                                             public Interpolation::templateImpl<I1,I2> {
+        class CubicInterpolationImpl final
+            : public Interpolation::templateImpl<I1, I2, CubicInterpolationBaseImpl> {
           public:
             CubicInterpolationImpl(const I1& xBegin,
                                    const I1& xEnd,
@@ -383,10 +382,10 @@ namespace QuantLib {
                                    Real leftConditionValue,
                                    CubicInterpolation::BoundaryCondition rightCondition,
                                    Real rightConditionValue)
-            : CubicInterpolationBaseImpl(xEnd-xBegin, leftConditionValue,
-                                         rightConditionValue),
-              Interpolation::templateImpl<I1,I2>(xBegin, xEnd, yBegin,
-                                                 Cubic::requiredPoints),
+            : Interpolation::templateImpl<I1, I2, CubicInterpolationBaseImpl>(
+                xBegin, xEnd, yBegin, Cubic::requiredPoints,
+                xEnd-xBegin, leftConditionValue, rightConditionValue),
+              n_(xEnd-xBegin),
               da_(da),
               monotonic_(monotonic),
               leftType_(leftCondition), rightType_(rightCondition),
@@ -424,11 +423,11 @@ namespace QuantLib {
                         break;
                       case CubicInterpolation::FirstDerivative:
                         L_.setFirstRow(1.0, 0.0);
-                        tmp_[0] = leftValue_;
+                        tmp_[0] = this->leftValue_;
                         break;
                       case CubicInterpolation::SecondDerivative:
                         L_.setFirstRow(2.0, 1.0);
-                        tmp_[0] = 3.0*S_[0] - leftValue_*dx_[0]/2.0;
+                        tmp_[0] = 3.0*S_[0] - this->leftValue_*dx_[0]/2.0;
                         break;
                       case CubicInterpolation::Periodic:
                         QL_FAIL("this end condition is not implemented yet");
@@ -456,11 +455,11 @@ namespace QuantLib {
                         break;
                       case CubicInterpolation::FirstDerivative:
                         L_.setLastRow(0.0, 1.0);
-                        tmp_[n_-1] = rightValue_;
+                        tmp_[n_-1] = this->rightValue_;
                         break;
                       case CubicInterpolation::SecondDerivative:
                         L_.setLastRow(1.0, 2.0);
-                        tmp_[n_-1] = 3.0*S_[n_-2] + rightValue_*dx_[n_-2]/2.0;
+                        tmp_[n_-1] = 3.0*S_[n_-2] + this->rightValue_*dx_[n_-2]/2.0;
                         break;
                       case CubicInterpolation::Periodic:
                         QL_FAIL("this end condition is not implemented yet");
@@ -676,8 +675,9 @@ namespace QuantLib {
                     }
                 }
 
-                std::fill(monotonicityAdjustments_.begin(),
-                          monotonicityAdjustments_.end(), false);
+                auto& monotonicityAdjustments = this->monotonicityAdjustments_;
+                std::fill(monotonicityAdjustments.begin(),
+                          monotonicityAdjustments.end(), false);
                 // Hyman monotonicity constrained filter
                 if (monotonic_) {
                     Real correction;
@@ -693,7 +693,7 @@ namespace QuantLib {
                             }
                             if (correction!=tmp_[i]) {
                                 tmp_[i] = correction;
-                                monotonicityAdjustments_[i] = true;
+                                monotonicityAdjustments[i] = true;
                             }
                         } else if (i==n_-1) {
                             if (tmp_[i]*S_[n_-2]>0.0) {
@@ -705,7 +705,7 @@ namespace QuantLib {
                             }
                             if (correction!=tmp_[i]) {
                                 tmp_[i] = correction;
-                                monotonicityAdjustments_[i] = true;
+                                monotonicityAdjustments[i] = true;
                             }
                         } else {
                             pm=(S_[i-1]*dx_[i]+S_[i]*dx_[i-1])/
@@ -744,7 +744,7 @@ namespace QuantLib {
                             }
                             if (correction!=tmp_[i]) {
                                 tmp_[i] = correction;
-                                monotonicityAdjustments_[i] = true;
+                                monotonicityAdjustments[i] = true;
                             }
                         }
                     }
@@ -753,44 +753,46 @@ namespace QuantLib {
 
                 // cubic coefficients
                 for (Size i=0; i<n_-1; ++i) {
-                    a_[i] = tmp_[i];
-                    b_[i] = (3.0*S_[i] - tmp_[i+1] - 2.0*tmp_[i])/dx_[i];
-                    c_[i] = (tmp_[i+1] + tmp_[i] - 2.0*S_[i])/(dx_[i]*dx_[i]);
+                    this->a_[i] = tmp_[i];
+                    this->b_[i] = (3.0*S_[i] - tmp_[i+1] - 2.0*tmp_[i])/dx_[i];
+                    this->c_[i] = (tmp_[i+1] + tmp_[i] - 2.0*S_[i])/(dx_[i]*dx_[i]);
                 }
 
-                primitiveConst_[0] = 0.0;
+                auto& primitiveConst = this->primitiveConst_;
+                primitiveConst[0] = 0.0;
                 for (Size i=1; i<n_-1; ++i) {
-                    primitiveConst_[i] = primitiveConst_[i-1]
+                    primitiveConst[i] = primitiveConst[i-1]
                         + dx_[i-1] *
                         (this->yBegin_[i-1] + dx_[i-1] *
-                         (a_[i-1]/2.0 + dx_[i-1] *
-                          (b_[i-1]/3.0 + dx_[i-1] * c_[i-1]/4.0)));
+                         (this->a_[i-1]/2.0 + dx_[i-1] *
+                          (this->b_[i-1]/3.0 + dx_[i-1] * this->c_[i-1]/4.0)));
                 }
             }
             Real value(Real x) const override {
                 Size j = this->locate(x);
                 Real dx_ = x-this->xBegin_[j];
-                return this->yBegin_[j] + dx_*(a_[j] + dx_*(b_[j] + dx_*c_[j]));
+                return this->yBegin_[j] + dx_*(this->a_[j] + dx_*(this->b_[j] + dx_*this->c_[j]));
             }
             Real primitive(Real x) const override {
                 Size j = this->locate(x);
                 Real dx_ = x-this->xBegin_[j];
-                return primitiveConst_[j]
-                    + dx_*(this->yBegin_[j] + dx_*(a_[j]/2.0
-                    + dx_*(b_[j]/3.0 + dx_*c_[j]/4.0)));
+                return this->primitiveConst_[j]
+                    + dx_*(this->yBegin_[j] + dx_*(this->a_[j]/2.0
+                    + dx_*(this->b_[j]/3.0 + dx_*this->c_[j]/4.0)));
             }
             Real derivative(Real x) const override {
                 Size j = this->locate(x);
                 Real dx_ = x-this->xBegin_[j];
-                return a_[j] + (2.0*b_[j] + 3.0*c_[j]*dx_)*dx_;
+                return this->a_[j] + (2.0*this->b_[j] + 3.0*this->c_[j]*dx_)*dx_;
             }
             Real secondDerivative(Real x) const override {
                 Size j = this->locate(x);
                 Real dx_ = x-this->xBegin_[j];
-                return 2.0*b_[j] + 6.0*c_[j]*dx_;
+                return 2.0*this->b_[j] + 6.0*this->c_[j]*dx_;
             }
 
           private:
+            Size n_;
             CubicInterpolation::DerivativeApprox da_;
             bool monotonic_;
             CubicInterpolation::BoundaryCondition leftType_, rightType_;
