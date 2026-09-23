d55eeedf2fcaa934c44bd93346c6794a085f9f5f
kp9991 <kp9991@gmail.com>
2026-09-24 05:49:43 +1000

    Trigger github actions

--- test-suite/piecewiseyieldcurve.cpp
+++ test-suite/piecewiseyieldcurve.cpp
@@ -2,7 +2,7 @@
 
 /*
  Copyright (C) 2005, 2006, 2007, 2008, 2009, 2017 StatPro Italia srl
- Copyright (C) 2026 Kyrylo Protsenko
+ Copyright (C) 2026 Kyrylo Protsenko 
 
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
